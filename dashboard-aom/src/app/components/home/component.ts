8ceeb23f001a34603df501a5659489c272f1afbc
samueleyre <samuel.eyre@hotmail.fr>
2018-11-13 19:19:58 +0100

    fake important value on front

--- dashboard-aom/src/app/components/home/component.ts
+++ dashboard-aom/src/app/components/home/component.ts
@@ -9,19 +9,42 @@ import {FileSaverService} from "ngx-filesaver";
 
 export class HomeComponent implements OnInit {
 
-
     private aomService;
+    private preuveKeyValues: any = [
+      {
+        name: "Cocovoit",
+        preuves: 545,
+        level: 2
+      },
+      {
+        name: "MaxiCovoit",
+        preuves: 123,
+        level: 4
+      },
+      {
+        name: "SuperCovoit",
+        preuves: 345,
+        level: 3
+      },
+
+    ];
 
     constructor(aomService: AomService,private fileSaverService: FileSaverService) {
       this.aomService = aomService;
     }
 
     ngOnInit() {
+      // this.aomService.preuveKeyValues().subscribe(res => {
+      //
+      //   this.preuveKeyValues = res;
+      //
+      // });
     }
 
     getCsv() {
       this.aomService.getCsv().subscribe(res => {
-        this.fileSaverService.save(res, "test.csv");
+        const d = new Date();
+        this.fileSaverService.save(res, "Preuves_"+d.toLocaleDateString()+".csv");
       });
     }
 
