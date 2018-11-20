df230e84b87dd8bd0bd4910b0061922b9ed0d7e5
samueleyre <samuel.eyre@hotmail.fr>
2018-11-20 13:43:51 +0100

    added proofs per operator

--- api/src/aom/aomModel.js
+++ api/src/aom/aomModel.js
@@ -6,6 +6,23 @@ const AomSchema = new Schema({
   siren: [{ type: String, required: true }],
   proofs_count: { type: Number, default: 0 },
   proofs_avg_level: { type: Number, default: 0 },
+  proofs_per_operator: [
+    {
+      operator: {
+        operator_id: Schema.Types.ObjectId,
+        name: String,
+        siren: String,
+      },
+      count: {
+        type: Number,
+        default: 0
+      },
+      avg_level: {
+        type: Number,
+        default: 0
+      }
+    }
+  ],
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now },
   deletedAt: { type: Date },
