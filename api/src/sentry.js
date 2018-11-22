18d2597a4a5aa4340dc0b0c2c68ae7e597e447b5
samueleyre <samuel.eyre@hotmail.fr>
2018-11-22 18:04:15 +0100

    changed env var for sentry and connected sam and jon variables on sentry website

--- api/src/sentry.js
+++ api/src/sentry.js
@@ -3,7 +3,7 @@ const Sentry = require("@sentry/node");
 Sentry.init({
   dsn: process.env.SENTRY_DSN,
   release: "pdc-api@" + require("../package.json").version,
-  environment: process.env.NODE_ENV
+  environment: process.env.CENTRY
 });
 
 module.exports = Sentry;
