3567f9fb44cd568761c6ae088fa6de35410c3076
Jonathan Fallon <jonathan@dotify.fr>
2018-11-26 11:10:45 +0100

    scope JWT algorithms and update code

--- api/src/config.js
+++ api/src/config.js
@@ -1,7 +1,7 @@
 // System configuration
 const PORT = process.env.PORT || 8080;
 const mongoUrl = process.env.DB_ENDPOINT || 'mongodb://mongo/pdc-collector?authSource=admin';
-const secret = process.env.SECRET || 'not-so-secret';
+const secret = process.env.JWT_SECRET || 'not-so-secret';
 
 console.log(`ENV : ${process.env.NODE_ENV}`);
 console.log(`PORT : ${PORT}`);
--- api/src/passport.js
+++ api/src/passport.js
@@ -7,8 +7,9 @@ const User = require('./users/user-model');
 
 module.exports = function (passport) {
   const opts = {};
-  opts.jwtFromRequest = ExtractJwt.fromAuthHeaderWithScheme('Bearer');
+  opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
   opts.secretOrKey = config.secret;
+  opts.algorithms = ['HS256', 'HS384', 'HS512'];
   passport.use(
     new JwtStrategy(opts, ((jwtPayload, next) => {
       User.findOne({ _id: jwtPayload._id }, (err, user) => {
