a9f42ad011bf122fe7ec07192e7a67ae15589d03
Samuel Eyre <samuel.eyre@hotmail.fr>
2019-04-08 15:38:47 +0200

    clean & move: shared components & services to shared folder

--- dashboard/src/app/shared/layout/components/menu/component.ts
+++ dashboard/src/app/shared/layout/components/menu/component.ts
@@ -28,7 +28,7 @@ export class MenuComponent implements OnInit {
             visible: true,
           },
           {
-            label: 'Import', icon: 'pi pi-fw pi-upload', routerLink: '/dashboard/operator/journey-import', routerLinkActive: 'is-active',
+            label: 'Import', icon: 'pi pi-fw pi-upload', routerLink: '/dashboard/operators/journey-import', routerLinkActive: 'is-active',
             visible: this.hasAnyGroup(['operators']),
           },
           {
@@ -36,7 +36,7 @@ export class MenuComponent implements OnInit {
             visible: this.hasAnyGroup(['registry']),
           },
           {
-            label: 'Statistiques', icon: 'pi pi-fw pi-chart-bar', routerLink: '/dashboard/aom/statistics', routerLinkActive: 'is-active',
+            label: 'Statistiques', icon: 'pi pi-fw pi-chart-bar', routerLink: '/dashboard/aoms/statistics', routerLinkActive: 'is-active',
             visible: this.hasAnyGroup(['aom']),
           },
         ],
@@ -47,12 +47,12 @@ export class MenuComponent implements OnInit {
         visible: this.hasAnyGroup(['hide']),
         items: [
           {
-            label: 'Politiques', icon: 'pi pi-fw pi-copy', routerLink: '/dashboard/incentive-policies',
+            label: 'Politiques', icon: 'pi pi-fw pi-copy', routerLink: '/dashboard/incentives/policies',
             routerLinkActive: 'is-active',
           },
           {
             label: 'Campagnes', icon: 'pi pi-fw pi-calendar',
-            routerLink: '/dashboard/incentive-campaigns', routerLinkActive: 'is-active',
+            routerLink: '/dashboard/incentives/campaigns', routerLinkActive: 'is-active',
           },
 
         ],
@@ -62,19 +62,19 @@ export class MenuComponent implements OnInit {
         visible: this.hasAnyGroup(['registry', 'operators']),
         items: [
           {
-            label: 'Tokens', icon: 'pi pi-fw pi-cog', routerLink: '/admin/tokens', routerLinkActiveOptions: { exact: true },
+            label: 'Tokens', icon: 'pi pi-fw pi-cog', routerLink: '/dashboard/operators/tokens', routerLinkActiveOptions: { exact: true },
             visible: this.hasAnyGroup(['operators']),
           },
           {
-            label: 'Utilisateurs', icon: 'pi pi-fw pi-users', routerLink: '/admin/users', routerLinkActive: 'is-active',
+            label: 'Utilisateurs', icon: 'pi pi-fw pi-users', routerLink: '/dashboard/users/admin', routerLinkActive: 'is-active',
             visible: this.hasAnyGroup(['registry']),
           },
           {
-            label: 'Opérateurs', icon: 'pi pi-fw pi-tablet', routerLink: '/admin/operators', routerLinkActive: 'is-active',
+            label: 'Opérateurs', icon: 'pi pi-fw pi-tablet', routerLink: '/dashboard/operators/admin', routerLinkActive: 'is-active',
             visible: this.hasAnyGroup(['registry']),
           },
           {
-            label: 'AOMs', icon: 'pi pi-fw pi-home', routerLink: '/admin/aoms', routerLinkActive: 'is-active',
+            label: 'AOMs', icon: 'pi pi-fw pi-home', routerLink: '/dashboard/aoms/admin', routerLinkActive: 'is-active',
             visible: this.hasAnyGroup(['registry']),
           },
         ],
