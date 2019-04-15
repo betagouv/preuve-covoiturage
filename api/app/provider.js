c1b481b157ffa4617fcbfca3daffb82abf9d37a1
Samuel Eyre <samuel.eyre@hotmail.fr>
2019-04-15 12:46:04 +0200

    commented: app/provider.js

--- api/app/provider.js
+++ api/app/provider.js
@@ -1,32 +1,32 @@
-class Provider {
-  constructor({ http, bus, errorHandler }) {
-    this.providers = {
-      http,
-      bus,
-      errorHandler,
-    };
-  }
-
-  get methods() {
-      return [];
-  }
-  get routes() {
-    return [];
-  }
-
-  get queues() {
-    return [];
-  }
-
-  boot() {
-    this.routes.forEach((route) => {
-      this.providers.http.use(route);
-    });
-
-    this.queues.forEach((queue) => {
-      this.providers.bus.declare(queue);
-    });
-  }
-}
-
-module.exports = Provider;
+// class Provider {
+//   constructor({ http, bus, errorHandler }) {
+//     this.providers = {
+//       http,
+//       bus,
+//       errorHandler,
+//     };
+//   }
+//
+//   get methods() {
+//       return [];
+//   }
+//   get routes() {
+//     return [];
+//   }
+//
+//   get queues() {
+//     return [];
+//   }
+//
+//   boot() {
+//     this.routes.forEach((route) => {
+//       this.providers.http.use(route);
+//     });
+//
+//     this.queues.forEach((queue) => {
+//       this.providers.bus.declare(queue);
+//     });
+//   }
+// }
+//
+// module.exports = Provider;
