94bfff7b33f1823103ef67fe1bd8a1520543c608
Samuel Eyre <samuel.eyre@hotmail.fr>
2019-04-15 12:49:15 +0200

    fix: operator nicename

--- api/services/organization/entities/models/operator.js
+++ api/services/organization/entities/models/operator.js
@@ -3,4 +3,11 @@ const OperatorSchema = require('../schemas/operator');
 
 module.exports = modelFactory('Operator', {
   schema: OperatorSchema,
+  virtuals: {
+    nicename: {
+      get(schema, doc) {
+        return doc.nom_commercial;
+      },
+    },
+  },
 });
