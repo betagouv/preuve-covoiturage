7cc7da3ed5514fd3afa28614386420fc510d97ba
Jonathan Fallon <jonathan.fallon@beta.gouv.fr>
2019-04-16 15:59:50 +0200

    fix #93. prefix journey_id with operator's slug

--- api/services/acquisition/entities/models/journey.js
+++ api/services/acquisition/entities/models/journey.js
@@ -1,31 +1,6 @@
-const _ = require('lodash');
-const mongoose = require('mongoose');
-const slugify = require('slugify');
-
 const modelFactory = require('@pdc/shared/providers/mongo/model-factory');
-
 const JourneySchema = require('../schemas/journey');
 
 module.exports = modelFactory('Journey', {
   schema: JourneySchema,
-  pre: {
-    save: [
-      async (schema, doc) => {
-        const isDoc = doc instanceof mongoose.Document;
-        const docObj = isDoc ? doc.toObject() : doc;
-        const isNew = isDoc ? false : !!doc.isNew;
-
-        if (doc.isModified('journey_id') || isNew) {
-          const opSlug = slugify(_.get(docObj.operator, 'nom_commercial', '')).toLowerCase();
-          if (opSlug !== '') {
-            // eslint-disable-next-line no-param-reassign
-            doc.journey_id = `${opSlug}:${doc.journey_id}`;
-          }
-        }
-
-        // console.log(doc);
-        return doc;
-      },
-    ],
-  },
 });
