fda84e23bd6cf2c636e0b5b9ad8078220780c2b5
Jonathan Fallon <jonathan.fallon@beta.gouv.fr>
2019-04-17 11:55:29 +0200

    imports...

--- api/shared/entities/schemas/identity.js
+++ api/shared/entities/schemas/identity.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const { regex, setters, validators } = require('../../providers/mongo/schema-validation');
 
+const { Schema } = mongoose;
+
 const CardSchema = new Schema({
   name: { type: String },
   number: { type: String, required: true },
