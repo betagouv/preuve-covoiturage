4e71d837cb48c28edb6a1b31e2c5d058b3b629b8
Samuel Eyre <samuel.eyre@hotmail.fr>
2019-04-22 12:23:15 +0200

    fix: filling of contacts in aom request

--- api/services/organization/aom.js
+++ api/services/organization/aom.js
@@ -36,14 +36,13 @@ service.find = async (query) => {
   const promises = data.map(async (itemDoc) => {
     const item = itemDoc.toObject();
     if (!item.contacts) return item;
-
-    const contacts = Object.values(item.contacts) || [];
+    const contacts = Object.values(item.contacts).filter(String) || [];
     (await User.find({ _id: { $in: contacts } }).exec())
       .forEach((userDoc) => {
         const user = userDoc.toObject();
         Object.keys(item.contacts).forEach((key) => {
           const val = item.contacts[key];
-          if (user._id.toString() === val.toString()) {
+          if (val && user._id.toString() === val.toString()) {
             item.contacts[key] = _.pick(user, [
               '_id',
               'firstname',
