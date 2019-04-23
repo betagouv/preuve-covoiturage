c2feedc0bb2f1e90fec7613360aee29a580c4ad4
Samuel Eyre <samuel.eyre@hotmail.fr>
2019-04-23 11:32:43 +0200

    remove: unused incentive mocks

--- dashboard/src/app/modules/incentive/mock/filterList.ts
+++
@@ -1,9 +0,0 @@
-export const filterList = [
-  'passager / conducteur / les deux', // dans les paramètres
-  'majeur / mineur / les deux', // dans les paramètres
-  'jour de la semaine',
-  'tranche horaire (multiple)',
-  'code insee', // dans les paramètres
-  'distance (range)',
-  'classe de preuve (multiple)',
-];
