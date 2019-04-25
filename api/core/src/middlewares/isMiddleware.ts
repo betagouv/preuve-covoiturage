b410c9271bef5ced80aa7f6b945db8040f19908a
Jonathan Fallon <jonathan.fallon@beta.gouv.fr>
2019-04-26 00:17:00 +0200

    add is and isAdmin middlewares

---
+++ api/core/src/middlewares/isMiddleware.ts
@@ -0,0 +1,25 @@
+import { MiddlewareInterface } from '~/interfaces/MiddlewareInterface';
+import { CallType } from '~/types/CallType';
+import { ForbiddenException } from '~/Exceptions/ForbiddenException';
+import { InvalidParamsException } from '~/Exceptions/InvalidParamsException';
+import { reduceRoles } from '~/helpers/reducers/reduceRoles';
+
+export function isMiddleware(...roles: string[]): MiddlewareInterface {
+  return async (call: CallType, next: Function): Promise<void> => {
+    const filtered: string[] = roles.filter(i => !!i);
+
+    if (!filtered.length) {
+      throw new InvalidParamsException('No role defined');
+    }
+
+    const { role, group } = call.context.user;
+
+    const pass = filtered.reduce(reduceRoles(filtered, group, role), true);
+
+    if (!pass) {
+      throw new ForbiddenException('Role mismatch');
+    }
+
+    await next();
+  };
+}
