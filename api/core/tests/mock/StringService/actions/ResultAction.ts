2132906e3ae3064172fb73794486ea72a2ec62f9
Jonathan Fallon <jonathan.fallon@beta.gouv.fr>
2019-04-25 18:00:13 +0200

    some incomplete tslint fixes...

--- api/core/tests/mock/StringService/actions/ResultAction.ts
+++ api/core/tests/mock/StringService/actions/ResultAction.ts
@@ -7,21 +7,20 @@ import { InvalidParamsException } from '~/exceptions/InvalidParamsException';
 import { RPCSingleResponseType } from '~/types/RPCSingleResponseType';
 
 export class ResultAction extends Action {
-    public readonly signature: string = 'result';
+  public readonly signature: string = 'result';
 
-    protected middlewares: MiddlewareInterface[] = [];
+  protected middlewares: MiddlewareInterface[] = [];
 
-    protected async handle(params: ParamsType, context: ContextType):Promise<ResultType> {
-      if (Array.isArray(params) || !('name' in params) || !('add' in params) || (!Array.isArray(params.add))) {
-        throw new InvalidParamsException();
-      }
-      const addResult = await <Promise<RPCSingleResponseType>>this.kernel.handle({
-        jsonrpc: '2.0',
-        method: 'math:add',
-        id: 1,
-        params: params.add,
-      });
-      return `Hello world ${params.name}, result is ${addResult.result}`;
+  protected async handle(params: ParamsType, context: ContextType): Promise<ResultType> {
+    if (Array.isArray(params) || !('name' in params) || !('add' in params) || (!Array.isArray(params.add))) {
+      throw new InvalidParamsException();
     }
- 
+    const addResult = await <Promise<RPCSingleResponseType>>this.kernel.handle({
+      jsonrpc: '2.0',
+      method: 'math:add',
+      id: 1,
+      params: params.add,
+    });
+    return `Hello world ${params.name}, result is ${addResult.result}`;
+  }
 }
