a55ccc63a9880701b7d4b9cccadf25ea729c66fb
Nicolas Mérigot <nicolas.merigot@gmail.com>
2019-04-25 16:25:52 +0200

    clean up

--- api/core/src/providers/CommandProvider.ts
+++ api/core/src/providers/CommandProvider.ts
@@ -6,7 +6,6 @@ import { KernelInterface } from '../interfaces/KernelInterface';
 export class CommandProvider extends Command implements ProviderInterface {
   readonly signature: string = 'command';
   private kernel: KernelInterface;
-  private config: object = {};
 
   constructor(kernel: KernelInterface) {
     super();
