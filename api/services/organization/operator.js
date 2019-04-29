502bb670ae9b4ed32033d6b3d6b537aa1b429bce
Samuel Eyre <samuel.eyre@hotmail.fr>
2019-04-29 17:55:35 +0200

    add: contact information to operator list request

--- api/services/organization/operator.js
+++ api/services/organization/operator.js
@@ -1,10 +1,10 @@
-// const _ = require('lodash');
+const _ = require('lodash');
 const { ObjectId } = require('mongoose').Types;
 const serviceFactory = require('@pdc/shared/providers/mongo/service-factory');
 const User = require('@pdc/service-user/entities/models/user');
 const Operator = require('./entities/models/operator');
 
-module.exports = serviceFactory(Operator, {
+const service = serviceFactory(Operator, {
   async addUser(id, userId) {
     const operator = await Operator.findOne({ _id: id });
     const user = await User.findOne({ _id: userId });
@@ -24,3 +24,42 @@ module.exports = serviceFactory(Operator, {
     return User.find({ operator: ObjectId(id) });
   },
 });
+
+
+/**
+ * Extend Operator.find to add contact details
+ */
+const baseFind = service.find;
+service.find = async (query) => {
+  const { meta, data } = await baseFind(query);
+  const promises = data.map(async (itemDoc) => {
+    const item = itemDoc.toObject();
+    if (!item.contacts) return item;
+    const contacts = Object.values(item.contacts).filter(String) || [];
+    (await User.find({ _id: { $in: contacts } }).exec())
+      .forEach((userDoc) => {
+        const user = userDoc.toObject();
+        Object.keys(item.contacts).forEach((key) => {
+          const val = item.contacts[key];
+          if (val && user._id.toString() === val.toString()) {
+            item.contacts[key] = _.pick(user, [
+              '_id',
+              'firstname',
+              'lastname',
+              'email',
+              'phone',
+            ]);
+          }
+        });
+      });
+
+    return item;
+  });
+
+  return {
+    meta,
+    data: await Promise.all(promises),
+  };
+};
+
+module.exports = service;
