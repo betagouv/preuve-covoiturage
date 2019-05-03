4cecb436cd07c1a5a6d1b089e888b8a498170860
Jonathan Fallon <jonathan.fallon@beta.gouv.fr>
2019-05-03 22:34:41 +0200

    [wip] json schema unit test validation

---
+++ api/services/sample/src/entities/schema/user.schema.spec.ts
@@ -0,0 +1,22 @@
+import {
+  isSecure,
+  isNotSecure,
+} from '@pdc/shared/entities/schemas/utils/is-schema-secure';
+
+import userSchemaJson from './user.schema.json';
+
+describe('check validation', () => {
+  it('fails', () => {
+    isNotSecure({ format: 'email' });
+  });
+
+  it('works', () => {
+    isSecure({ format: 'email', maxLength: 256 });
+  });
+});
+
+describe('domain schema', () => {
+  it('user.schema', () => {
+    isSecure(userSchemaJson);
+  });
+});
