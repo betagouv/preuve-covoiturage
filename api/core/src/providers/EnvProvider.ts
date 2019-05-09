96e3f543513a65d909d5b87ef49d812719212ae2
Nicolas Mérigot <nicolas.merigot@gmail.com>
2019-05-09 17:45:49 +0200

    feat(core): Env provider copy APP_ prexifed env variable

--- api/core/src/providers/EnvProvider.ts
+++ api/core/src/providers/EnvProvider.ts
@@ -1,4 +1,5 @@
 import dotenv from 'dotenv';
+import path from 'path';
 
 import { provider } from '../container';
 
@@ -16,18 +17,30 @@ export class EnvProvider implements ProviderInterface {
   private env: Map<string, any> = new Map();
 
   boot() {
-    const result = dotenv.config();
+    let envPath = path.resolve(process.cwd(), '.env');
+
+    if ('APP_ROOT_PATH' in process.env) {
+      envPath = path.resolve(process.env.APP_ROOT_PATH, '.env');
+    }
+
+    const result = dotenv.config({ path: envPath });
 
     if (!result.error) {
       Reflect.ownKeys(result.parsed).forEach((key: string) => {
         this.env.set(key, result.parsed[key]);
       });
     }
+    
+    Reflect.ownKeys(process.env)
+      .filter((key: string) => key === key.toUpperCase() && key.startsWith('APP_'))
+      .forEach((key: string) => {
+        this.env.set(key, process.env[key]);
+      });
   }
 
   get(key: string, fallback?: any): any {
     if (!this.env.has(key)) {
-      if (fallback) {
+      if (fallback !== undefined) {
         return fallback;
       }
       throw new Error(`Unknown env key ${key}`);
