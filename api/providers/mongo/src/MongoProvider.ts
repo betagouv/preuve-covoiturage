6bdbb673795cf662250ef0b6ab92eed12f98f0f2
Nicolas Mérigot <nicolas.merigot@gmail.com>
2019-05-20 13:08:33 +0200

    feat(mongo): add mongo provider

---
+++ api/providers/mongo/src/MongoProvider.ts
@@ -0,0 +1,52 @@
+import { MongoClient, Collection, Db } from 'mongodb';
+import { Container, Providers, Interfaces } from '@pdc/core';
+
+@Container.provider()
+export class MongoProvider implements Interfaces.ProviderInterface {
+  protected client: MongoClient;
+  protected connected = false;
+
+  constructor(
+    protected config: Providers.ConfigProvider,
+  ) {}
+
+  async boot() {
+    const mongoConfig = {
+      useNewUrlParser: true,
+      ...this.config.get('mongo.config', {}),
+    };
+
+    const mongoUrl = this.config.get('mongo.url');
+
+    this.client = new MongoClient(mongoUrl, mongoConfig);
+  }
+
+  protected async ensureConnected() {
+    try {
+      if (this.connected) {
+        return;
+      }
+      await this.client.connect();
+      this.connected = true;
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  async getDb(name: string): Promise<Db> {
+    await this.ensureConnected();
+    return this.client.db(name);
+  }
+
+  async getCollectionFromDb(collection: string, db: string): Promise<Collection> {
+    await this.ensureConnected();
+    return this.client.db(db).collection(collection);
+  }
+
+  async close() {
+    if (this.connected) {
+      await this.client.close();
+      this.connected = false;
+    }
+  }
+}
---
+++ api/providers/mongo/src/index.ts
@@ -0,0 +1,3 @@
+export { Collection as CollectionInterface, Db as DbInterface } from 'mongodb';
+export { MongoProvider } from './MongoProvider';
+export { MongoException } from './exceptions/MongoException';
