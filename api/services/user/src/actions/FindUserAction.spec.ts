f6ad379820d252668991f6cdd50015cc78107969
Samuel Eyre <samuel.eyre@hotmail.fr>
2019-05-20 16:02:17 +0200

    add: createUser & findUser actions

--- api/services/user/src/actions/FindUserAction.spec.ts
+++ api/services/user/src/actions/FindUserAction.spec.ts
@@ -3,13 +3,14 @@ import chaiAsPromised from 'chai-as-promised';
 import sinon from 'sinon';
 
 import { UserRepositoryProvider } from '../providers/UserRepositoryProvider';
-import { CreateUserAction } from './CreateUserAction';
 import { User } from '../entities/User';
+import { FindUserAction } from './FindUserAction';
 
 chai.use(chaiAsPromised);
 const { expect, assert } = chai;
 
 const mockUser = <User>{
+  _id: '1ab',
   email: 'john.schmidt@example.com',
   firstname: 'john',
   lastname: 'schmidt',
@@ -17,26 +18,17 @@ const mockUser = <User>{
 };
 
 const fakeUserRepository = <UserRepositoryProvider>sinon.createStubInstance(UserRepositoryProvider, {
-  async create(user: User) {
-    return {
-      ...user,
-      _id: '1ab',
-    };
+  async find(id: string) {
+    return mockUser;
   },
 });
 
-const action = new CreateUserAction(fakeUserRepository);
+const action = new FindUserAction(fakeUserRepository);
 
 describe('Create user action', () => {
   it('should work', async () => {
-    const result = await action.handle({ user: {
-      email: 'john.schmidt@example.com',
-      firstname: 'john',
-      lastname: 'schmidt',
-      phone: '0624857425',
-    },
-    });
-    expect(result).to.equal({ ...mockUser, _id: '1ab' });
+    const result = await action.handle({ id: mockUser['_id'] });
+    expect(result).to.equal(mockUser);
   });
 });
 
--- api/services/user/src/actions/FindUserAction.ts
+++ api/services/user/src/actions/FindUserAction.ts
@@ -1,19 +1,18 @@
 import { Parents, Container } from '@pdc/core';
 import { UserRepositoryProvider } from '../providers/UserRepositoryProvider';
-import { User } from '../entities/User';
 
 @Container.handler({
   service: 'user',
-  method: 'create',
+  method: 'find',
 })
-export class CreateUserAction extends Parents.Action {
+export class FindUserAction extends Parents.Action {
   constructor(
     private userRepository: UserRepositoryProvider,
   ) {
     super();
   }
 
-  public async handle(request: {user: User}): Promise<void> {
-    return this.userRepository.create(request.user);
+  public async handle(request: {id: string}): Promise<void> {
+    return this.userRepository.find(request.id);
   }
 }
