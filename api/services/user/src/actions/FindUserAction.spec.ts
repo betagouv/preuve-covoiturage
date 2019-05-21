0a5574877826410709b24436f7f4c3397d796f1f
Samuel Eyre <samuel.eyre@hotmail.fr>
2019-05-21 09:30:54 +0200

    WIP

--- api/services/user/src/actions/FindUserAction.spec.ts
+++ api/services/user/src/actions/FindUserAction.spec.ts
@@ -8,7 +8,7 @@ import { User } from '../entities/User';
 chai.use(chaiAsPromised);
 const { expect } = chai;
 
-const mockUser = <User>{
+const mockUser = {
   _id: '1ab',
   email: 'john.schmidt@example.com',
   firstname: 'john',
