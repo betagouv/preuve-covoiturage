0a9e351631a1a7e2914968c307d392ccc5731e61
Nicolas Mérigot <nicolas.merigot@gmail.com>
2019-05-22 16:25:59 +0200

    fix(mongo): export ObjectId from mongo

--- api/providers/mongo/src/index.ts
+++ api/providers/mongo/src/index.ts
@@ -1,3 +1,3 @@
-export { Collection as CollectionInterface, Db as DbInterface } from 'mongodb';
+export { Collection as CollectionInterface, Db as DbInterface, ObjectId, Timestamp } from 'mongodb';
 export { MongoProvider } from './MongoProvider';
 export { MongoException } from './exceptions/MongoException';
