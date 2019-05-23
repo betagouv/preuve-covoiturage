3816046167d2614c05840b4299641f7023e60a82
Jonathan Fallon <jonathan.fallon@beta.gouv.fr>
2019-05-23 12:29:36 +0200

    update CSV export scripts

--- api/scalingo/csv-journeys.sh
+++ api/scalingo/csv-journeys.sh
@@ -12,11 +12,13 @@ mongoexport \
     --collection=journeys \
     --type=csv \
     --forceTableScan \
-    --fields='_id,operator_class,status,operator._id,journey_id,passenger.distance,passenger.duration,passenger.cost,passenger.incentive,passenger.remaining_fee,passenger.contribution,passenger.seats,passenger.start.postcodes.0,passenger.start.datetime.$date,passenger.start.lon,passenger.start.lat,passenger.start.literal,passenger.start.country,passenger.end.postcodes.0,passenger.end.datetime.$date,passenger.end.lon,passenger.end.lat,passenger.end.literal,passenger.end.country,driver.distance,driver.duration,driver.cost,driver.incentive,driver.remaining_fee,driver.revenue,driver.expense,driver.start.postcodes.0,driver.start.datetime.$date,driver.start.lon,driver.start.lat,driver.start.literal,driver.start.country,driver.end.postcodes.0,driver.end.datetime.$date,driver.end.lon,driver.end.lat,driver.end.literal,driver.end.country' \
+    --fields='_id,operator_class,status,passenger.distance,passenger.duration,passenger.cost,passenger.incentive,passenger.remaining_fee,passenger.contribution,passenger.seats,passenger.start.postcodes.0,passenger.start.datetime,passenger.start.lon,passenger.start.lat,passenger.start.literal,passenger.start.country,passenger.end.postcodes.0,passenger.end.datetime,passenger.end.lon,passenger.end.lat,passenger.end.literal,passenger.end.country,driver.distance,driver.duration,driver.cost,driver.incentive,driver.remaining_fee,driver.revenue,driver.expense,driver.start.postcodes.0,driver.start.datetime,driver.start.lon,driver.start.lat,driver.start.literal,driver.start.country,driver.end.postcodes.0,driver.end.datetime,driver.end.lon,driver.end.lat,driver.end.literal,driver.end.country' \
     --out="$FOLDER/$FILENAME"
 
 # install dependencies and run the opendata filter
+cd scalingo
 yarn
 node opendata.js "$FOLDER/$FILENAME"
 
+echo "Upload file"
 curl --upload-file "$FOLDER/$OPENDATA_FILENAME" "$TRANSFERSH_SERVER/$OPENDATA_FILENAME" && echo ""
--- api/scalingo/opendata.js
+++ api/scalingo/opendata.js
@@ -2,22 +2,22 @@
 const fs = require('fs');
 const csv = require('csv');
 
-const [,, ...args] = process.argv;
+const [, , ...args] = process.argv;
 
 const readStream = fs.createReadStream(args[0]);
 const writeStream = fs.createWriteStream(`${args[0]}`.replace('.csv', '-opendata.csv'));
 const csvStream = csv.parse();
 
-csvStream.on('data', (data) => {
-  // rename ObjectId
+csvStream.on('data', data => {
   if (data[0] !== '_id') {
-    [0, 3].forEach((i) => {
+    // rename ObjectId
+    [0].forEach(i => {
       data[i] = data[i].substring(9, data[i].length - 1);
     });
 
     // truncate lon/lat
-    [14, 15, 20, 21, 33, 34, 39, 40].forEach((i) => {
-      data[i] = (parseFloat(data[i]) * 1000 | 0) / 1000;
+    [12, 13, 18, 19, 31, 32, 37, 38].forEach(i => {
+      data[i] = ((parseFloat(data[i]) * 1000) | 0) / 1000;
     });
   }
 
@@ -26,7 +26,7 @@ csvStream.on('data', (data) => {
 });
 csvStream.on('error', console.log);
 csvStream.on('end', () => {
-  console.log('done!');
+  console.log('Open data cleaning done');
 });
 
 readStream.pipe(csvStream);
