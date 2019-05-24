576da4f6702cf651087ac05b0e8f07794816b228
Nicolas Mérigot <nicolas.merigot@gmail.com>
2019-05-24 11:24:10 +0200

    feat(validator): add middleware

--- api/providers/validator/src/index.ts
+++ api/providers/validator/src/index.ts
@@ -1,2 +1,3 @@
 export { ValidatorProvider } from './ValidatorProvider';
+export { ValidatorMiddleware } from './ValidatorMiddleware';
 export { ValidatorProviderInterface, ValidatorProviderInterfaceResolver } from './ValidatorProviderInterface';
