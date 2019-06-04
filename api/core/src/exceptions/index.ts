3c16cc7d33fdd658aea06459493ed470cc13275c
Samuel Eyre <samuel.eyre@hotmail.fr>
2019-06-04 11:28:10 +0200

    fix: exports of notfound & conflict exceptions

--- api/core/src/exceptions/index.ts
+++ api/core/src/exceptions/index.ts
@@ -6,7 +6,5 @@ export { ParseErrorException } from './ParseErrorException';
 export { RPCException } from './RPCException';
 export { ServiceException } from './ServiceException';
 export { UnauthorizedException } from './UnauthorizedException';
-
-// ddb
-export { DDBNotFoundException } from './database/DDBNotFoundException';
-export { DDBConflictException } from './database/DDBConflictException';
+export { NotFoundException } from './NotFoundException';
+export { ConflictException } from './ConflictException';
