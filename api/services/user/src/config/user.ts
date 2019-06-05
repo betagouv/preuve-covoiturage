a1883e3a5b9dba7c9463f07e2a0cc45a8640203c
Samuel Eyre <samuel.eyre@hotmail.fr>
2019-06-05 16:07:41 +0200

    add: default limit & skip to user config

--- api/services/user/src/config/user.ts
+++ api/services/user/src/config/user.ts
@@ -1,3 +1,5 @@
 declare function env(key: string, fallback?: string): any;
 
 export const collectionName = env('APP_USER_DB', 'user');
+export const defaultLimit = '1000';
+export const defaultSkip = '0';
