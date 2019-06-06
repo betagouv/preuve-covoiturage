d28e5e8c211422fd5b8712597cca3b9464fb68d3
Samuel Eyre <samuel.eyre@hotmail.fr>
2019-06-06 10:07:54 +0200

    add: default env api_url value for debug

--- api/services/user/src/config/url.ts
+++ api/services/user/src/config/url.ts
@@ -1,4 +1,4 @@
 declare function env(key: string, fallback?: string): any;
 
-export const apiUrl = env('API_URL');
+export const apiUrl = env('API_URL', 'nope');
 export const appUrl = env('APP_URL') ? env('APP_URL') : apiUrl.replace('api', 'dashboard');
