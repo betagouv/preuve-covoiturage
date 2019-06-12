10d21a3b67bd3d1d63bb85d4014092bd179325b9
Jonathan Fallon <jonathan.fallon@beta.gouv.fr>
2019-06-12 12:00:48 +0200

    add tests for phone and BIC string formats

--- api/providers/validator/src/ValidatorProvider.ts
+++ api/providers/validator/src/ValidatorProvider.ts
@@ -2,13 +2,11 @@ import { Container } from '@ilos/core';
 import { AjvValidatorProvider, ValidatorProviderInterface } from '@ilos/provider-validator';
 
 import { bicCustomFormat } from './formats/bicCustomFormat';
-import { dateCustomFormat } from './formats/dateCustomFormat';
-import { emailCustomFormat } from './formats/emailCustomFormat';
 import { euVatCustomFormat } from './formats/euVatCustomFormat';
 import { ibanCustomFormat } from './formats/ibanCustomFormat';
 import { inseeCustomFormat } from './formats/inseeCustomFormat';
-import { latCustomFormat } from './formats/latCustomFormat';
-import { lonCustomFormat } from './formats/lonCustomFormat';
+import { latCustomFormat } from './keywords/latKeyword';
+import { lonCustomFormat } from './keywords/lonKeyword';
 import { nafCustomFormat } from './formats/nafCustomFormat';
 import { nicCustomFormat } from './formats/nicCustomFormat';
 import { phoneCustomFormat } from './formats/phoneCustomFormat';
@@ -21,19 +19,18 @@ export class ValidatorProvider extends AjvValidatorProvider implements Validator
   boot() {
     super.boot();
 
-    this.addFormat('bic', bicCustomFormat);
-    this.addFormat('date', dateCustomFormat);
-    this.addFormat('email', emailCustomFormat);
-    this.addFormat('euvat', euVatCustomFormat);
-    this.addFormat('iban', ibanCustomFormat);
-    this.addFormat('insee', inseeCustomFormat);
-    this.addFormat('lat', latCustomFormat);
-    this.addFormat('lon', lonCustomFormat);
-    this.addFormat('naf', nafCustomFormat);
-    this.addFormat('nic', nicCustomFormat);
-    this.addFormat('phone', phoneCustomFormat);
-    this.addFormat('postcode', postcodeCustomFormat);
-    this.addFormat('siren', sirenCustomFormat);
-    this.addFormat('siret', siretCustomFormat);
+    // register string formats
+    this.registerCustomKeyword({ name: 'bic', type: 'format', definition: bicCustomFormat });
+    this.registerCustomKeyword({ name: 'euvat', type: 'format', definition: euVatCustomFormat });
+    this.registerCustomKeyword({ name: 'iban', type: 'format', definition: ibanCustomFormat });
+    this.registerCustomKeyword({ name: 'insee', type: 'format', definition: inseeCustomFormat });
+    this.registerCustomKeyword({ name: 'lat', type: 'format', definition: latCustomFormat });
+    this.registerCustomKeyword({ name: 'lon', type: 'format', definition: lonCustomFormat });
+    this.registerCustomKeyword({ name: 'naf', type: 'format', definition: nafCustomFormat });
+    this.registerCustomKeyword({ name: 'nic', type: 'format', definition: nicCustomFormat });
+    this.registerCustomKeyword({ name: 'phone', type: 'format', definition: phoneCustomFormat });
+    this.registerCustomKeyword({ name: 'postcode', type: 'format', definition: postcodeCustomFormat });
+    this.registerCustomKeyword({ name: 'siren', type: 'format', definition: sirenCustomFormat });
+    this.registerCustomKeyword({ name: 'siret', type: 'format', definition: siretCustomFormat });
   }
 }
--- api/providers/validator/src/formats/emailCustomFormat.ts
+++
@@ -1,8 +0,0 @@
-import { isValidBIC } from 'ibantools';
-
-export function emailCustomFormat(data: string): boolean {
-  // tslint:disable-next-line: max-line-length
-  const re: RegExp = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
-  return re.test(data);
-}
