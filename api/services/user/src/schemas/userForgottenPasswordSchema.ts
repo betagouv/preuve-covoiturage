593ef45ed53969ec5c20ec8957e1f48922ffe905
Samuel Eyre <samuel.eyre@hotmail.fr>
2019-06-12 18:01:30 +0200

    up: schema for changeEmail, forgotten, patch

--- api/services/user/src/schemas/userForgottenPasswordSchema.ts
+++ api/services/user/src/schemas/userForgottenPasswordSchema.ts
@@ -1,7 +1,7 @@
 export const userForgottenPasswordSchema = {
   $id: 'user.forgottenPassword',
   type: 'object',
-  required: ['id'],
+  required: [],
   additionalProperties: false,
   properties: {
     id: {
@@ -9,5 +9,23 @@ export const userForgottenPasswordSchema = {
       maxLength: 255,
       minLength: 1,
     },
+    email: {
+      type: 'string',
+      format: 'email',
+      maxLength: 128,
+      minLength: 1,
+      // match: regex.email,
+      // trim: true,
+      // lowercase: true,
+      // unique: true,
+    },
   },
+  oneOf: [
+    {
+      required: ['id'],
+    },
+    {
+      required: ['email'],
+    },
+  ],
 };
