e6e36cd0a17819f10792ef822c6618f494267b22
Samuel Eyre <samuel.eyre@hotmail.fr>
2019-06-13 18:14:32 +0200

    add(provider-crypto): cryptToken method

--- api/providers/crypto/src/interfaces/CryptoProviderInterface.ts
+++ api/providers/crypto/src/interfaces/CryptoProviderInterface.ts
@@ -1,26 +1,32 @@
-export interface CryptoProviderInterface {
+import { Interfaces } from '@ilos/core';
+
+export interface CryptoProviderInterface extends Interfaces.ProviderInterface{
   boot();
   cryptPassword(plainPassword): Promise<string>;
+  cryptToken(plainToken): Promise<string>;
   comparePassword(oldPwd: string, newPwd: string): Promise<boolean>;
   compareForgottenToken(oldToken: string, newToken: string): Promise<boolean>;
   generateToken(length: number): string;
 }
 
-export abstract class CryptoProviderInterfaceResolver {
+export abstract class CryptoProviderInterfaceResolver implements CryptoProviderInterface{
   async boot() {
     //
   }
   async cryptPassword(plainPassword: string): Promise<string> {
-    return;
+    throw new Error();
+  }
+  async cryptToken(plainToken: string): Promise<string> {
+    throw new Error();
   }
   async comparePassword(oldPwd: string, newPwd: string): Promise<boolean> {
-    return;
+    throw new Error();
   }
 
   async compareForgottenToken(oldToken: string, newToken: string): Promise<boolean> {
-    return;
+    throw new Error();
   }
   generateToken(length: number = 12): string {
-    return;
+    throw new Error();
   }
 }
