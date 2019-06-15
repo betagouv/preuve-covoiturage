04edbf892f518e38e6577384c84e28857492144b
Samuel Eyre <samuel.eyre@hotmail.fr>
2019-06-15 18:18:46 +0200

    (service-user) update: filter output middleware for arrays & clean models

--- api/services/user/src/middlewares/FilterOutputMiddleware.ts
+++ api/services/user/src/middlewares/FilterOutputMiddleware.ts
@@ -1,6 +1,9 @@
 import { Types, Interfaces, Container } from '@ilos/core';
 
-export type FilterOutputMiddlewareOptionsType = string[];
+export type FilterOutputMiddlewareOptionsType = {
+  whiteList?: string[],
+  blackList?: string[],
+};
 
 
 /*
@@ -14,18 +17,60 @@ export class FilterOutputMiddleware implements Interfaces.MiddlewareInterface {
     next: Function,
     filterProperties: FilterOutputMiddlewareOptionsType,
   ): Promise<Types.ResultType> {
-    const result = await next(params, context);
-    filterProperties.forEach((prop: string) => {
-      if (result instanceof Array) {
-        result.forEach((model: object) => {
-          if (prop in model) {
-            delete model[prop];
+    let result = await next(params, context);
+
+    if ('whiteList' in filterProperties && 'blackList' in filterProperties) {
+      throw new Error();
+    }
+
+    if ('blackList' in filterProperties) {
+      filterProperties.blackList.forEach((prop: string) => {
+        if (result instanceof Array) {
+          result.forEach((model: object) => {
+            if (prop in model) {
+              delete model[prop];
+            }
+          });
+        } else if (prop in result) {
+          delete result[prop];
+        }
+      });
+    }
+
+    if ('whiteList' in filterProperties) {
+      const filteredResult = {};
+      filterProperties.whiteList.forEach((prop: string) => {
+        if (result instanceof Array) {
+          result.forEach((model: object) => {
+            if (prop in model) {
+              filteredResult[prop] = model[prop];
+            }
+          });
+        } else if (prop in result) {
+          filteredResult[prop] = result[prop];
+        }
+      });
+      result = filteredResult;
+    }
+    return this.clean(result);
+  }
+
+  private clean(result: any) {
+    if (result instanceof Array) {
+      result.forEach((model: object) => {
+        for (const key in Object.keys(model)) {
+          if (model[key] === undefined || model[key] === null) {
+            delete model[key];
           }
-        });
-      } else if (prop in result) {
-        delete result[prop];
+        }
+      });
+    } else if (result instanceof Object) {
+      for (const key in result) {
+        if (result[key] === undefined || result[key] === null) {
+          delete result[key];
+        }
       }
-    });
+    }
 
     return result;
   }
