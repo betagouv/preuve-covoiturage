0564f9210f14959349a7c52af327657ae49b978b
Jonathan Fallon <jonathan@dotify.fr>
2019-06-17 17:45:15 +0200

    update parts and schemas

--- api/services/operator/src/schemas/operatorCreateSchema.ts
+++ api/services/operator/src/schemas/operatorCreateSchema.ts
@@ -2,6 +2,7 @@ import { companySchema } from './parts/companySchema';
 import { addressSchema } from './parts/addressSchema';
 import { bankSchema } from './parts/bankSchema';
 import { contactsSchema } from './parts/contactsSchema';
+import { cguSchema } from './parts/cguSchema';
 
 export const operatorCreateSchema = {
   $id: 'operator.create',
@@ -11,13 +12,18 @@ export const operatorCreateSchema = {
   properties: {
     nom_commercial: {
       type: 'string',
+      minLength: 3,
+      maxLength: 256,
     },
     raison_sociale: {
       type: 'string',
+      minLength: 3,
+      maxLength: 256,
     },
     company: companySchema,
     address: addressSchema,
     bank: bankSchema,
     contacts: contactsSchema,
+    cgu: cguSchema,
   },
 };
--- api/services/operator/src/schemas/parts/companySchema.ts
+++ api/services/operator/src/schemas/parts/companySchema.ts
@@ -5,29 +5,42 @@ export const companySchema = {
   properties: {
     siren: {
       type: 'string',
-      // match: regex.siren,
+      format: 'siren',
+      minLength: 9,
+      maxLength: 9,
     },
     naf_etablissement: {
       type: 'string',
-      // match: regex.naf,
+      format: 'naf',
+      minLength: 5,
+      maxLength: 5,
     },
     naf_entreprise: {
       type: 'string',
-      // match: regex.naf,
+      format: 'naf',
+      minLength: 5,
+      maxLength: 5,
     },
     nature_juridique: {
       type: 'string',
+      minLength: 2,
+      maxLength: 256,
     },
     cle_nic: {
       type: 'string',
-      // match: regex.nic,
+      format: 'nic',
+      minLength: 5,
+      maxLength: 5,
     },
     rna: {
       type: 'string',
+      format: 'rna',
     },
     vat_intra: {
       type: 'string',
-      // match: regex.vatIntra
+      format: 'euvat',
+      minLength: 13,
+      maxLength: 13,
     },
   },
 };
