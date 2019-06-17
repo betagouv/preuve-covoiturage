0504f3b7ad09ba70bcef4f9a48f5dd8fbe1dbaf5
Jonathan Fallon <jonathan@dotify.fr>
2019-06-17 17:31:39 +0200

    add coordinates keyword to validator

--- api/providers/validator/src/ValidatorProvider.ts
+++ api/providers/validator/src/ValidatorProvider.ts
@@ -6,8 +6,6 @@ import { bicCustomFormat } from './formats/bicCustomFormat';
 import { euVatCustomFormat } from './formats/euVatCustomFormat';
 import { ibanCustomFormat } from './formats/ibanCustomFormat';
 import { inseeCustomFormat } from './formats/inseeCustomFormat';
-import { latCustomFormat } from './keywords/latKeyword';
-import { lonCustomFormat } from './keywords/lonKeyword';
 import { nafCustomFormat } from './formats/nafCustomFormat';
 import { nicCustomFormat } from './formats/nicCustomFormat';
 import { objectidCustomFormat } from './formats/objectidCustomFormat';
@@ -15,6 +13,7 @@ import { phoneCustomFormat } from './formats/phoneCustomFormat';
 import { postcodeCustomFormat } from './formats/postcodeCustomFormat';
 import { sirenCustomFormat } from './formats/sirenCustomFormat';
 import { siretCustomFormat } from './formats/siretCustomFormat';
+import { coordinatesKeyword } from './keywords/coordinatesKeyword';
 
 @Container.provider()
 export class ValidatorProvider extends AjvValidatorProvider implements ValidatorProviderInterface {
@@ -30,8 +29,6 @@ export class ValidatorProvider extends AjvValidatorProvider implements Validator
     this.registerCustomKeyword({ name: 'euvat', type: 'format', definition: euVatCustomFormat });
     this.registerCustomKeyword({ name: 'iban', type: 'format', definition: ibanCustomFormat });
     this.registerCustomKeyword({ name: 'insee', type: 'format', definition: inseeCustomFormat });
-    this.registerCustomKeyword({ name: 'lat', type: 'format', definition: latCustomFormat });
-    this.registerCustomKeyword({ name: 'lon', type: 'format', definition: lonCustomFormat });
     this.registerCustomKeyword({ name: 'naf', type: 'format', definition: nafCustomFormat });
     this.registerCustomKeyword({ name: 'nic', type: 'format', definition: nicCustomFormat });
     this.registerCustomKeyword({ name: 'objectid', type: 'format', definition: objectidCustomFormat });
@@ -39,5 +36,8 @@ export class ValidatorProvider extends AjvValidatorProvider implements Validator
     this.registerCustomKeyword({ name: 'postcode', type: 'format', definition: postcodeCustomFormat });
     this.registerCustomKeyword({ name: 'siren', type: 'format', definition: sirenCustomFormat });
     this.registerCustomKeyword({ name: 'siret', type: 'format', definition: siretCustomFormat });
+
+    // register keywords (compile, macro...)
+    this.registerCustomKeyword({ name: 'coordinates', type: 'keyword', definition: coordinatesKeyword });
   }
 }
