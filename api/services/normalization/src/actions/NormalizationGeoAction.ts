73a5b1812d554b2a66252527b757eb3f2c381a2d
Samuel Eyre <samuel.eyre@hotmail.fr>
2019-06-21 11:53:43 +0200

    (service-normalization) add: geo & territory actions

---
+++ api/services/normalization/src/actions/NormalizationGeoAction.ts
@@ -0,0 +1,100 @@
+import { Parents, Container, Types, Interfaces } from '@ilos/core';
+import * as _ from 'lodash';
+import { GeoProviderInterfaceResolver } from '@pdc/provider-geo';
+import { ConfigProviderInterfaceResolver } from '@ilos/provider-config';
+
+import { PositionInterface } from '../interfaces/PositionInterface';
+
+interface NormalizationGeoParamsInterface {
+  journey: any;
+}
+
+/*
+ * Enrich journey with Geo data
+ */
+@Container.handler({
+  service: 'normalization',
+  method: 'geo',
+})
+export class NormalizationGeoAction extends Parents.Action {
+  constructor(
+    private kernel: Interfaces.KernelInterfaceResolver,
+    private geoProvider: GeoProviderInterfaceResolver,
+    private config: ConfigProviderInterfaceResolver,
+  ) {
+    super();
+  }
+
+  public async handle(param: NormalizationGeoParamsInterface, context: Types.ContextType): Promise<void> {
+    const paths = this.config.get('normalization.positionPaths');
+
+    let normalizedJourney = {};
+
+    await Promise.all(paths.map(async (path) => {
+      const position = _.get(param.journey, path);
+      const positionEnrichedWithTown = await this.findTown(position);
+      normalizedJourney = this.processTownResponse(param.journey, path, position, positionEnrichedWithTown);
+    }));
+
+    await this.kernel.notify(
+      'normalization:territory',
+      {
+        journey: normalizedJourney,
+      },
+      {
+        call: context.call,
+        channel: {
+          ...context.channel,
+          service: 'normalization',
+        },
+      },
+    );
+
+    return;
+  }
+
+  private async findTown(position: PositionInterface): Promise<PositionInterface> {
+    return this.geoProvider.getTown({ lon: position.lon, lat: position.lat, insee: position.insee, literal: position.literal });
+  }
+
+
+  /**
+   * Complete position with data relative to town
+   */
+  private processTownResponse(journey, path, position, determinedPosition) {
+    // console.log(journey, path, position, determinedPosition)
+    if (determinedPosition.lon && !position.lon) {
+      position.lon = determinedPosition.lon;
+      _.set(journey, `${path}.lon`, determinedPosition.lon);
+    }
+
+    if (determinedPosition.lat && !position.lat) {
+      position.lat = determinedPosition.lat;
+      _.set(journey, `${path}.lat`, determinedPosition.lat);
+    }
+
+    if (determinedPosition.insee && !position.insee) {
+      position.insee = determinedPosition.insee;
+      _.set(journey, `${path}.insee`, determinedPosition.insee);
+    }
+
+    if (determinedPosition.town && !position.town) {
+      position.town = determinedPosition.town;
+      _.set(journey, `${path}.town`, determinedPosition.town);
+    }
+
+    if (determinedPosition.country && !position.country) {
+      position.country = determinedPosition.country;
+      _.set(journey, `${path}.country`, determinedPosition.country);
+    }
+
+    // concat postcodes with existing ones
+    const pcs = _.get(journey, `${path}.postcodes`, []).concat(determinedPosition.postcodes);
+    position.postcodes = _.uniq(pcs);
+    _.set(journey, `${path}.postcodes`, position.postcodes);
+
+
+    // console.log({ journey });
+    return journey;
+  }
+}
---
+++ api/services/normalization/src/actions/NormalizationTerritoryAction.ts
@@ -0,0 +1,93 @@
+import { Parents, Container, Types, Interfaces, Exceptions } from '@ilos/core';
+import * as _ from 'lodash';
+import { GeoProviderInterfaceResolver } from '@pdc/provider-geo';
+import { ConfigProviderInterfaceResolver } from '@ilos/provider-config';
+import { ObjectId } from '@ilos/provider-mongo';
+
+import { PositionInterface } from '../interfaces/PositionInterface';
+
+interface NormalizationTerritoryParamsInterface {
+  journey: any;
+}
+
+/*
+ * Enrich journey with Territories
+ */
+@Container.handler({
+  service: 'normalization',
+  method: 'territory',
+})
+export class NormalizationTerritoryAction extends Parents.Action {
+  constructor(
+    private kernel: Interfaces.KernelInterfaceResolver,
+    private geoProvider: GeoProviderInterfaceResolver,
+    private config: ConfigProviderInterfaceResolver,
+  ) {
+    super();
+  }
+
+  public async handle(param: NormalizationTerritoryParamsInterface, context: Types.ContextType): Promise<void> {
+    const paths = this.config.get('normalization.positionPaths');
+
+    const territoriesEnrichedJourney = {
+      ...param.journey,
+    };
+
+    await Promise.all(paths.map(async (path) => {
+      const position = _.get(param.journey, path);
+      const territories = await this.findTerritories(position, context);
+      _.set(territoriesEnrichedJourney, `${path}.territories`, territories);
+    }));
+
+    await this.kernel.notify(
+      'crosscheck:process',
+      {
+        journey: territoriesEnrichedJourney,
+      },
+      {
+        call: context.call,
+        channel: {
+          ...context.channel,
+          service: 'normalization',
+        },
+      },
+    );
+
+    return;
+  }
+
+  public async findTerritories(position: PositionInterface, context: Types.ContextType): Promise<ObjectId[]> {
+    if ('insee' in position) {
+      return this.kernel.call(
+        'territory:findByInsee',
+        {
+          insee: position.insee,
+        },
+        {
+          call: context.call,
+          channel: {
+            ...context.channel,
+            service: 'normalization',
+          },
+        },
+      );
+    }
+    if ('lat' in position && 'lon' in position) {
+      return this.kernel.call(
+        'territory:findByLatLon',
+        {
+          lat: position.lat,
+          lon: position.lon,
+        },
+        {
+          call: context.call,
+          channel: {
+            ...context.channel,
+            service: 'normalization',
+          },
+        },
+      );
+    }
+    throw new Exceptions.InvalidParamsException('missing insee or lat & lon');
+  }
+}
