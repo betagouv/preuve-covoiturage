94e43d3967a531de1723f42694da8ec8bd49f2df
Jonathan Fallon <jonathan@dotify.fr>
2019-07-16 14:55:45 +0200

    remove double newlines in serve-dev

--- api/serve-dev.ts
+++ api/serve-dev.ts
@@ -9,7 +9,7 @@ import { spawn } from 'child_process';
 const server = spawn('yarn', ['workspace', '@pdc/proxy', 'run', 'ilos', 'http', '8080']);
 
 server.stdout.setEncoding('utf8');
-server.stdout.on('data', (data) => console.log(`(server)> ${data.toString()}`));
+server.stdout.on('data', (data) => console.log(`(server)> ${data.toString()}`.replace(/(\r?\n|\r)$/, '')));
 
 server.stderr.setEncoding('utf8');
-server.stderr.on('data', (data) => console.log(`(error)> ${data.toString()}`));
+server.stderr.on('data', (data) => console.log(`(error)> ${data.toString()}`.replace(/(\r?\n|\r)$/, '')));
