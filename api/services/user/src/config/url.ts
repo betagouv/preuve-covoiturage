ebbbcad9fa509dfa5e1af678d6281f9fad0e63ac
Jonathan Fallon <jonathan@dotify.fr>
2019-07-18 00:43:52 +0200

    set defaults for APP_URL config keys

--- api/services/user/src/config/url.ts
+++ api/services/user/src/config/url.ts
@@ -1,4 +1,4 @@
 declare function env(key: string, fallback?: string): any;
 
-export const apiUrl = env('APP_API_URL', 'nope');
-export const appUrl = env('APP_APP_URL') ? env('APP_APP_URL') : apiUrl.replace('api', 'dashboard');
+export const apiUrl = env('APP_API_URL', 'http://localhost:8080');
+export const appUrl = env('APP_APP_URL', 'http://localhost:4200');
