0cc7831196354c2f73fd629cde63295036e26688
Jonathan Fallon <jonathan@dotify.fr>
2019-07-18 23:03:34 +0200

    scalingo deploy

--- api/scalingo/prebuild.sh
+++ api/scalingo/prebuild.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-pwd
+# script runs from the api folder
 
 # install global dependencies
-# yarn global add npm-run-all lerna
+yarn global add npm-run-all lerna
 
 # install ilos dev branch
-# git clone https://github.com/betagouv/ilos
-# cd ilos
-# git checkout dev
-# yarn
-# yarn build
+git clone https://github.com/betagouv/ilos
+cd ilos
+git checkout dev
+yarn
+yarn build
