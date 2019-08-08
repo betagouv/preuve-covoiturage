f415fa0e42eb9781c78962e5f2a95c177365a2bf
Vincent Lainé <vincent.laine.utc@gmail.com>
2019-08-08 17:57:42 +0200

    begin rules form

--- dashboard/src/app/modules/campaign/components/campaign-form/rules-form/rules-form.component.ts
+++ dashboard/src/app/modules/campaign/components/campaign-form/rules-form/rules-form.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+import { TripClass } from '~/core/entities/trip/trip-class';
+
 @Component({
   selector: 'app-rules-form',
   templateUrl: './rules-form.component.html',
   styleUrls: ['./rules-form.component.scss'],
 })
 export class RulesFormComponent implements OnInit {
+  tripClassKeys = Object.keys(TripClass);
+
   constructor() {}
 
   ngOnInit() {}
