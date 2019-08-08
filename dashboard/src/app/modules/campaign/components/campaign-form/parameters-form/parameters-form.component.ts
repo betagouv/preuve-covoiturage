01b8091586336dba568768af86fd9bdd8bcadb8f
Vincent Lainé <vincent.laine.utc@gmail.com>
2019-08-08 16:57:17 +0200

    init components forms for campaign creation + init policy form

---
+++ dashboard/src/app/modules/campaign/components/campaign-form/parameters-form/parameters-form.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-parameters-form',
+  templateUrl: './parameters-form.component.html',
+  styleUrls: ['./parameters-form.component.scss'],
+})
+export class ParametersFormComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
---
+++ dashboard/src/app/modules/campaign/components/campaign-form/summary-form/summary-form.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-summary-form',
+  templateUrl: './summary-form.component.html',
+  styleUrls: ['./summary-form.component.scss'],
+})
+export class SummaryFormComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
