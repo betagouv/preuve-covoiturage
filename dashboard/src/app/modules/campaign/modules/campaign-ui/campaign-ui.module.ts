cc49b040b054ce2966231ca1726af33c0c0e2cda
Samuel Eyre <samuel.eyre@hotmail.fr>
2019-08-13 17:15:58 +0200

    (dashboard: campaign) add: campaign-ui module, add: campaign auto-complete component

---
+++ dashboard/src/app/modules/campaign/modules/campaign-ui/campaign-ui.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { MaterialModule } from '~/shared/material/material.module';
+
+import { CampaignAutoCompleteComponent } from './components/campaign-auto-complete/campaign-auto-complete.component';
+
+@NgModule({
+  declarations: [CampaignAutoCompleteComponent],
+  imports: [CommonModule, MaterialModule, FormsModule, ReactiveFormsModule],
+  exports: [CampaignAutoCompleteComponent],
+})
+export class CampaignUiModule {}
