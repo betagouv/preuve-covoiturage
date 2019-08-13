abc978ab24f9f166c5373efa4d6cbc7f9ed62bbc
Samuel Eyre <samuel.eyre@hotmail.fr>
2019-08-13 17:14:04 +0200

    (dashboard: filter) implemente: filter module

---
+++ dashboard/src/app/modules/filter/filter.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { MaterialModule } from '~/shared/material/material.module';
+import { CampaignUiModule } from '~/modules/campaign/modules/campaign-ui/campaign-ui.module';
+
+import { FilterComponent } from './components/filter/filter.component';
+import { OperatorUiModule } from '~/modules/operator/modules/operator-ui/operator-ui.module';
+import { TownsAutocompleteComponent } from './components/towns-autocomplete/towns-autocomplete.component';
+
+@NgModule({
+  declarations: [FilterComponent, TownsAutocompleteComponent],
+  imports: [CommonModule, MaterialModule, FormsModule, ReactiveFormsModule, CampaignUiModule, OperatorUiModule],
+  exports: [FilterComponent],
+})
+export class FilterModule {}
