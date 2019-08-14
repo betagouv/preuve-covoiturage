be0a7dfe3740e47420b3df096f5e49431790b385
Samuel Eyre <samuel.eyre@hotmail.fr>
2019-08-14 15:31:11 +0200

    (dashboard: filter) add: api-geo of api.gouv to get city names

--- dashboard/src/app/modules/filter/components/towns-autocomplete/towns-autocomplete.component.ts
+++ dashboard/src/app/modules/filter/components/towns-autocomplete/towns-autocomplete.component.ts
@@ -1,10 +1,11 @@
 import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
-import { tap } from 'rxjs/operators';
+import { debounceTime, tap } from 'rxjs/operators';
 import { MatAutocompleteSelectedEvent } from '@angular/material';
 import * as _ from 'lodash';
 
 import { TownInterface } from '~/core/interfaces/geography/townInterface';
+import { TownService } from '~/modules/filter/services/town.service';
 
 @Component({
   selector: 'app-towns-autocomplete',
@@ -14,28 +15,29 @@ import { TownInterface } from '~/core/interfaces/geography/townInterface';
 export class TownsAutocompleteComponent implements OnInit {
   public townCtrl = new FormControl();
   public townForm;
-  public filteredTowns: TownInterface[];
-
-  // TODO TMP REMOVE WHEN FINISHED
-  public mockTowns: TownInterface[] = [
-    {
-      name: 'Lyon',
-    },
-    {
-      name: 'Paris',
-    },
-  ];
+  public filteredTowns: TownInterface[] = [];
 
   @Input() parentForm: FormGroup;
 
   @ViewChild('townInput', { static: false }) townInput: ElementRef;
 
-  constructor() {}
+  constructor(private townService: TownService) {}
 
   ngOnInit() {
-    this.filterTowns();
     this.townForm = this.parentForm.get('towns');
-    this.townForm.valueChanges.pipe(tap((towns: TownInterface[]) => this.filterTowns(towns))).subscribe();
+    this.townForm.valueChanges
+      .pipe(
+        tap((towns: TownInterface[]) => {
+          this.filteredTowns = _.differenceWith(this.filteredTowns, towns, (x, y) => x.name === y.name);
+        }),
+      )
+      .subscribe();
+    this.townCtrl.valueChanges
+      .pipe(
+        debounceTime(1000),
+        tap((literal: string) => this.findTowns(literal)),
+      )
+      .subscribe();
   }
 
   public remove(town: TownInterface): void {
@@ -47,6 +49,15 @@ export class TownsAutocompleteComponent implements OnInit {
     }
   }
 
+  public findTowns(literal: string = ''): void {
+    if (!literal) {
+      return;
+    }
+    this.townService.findTowns(literal).subscribe((towns: TownInterface[]) => {
+      this.filteredTowns = _.differenceWith(towns, this.townForm.value, (x, y) => x.name === y.name);
+    });
+  }
+
   public onTownSelect(event: MatAutocompleteSelectedEvent): void {
     const towns: TownInterface[] = this.townForm.value || [];
     towns.push({ name: event.option.viewValue });
@@ -55,7 +66,7 @@ export class TownsAutocompleteComponent implements OnInit {
     this.townCtrl.setValue(null);
   }
 
-  private filterTowns(towns: TownInterface[] = []): void {
-    this.filteredTowns = _.differenceWith(this.mockTowns, towns, (x, y) => x.name === y.name);
+  private filterTowns(): void {
+    const selectedTowns = this.townForm.value;
   }
 }
