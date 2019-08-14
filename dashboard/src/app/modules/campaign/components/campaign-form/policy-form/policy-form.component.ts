74a4e6f803cf89d270e27281fa3119cea62f9b75
Vincent Lainé <vincent.laine.utc@gmail.com>
2019-08-14 15:45:20 +0200

    Init summary form + templates data

--- dashboard/src/app/modules/campaign/components/campaign-form/policy-form/policy-form.component.ts
+++ dashboard/src/app/modules/campaign/components/campaign-form/policy-form/policy-form.component.ts
@@ -1,9 +1,11 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
 import { FormControl } from '@angular/forms';
+import { MatStepper } from '@angular/material';
 
 import { CampaignService } from '~/modules/campaign/services/campaign.service';
 import { Campaign } from '~/core/entities/campaign/campaign';
+import { IncentiveRules } from '~/core/entities/campaign/incentive-rules';
 
 @Component({
   selector: 'app-policy-form',
@@ -12,6 +14,7 @@ import { Campaign } from '~/core/entities/campaign/campaign';
 })
 export class PolicyFormComponent implements OnInit {
   @Input() control: FormControl;
+  @Input() matStepper: MatStepper;
   templates: Campaign[];
 
   constructor(public campaignService: CampaignService, private toastr: ToastrService) {}
@@ -22,6 +25,9 @@ export class PolicyFormComponent implements OnInit {
 
   onTemplateCardClick(template: Campaign) {
     this.control.setValue(template);
+    if (template === null) {
+      this.matStepper.next();
+    }
   }
 
   private loadCampaignTemplates() {
@@ -38,11 +44,29 @@ export class PolicyFormComponent implements OnInit {
           _id: '1',
           name: 'Encourager le covoiturage',
           description: 'Cras quis nulla commodo, aliquam lectus sed, blandit augue.',
+          rules: <IncentiveRules>{
+            weekday: [0, 1, 2, 3, 4, 5, 6],
+            time: [{ start: '08:00', end: '19:00' }],
+            range: { min: 0, max: 100 },
+            ranks: ['A', 'B'],
+            onlyMajorPeople: false,
+            forDriver: true,
+            forPassenger: true,
+          },
         });
         this.templates.push(<Campaign>{
           _id: '2',
           name: 'Limiter le trafic en semaine',
           description: 'Fusce vehicula dolor arcu, sit amet blandit dolor mollis.',
+          rules: <IncentiveRules>{
+            weekday: [0, 1, 2, 3, 4],
+            time: [{ start: '06:00', end: '09:00' }, { start: '16:00', end: '19:00' }],
+            range: { min: 0, max: 15 },
+            ranks: ['A', 'B', 'C'],
+            onlyMajorPeople: false,
+            forDriver: true,
+            forPassenger: true,
+          },
         });
         this.templates.push(<Campaign>{
           _id: '3',
