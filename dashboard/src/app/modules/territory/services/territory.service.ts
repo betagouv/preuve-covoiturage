a1a4358b3b6b6f8b712a3191c848267cc0699f05
Vincent Lainé <vincent.laine.utc@gmail.com>
2019-08-20 16:37:44 +0200

    Add Operator Service

--- dashboard/src/app/modules/territory/services/territory.service.ts
+++ dashboard/src/app/modules/territory/services/territory.service.ts
@@ -1,12 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { BehaviorSubject, Observable } from 'rxjs';
-import { finalize, tap } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 import { JsonRPCService } from '~/core/services/api/json-rpc.service';
 import { ApiService } from '~/core/services/api/api.service';
 import { Territory } from '~/core/entities/territory/territory';
-import { JsonRPCParam } from '~/core/entities/api/jsonRPCParam';
 
 @Injectable({
   providedIn: 'root',
