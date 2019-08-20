73d7d7dcb82008712f9ed4fbbb3246267d6295a7
Vincent Lainé <vincent.laine.utc@gmail.com>
2019-08-20 16:42:01 +0200

    lint

--- dashboard/src/app/modules/administration/administration.module.ts
+++ dashboard/src/app/modules/administration/administration.module.ts
@@ -8,13 +8,13 @@ import { MaterialModule } from '~/shared/modules/material/material.module';
 import { ChangePasswordModule } from '~/modules/authentication/modules/change-password/change-password.module';
 import { UiUserModule } from '~/modules/user/modules/ui-user/ui-user.module';
 import { TerritoryUiModule } from '~/modules/territory/modules/territory-ui/territory-ui.module';
+import { SharedModule } from '~/shared/shared.module';
 
 import { AdministrationRoutingModule } from './administration-routing.module';
 import { AdministrationLayoutComponent } from './administration-layout/administration-layout.component';
 import { ProfileComponent } from './pages/profile/profile.component';
 import { TerritoryComponent } from './pages/territory/territory.component';
 import { UsersComponent } from './pages/users/users.component';
-import { SharedModule } from '~/shared/shared.module';
 
 @NgModule({
   declarations: [AdministrationLayoutComponent, ProfileComponent, TerritoryComponent, UsersComponent],
