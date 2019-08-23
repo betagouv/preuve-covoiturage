cecf21a6a5d819acff20a7797e6c4a3816126fff
Samuel Eyre <samuel.eyre@hotmail.fr>
2019-08-23 16:57:41 +0200

    (dashboard) clean translations

---
+++ dashboard/src/app/core/const/trip/tripStatus.const.ts
@@ -0,0 +1,3 @@
+import { TripStatusType } from '~/core/types/trip/statusType';
+
+export const TRIP_STATUS: TripStatusType[] = ['pending', 'active', 'error'];
---
+++ dashboard/src/app/modules/user/const/role_fr.const.ts
@@ -0,0 +1,4 @@
+export const ROLE_FR = {
+  user: 'consultation',
+  admin: 'administrateur',
+};
