b013719c127ae2c721c53037521b4f018d1be28d
Vincent Lainé <vincent.laine.utc@gmail.com>
2019-08-26 18:00:33 +0200

    add territories on map + number of campaigns + filter when zoom on map

--- dashboard/src/app/modules/campaign/pages/campaign-discover/campaign-discover.component.ts
+++ dashboard/src/app/modules/campaign/pages/campaign-discover/campaign-discover.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { CampaignService } from '~/modules/campaign/services/campaign.service';
-import { Campaign } from '~/core/entities/campaign/campaign';
 import { ToastrService } from 'ngx-toastr';
+
+import { Campaign } from '~/core/entities/campaign/campaign';
 import { CampaignStatus } from '~/core/entities/campaign/campaign-status';
+import { CampaignService } from '~/modules/campaign/services/campaign.service';
 
 @Component({
   selector: 'app-campaign-discover',
@@ -11,6 +12,7 @@ import { CampaignStatus } from '~/core/entities/campaign/campaign-status';
 })
 export class CampaignDiscoverComponent implements OnInit {
   campaigns: Campaign[];
+  campaignsToShow: Campaign[];
 
   constructor(public campaignService: CampaignService, private toastr: ToastrService) {}
 
@@ -18,10 +20,23 @@ export class CampaignDiscoverComponent implements OnInit {
     this.loadCampaigns();
   }
 
+  onMapResize($event) {
+    this.campaignsToShow = this.campaigns.filter((c: Campaign) => {
+      const territoryCoords = c.territory.coordinates;
+      return (
+        $event[0] <= territoryCoords[0] &&
+        $event[2] >= territoryCoords[0] &&
+        $event[1] <= territoryCoords[1] &&
+        $event[3] >= territoryCoords[1]
+      );
+    });
+  }
+
   private loadCampaigns(): void {
     this.campaignService.load().subscribe(
       (campaigns) => {
         this.campaigns = campaigns;
+        this.campaignsToShow = this.campaigns;
       },
       (err) => {
         this.toastr.error(err.message);
@@ -31,6 +46,7 @@ export class CampaignDiscoverComponent implements OnInit {
         for (let i = 0; i < 30; i = i + 1) {
           this.campaigns.push(this.generateCampaigns(i));
         }
+        this.campaignsToShow = this.campaigns;
       },
     );
   }
@@ -48,6 +64,18 @@ export class CampaignDiscoverComponent implements OnInit {
     campaignToReturn.trips_number = Math.floor(Math.random() * 10000);
     campaignToReturn.amount_spent = Math.floor(Math.random() * 20000);
     campaignToReturn.territory = {};
+    campaignToReturn.territory._id = Math.floor(Math.random() * 3);
+    switch (campaignToReturn.territory._id) {
+      case 0:
+        campaignToReturn.territory.coordinates = [5.36978, 43.296482];
+        break;
+      case 1:
+        campaignToReturn.territory.coordinates = [4.835659, 45.764043];
+        break;
+      case 2:
+        campaignToReturn.territory.coordinates = [2.3522219, 48.856614];
+        break;
+    }
     campaignToReturn.territory.name = 'Communauté d’Agglomération du Pays de Gex';
     return campaignToReturn;
   }
