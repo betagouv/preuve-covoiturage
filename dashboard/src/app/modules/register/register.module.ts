58a7d89b44590dfaa4ec548e48cf5745ee3b3c05
gilles boisson <gilles@dotify.fr>
2019-09-04 11:12:03 +0200

    Register module setup + layout

---
+++ dashboard/src/app/modules/register/register.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { RegisterLayoutComponent } from './register-layout/register-layout.component';
+
+@NgModule({
+  declarations: [RegisterLayoutComponent],
+  imports: [CommonModule],
+})
+export class RegisterModule {}
