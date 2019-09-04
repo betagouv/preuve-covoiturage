bf0091b88f1429ad35dcaf197d135171a1c1926f
gilles boisson <gilles@dotify.fr>
2019-09-04 11:12:03 +0200

    Register module setup + layout

---
+++ dashboard/src/app/modules/register/register-layout/register-layout.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-register-layout',
+  templateUrl: './register-layout.component.html',
+  styleUrls: ['./register-layout.component.scss'],
+})
+export class RegisterLayoutComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
