12830b04e0e560ede8e114739405e593c9049e1d
gilles boisson <gilles@dotify.fr>
2019-09-05 15:49:58 +0200

    users admin : edit / create user form variation + validation

--- dashboard/src/app/modules/user/modules/ui-user/components/create-edit-user-form/create-edit-user-form.component.ts
+++ dashboard/src/app/modules/user/modules/ui-user/components/create-edit-user-form/create-edit-user-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 import { ToastrService } from 'ngx-toastr';
 import { takeUntil } from 'rxjs/operators';
 
