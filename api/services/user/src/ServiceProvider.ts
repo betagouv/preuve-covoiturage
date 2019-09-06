1671bb87e354145a7121e0f119c8ad6b71af50d6
Jonathan Fallon <jonathan@dotify.fr>
2019-09-06 14:33:20 +0200

    use NoopMiddleware in local env

--- api/services/user/src/ServiceProvider.ts
+++ api/services/user/src/ServiceProvider.ts
@@ -1,7 +1,7 @@
 import path from 'path';
 import { Extensions, ServiceProvider as AbstractServiceProvider } from '@ilos/core';
 import { serviceProvider, NewableType, ExtensionInterface } from '@ilos/common';
-import { PermissionMiddleware } from '@ilos/package-acl';
+// import { PermissionMiddleware } from '@ilos/package-acl';
 import { MongoConnection } from '@ilos/connection-mongo';
 import { ConfigExtension } from '@ilos/config';
 import { ConnectionManagerExtension } from '@ilos/connection-manager';
@@ -13,6 +13,7 @@ import {
   ScopeToSelfMiddleware,
   ContentBlacklistMiddleware,
   ContentWhitelistMiddleware,
+  NoopMiddleware as PermissionMiddleware,
 } from '@pdc/provider-middleware';
 import {
   userPatchSchema,
