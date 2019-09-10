d182a2b979e16d2b5b798cd220d00de932ac6be3
Samuel Eyre <samuel.eyre@hotmail.fr>
2019-09-10 10:49:51 +0200

    (dashboard: core) fix: campaign entity, add: campaign template entity

---
+++ dashboard/src/app/core/interfaces/campaign/templateInterface.ts
@@ -0,0 +1,4 @@
+import { CreateCampaignInterface } from '~/core/interfaces/campaign/campaignInterface';
+
+// tslint:disable-next-line:no-empty-interface
+export interface TemplateInterface extends CreateCampaignInterface {}
