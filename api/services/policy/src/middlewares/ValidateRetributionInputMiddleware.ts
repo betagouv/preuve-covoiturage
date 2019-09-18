fcbae961e48f3e50be5122456d90f67a3bf5afcc
Nicolas Mérigot <nicolas.merigot@gmail.com>
2019-09-18 14:38:33 +0200

    fix(policy): clean up on validate retribution middleware

--- api/services/policy/src/middlewares/ValidateRetributionInputMiddleware.ts
+++ api/services/policy/src/middlewares/ValidateRetributionInputMiddleware.ts
@@ -15,7 +15,6 @@ export class ValidateRetributionInputMiddleware implements MiddlewareInterface, 
   constructor(private validator: ValidatorInterfaceResolver) {}
 
   async init() {
-    console.log('JE SUIS LA');
     for (const policy of policies) {
       this.validator.registerValidator(policy.schema, `policies.${policy.slug}`);
     }
@@ -23,23 +22,25 @@ export class ValidateRetributionInputMiddleware implements MiddlewareInterface, 
 
   async process(params: CampaignInterface, context: ContextType, next?: Function, options?: any): Promise<ResultType> {
     const retributionRules = params.retribution_rules;
-    const availablePolicies = policies.map((policy) => policy.slug);
+    if (retributionRules) {
+      const availablePolicies = policies.map((policy) => policy.slug);
 
-    const notAppliablePolicies = retributionRules.filter((policy) => availablePolicies.indexOf(policy.slug) < 0);
-    if (notAppliablePolicies.length > 0) {
-      throw new InvalidParamsException(
-        `Unknown retribution rules: ${notAppliablePolicies.map((rule) => rule.slug).join(', ')}`,
-      );
-    }
-
-    for (const rule of retributionRules) {
-      if (!('parameters' in rule)) {
-        throw new InvalidParamsException(`Unparametred rule ${rule.slug}`);
+      const notAppliablePolicies = retributionRules.filter((policy) => availablePolicies.indexOf(policy.slug) < 0);
+      if (notAppliablePolicies.length > 0) {
+        throw new InvalidParamsException(
+          `Unknown retribution rules: ${notAppliablePolicies.map((rule) => rule.slug).join(', ')}`,
+        );
       }
-      try {
-        await this.validator.validate(rule.parameters, `policies.${rule.slug}`);
-      } catch (e) {
-        throw new InvalidParamsException(e.message);
+
+      for (const rule of retributionRules) {
+        if (!('parameters' in rule)) {
+          throw new InvalidParamsException(`Unparametred rule ${rule.slug}`);
+        }
+        try {
+          await this.validator.validate(rule.parameters, `policies.${rule.slug}`);
+        } catch (e) {
+          throw new InvalidParamsException(e.message);
+        }
       }
     }
 
