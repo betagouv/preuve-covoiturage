1f54e1d97373a312935b3bfe5a0c24f23cd93765
Samuel Eyre <samuel.eyre@hotmail.fr>
2019-09-18 13:41:54 +0200

    (dashboard) up: group permissions : operator >> operators, territory >> territories

--- dashboard/src/app/modules/campaign/campaign-routing.module.ts
+++ dashboard/src/app/modules/campaign/campaign-routing.module.ts
@@ -7,11 +7,12 @@ import { CampaignDashboardComponent } from '~/modules/campaign/pages/campaign-da
 // tslint:disable-next-line:max-line-length
 import { CampaignCreateEditComponent } from '~/modules/campaign/pages/campaign-create-edit/campaign-create-edit.component';
 import { CampaignDiscoverComponent } from '~/modules/campaign/pages/campaign-discover/campaign-discover.component';
+import { UserGroupEnum } from '~/core/enums/user/user-group.enum';
 
 const routes: Routes = [
   {
     path: '',
-    data: { groups: ['territory'] },
+    data: { groups: [UserGroupEnum.TERRITORY] },
     canActivateChild: [AuthGuard],
     children: [
       {
