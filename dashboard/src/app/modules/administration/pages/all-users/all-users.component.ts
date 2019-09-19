66c9c6b2d49c381b847d245974f1e33d2ab07701
gilles boisson <gilles@dotify.fr>
2019-09-19 11:01:31 +0200

    user : group filter in front end

--- dashboard/src/app/modules/administration/pages/all-users/all-users.component.ts
+++ dashboard/src/app/modules/administration/pages/all-users/all-users.component.ts
@@ -101,7 +101,7 @@ export class AllUsersComponent extends DestroyObservable implements OnInit {
   private filterUsers() {
     const query = this.searchFilters.controls.query.value;
     this.usersToShow = this.users.filter(
-      (u) => u.email.includes(query) || `${u.firstname} ${u.lastname}`.includes(query),
+      (u) => (u.email.includes(query) || `${u.firstname} ${u.lastname}`.includes(query)) && this.userGroup === u.group,
     );
   }
 }
