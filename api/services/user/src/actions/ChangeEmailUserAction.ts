fe5fb9a5430847a75a465796c427679ac5e0884b
Jonathan Fallon <jonathan@dotify.fr>
2019-09-23 15:12:36 +0200

    add support for user statuses on user:login

---
+++ api/services/user/src/actions/ChangeEmailUserAction.ts
@@ -0,0 +1,107 @@
+// tslint:disable: variable-name
+
+import { Action as AbstractAction } from '@ilos/core';
+import { ConfigInterfaceResolver, handler, ContextType, KernelInterfaceResolver } from '@ilos/common';
+import { CryptoProviderInterfaceResolver } from '@pdc/provider-crypto';
+import { UserChangeEmailParamsInterface } from '@pdc/provider-schema';
+
+import { UserRepositoryProviderInterfaceResolver } from '../interfaces/UserRepositoryProviderInterface';
+import { User } from '../entities/User';
+import { userWhiteListFilterOutput } from '../config/filterOutput';
+
+/*
+ * Update email of user and send email for confirmation
+ */
+@handler({
+  service: 'user',
+  method: 'changeEmail',
+})
+export class ChangeEmailUserAction extends AbstractAction {
+  public readonly middlewares: (string | [string, any])[] = [
+    ['validate', 'user.changeEmail'],
+    [
+      'scopeIt',
+      [
+        ['user.update'],
+        [
+          (params, context) => {
+            if ('_id' in params && params._id === context.call.user._id) {
+              return 'profile.update';
+            }
+          },
+          (_params, context) => {
+            if (context.call.user.territory) {
+              return 'territory.users.update';
+            }
+          },
+          (_params, context) => {
+            if (context.call.user.operator) {
+              return 'operator.users.update';
+            }
+          },
+        ],
+      ],
+    ],
+    ['content.whitelist', userWhiteListFilterOutput],
+  ];
+  constructor(
+    private config: ConfigInterfaceResolver,
+    private cryptoProvider: CryptoProviderInterfaceResolver,
+    private kernel: KernelInterfaceResolver,
+    private userRepository: UserRepositoryProviderInterfaceResolver,
+  ) {
+    super();
+  }
+
+  public async handle(params: UserChangeEmailParamsInterface, context: ContextType): Promise<User> {
+    const user = await this.userRepository.find(params._id);
+    const contextParam: { territory?: string; operator?: string } = {};
+
+    if (context.call.user.territory) {
+      contextParam.territory = context.call.user.territory;
+    }
+
+    if (context.call.user.operator) {
+      contextParam.operator = context.call.user.operator;
+    }
+
+    const email_change_at = new Date();
+    const confirm = this.cryptoProvider.generateToken();
+    const token = this.cryptoProvider.generateToken();
+
+    const cryptedToken = await this.cryptoProvider.cryptToken(token);
+
+    const requester = new User(context.call.user);
+
+    const patch = {
+      email_change_at,
+      email_confirm: confirm,
+      email_token: cryptedToken,
+      email: params.email,
+      status: 'pending',
+    };
+
+    const patchedUser = this.userRepository.patchUser(params._id, patch, contextParam);
+
+    await this.kernel.call(
+      'user:notify',
+      {
+        template: this.config.get('email.templates.confirm'),
+        email: patch.email,
+        fullname: user.fullname,
+        requester: requester.fullname,
+        organization: 'TerritoryOrOperatorOrganisation',
+        link: `${this.config.get('url.appUrl')}/confirm-email/${confirm}/${token}`,
+      },
+      {
+        call: context.call,
+        channel: {
+          ...context.channel,
+          service: 'user',
+        },
+      },
+    );
+
+    return patchedUser;
+  }
+}
