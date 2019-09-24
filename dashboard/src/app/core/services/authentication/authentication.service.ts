056f378b6abb328fda78e2ba7d95b148e0454469
gilles boisson <gilles@dotify.fr>
2019-09-24 14:04:13 +0200

    auth : link with API activation process

--- dashboard/src/app/core/services/authentication/authentication.service.ts
+++ dashboard/src/app/core/services/authentication/authentication.service.ts
@@ -150,7 +150,6 @@ export class AuthenticationService {
     }
     return !groups.length || ('group' in user && groups.includes(user.group));
   }
-
   /**
    * Check if connected user has role
    */
