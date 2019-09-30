2b671f7bd8255d1cef2a5a537b55f70cb646e9ba
Samuel Eyre <samuel.eyre@hotmail.fr>
2019-09-30 11:13:10 +0200

    (dashboard) fix: calls to connected user data in services

--- dashboard/src/app/modules/operator/services/operator-token.service.ts
+++ dashboard/src/app/modules/operator/services/operator-token.service.ts
@@ -8,13 +8,17 @@ import { OperatorToken } from '~/core/entities/operator/operatorToken';
 import { JsonRPCService } from '~/core/services/api/json-rpc.service';
 import { JsonRPCParam } from '~/core/entities/api/jsonRPCParam';
 import { OperatorTokenInterface } from '~/core/interfaces/operator/operatorTokenInterface';
-import { UserService } from '~/core/services/authentication/user.service';
+import { AuthenticationService } from '~/core/services/authentication/authentication.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class OperatorTokenService extends ApiService<OperatorTokenInterface> {
-  constructor(private _http: HttpClient, private _jsonRPC: JsonRPCService, private _userService: UserService) {
+  constructor(
+    private _http: HttpClient,
+    private _jsonRPC: JsonRPCService,
+    private _authService: AuthenticationService,
+  ) {
     super(_http, _jsonRPC, 'application');
   }
 
@@ -31,8 +35,8 @@ export class OperatorTokenService extends ApiService<OperatorTokenInterface> {
   }
 
   load(): Observable<OperatorTokenInterface[]> {
-    if ('operator' in this._userService.user) {
-      const operatorId = this._userService.user.operator;
+    if ('operator' in this._authService.user) {
+      const operatorId = this._authService.user.operator;
       return super.load({
         operator_id: operatorId,
       });
@@ -42,8 +46,8 @@ export class OperatorTokenService extends ApiService<OperatorTokenInterface> {
   }
 
   public createToken(applicationName: OperatorToken): Observable<{ token: string }> {
-    if ('operator' in this._userService.user) {
-      const operatorId = this._userService.user.operator;
+    if ('operator' in this._authService.user) {
+      const operatorId = this._authService.user.operator;
       const jsonRPCParam = new JsonRPCParam(`${this._method}:create`, {
         operator_id: operatorId,
         permissions: ['journey.create'],
