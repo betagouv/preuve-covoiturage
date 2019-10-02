472a03da0c537d00fa6acbc55d8be7db9412ddfc
Jonathan Fallon <jonathan@dotify.fr>
2019-10-02 22:11:34 +0200

    fix #360. revoke apps

--- api/services/application/src/interfaces/ApplicationRepositoryProviderInterface.ts
+++ api/services/application/src/interfaces/ApplicationRepositoryProviderInterface.ts
@@ -4,14 +4,14 @@ declare type Model = any;
 
 export interface ApplicationRepositoryProviderInterface extends RepositoryInterface {
   allByOperator(params: { operator_id: string }): Promise<Model[]>;
-  softDelete(params: { _id: string; operator_id: string }): Promise<boolean>;
+  softDelete(params: { _id: string; operator_id: string; deleted_at?: any }): Promise<boolean>;
 }
 
 export abstract class ApplicationRepositoryProviderInterfaceResolver extends RepositoryInterfaceResolver {
   async allByOperator(params: { operator_id: string }): Promise<Model[]> {
     throw new Error('Not implemented');
   }
-  async softDelete(params: { _id: string; operator_id: string }): Promise<Model> {
+  async softDelete(params: { _id: string; operator_id: string; deleted_at?: any }): Promise<Model> {
     throw new Error('Not implemented');
   }
 }
