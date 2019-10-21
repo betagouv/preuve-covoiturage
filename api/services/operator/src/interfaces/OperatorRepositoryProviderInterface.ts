cc42ad6a9dddaf6c7d05e4af2411d9d5b0f1f8bb
Nicolas Mérigot <nicolas.merigot@gmail.com>
2019-10-21 12:01:57 +0200

    fix(operator): clean up on interfaces

--- api/services/operator/src/interfaces/OperatorRepositoryProviderInterface.ts
+++ api/services/operator/src/interfaces/OperatorRepositoryProviderInterface.ts
@@ -1,5 +1,28 @@
-import { RepositoryInterface, RepositoryInterfaceResolver } from '@ilos/common';
+import { RepositoryInterfaceResolver } from '@ilos/common';
+import { OperatorInterface } from '@pdc/provider-schema/dist';
 
-export interface OperatorRepositoryProviderInterface extends RepositoryInterface {}
+export interface OperatorRepositoryProviderInterface {
+  find(id: string): Promise<OperatorInterface>;
+  all(): Promise<OperatorInterface[]>;
+  create(data: OperatorInterface): Promise<OperatorInterface>;
+  delete(_id: string): Promise<void>;
+  patch(id: string, patch: { [k: string]: any }): Promise<OperatorInterface>;
+}
 
-export abstract class OperatorRepositoryProviderInterfaceResolver extends RepositoryInterfaceResolver {}
+export abstract class OperatorRepositoryProviderInterfaceResolver implements OperatorRepositoryProviderInterface {
+  async find(id: string): Promise<OperatorInterface> {
+    throw new Error();
+  }
+  async all(): Promise<OperatorInterface[]> {
+    throw new Error();
+  }
+  async create(data: OperatorInterface): Promise<OperatorInterface> {
+    throw new Error();
+  }
+  async delete(_id: string): Promise<void> {
+    throw new Error();
+  }
+  async patch(id: string, patch: { [k: string]: any }): Promise<OperatorInterface> {
+    throw new Error();
+  }
+}
