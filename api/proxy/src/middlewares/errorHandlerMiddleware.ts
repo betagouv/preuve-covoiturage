0bc463f64f4e935976d93c0b70ae92619c719241
Nicolas Mérigot <nicolas.merigot@gmail.com>
2019-10-29 11:29:09 +0100

    fix(proxy): log proxy error

--- api/proxy/src/middlewares/errorHandlerMiddleware.ts
+++ api/proxy/src/middlewares/errorHandlerMiddleware.ts
@@ -7,54 +7,8 @@ import express = require('express');
 export function errorHandlerMiddleware(
   err: Error,
   _req: express.Request,
-  res: express.Response,
+  _res: express.Response,
   _next: express.NextFunction,
 ) {
-  let code: number;
-
-  switch (err.message) {
-    case 'MongoError':
-      switch (err.message.match(/^([A-Z0-9])+/)[0]) {
-        case 'E11000':
-          code = 409;
-          break;
-        default:
-          code = 500;
-      }
-      break;
-
-    case 'Bad Request Error':
-    case 'Validation Error':
-      code = 400;
-      break;
-
-    case 'Unauthorized Error':
-      code = 401;
-      break;
-
-    case 'Forbidden':
-      code = 403;
-      break;
-
-    case 'Not Found':
-      code = 404;
-      break;
-
-    case 'Conflict':
-      code = 409;
-      break;
-
-    case 'Internal Server Error':
-      code = 500;
-      break;
-
-    default:
-      code = 500;
-  }
-
-  res.status(code).json({
-    id: 1,
-    jsonrpc: '2.0',
-    error: { code, data: err.name, message: err.message },
-  });
+  console.error(err);
 }
