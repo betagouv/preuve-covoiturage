93a08a5ca730e18343ac757d203f4ff1d16689d6
Samuel Eyre <samuel.eyre@hotmail.fr>
2019-10-29 15:08:37 +0100

    (dash) up: uistatus interface

--- dashboard/src/app/core/interfaces/campaign/ui-status.interface.ts
+++ dashboard/src/app/core/interfaces/campaign/ui-status.interface.ts
@@ -1,7 +1,26 @@
 export interface UiStatusInterface {
-  expert_mode?: boolean;
   for_driver: boolean;
   for_passenger: boolean;
   for_trip: boolean;
   staggered: boolean;
+  insee_filter?: {
+    blackListFilter: UiStatusInseeFilterInterface[];
+    whiteListFilter: UiStatusInseeFilterInterface[];
+  };
+}
+
+export interface UiStatusInseeFilterInterface {
+  start: UiStatusInseeAndTerritoryInterface;
+  end: UiStatusInseeAndTerritoryInterface;
+  operator: InseeFilterOperatorEnum;
+}
+
+export interface UiStatusInseeAndTerritoryInterface {
+  territory_literal: string;
+  insees: string[];
+}
+
+export enum InseeFilterOperatorEnum {
+  AND = 'and',
+  OR = 'or',
 }
