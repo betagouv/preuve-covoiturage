f341c314d3e0808ce122d79a1416fb8b8f254329
Nicolas Mérigot <nicolas.merigot@gmail.com>
2019-10-29 17:28:05 +0100

    feat(policy): use pg provider

--- api/services/policy/src/ServiceProvider.ts
+++ api/services/policy/src/ServiceProvider.ts
@@ -1,7 +1,6 @@
 import { ServiceProvider as AbstractServiceProvider } from '@ilos/core';
 import { serviceProvider, NewableType, ExtensionInterface } from '@ilos/common';
 import { PermissionMiddleware } from '@ilos/package-acl';
-import { MongoConnection } from '@ilos/connection-mongo';
 import { PostgresConnection } from '@ilos/connection-postgres';
 
 import { ValidatorExtension, ValidatorMiddleware } from '@pdc/provider-validator';
@@ -29,7 +28,7 @@ import { CampaignMetadataRepositoryProvider } from './engine/CampaignMetadataRep
   config: __dirname,
   providers: [
     CampaignPgRepositoryProvider,
-    CampaignMetadataRepositoryProvider,
+    // CampaignMetadataRepositoryProvider, // FIXME: migrate to pg
     ['validate.rules', ValidateRuleParametersMiddleware],
     PolicyEngine,
   ],
@@ -48,7 +47,7 @@ import { CampaignMetadataRepositoryProvider } from './engine/CampaignMetadataRep
     ListCampaignAction,
     ListCampaignTemplateAction,
   ],
-  connections: [[MongoConnection, 'connections.mongo'], [PostgresConnection, 'connections.postgres']],
+  connections: [[PostgresConnection, 'connections.postgres']],
   middlewares: [
     ['can', PermissionMiddleware],
     ['validate', ValidatorMiddleware],
--- api/services/policy/src/config/connections.ts
+++ api/services/policy/src/config/connections.ts
@@ -1,8 +1,8 @@
 declare function env(key: string, fallback?: string): any;
 
-export const mongo = {
-  connectionString: env('APP_MONGO_URL'),
-};
+// export const mongo = {
+//   connectionString: env('APP_MONGO_URL'),
+// };
 
 // export const redis = {
 //   connectionString: env('APP_REDIS_URL'),
