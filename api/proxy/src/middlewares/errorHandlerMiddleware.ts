e1b19f8a810782b5c3e77cec751f460db43679f3
gilles boisson <gilles@dotify.fr>
2019-10-29 16:10:16 +0100

    clean errorHandlerMiddleware.ts

--- api/proxy/src/middlewares/errorHandlerMiddleware.ts
+++ api/proxy/src/middlewares/errorHandlerMiddleware.ts
@@ -7,8 +7,54 @@ import express = require('express');
 export function errorHandlerMiddleware(
   err: Error,
   _req: express.Request,
-  _res: express.Response,
+  res: express.Response,
   _next: express.NextFunction,
 ) {
-  console.error(err);
+  let code: number;
+
+  switch (err.message) {
+    case 'MongoError':
+      switch (err.message.match(/^([A-Z0-9])+/)[0]) {
+        case 'E11000':
+          code = 409;
+          break;
+        default:
+          code = 500;
+      }
+      break;
+
+    case 'Bad Request Error':
+    case 'Validation Error':
+      code = 400;
+      break;
+
+    case 'Unauthorized Error':
+      code = 401;
+      break;
+
+    case 'Forbidden':
+      code = 403;
+      break;
+
+    case 'Not Found':
+      code = 404;
+      break;
+
+    case 'Conflict':
+      code = 409;
+      break;
+
+    case 'Internal Server Error':
+      code = 500;
+      break;
+
+    default:
+      code = 500;
+  }
+  console.error(err.name, err.message);
+  res.status(code).json({
+    id: 1,
+    jsonrpc: '2.0',
+    error: { code, data: err.name, message: err.message },
+  });
 }
