dfa266456305ff094ea211625d712a7638f6d8cd
Jonathan Fallon <jonathan@dotify.fr>
2019-10-31 18:07:19 +0100

    fix date-time pattern waiting for AJV 6.10.3 release

--- api/providers/validator/src/macros/timestampMacro.ts
+++ api/providers/validator/src/macros/timestampMacro.ts
@@ -1,7 +1,11 @@
 export function timestampMacro(schema) {
   return {
     type: 'string',
-    format: 'date-time',
+    // from https://github.com/epoberezkin/ajv/commit/382c2b9ed151287e4ff0f233d689563f90cd5c20
+    // waiting for the release of 6.10.3
+    // format: 'date-time',
+    // prettier-ignore
+    pattern: '^\\d\\d\\d\\d-[0-1]\\d-[0-3]\\d[T\\s](?:[0-2]\\d:[0-5]\\d:[0-5]\\d|23:59:60)(?:\\.\\d+)?(?:Z|[+-]\\d\\d(?::?\\d\\d)?)$',
     cast: 'date',
   };
 }
