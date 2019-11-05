59595fb0eb47cfb755106a427544a440a7249a79
Jonathan Fallon <jonathan@dotify.fr>
2019-11-05 15:33:43 +0100

    fix applications schemas and operator_id in user

--- api/services/application/src/helpers/setOwner.ts
+++ api/services/application/src/helpers/setOwner.ts
@@ -2,10 +2,11 @@ import { InvalidParamsException, ContextType } from '@ilos/common';
 
 export function setOwner<T>(service: 'operator', params: any, context: ContextType): T {
   const data = <T & { owner_id: string; owner_service: string }>{ ...params };
+  const key = `${service}_id`;
 
   // force owner to be the user's service
-  if (service in context.call.user) {
-    data.owner_id = context.call.user[service];
+  if (key in context.call.user) {
+    data.owner_id = context.call.user[key];
     data.owner_service = service;
   }
 
