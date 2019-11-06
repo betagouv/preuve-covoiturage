ed727b6a90b6baec630f04dbe342fbdfe3c79d50
Samuel Eyre <samuel.eyre@hotmail.fr>
2019-11-06 12:01:50 +0100

    (dashboard) fix: init of user in auth, update cypress, fix: calls to role enums

--- dashboard/src/app/core/interfaces/admin/adminLayoutInterface.ts
+++ dashboard/src/app/core/interfaces/admin/adminLayoutInterface.ts
@@ -1,6 +1,9 @@
+import { UserGroupEnum } from '~/core/enums/user/user-group.enum';
+import { UserManyRoleEnum, UserRoleEnum } from '~/core/enums/user/user-role.enum';
+
 export interface MenuTabInterface {
   path: string;
   label: string;
-  groups?: string[];
-  role?: string;
+  groups?: UserGroupEnum[];
+  role?: UserRoleEnum | UserManyRoleEnum;
 }
