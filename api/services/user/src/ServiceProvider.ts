9634d6d93bdd64a026888998e6f7da122e9642d6
Jonathan Fallon <jonathan@dotify.fr>
2019-11-06 11:52:26 +0100

    feat(user) data migrate command

--- api/services/user/src/ServiceProvider.ts
+++ api/services/user/src/ServiceProvider.ts
@@ -24,10 +24,9 @@ import { forgottenPassword } from './shared/user/forgottenPassword.schema';
 import { list } from './shared/user/list.schema';
 import { login } from './shared/user/login.schema';
 import { patch } from './shared/user/patch.schema';
-// import { register } from './shared/user/register.schema';
 import { sendConfirmEmail } from './shared/user/sendConfirmEmail.schema';
 import { UserPgRepositoryProvider } from './providers/UserPgRepositoryProvider';
-import { FixPermissionsCommand } from './commands/FixPermissionsCommand';
+import { MigrateDataCommand } from './commands/MigrateDataCommand';
 import {
   ChangePasswordUserAction,
   ChangePasswordWithTokenUserAction,
@@ -66,7 +65,6 @@ import { UserNotificationProvider } from './providers/UserNotificationProvider';
     ['user.list', list],
     ['user.login', login],
     ['user.patch', patch],
-    // ['user.register', register],
     ['user.sendConfirmEmail', sendConfirmEmail],
   ],
   middlewares: [
@@ -92,7 +90,6 @@ import { UserNotificationProvider } from './providers/UserNotificationProvider';
     MeUserAction,
     NotifyUserAction,
     PatchUserAction,
-    // RegisterUserAction,
     SendConfirmEmailUserAction,
     SendInvitationEmailUserAction,
   ],
@@ -101,7 +98,7 @@ import { UserNotificationProvider } from './providers/UserNotificationProvider';
     template: path.resolve(__dirname, 'templates'),
     templateMeta: 'template',
   },
-  commands: [FixPermissionsCommand],
+  commands: [MigrateDataCommand],
 })
 export class ServiceProvider extends AbstractServiceProvider {
   readonly extensions: NewableType<ExtensionInterface>[] = [ValidatorExtension, NotificationExtension];
---
+++ api/services/user/src/commands/MigrateDataCommand.ts
@@ -0,0 +1,111 @@
+import { command, CommandInterface, CommandOptionType } from '@ilos/common';
+import { MongoConnection } from '@ilos/connection-mongo';
+import { PostgresConnection } from '@ilos/connection-postgres';
+
+@command()
+export class MigrateDataCommand implements CommandInterface {
+  static readonly signature: string = 'migrate-data:user';
+  static readonly description: string = 'Migrate data from old table to new table';
+  static readonly options: CommandOptionType[] = [
+    {
+      signature: '-u, --database-uri <uri>',
+      description: 'Postgres connection string',
+      default: process.env.APP_POSTGRES_URL,
+    },
+    {
+      signature: '-m, --mongo-uri <uri>',
+      description: 'MongoDB Connection string',
+      default: process.env.APP_MONGO_URL,
+    },
+    {
+      signature: '-d, --db <uri>',
+      description: 'MongoDB database',
+      default: process.env.APP_MONGO_DB,
+    },
+    {
+      signature: '-c, --collection <uri>',
+      description: 'MongoDB collection',
+      default: 'users',
+    },
+  ];
+
+  // tslint:disable-next-line: no-shadowed-variable
+  public async call(options): Promise<string> {
+    const mongo = new MongoConnection({
+      connectionString: options.mongoUri,
+    });
+
+    const postgres = new PostgresConnection({
+      connectionString: options.databaseUri,
+    });
+
+    await mongo.up();
+    await postgres.up();
+
+    const readClient = mongo
+      .getClient()
+      .db(options.db)
+      .collection(options.collection);
+    const writeClient = postgres.getClient();
+
+    const cursor = readClient.find({});
+
+    // tslint:disable-next-line: no-constant-condition
+    while (true) {
+      if (!(await cursor.hasNext())) break;
+      try {
+        const doc = await cursor.next();
+        console.log(doc._id, doc.group, doc.role, doc.email);
+        await writeClient.query({
+          text: `
+        INSERT INTO auth.users
+        (
+          operator_id,
+          territory_id,
+          email,
+          firstname,
+          lastname,
+          phone,
+          password,
+          status,
+          role,
+          created_at,
+          updated_at,
+          deleted_at
+        )
+        VALUES ( $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12 )
+        `,
+          values: [
+            doc.operator ? doc.operator.toString() : null,
+            doc.territory ? doc.territory.toString() : null,
+            doc.email,
+            doc.lastname,
+            doc.firstname,
+            doc.phone,
+            doc.password,
+            doc.status,
+            `${this.singularize(doc.group)}.${doc.role}`,
+            doc.created_at || new Date(),
+            doc.updated_at || new Date(),
+            doc.deleted_at || null,
+          ],
+        });
+      } catch (e) {
+        console.log(e);
+      }
+    }
+
+    return 'Done!';
+  }
+
+  private singularize(group: 'operators' | 'territories'): string {
+    switch (group) {
+      case 'operators':
+        return 'operator';
+      case 'territories':
+        return 'territory';
+      default:
+        return group;
+    }
+  }
+}
