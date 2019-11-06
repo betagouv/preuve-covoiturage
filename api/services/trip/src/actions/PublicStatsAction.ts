c6ad3d264b7c40cb47aee64a4d601f8c26f52dc8
Samuel Eyre <samuel.eyre@hotmail.fr>
2019-11-06 13:06:28 +0100

    update: publicstat action & add: publicstat contract & schema

--- api/services/trip/src/actions/PublicStatsAction.ts
+++ api/services/trip/src/actions/PublicStatsAction.ts
@@ -1,22 +1,21 @@
+// tslint:disable:variable-name
 import { Action } from '@ilos/core';
 import { handler, ContextType } from '@ilos/common';
-import { TripSearchInterface } from '@pdc/provider-schema';
 
+import { handlerConfig, ParamsInterface, ResultInterface } from '../shared/trip/publicStats.contract';
 import { TripPgRepositoryProvider } from '../providers/TripPgRepositoryProvider';
+import { ActionMiddleware } from '../shared/common/ActionMiddlewareInterface';
+import { alias } from '../shared/trip/publicStats.schema';
 
-/*
- * Public stats for trips
- */
-@handler({
-  service: 'trip',
-  method: 'publicStats',
-})
+@handler(handlerConfig)
 export class PublicStatsAction extends Action {
+  public readonly middlewares: ActionMiddleware[] = [['validate', alias]];
+
   constructor(private pg: TripPgRepositoryProvider) {
     super();
   }
 
-  public async handle(params: TripSearchInterface, context: ContextType): Promise<any> {
+  public async handle(params: ParamsInterface, context: ContextType): Promise<ResultInterface> {
     // todo: create specific interface for stats
     return (await this.pg.stats({ skip: 0, limit: 0 })) || [];
   }
