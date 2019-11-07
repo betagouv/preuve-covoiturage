9dd1d69eef4cd338420038736cb7826f220046c5
Nicolas Mérigot <nicolas.merigot@gmail.com>
2019-11-07 14:38:18 +0100

    fix(normalization): use geo provider new interface

--- api/services/normalization/src/actions/NormalizationGeoAction.ts
+++ api/services/normalization/src/actions/NormalizationGeoAction.ts
@@ -1,10 +1,9 @@
-import * as _ from 'lodash';
+import { get, set } from 'lodash';
 import { Action as AbstractAction } from '@ilos/core';
 import { handler, ContextType } from '@ilos/common';
 import { GeoProviderInterfaceResolver } from '@pdc/provider-geo';
 
 import { handlerConfig, ParamsInterface, ResultInterface } from '../shared/normalization/geo.contract';
-import { PositionInterface } from '../shared/common/interfaces/PositionInterface';
 import { ActionMiddleware } from '../shared/common/ActionMiddlewareInterface';
 import { WorkflowProvider } from '../providers/WorkflowProvider';
 
@@ -22,69 +21,16 @@ export class NormalizationGeoAction extends AbstractAction {
     this.logger.debug(`Normalization:geo on ${journey._id}`);
 
     for (const path of ['passenger.start', 'passenger.end', 'driver.start', 'driver.end']) {
-      const position = _.get(journey, path);
-      const positionEnrichedWithTown = await this.findTown(position);
-      normalizedJourney = this.processTownResponse(journey, path, position, positionEnrichedWithTown);
+      const { lat, lon, insee, literal } = get(journey, path);
+      const result = await this.geoProvider.checkAndComplete({ lat, lon, insee, literal });
+      set(normalizedJourney, `${path}.lat`, result.lat);
+      set(normalizedJourney, `${path}.lon`, result.lon);
+      set(normalizedJourney, `${path}.insee`, result.insee);
     }
 
     // Call the next step asynchronously
     await this.wf.next('normalization:geo', normalizedJourney);
 
     return normalizedJourney;
-  }
-
-  private async findTown(position: PositionInterface): Promise<PositionInterface> {
-    const foundPosition = await this.geoProvider.getTown({
-      lon: position.lon,
-      lat: position.lat,
-      insee: position.insee,
-      literal: position.literal,
-    });
-    return {
-      ...position,
-      ...foundPosition,
-    };
-  }
-
-  /**
-   * Complete position with data relative to town
-   */
-  private processTownResponse(
-    journey: ParamsInterface,
-    path: string,
-    position: PositionInterface,
-    determinedPosition: PositionInterface,
-  ): ParamsInterface {
-    if (determinedPosition.lon && !position.lon) {
-      position.lon = determinedPosition.lon;
-      _.set(journey, `${path}.lon`, determinedPosition.lon);
-    }
-
-    if (determinedPosition.lat && !position.lat) {
-      position.lat = determinedPosition.lat;
-      _.set(journey, `${path}.lat`, determinedPosition.lat);
-    }
-
-    if (determinedPosition.insee && !position.insee) {
-      position.insee = determinedPosition.insee;
-      _.set(journey, `${path}.insee`, determinedPosition.insee);
-    }
-
-    if (determinedPosition.town && !position.town) {
-      position.town = determinedPosition.town;
-      _.set(journey, `${path}.town`, determinedPosition.town);
-    }
-
-    if (determinedPosition.country && !position.country) {
-      position.country = determinedPosition.country;
-      _.set(journey, `${path}.country`, determinedPosition.country);
-    }
-
-    // concat postcodes with existing ones
-    const pcs = _.get(journey, `${path}.postcodes`, []).concat(determinedPosition.postcodes);
-    position.postcodes = _.uniq(pcs);
-    _.set(journey, `${path}.postcodes`, position.postcodes);
-
-    return journey;
   }
 }
