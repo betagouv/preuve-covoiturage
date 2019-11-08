b9154a806880109732d6a8fb13e21806dae1c7ac
Nicolas Mérigot <nicolas.merigot@gmail.com>
2019-11-08 13:39:34 +0100

    feat(db): add company schema

---
+++ api/db/migrations/company/001_create_company_table.up.sql
@@ -0,0 +1,25 @@
+CREATE TABLE IF NOT EXISTS company.companies
+(
+  siret varchar(14) primary key,
+  siren varchar(9) NOT NULL,
+  nic varchar(5) NOT NULL,
+
+  legal_name varchar(256) NOT NULL,
+
+  company_naf_code varchar(5) NOT NULL,
+  establishment_naf_code varchar(5) NOT NULL,
+  
+  legal_nature_code varchar(10),
+  legal_nature_label varchar(256),
+
+  nonprofit_code varchar(10),
+  intra_vat varchar (16),
+
+  geo geography,
+  address varchar,
+  headquarter boolean NOT NULL,
+
+  updated_at timestamp NOT NULL DEFAULT NOW()
+);
+
+CREATE INDEX ON company.companies (siren);
---
+++ api/db/migrations/company/README.md
@@ -0,0 +1,10 @@
+# Company schema
+
+## Policies
+
+- [INSERT, UPDATE, SELECT] for company role
+
+## Notes
+
+Access data throught common.companies
+
