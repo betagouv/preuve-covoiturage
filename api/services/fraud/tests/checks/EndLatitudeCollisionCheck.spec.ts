023a9ed914f93ec034f15595731b7640e78ea11f
Nicolas Mérigot <nicolas.merigot@gmail.com>
2019-11-19 12:42:30 +0100

    feat(fraud): add end latitude collision check

---
+++ api/services/fraud/tests/checks/EndLatitudeCollisionCheck.spec.ts
@@ -0,0 +1,72 @@
+import { describe } from 'mocha';
+import { expect } from 'chai';
+import { EndLatitudeCollisionCheck as Check } from '../../src/engine/checks/EndLatitudeCollisionCheck';
+
+import { faker } from './faker';
+
+describe(`Check: ${Check.key}`, async () => {
+  beforeEach(async () => {
+    await faker.up();
+  });
+
+  afterEach(async () => {
+    await faker.down();
+  });
+
+  it('works with max', async () => {
+    const check = faker.get(Check);
+    const fakeData = await faker.setData(check);
+    await faker.setData(check, {
+      acquisition_id: fakeData.acquisition_id,
+      is_driver: true,
+      end_position: {
+        lat: fakeData.end_position.lat + 1,
+        lon: fakeData.end_position.lon,
+      }
+    });
+
+    const res = await check.handle(fakeData.acquisition_id);
+
+    expect(res).to.have.all.keys(['meta', 'karma']);
+    expect(res.karma).to.be.eq(100);
+  });
+
+  it('works with min', async () => {
+    const check = faker.get(Check);
+    const fakeData = await faker.setData(check);
+
+    await faker.setData(check, {
+      acquisition_id: fakeData.acquisition_id,
+      is_driver: true,
+      end_position: {
+        lat: fakeData.end_position.lat,
+        lon: fakeData.end_position.lon,
+      }
+    });
+
+    const res = await check.handle(fakeData.acquisition_id);
+
+    expect(res).to.have.all.keys(['meta', 'karma']);
+    expect(res.karma).to.be.eq(0);
+  });
+
+  it('works between', async () => {
+    const check = faker.get(Check);
+    const fakeData = await faker.setData(check);
+
+    await faker.setData(check, {
+      acquisition_id: fakeData.acquisition_id,
+      is_driver: true,
+      end_position: {
+        lat: fakeData.end_position.lat + 0.5,
+        lon: fakeData.end_position.lon,
+      }
+    });
+
+    const res = await check.handle(fakeData.acquisition_id);
+
+    expect(res).to.have.all.keys(['meta', 'karma']);
+    expect(res.karma).to.be.gt(0);
+    expect(res.karma).to.be.lt(100);
+  });
+});
