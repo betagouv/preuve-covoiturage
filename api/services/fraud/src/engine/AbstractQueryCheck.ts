9a29dc7ebdcc1ad7198d952bdd9dab79cabd59e4
Nicolas Mérigot <nicolas.merigot@gmail.com>
2019-11-19 13:29:39 +0100

    feat(fraud): allow processing several carpool (avg + push)

--- api/services/fraud/src/engine/AbstractQueryCheck.ts
+++ api/services/fraud/src/engine/AbstractQueryCheck.ts
@@ -17,16 +17,43 @@ export abstract class AbstractQueryCheck<P = any, R extends DefaultMetaInterface
   
   async handle(acquisitionId: number, initialMeta?: R | R[]): Promise<FraudCheckResult<R | R[]>> {
     const query = {
-      text: `WITH data as (${this.query}) SELECT * from data WHERE acquisition_id = $1 LIMIT 1`,
+      text: `WITH data as (${this.query}) SELECT * from data WHERE acquisition_id = $1`,
       values: [acquisitionId],
     };
-    const line = await this.connection.getClient().query(query);
 
-    if (line.rowCount !== 1) {
-      throw new Error();
+    const dbResult = await this.connection.getClient().query(query);
+
+    const result: FraudCheckResult<R|R[]> = {
+      karma: 0,
+      meta: null,
+    };
+
+    for(const row of dbResult.rows) {
+      const { karma, meta } = await this.cursor(row, initialMeta);
+      result.karma += karma;
+
+      if (!result.meta) {
+        result.meta = meta;
+      } else {
+        if (!Array.isArray(result.meta)) {
+          result.meta = [result.meta];
+        }
+        result.meta.push(meta);
+      }
+      
+      for(const k of Object.keys(meta)) {
+        if(k in result.meta) {
+          if (!Array.isArray(result.meta[k])) {
+            result.meta[k] = [result.meta[k]];
+          }
+          result.meta[k].push(meta[k]);
+        } else {
+          result.meta[k] = meta[k];
+        }
+      }
     }
 
-    const result = await this.cursor(line.rows[0], meta);
+    result.karma = Math.round((result.karma / dbResult.rowCount));
     return result;
   }
 
