281d132605ada2de4a1a0ba12fa207f0bc37f0c8
gilles boisson <gilles@dotify.fr>
2019-11-20 11:14:12 +0100

    front : refacto reintegration of new rpc services

---
+++ dashboard/src/app/core/services/api/json-rpc-crud.service.ts
@@ -0,0 +1,79 @@
+import { HttpClient } from '@angular/common/http';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import { JsonRPC, JsonRPCService } from '~/core/services/api/json-rpc.service';
+import { JsonRPCParam } from '~/core/entities/api/jsonRPCParam';
+import { IModel } from '~/core/entities/IModel';
+
+export interface DeleteResponse {
+  _id: string;
+  success: boolean;
+}
+
+export enum CrudActions {
+  FIND = 'find',
+  LIST = 'list',
+  CREATE = 'create',
+  UPDATE = 'update',
+  PATCH = 'patch',
+  DELETE = 'delete',
+}
+
+export class JsonRpcCrudService<ListEntityT extends IModel, EntityT extends IModel> extends JsonRPC {
+  constructor(http: HttpClient, router: Router, activedRoute: ActivatedRoute, protected method: string, url?: string) {
+    super(http, router, activedRoute, url);
+  }
+
+  protected defaultListParam: any = {};
+
+  paramGetList(params?: any) {
+    return new JsonRPCParam(`${this.method}::${CrudActions.LIST}`, { ...this.defaultListParam, ...params });
+  }
+
+  paramGet(params: any) {
+    return new JsonRPCParam(`${this.method}:${CrudActions.FIND}`, params);
+  }
+
+  paramGetById(id: any) {
+    return this.paramGet({ _id: id });
+  }
+
+  get(params: any): Observable<EntityT> {
+    return this.callOne(this.paramGet(params)).pipe(map((data) => data.data));
+  }
+
+  getList(params: any = {}): Observable<ListEntityT[]> {
+    return this.callOne(this.paramGetList(params)).pipe(map((data) => data.data));
+  }
+
+  getById(id: string): Observable<EntityT> {
+    return this.get({ _id: id });
+  }
+
+  create(item: EntityT): Observable<EntityT> {
+    const jsonRPCParam = new JsonRPCParam(`${this.method}::${CrudActions.CREATE}`, item);
+    return this.callOne(jsonRPCParam).pipe(map((data) => data.data));
+  }
+
+  update(item: EntityT): Observable<EntityT> {
+    const jsonRPCParam = new JsonRPCParam(`${this.method}::${CrudActions.UPDATE}`, item);
+    return this.callOne(jsonRPCParam).pipe(map((data) => data.data));
+  }
+
+  patch(params: any) {
+    const jsonRPCParam = new JsonRPCParam(`${this.method}::${CrudActions.PATCH}`, params);
+    return this.callOne(jsonRPCParam).pipe(map((data) => data.data));
+  }
+
+  delete(id: string): Observable<DeleteResponse> {
+    const jsonRPCParam = new JsonRPCParam(`${this.method}::${CrudActions.DELETE}`, { _id: id });
+    return this.callOne(jsonRPCParam).pipe(
+      map((data) => ({
+        success: data.data,
+        _id: id,
+      })),
+    );
+  }
+}
