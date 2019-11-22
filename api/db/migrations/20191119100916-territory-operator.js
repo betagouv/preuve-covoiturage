858f9ee803c229ac6a4664ea08f47c91a9d56b0d
gilles boisson <gilles@dotify.fr>
2019-11-22 15:31:56 +0100

    front : fix migration

--- api/db/migrations/20191119100916-territory-operator.js
+++ api/db/migrations/20191119100916-territory-operator.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var { createMigration } = require('../helpers/createMigration');
-var { setup, up, down } = createMigration(['application/002_create_territory_operator_table'], __dirname);
+var { setup, up, down } = createMigration(['territory/002_create_territory_operator_table'], __dirname);
 
 exports.setup = setup;
 exports.up = up;
