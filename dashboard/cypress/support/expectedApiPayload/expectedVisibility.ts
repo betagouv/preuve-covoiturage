faec0d1e162b10c38cd9ffdf1b942466b3db5001
Samuel Eyre <samuel.eyre@hotmail.fr>
2019-11-25 11:16:40 +0100

    (dash: cypress) fix: visibility, up: readme

--- dashboard/cypress/support/expectedApiPayload/expectedVisibility.ts
+++ dashboard/cypress/support/expectedApiPayload/expectedVisibility.ts
@@ -1,7 +1,7 @@
 import { OperatorVisibilityType } from '~/core/interfaces/operator/operatorVisibilityType';
 
 import { territoryStubs } from '../stubs/territory/territory.list';
-import { visibilityList } from '../stubs/operator/operator.visibleInTerritories';
+import { visibilityList } from '../stubs/territory/territory.listOperator';
 
 export class ExpectedVisibility {
   public static get(): OperatorVisibilityType {
