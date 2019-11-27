35a33f85c17a4f54ebae71c4947f91eed910904f
Nicolas Mérigot <nicolas.merigot@gmail.com>
2019-11-27 14:19:17 +0100

    feat(trip): add view refresh cron job

---
+++ api/services/trip/src/actions/RefreshAction.ts
@@ -0,0 +1,60 @@
+import { Action } from '@ilos/core';
+import { handler, ContextType, InitHookInterface, KernelInterfaceResolver } from '@ilos/common';
+import { PostgresConnection } from '@ilos/connection-postgres';
+
+import { handlerConfig, ParamsInterface, ResultInterface } from '../shared/trip/refresh.contract';
+import { ActionMiddleware } from '../shared/common/ActionMiddlewareInterface';
+
+@handler(handlerConfig)
+export class RefreshAction extends Action implements InitHookInterface {
+  public readonly middlewares: ActionMiddleware[] = [
+    ['channel.service.only', [handlerConfig.service]],
+  ];
+
+  constructor(private pg: PostgresConnection, private kernel: KernelInterfaceResolver) {
+    super();
+  }
+
+  async init() {
+    await this.kernel.notify('trip:refresh', 'list', {
+      call: {
+        user: {},
+      },
+      channel: {
+        service: 'trip',
+        metadata: {
+          repeat: {
+            cron: '0 1 * * *',
+          },
+          jobId: 'trip.refresh_view.list',
+        },
+      },
+    });
+
+    await this.kernel.notify('trip:refresh', 'export', {
+      call: {
+        user: {},
+      },
+      channel: {
+        service: 'trip',
+        metadata: {
+          repeat: {
+            cron: '0 2 * * *',
+          },
+          jobId: 'trip.refresh_view.export',
+        },
+      },
+    });
+  }
+
+  public async handle(params: ParamsInterface, context: ContextType): Promise<ResultInterface> {
+    switch (params) {
+      case 'list':
+        await this.pg.getClient().query(`REFRESH VIEW trip.list`);
+        return;
+      case 'export':
+        await this.pg.getClient().query(`REFRESH VIEW trip.opendata_list`);
+        return;
+    }
+  }
+}
