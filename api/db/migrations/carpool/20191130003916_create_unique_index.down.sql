20b8f79df24daaf52c511305e857c8b65a95ecdc
Jonathan Fallon <jonathan@dotify.fr>
2019-11-30 01:49:28 +0100

    add unique index on carpool.carpools

---
+++ api/db/migrations/carpool/20191130003916_create_unique_index.down.sql
@@ -0,0 +1,1 @@
+DROP INDEX IF EXISTS carpools_acquisition_id_is_driver_idx;
---
+++ api/db/migrations/carpool/20191130003916_create_unique_index.up.sql
@@ -0,0 +1,1 @@
+CREATE UNIQUE INDEX IF NOT EXISTS carpools_acquisition_id_is_driver_idx ON carpool.carpools (acquisition_id, is_driver);
