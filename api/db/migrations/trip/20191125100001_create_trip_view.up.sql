09ceb14d247b9cb6912e1ca58b5e488cd29934ae
Jonathan Fallon <jonathan.fallon@beta.gouv.fr>
2019-12-03 14:48:22 +0100

    Update 20191125100001_create_trip_view.up.sql

--- api/db/migrations/trip/20191125100001_create_trip_view.up.sql
+++ api/db/migrations/trip/20191125100001_create_trip_view.up.sql
@@ -3,7 +3,7 @@ CREATE MATERIALIZED VIEW trip.list AS (
     cp.trip_id as trip_id,
     cp.start_insee as start_insee,
     cp.end_insee as end_insee,
-    cp.operator_id::int as operator_id,
+    cp.operator_id as operator_id,
     cp.operator_class as operator_class,
 
     cp.datetime as datetime,
