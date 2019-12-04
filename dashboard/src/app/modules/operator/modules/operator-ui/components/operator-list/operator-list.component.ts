dbcf47eeae18f89ea365551025ce157b6261665a
Samuel Eyre <samuel.eyre@hotmail.fr>
2019-12-04 14:15:05 +0100

    (dash: operator) add: delete action to operator list

--- dashboard/src/app/modules/operator/modules/operator-ui/components/operator-list/operator-list.component.ts
+++ dashboard/src/app/modules/operator/modules/operator-ui/components/operator-list/operator-list.component.ts
@@ -1,10 +1,13 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { takeUntil } from 'rxjs/operators';
+import { ToastrService } from 'ngx-toastr';
 
-import { OperatorsPermissionsAdminType } from '~/core/types/permissionType';
+import { RegistryPermissionsAdminType } from '~/core/types/permissionType';
 import { AuthenticationService } from '~/core/services/authentication/authentication.service';
 import { DestroyObservable } from '~/core/components/destroy-observable';
 import { Operator } from '~/core/entities/operator/operator';
 import { OperatorStoreService } from '~/modules/operator/services/operator-store.service';
+import { DialogService } from '~/core/services/dialog.service';
 
 @Component({
   selector: 'app-operator-list',
@@ -12,14 +15,20 @@ import { OperatorStoreService } from '~/modules/operator/services/operator-store
   styleUrls: ['./operator-list.component.scss'],
 })
 export class OperatorListComponent extends DestroyObservable implements OnInit {
-  public editPermission: OperatorsPermissionsAdminType = 'operator.update';
+  public editPermission: RegistryPermissionsAdminType = 'operator.update';
+  public deletePermission: RegistryPermissionsAdminType = 'operator.delete';
 
   displayedColumns: string[] = ['name', 'actions'];
 
   @Output() edit = new EventEmitter();
   @Input() operators: Operator[];
 
-  constructor(public operatorStoreService: OperatorStoreService, public authenticationService: AuthenticationService) {
+  constructor(
+    public operatorStoreService: OperatorStoreService,
+    public authenticationService: AuthenticationService,
+    private _dialogService: DialogService,
+    private _toastr: ToastrService,
+  ) {
     super();
   }
 
@@ -27,5 +36,23 @@ export class OperatorListComponent extends DestroyObservable implements OnInit {
 
   onEdit(operator) {
     this.edit.emit(operator);
+  }
+
+  onDelete(operator) {
+    this._dialogService
+      .confirm('Voulez-vous supprimer cet opérateur ?', '')
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((hasConfirmed) => {
+        if (hasConfirmed) {
+          this.operatorStoreService.delete(operator).subscribe(
+            () => {
+              this._toastr.success(`L'opérateur ${operator.name} a été supprimé`);
+            },
+            (err) => {
+              this._toastr.error(err.message);
+            },
+          );
+        }
+      });
   }
 }
