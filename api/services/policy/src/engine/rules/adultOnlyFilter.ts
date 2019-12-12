bfee0bd1fc0b636afa36dc5989681ff6aee405b1
Nicolas Mérigot <nicolas.merigot@gmail.com>
2019-12-12 15:53:54 +0100

    fix(policy): accept null value on is_over_18 as true

--- api/services/policy/src/engine/rules/adultOnlyFilter.ts
+++ api/services/policy/src/engine/rules/adultOnlyFilter.ts
@@ -12,7 +12,7 @@ export const adultOnlyFilter: ApplicableRuleInterface = {
   index: HIGH,
   apply() {
     return async (ctx, next) => {
-      if (!ctx.person.is_over_18) {
+      if ('is_over_18' in ctx.person && ctx.person.is_over_18 === false) {
         throw new NotApplicableTargetException(adultOnlyFilter);
       }
       return next();
