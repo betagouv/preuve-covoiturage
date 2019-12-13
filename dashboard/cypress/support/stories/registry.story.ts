6493153b199767f9275c545a7fe91456be772fc5
Jonathan Fallon <jonathan.fallon@beta.gouv.fr>
2019-12-13 14:49:57 +0100

    (dash) clean cypress
    Ajout du champs nom opérateur dans les export de trajets

--- dashboard/cypress/support/stories/registry.story.ts
+++ dashboard/cypress/support/stories/registry.story.ts
@@ -6,31 +6,32 @@ import { cypress_logging_users } from '../stubs/auth/login';
 import { cypress_logout } from '../reusables/auth/cypress_logout';
 import { cypress_users } from '../reusables/user/users.cypress';
 import { cypress_campaign_list } from '../reusables/campaign/admin-campaign-list.cypress';
+import { TestsInterface } from '../../config/tests.interface';
 
-export function testRegistryStory(profile = true, filters = true, users = true, campaigns = true) {
+export function testRegistryStory(config: TestsInterface['registry']) {
   // PROFILE UPDATE
-  if (profile) {
+  if (config.profile) {
     describe('Profile update', () => {
       cypress_profile(cypress_logging_users.registry);
     });
   }
 
   // FILTERS
-  if (filters) {
+  if (config.filters) {
     describe('Filter trips', () => {
       cypress_filter(false, UserGroupEnum.REGISTRY);
     });
   }
 
-  // // USERS
-  // if (users) {
-  //   describe('Manage users', () => {
-  //     cypress_users();
-  //   });
-  // }
+  // USERS
+  if (config.users) {
+    describe('Manage users', () => {
+      cypress_users();
+    });
+  }
 
   // CAMPAIGNS
-  if (campaigns) {
+  if (config.campaigns) {
     describe('Follow campaigns', () => {
       cypress_campaign_list();
     });
