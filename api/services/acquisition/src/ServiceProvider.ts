64b7d9baef8fea3d09edaa210e663cdf05e231db
Jonathan Fallon <jonathan@dotify.fr>
2019-12-17 18:14:29 +0100

    update migrate-data commands to skip existing data

--- api/services/acquisition/src/ServiceProvider.ts
+++ api/services/acquisition/src/ServiceProvider.ts
@@ -29,10 +29,7 @@ import { LogErrorAction } from './actions/LogErrorAction';
     ['validate', ValidatorMiddleware],
     ['channel.service.only', ChannelServiceWhitelistMiddleware],
   ],
-  connections: [
-    [PostgresConnection, 'connections.postgres'],
-    [RedisConnection, 'connections.redis'],
-  ],
+  connections: [[PostgresConnection, 'connections.postgres'], [RedisConnection, 'connections.redis']],
   handlers: [CreateJourneyLegacyAction, CreateJourneyAction, LogErrorAction],
 })
 export class ServiceProvider extends AbstractServiceProvider {
