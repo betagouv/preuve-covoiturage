6cc391a8038240bb1d024265f03521b68afd9a6f
gilles boisson <gilles@dotify.fr>
2019-12-19 11:08:36 +0100

    fix : iban / bic bank validator

--- dashboard/src/app/core/const/validators.const.ts
+++ dashboard/src/app/core/const/validators.const.ts
@@ -12,8 +12,6 @@ export const REGEXP = {
   cedex: /^[0-9]{5}$/,
   lon: /^-?[0-9]{1,3}(\.[0-9]+)?$/,
   lat: /^-?[0-9]{1,2}(\.[0-9]+)?$/,
-  iban: /^(?:(?:IT|SM)\d{2}[A-Z]\d{22}|CY\d{2}[A-Z]\d{23}|NL\d{2}[A-Z]{4}\d{10}|LV\d{2}[A-Z]{4}\d{13}|(?:BG|BH|GB|IE)\d{2}[A-Z]{4}\d{14}|GI\d{2}[A-Z]{4}\d{15}|RO\d{2}[A-Z]{4}\d{16}|KW\d{2}[A-Z]{4}\d{22}|MT\d{2}[A-Z]{4}\d{23}|NO\d{13}|(?:DK|FI|GL|FO)\d{16}|MK\d{17}|(?:AT|EE|KZ|LU|XK)\d{18}|(?:BA|HR|LI|CH|CR)\d{19}|(?:GE|DE|LT|ME|RS)\d{20}|IL\d{21}|(?:AD|CZ|ES|MD|SA)\d{22}|PT\d{23}|(?:BE|IS)\d{24}|(?:FR|MR|MC)\d{25}|(?:AL|DO|LB|PL)\d{26}|(?:AZ|HU)\d{27}|(?:GR|MU)\d{28})$/, // tslint:disable max-line-length
-  bic: /([a-zA-Z]{4}[a-zA-Z]{2}[a-zA-Z0-9]{2}([a-zA-Z0-9]{3})?)/,
 };
 
 export const PASSWORD = {
