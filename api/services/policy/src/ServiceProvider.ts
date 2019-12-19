940105e3a15a7056830cdffd7ab14bf13946286a
Jonathan Fallon <jonathan@dotify.fr>
2019-12-19 16:44:34 +0100

    add redis connection and campaign queue to policy-service

--- api/services/policy/src/ServiceProvider.ts
+++ api/services/policy/src/ServiceProvider.ts
@@ -2,6 +2,7 @@ import { ServiceProvider as AbstractServiceProvider } from '@ilos/core';
 import { serviceProvider, NewableType, ExtensionInterface } from '@ilos/common';
 import { PermissionMiddleware } from '@ilos/package-acl';
 import { PostgresConnection } from '@ilos/connection-postgres';
+import { RedisConnection } from '@ilos/connection-redis';
 
 import { ValidatorExtension, ValidatorMiddleware } from '@pdc/provider-validator';
 import { ScopeToSelfMiddleware, ChannelServiceWhitelistMiddleware } from '@pdc/provider-middleware';
@@ -47,7 +48,8 @@ import { PolicyProcessCommand } from './commands/PolicyProcessCommand';
     ListCampaignAction,
     ApplyAction,
   ],
-  connections: [[PostgresConnection, 'connections.postgres']],
+  connections: [[PostgresConnection, 'connections.postgres'], [RedisConnection, 'connections.redis']],
+  queues: ['campaign'],
   middlewares: [
     ['can', PermissionMiddleware],
     ['validate', ValidatorMiddleware],
