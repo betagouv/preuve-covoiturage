a3b4f210aa0bddf6163a81e413ed53181a21d031
gilles boisson <gilles@dotify.fr>
2019-12-19 11:08:36 +0100

    fix : iban / bic bank validator

---
+++ dashboard/src/app/shared/modules/form/validators/bic.validator.ts
@@ -0,0 +1,6 @@
+import { ValidatorFn, FormGroup, ValidationErrors } from '@angular/forms';
+
+export const bicValidator: ValidatorFn = (formGroup: FormGroup): ValidationErrors | null =>
+  /^([A-Z]{6}[A-Z2-9][A-NP-Z1-2])(X{3}|[A-WY-Z0-9][A-Z0-9]{2})?$/.test(formGroup.value.toUpperCase())
+    ? null
+    : { bicWrongFormat: true };
