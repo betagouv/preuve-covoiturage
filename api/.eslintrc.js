2e31650683727d7ab1cc76958a9f7ccc489172d9
Jonathan Fallon <jonathan@dotify.fr>
2020-01-07 10:17:47 +0100

    switch to eslint config

--- api/.eslintrc.js
+++ api/.eslintrc.js
@@ -4,7 +4,6 @@ module.exports = {
     node: true,
     mocha: true,
   },
-  plugins: ['prettier'],
   extends: [
     'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
   ],
@@ -13,14 +12,13 @@ module.exports = {
     sourceType: 'module', // Allows for the use of imports
   },
   rules: {
-    semi: ['error', 'always'],
-    'prettier/prettier': 'error',
-    'max-len': ['warn', { code: 120 }],
     '@typescript-eslint/no-empty-interface': ['error', { allowSingleExtends: true }],
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/camelcase': 'off', // postgresql compat
-    '@typescript-eslint/no-unused-vars': ['error', { vars: 'all', args: 'none', ignoreRestSiblings: true }],
-    '@typescript-eslint/no-empty-function': 'off',
-    '@typescript-eslint/ban-ts-ignore': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      { vars: 'all', args: 'after-used', ignoreRestSiblings: true, argsIgnorePattern: '_?next|_?req|_?res' },
+    ],
+    '@typescript-eslint/ban-ts-ignore': 'warn',
   },
 };
