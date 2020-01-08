bd53b62b1630afcc4cfd966539eb75dc11fdaf8b
gilles boisson <gilles@dotify.fr>
2020-01-08 10:04:33 +0100

    territory > user can can't see campaign drafts

--- dashboard/src/app/modules/campaign/pages/campaign-dashboard/campaign-dashboard.component.ts
+++ dashboard/src/app/modules/campaign/pages/campaign-dashboard/campaign-dashboard.component.ts
@@ -26,7 +26,15 @@ export class CampaignDashboardComponent extends DestroyObservable implements OnI
 
   ngOnInit() {
     this.canCreateCampaign$ = this._authService.user$.pipe(
-      map((user) => user && this._authService.hasAnyPermission(['incentive-campaign.create'])),
+      map(
+        (user) => user && this._authService.hasAnyPermission(['incentive-campaign.create']),
+        takeUntil(this.destroy$),
+      ),
+    );
+
+    this.camSeeDraft$ = this._authService.user$.pipe(
+      map((user) => user && this._authService.isAdmin),
+      takeUntil(this.destroy$),
     );
     this.loadCampaigns();
   }
