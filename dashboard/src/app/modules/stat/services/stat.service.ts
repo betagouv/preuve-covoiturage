e0a60c107f4241a13e7a1888494ff775861b070b
Samuel Eyre <samuel.eyre@hotmail.fr>
2020-01-13 11:51:47 +0100

    (dash: core) delete api.service

--- dashboard/src/app/modules/stat/services/stat.service.ts
+++ dashboard/src/app/modules/stat/services/stat.service.ts
@@ -2,14 +2,12 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
-import { map, take, tap } from 'rxjs/operators';
-import * as _ from 'lodash';
+import { map, tap } from 'rxjs/operators';
 
 import { JsonRPCService } from '~/core/services/api/json-rpc.service';
 import { JsonRPCParam } from '~/core/entities/api/jsonRPCParam';
 import { FormatedStatInterface } from '~/core/interfaces/stat/formatedStatInterface';
 import { UserGroupEnum } from '~/core/enums/user/user-group.enum';
-import { FilterInterface } from '~/core/interfaces/filter/filterInterface';
 import { AuthenticationService } from '~/core/services/authentication/authentication.service';
 import { StatFormatService } from '~/modules/stat/services/stat-format.service';
 import { StatInterface } from '~/core/interfaces/stat/StatInterface';
