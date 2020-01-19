dec514f941679a86942b75773cc78309321ff918
gilles boisson <gilles@dotify.fr>
2020-01-19 16:28:42 +0100

    Phone number format check on identity field

--- dashboard/src/app/modules/certificate/validators/identity-phone.validator.ts
+++ dashboard/src/app/modules/certificate/validators/identity-phone.validator.ts
@@ -1,7 +1,7 @@
 import { FormGroup, ValidationErrors, ValidatorFn } from '@angular/forms';
 
 export const phoneIdentityValidator: ValidatorFn = (formGroup: FormGroup): ValidationErrors | null => {
-  if ((<string>formGroup.value).match(/^\+[1-9]{2}[67]([0-9]{8})$/)) {
+  if ((<string>formGroup.value).match(/^\+[1-9]{3}([0-9]{8})$/)) {
     return null;
   }
   return { phoneIdentity: true };
