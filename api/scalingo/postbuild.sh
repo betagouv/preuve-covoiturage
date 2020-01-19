83748d3c178b388feaa44b4e7374ec2797c03104
Jonathan Fallon <jonathan@dotify.fr>
2020-01-19 23:38:35 +0100

    cleanup in postbuild

--- api/scalingo/postbuild.sh
+++ api/scalingo/postbuild.sh
@@ -5,3 +5,7 @@
 # clean up ilos folders to save space
 rm -rf ./ilos/.git
 find ./ilos -type d -name node_modules -exec rm -rf {} +
+
+# clean up
+find . -type f -name "*.js.map" -delete
+find . -type f -name "*.d.ts" -delete
