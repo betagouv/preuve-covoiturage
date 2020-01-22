75c409efa0d0e6780b7ee59434736f16c1410859
Nicolas Mérigot <nicolas.merigot@gmail.com>
2020-01-22 14:20:28 +0100

    feat(policy): add bounded modifier

--- api/services/policy/src/engine/rules/modifiers/index.ts
+++ api/services/policy/src/engine/rules/modifiers/index.ts
@@ -3,5 +3,6 @@ import { StaticRuleInterface } from '../../interfaces/RuleInterface';
 import { PerPassengerModifier } from './PerPassengerModifier';
 import { PerSeatModifier } from './PerSeatModifier';
 import { PerKmModifier } from './PerKmModifier';
+import { BoundedModifier } from './BoundedModifier';
 
-export const modifiers: StaticRuleInterface[] = [PerKmModifier, PerSeatModifier, PerPassengerModifier];
+export const modifiers: StaticRuleInterface[] = [PerKmModifier, PerSeatModifier, PerPassengerModifier, BoundedModifier];
