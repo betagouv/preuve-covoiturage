b575737462a79357f3d586f7848147cf4e7c8222
gilles boisson <gilles@dotify.fr>
2020-01-29 15:13:17 +0100

    create demo user role on user form

--- dashboard/src/app/core/enums/user/user-role.enum.ts
+++ dashboard/src/app/core/enums/user/user-role.enum.ts
@@ -3,6 +3,7 @@ import { USER_GROUPS, UserGroupEnum } from '~/core/enums/user/user-group.enum';
 export enum UserManyRoleEnum {
   USER = 'user',
   ADMIN = 'admin',
+  DEMO = 'demo',
   TERRITORY = 'territory',
   OPERATOR = 'operator',
   REGISTRY = 'registry',
@@ -19,14 +20,20 @@ export enum UserRoleEnum {
   OPERATOR_USER = 'operator.user',
   REGISTRY_USER = 'registry.user',
   TERRITORY_ADMIN = 'territory.admin',
+  TERRITORY_DEMO = 'territory.demo',
   OPERATOR_ADMIN = 'operator.admin',
   REGISTRY_ADMIN = 'registry.admin',
 }
 
-export const USER_ROLES: UserManyRoleEnum[] = [UserManyRoleEnum.USER, UserManyRoleEnum.ADMIN];
+export const USER_GROUP_ROLES = {
+  territories: [UserManyRoleEnum.USER, UserManyRoleEnum.ADMIN, UserManyRoleEnum.DEMO],
+  registry: [UserManyRoleEnum.USER, UserManyRoleEnum.ADMIN],
+  operators: [UserManyRoleEnum.USER, UserManyRoleEnum.ADMIN],
+};
 
 export const USER_ROLES_FR = {
   user: 'consultation',
   admin: 'administrateur',
+  demo: 'Découverte',
   // admin: 'administrateur',
 };
