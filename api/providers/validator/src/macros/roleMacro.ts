6a003bfc664493c3c82d14ac8ca6c8a47f01615f
gilles boisson <gilles@dotify.fr>
2020-01-29 15:33:54 +0100

    Update validation rules

--- api/providers/validator/src/macros/roleMacro.ts
+++ api/providers/validator/src/macros/roleMacro.ts
@@ -1,7 +1,15 @@
 export function roleMacro(): { type: string; enum: string[]; default: string } {
   return {
     type: 'string',
-    enum: ['operator.admin', 'operator.user', 'registry.admin', 'registry.user', 'territory.admin', 'territory.user'],
+    enum: [
+      'operator.admin',
+      'operator.user',
+      'registry.admin',
+      'registry.user',
+      'territory.admin',
+      'territory.user',
+      'territory.demo',
+    ],
     default: 'user',
   };
 }
