0c66c700f56573bad5e0f03b97d24c7854b2ea2c
Jonathan Fallon <jonathan@dotify.fr>
2020-02-06 16:51:43 +0100

    CertificatePgRepositoryProvider tests

--- shared/certificate/common/interfaces/CertificateAccessLogInterface.ts
+++ shared/certificate/common/interfaces/CertificateAccessLogInterface.ts
@@ -1,8 +1,8 @@
 export interface CertificateAccessLogInterface {
-  certificate_id?: number;
+  certificate_id?: string;
+  created_at?: Date;
   ip: string;
   user_agent: string;
   user_id: string;
   content_type: string;
-  created_at: Date;
 }
