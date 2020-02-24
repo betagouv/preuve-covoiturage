237f273b44d815616e182cb40792316a10d19180
Jonathan Fallon <jonathan@dotify.fr>
2020-02-24 11:22:54 +0100

    wip add IDFM strike rule

--- api/services/policy/src/engine/rules/posts/index.ts
+++ api/services/policy/src/engine/rules/posts/index.ts
@@ -2,5 +2,6 @@ import { MetaAmountPost } from './MetaAmountPost';
 import { StaticRuleInterface } from '../../interfaces';
 import { MetaTripPost } from './MetaTripPost';
 import { IdfmRegular } from './IdfmRegular';
+import { IdfmStrikeJan2020 } from './IdfmStrikeJan2020';
 
-export const posts: StaticRuleInterface[] = [MetaAmountPost, MetaTripPost, IdfmRegular];
+export const posts: StaticRuleInterface[] = [MetaAmountPost, MetaTripPost, IdfmRegular, IdfmStrikeJan2020];
