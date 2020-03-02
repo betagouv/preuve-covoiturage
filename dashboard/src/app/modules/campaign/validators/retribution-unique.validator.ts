4a52ec467fb5d6bb9af33eaa4b9d636b1174b491
gilles boisson <gilles@dotify.fr>
2020-03-02 16:22:32 +0100

    fix : validation error on free for passenger retribution

--- dashboard/src/app/modules/campaign/validators/retribution-unique.validator.ts
+++ dashboard/src/app/modules/campaign/validators/retribution-unique.validator.ts
@@ -3,11 +3,14 @@ import { FormGroup, ValidationErrors, ValidatorFn } from '@angular/forms';
 export const uniqueRetributionValidator: ValidatorFn = (formGroup: FormGroup): ValidationErrors | null => {
   // return null;
 
-  const errors = [];
+  let errors = [];
   let hasErrors = false;
+  console.log('formGroup.value : ', formGroup.value);
 
   for (const retribution of formGroup.value) {
-    const hasError = !retribution.for_passenger.amount && !retribution.for_driver.amount;
+    const hasError =
+      retribution.for_passenger.free !== true && !retribution.for_passenger.amount && !retribution.for_driver.amount;
+
     errors.push(hasError);
     hasErrors = hasErrors || hasError;
   }
