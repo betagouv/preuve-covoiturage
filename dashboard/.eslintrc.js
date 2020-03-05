df8f57fcb90bee9025c0aa1c73a45bcbfbcebf00
gilles boisson <gilles@dotify.fr>
2020-03-05 17:15:22 +0100

    fix : eslint align with api config

--- dashboard/.eslintrc.js
+++ dashboard/.eslintrc.js
@@ -15,19 +15,9 @@ module.exports = {
   ignorePatterns: ['providers/qrcode/src/lib/*'], // some JS converted to TS. needs to be cleaned up
   rules: {
     semi: ['error', 'always'],
-
-    // Extra rules
-    // '@typescript-eslint/consistent-type-assertions': 'off',
-    // '@typescript-eslint/explicit-function-return-type': 'off',
-    // '@typescript-eslint/no-unused-vars': 'off',
-    // '@typescript-eslint/no-use-before-define': 'off',
-    // '@typescript-eslint/no-inferrable-types': 'off',
-    // '@typescript-eslint/interface-name-prefix': 'off',
-    // 'prefer-const': 'off',
-    '@typescript-eslint/no-empty-interface': 'off',
     'prettier/prettier': 'error',
     'max-len': ['warn', { code: 120 }],
-    // '@typescript-eslint/no-empty-interface': ['error', { allowSingleExtends: true }],
+    '@typescript-eslint/no-empty-interface': ['error', { allowSingleExtends: true }],
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/camelcase': 'off', // postgresql compat
     '@typescript-eslint/no-unused-vars': ['error', { vars: 'all', args: 'none', ignoreRestSiblings: true }],
