8b78619376da2f7304ffa2ca1c8965bb6520ff5e
gilles boisson <gilles@dotify.fr>
2020-03-05 09:51:05 +0100

    setup eslint

---
+++ dashboard/.eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    mocha: true,
+  },
+  plugins: ['prettier'],
+  extends: [
+    'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+  ],
+  parserOptions: {
+    ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
+    sourceType: 'module', // Allows for the use of imports
+  },
+  ignorePatterns: ['providers/qrcode/src/lib/*'], // some JS converted to TS. needs to be cleaned up
+  rules: {
+    semi: ['error', 'always'],
+
+    // Extra rules
+    '@typescript-eslint/consistent-type-assertions': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    '@typescript-eslint/no-use-before-define': 'off',
+    '@typescript-eslint/no-inferrable-types': 'off',
+    '@typescript-eslint/interface-name-prefix': 'off',
+    'prefer-const': 'off',
+    '@typescript-eslint/no-empty-interface': 'off',
+    'prettier/prettier': 'error',
+    'max-len': ['warn', { code: 120 }],
+    // '@typescript-eslint/no-empty-interface': ['error', { allowSingleExtends: true }],
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/camelcase': 'off', // postgresql compat
+    // '@typescript-eslint/no-unused-vars': ['error', { vars: 'all', args: 'none', ignoreRestSiblings: true }],
+    '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/ban-ts-ignore': 'off',
+  },
+};
