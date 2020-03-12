f66b19d4a44c0f9bbfae5f4c172d0cc5fbfb0aaa
gilles boisson <gilles@dotify.fr>
2020-03-12 15:29:28 +0100

    inline IE detection

--- dashboard/src/main.ts
+++ dashboard/src/main.ts
@@ -7,34 +7,12 @@ import { AppModule } from '~/app.module';
 
 import { environment } from './environments/environment';
 
-function getInternetExplorerVersion(): number {
-  let rv = -1;
-  if (navigator.appName == 'Microsoft Internet Explorer') {
-    const ua = navigator.userAgent;
-    const re = new RegExp('MSIE ([0-9]{1,}[\\.0-9]{0,})');
-    if (re.exec(ua) != null) rv = parseFloat(RegExp.$1);
-  } else if (navigator.appName == 'Netscape') {
-    const ua = navigator.userAgent;
-    const re = new RegExp('Trident/.*rv:([0-9]{1,}[\\.0-9]{0,})');
-    if (re.exec(ua) != null) rv = parseFloat(RegExp.$1);
-  }
-  return rv;
+if (environment.production) {
+  enableProdMode();
 }
 
-// IE 11 and less detection
-const ieVersion = getInternetExplorerVersion();
+moment.locale('fr');
 
-if (ieVersion === -1 || ieVersion > 11) {
-  if (environment.production) {
-    enableProdMode();
-  }
-
-  moment.locale('fr');
-
-  platformBrowserDynamic()
-    .bootstrapModule(AppModule)
-    .catch((err) => console.error(err));
-} else {
-  document.getElementById('ie-warning').classList.remove('hidden');
-  document.getElementsByClassName('splashscreen')[0].classList.add('hidden');
-}
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.error(err));
