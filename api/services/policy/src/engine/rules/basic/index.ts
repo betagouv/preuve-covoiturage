2cb7217164eea80dacb2be81c0c33bd67186d9e4
Nicolas Mérigot <nicolas.merigot@gmail.com>
2020-03-17 17:55:10 +0100

    clean(policy): remove duplicate files

--- api/services/policy/src/engine/rules/basic/index.ts
+++
@@ -1,5 +0,0 @@
-import { IdfmRegular } from './IdfmRegular';
-import { IdfmStrikeJan2020 } from './IdfmStrikeJan2020';
-import { StaticRuleInterface } from '../../interfaces';
-
-export const basic: StaticRuleInterface[] = [IdfmRegular, IdfmStrikeJan2020];
