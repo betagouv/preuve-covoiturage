e8c9e5a2766025abdd20003d3a09dd5102a2fcd0
Nicolas Mérigot <nicolas.merigot@gmail.com>
2020-03-24 22:18:26 +0100

    feat(policy): add campaign repository test

--- api/services/policy/src/providers/CampaignPgRepositoryProvider.ts
+++ api/services/policy/src/providers/CampaignPgRepositoryProvider.ts
@@ -274,41 +274,41 @@ export class CampaignPgRepositoryProvider implements CampaignRepositoryProviderI
     return result.rows;
   }
 
-  async findApplicableCampaigns(territories: number[], date: Date): Promise<CampaignInterface[]> {
-    const query = {
-      text: `
-        SELECT
-          _id,
-          parent_id,
-          ui_status,
-          territory_id,
-          name,
-          description,
-          start_date,
-          end_date,
-          unit,
-          status,
-          global_rules,
-          rules
-        FROM ${this.table}
-        WHERE 
-          territory_id = ANY($1::integer[])
-          AND start_date <= $2
-          AND end_date >= $2
-          AND status = $3
-          AND deleted_at IS NULL
-      `,
-      values: [territories, date, 'active'],
-    };
+  // async findApplicableCampaigns(territories: number[], date: Date): Promise<CampaignInterface[]> {
+  //   const query = {
+  //     text: `
+  //       SELECT
+  //         _id,
+  //         parent_id,
+  //         ui_status,
+  //         territory_id,
+  //         name,
+  //         description,
+  //         start_date,
+  //         end_date,
+  //         unit,
+  //         status,
+  //         global_rules,
+  //         rules
+  //       FROM ${this.table}
+  //       WHERE 
+  //         territory_id = ANY($1::integer[])
+  //         AND start_date <= $2
+  //         AND end_date >= $2
+  //         AND status = $3
+  //         AND deleted_at IS NULL
+  //     `,
+  //     values: [territories, date, 'active'],
+  //   };
 
-    const result = await this.connection.getClient().query(query);
+  //   const result = await this.connection.getClient().query(query);
 
-    if (result.rowCount === 0) {
-      return [];
-    }
+  //   if (result.rowCount === 0) {
+  //     return [];
+  //   }
 
-    return result.rows;
-  }
+  //   return result.rows;
+  // }
 
   async getTemplates(): Promise<CampaignInterface[]> {
     const result = await this.connection.getClient().query(`
