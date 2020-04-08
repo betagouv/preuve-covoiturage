2b03bd545056080e62bc68ef5d0dd7809a59594d
Nicolas Mérigot <nicolas.merigot@gmail.com>
2020-04-08 14:23:45 +0200

    WIP(sql): territory relationships

--- api/db/migrations/policy/20200406000000_update_territory_policy_relation.up.sql
+++ api/db/migrations/policy/20200406000000_update_territory_policy_relation.up.sql
@@ -1,1 +1,13 @@
--- TODO : map territory_id to new _id
+UPDATE policy.policies
+SET territory_id=sub.territory_id
+FROM (
+  SELECT
+    _id as territory_id,
+    old_territory_id
+  FROM territory.territories
+  WHERE
+    old_territory_id IN (SELECT territory_id FROM policy.policies)
+) as sub
+WHERE policies.territory_id = sub.old_territory_id;
+
+-- TODO UDPATE MATERIALIZED VIEW
