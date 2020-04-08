9593ae477f44804e9f1516444371c52bbf89a566
Nicolas Mérigot <nicolas.merigot@gmail.com>
2020-04-08 23:52:39 +0200

    wip(territories): update action contracts

--- shared/territory/find.contract.ts
+++ shared/territory/find.contract.ts
@@ -1,11 +1,35 @@
-import { TerritoryInterface } from './common/interfaces/TerritoryInterface';
+import { TerritoryCodesInterface, TerritoryCodeEnum } from './common/interfaces/TerritoryCodeInterface';
+import { GeoPositionInterface } from '../common/interfaces/GeoPositionInterface';
+import {
+  SortEnum,
+  BasicFieldEnum,
+  ActiveFieldEnum,
+  GeoFieldEnum,
+  RelationFiedEnum,
+} from './common/interfaces/TerritoryQueryInterface';
+import { TerritoryBaseInterface } from './common/interfaces/TerritoryInterface';
+import { TerritoryDbMetaInterface } from './common/interfaces/TerritoryDbMetaInterface';
+import { TerritoryRelationInterface } from './common/interfaces/TerritoryRelationInterface';
+
+interface QueryParamsInterface extends Partial<TerritoryCodesInterface> {
+  _id?: number;
+  position?: GeoPositionInterface;
+  active?: boolean;
+}
 
 export interface ParamsInterface {
-  _id: number;
+  query: QueryParamsInterface;
+  sort: SortEnum;
+  projection: BasicFieldEnum | ActiveFieldEnum | GeoFieldEnum | RelationFiedEnum | TerritoryCodeEnum;
 }
-export interface ResultInterface extends TerritoryInterface {
-  _id: number;
-}
+
+export interface ResultInterface
+  extends Partial<TerritoryBaseInterface>,
+    Partial<TerritoryDbMetaInterface>,
+    Partial<TerritoryCodesInterface>,
+    Partial<TerritoryRelationInterface> {}
+
+
 export const handlerConfig = {
   service: 'territory',
   method: 'find',
