31dbfca8df2593e600149be74e0010a3c01b6ef5
gilles boisson <gilles@dotify.fr>
2020-04-08 13:01:11 +0200

    post build test

--- api/scalingo/build.sh
+++ api/scalingo/build.sh
@@ -4,3 +4,13 @@ lerna run --scope @pdc/* --sort build
 # clean up
 find . -type f -name "*.js.map" -delete
 find . -type f -name "*.d.ts" -delete
+
+# Post build
+rm -rf ./ilos/.git
+find ./ilos -type d -name node_modules -exec rm -rf {} +
+
+# clean up
+find . -type f -name "*.js.map" -delete
+find . -type f -name "*.d.ts" -delete
+
+echo -n "[app] size: "; du -ch | tail -n 1
