ef65e5cf0133a1805ea1b1b930737a54df7297c1
gilles boisson <gilles@dotify.fr>
2020-04-08 14:37:40 +0200

    safe date casting

--- api/services/carpool/src/actions/CrosscheckAction.ts
+++ api/services/carpool/src/actions/CrosscheckAction.ts
@@ -83,7 +83,11 @@ export class CrosscheckAction extends Action {
   protected getStatus(created: Date, dates: Date[]): string {
     const maxDiff = this.config.get('rules.maxAge');
     return dates
-      .map((d) => d.getTime() - created.getTime())
+      .map(
+        (d) =>
+          (d.getTime ? d.getTime() : new Date(d).getTime()) -
+          (created.getTime ? created.getTime() : new Date(created).getTime()),
+      )
       .map((diff) => diff >= maxDiff)
       .reduce((status, isExpired) => (isExpired ? 'expired' : status), 'ok');
   }
