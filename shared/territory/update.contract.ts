2ea7714ba403537c3b4bac672673f9642db5dc4d
Nicolas Mérigot <nicolas.merigot@gmail.com>
2020-04-09 13:34:52 +0200

    wip(territories): update contract

--- shared/territory/update.contract.ts
+++ shared/territory/update.contract.ts
@@ -1,13 +1,16 @@
-import { TerritoryInterface } from './common/interfaces/TerritoryInterface';
+import { TerritoryBaseInterface } from './common/interfaces/TerritoryInterface';
+import { TerritoryDbMetaInterface } from './common/interfaces/TerritoryDbMetaInterface';
 
-interface Territory extends TerritoryInterface {
+interface Territory extends TerritoryBaseInterface {
   _id: number;
 }
 
 export interface ParamsInterface extends Territory {}
-export interface ResultInterface extends Territory {}
+export interface ResultInterface extends TerritoryBaseInterface, TerritoryDbMetaInterface {}
+
 export const handlerConfig = {
   service: 'territory',
   method: 'update',
 };
+
 export const signature = `${handlerConfig.service}:${handlerConfig.method}`;
--- shared/territory/update.schema.ts
+++ shared/territory/update.schema.ts
@@ -1,16 +1,48 @@
-import { create as createSchema } from './create.schema';
-
-const upd = JSON.parse(JSON.stringify(createSchema));
+import { contacts } from '../common/schemas/contacts';
 
 export const alias = 'territory.update';
+
 export const update = {
   $id: alias,
   type: 'object',
-  required: ['_id'],
+  required: ['_id', 'level', 'name'],
   additionalProperties: false,
-  minProperties: 2,
   properties: {
-    ...upd.properties,
     _id: { macro: 'serial' },
+    contacts,
+    name: { macro: 'varchar' },
+    level: {
+      type: 'string',
+      enum: [
+        'town',
+        'towngroup',
+        'district',
+        'megalopolis',
+        'region',
+        'state',
+        'country',
+        'countrygroup',
+        'other',
+      ],
+    },
+    company_id: {
+      macro: 'serial',
+    },
+    active: {
+      type: 'boolean',
+      default: false,
+    },
+    active_since: {
+      macro: 'timestamp',
+    },
+    density: {
+      type: 'integer',
+      minimum: 0,
+    },
+    // geo: {
+    //  TODO geography type
+    // },
   },
 };
+
+
