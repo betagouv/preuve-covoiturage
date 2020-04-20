a69eee67f69f98ecbba39b5ac77d372d9d9bb751
Jonathan Fallon <jonathan@dotify.fr>
2020-04-20 15:43:48 +0200

    export fixturesMacro at the package level

--- api/providers/test/src/index.ts
+++ api/providers/test/src/index.ts
@@ -1,3 +1,4 @@
 export { serviceProviderMacro } from './serviceProviderMacro';
 export { handlerMacro } from './handlerMacro';
 export { KernelTestInterface, makeKernel } from './helpers';
+export { fixturesMacro, MacroTestContext } from './fixturesMacro';
