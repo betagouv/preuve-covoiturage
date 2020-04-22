00b7628bbb9dc088b12d555787e230aa046a7fda
gilles boisson <gilles@dotify.fr>
2020-04-22 14:28:24 +0200

    territories find query

--- shared/territory/find.contract.ts
+++ shared/territory/find.contract.ts
@@ -1,17 +1,11 @@
 import { TerritoryCodesInterface, TerritoryCodeEnum } from './common/interfaces/TerritoryCodeInterface';
 import { GeoPositionInterface } from '../common/interfaces/GeoPositionInterface';
-import {
-  SortEnum,
-  BasicFieldEnum,
-  ActiveFieldEnum,
-  GeoFieldEnum,
-  RelationFiedEnum,
-} from './common/interfaces/TerritoryQueryInterface';
+import { SortEnum, ProjectionFieldsEnum } from './common/interfaces/TerritoryQueryInterface';
 import { TerritoryBaseInterface } from './common/interfaces/TerritoryInterface';
 import { TerritoryDbMetaInterface } from './common/interfaces/TerritoryDbMetaInterface';
 import { TerritoryRelationInterface } from './common/interfaces/TerritoryRelationInterface';
 
-interface QueryParamsInterface extends Partial<TerritoryCodesInterface> {
+export interface QueryParamsInterface extends Partial<TerritoryCodesInterface> {
   _id?: number;
   position?: GeoPositionInterface;
   active?: boolean;
@@ -19,8 +13,8 @@ interface QueryParamsInterface extends Partial<TerritoryCodesInterface> {
 
 export interface ParamsInterface {
   query: QueryParamsInterface;
-  sort: SortEnum;
-  projection: BasicFieldEnum | ActiveFieldEnum | GeoFieldEnum | RelationFiedEnum | TerritoryCodeEnum;
+  sort: SortEnum[];
+  projection: ProjectionFieldsEnum;
 }
 
 export interface ResultInterface
--- shared/territory/find.schema.ts
+++ shared/territory/find.schema.ts
@@ -1,3 +1,5 @@
+import { allProjectionFields } from './common/interfaces/TerritoryQueryInterface';
+
 export const alias = 'territory.find';
 export const find = {
   $id: alias,
@@ -15,6 +17,22 @@ export const find = {
         insee: { macro: 'insee' },
         postcode: { macro: 'postcode' },
         active: { type: 'boolean' },
+        search: { type: 'string' },
+        company_name: { type: 'string' },
+        has_child_id: {
+          type: 'number',
+        },
+        has_parent_id: {
+          type: 'number',
+        },
+
+        has_ancestor_id: {
+          type: 'number',
+        },
+        has_descendant_id: {
+          type: 'number',
+        },
+
         position: {
           type: 'object',
           required: ['lat', 'lon'],
@@ -27,32 +45,17 @@ export const find = {
       },
     },
     sort: {
-      type: 'string',
-      enum: ['+name', '-name', '+area', '-area'],
+      type: 'array',
+      items: {
+        type: 'string',
+        enum: ['name ASC', 'name DESC' /*, '+area', '-area' */],
+      },
     },
     projection: {
       type: 'array',
       items: {
         type: 'string',
-        enum: [
-          '_id',
-          'level',
-          'name',
-          'active',
-          'company_id',
-          'density',
-          'geo',
-          'children',
-          'parents',
-          'descendants',
-          'ancestors',
-          'insee',
-          'postcode',
-          'active_since',
-          'contacts',
-          'created_at',
-          'updated_at',
-        ],
+        enum: allProjectionFields,
       },
     },
   },
