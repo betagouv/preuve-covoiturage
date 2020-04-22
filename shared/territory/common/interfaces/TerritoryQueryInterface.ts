414cda650b691dc6bd6486fbb8e10926c6baf933
gilles boisson <gilles@dotify.fr>
2020-04-22 14:28:24 +0200

    territories find query

--- shared/territory/common/interfaces/TerritoryQueryInterface.ts
+++ shared/territory/common/interfaces/TerritoryQueryInterface.ts
@@ -2,6 +2,7 @@ import { GeoPositionInterface } from '../../../common/interfaces/GeoPositionInte
 import { TerritoryCodesInterface } from './TerritoryCodeInterface';
 
 export interface TerritoryQueryInterface extends Partial<TerritoryCodesInterface> {
+  _id?: number;
   search?: string;
   has_child_id?: number;
   has_parent_id?: number;
@@ -13,12 +14,50 @@ export interface TerritoryQueryInterface extends Partial<TerritoryCodesInterface
   active?: boolean;
 }
 
-export enum SortEnum {
-  AreaAsc = '+area',
-  AreaDesc = '-area',
-  NameAsc = '+name',
-  NameDesc = '-name',
+export enum TerritoryQueryEnum {
+  Id = '_id',
+  Search = 'search',
+  HasChildId = 'has_child_id',
+  HasParentId = 'has_parent_id',
+  HasAncestorId = 'has_ancestor_id',
+  HasDescendantId = 'has_descendant_id',
+  Name = 'name',
+  CompanyName = 'company_legal_name',
+  Position = 'position',
+  Active = 'active',
 }
+
+export const allTerritoryQueryRelationFields: TerritoryQueryEnum[] = [
+  TerritoryQueryEnum.HasChildId,
+  TerritoryQueryEnum.HasParentId,
+  TerritoryQueryEnum.HasAncestorId,
+  TerritoryQueryEnum.HasDescendantId,
+];
+
+export const allTerritoryQueryCompanyFields: TerritoryQueryEnum[] = [TerritoryQueryEnum.CompanyName];
+
+export const allTerritoryQueryDirectFields: TerritoryQueryEnum[] = [
+  TerritoryQueryEnum.Id,
+  TerritoryQueryEnum.Name,
+  TerritoryQueryEnum.Position,
+  TerritoryQueryEnum.Active,
+];
+
+export const allTerritoryQueryFields: TerritoryQueryEnum[] = [
+  ...allTerritoryQueryRelationFields,
+  ...allTerritoryQueryCompanyFields,
+  ...allTerritoryQueryDirectFields,
+  TerritoryQueryEnum.Search,
+];
+
+export enum SortEnum {
+  // AreaAsc = '+area',
+  // AreaDesc = '-area',
+  NameAsc = 'name ASC',
+  NameDesc = 'name DESC',
+}
+
+export const allSortField = [SortEnum.NameAsc, SortEnum.NameDesc];
 
 export enum BasicFieldEnum {
   Id = '_id',
@@ -29,7 +68,7 @@ export enum BasicFieldEnum {
 }
 
 export enum ActiveFieldEnum {
-  ActiveSince = 'active_since',
+  Active = 'active',
   Contacts = 'contacts',
   CreatedAt = 'created_at',
   UpdatedAt = 'updated_at',
@@ -40,16 +79,85 @@ export enum GeoFieldEnum {
   Geo = 'geo',
 }
 
-export enum RelationFiedEnum {
+export enum RelationFieldEnum {
+  // Company = 'company',
   Children = 'children',
-  Parents = 'parents',
+  Parent = 'parent',
   Descendants = 'descendants',
   Ancestors = 'ancestors',
   // ActiveDescendants = 'activeDescendants',
   // EndingDescendants = 'endingDescendants',
 }
 
+export enum TerritoryCodeEnum {
+  Insee = 'insee',
+  Postcode = 'postcode',
+}
+
+export enum CompanyEnum {
+  Siret = 'siret',
+  Siren = 'siren',
+  Nic = 'nic',
+  LegalName = 'legal_name',
+}
+
 export interface PaginationInterface {
   skip: number;
   limit: number;
 }
+
+export type ProjectionFieldsEnum = (
+  | RelationFieldEnum
+  | BasicFieldEnum
+  | ActiveFieldEnum
+  | GeoFieldEnum
+  | TerritoryCodeEnum
+  | CompanyEnum
+)[];
+
+export const allGeoFieldEnum: ProjectionFieldsEnum = [GeoFieldEnum.Density, GeoFieldEnum.Geo];
+
+export const allBasicFieldEnum: ProjectionFieldsEnum = [
+  BasicFieldEnum.Id,
+  BasicFieldEnum.CompanyId,
+  BasicFieldEnum.Level,
+  BasicFieldEnum.Name,
+  BasicFieldEnum.Active,
+];
+
+export const allTerritoryCodeEnum: ProjectionFieldsEnum = [TerritoryCodeEnum.Insee, TerritoryCodeEnum.Postcode];
+
+export const allAncestorRelationFieldEnum: ProjectionFieldsEnum = [
+  RelationFieldEnum.Descendants,
+  RelationFieldEnum.Ancestors,
+  RelationFieldEnum.Children,
+  RelationFieldEnum.Parent,
+  TerritoryCodeEnum.Insee,
+  TerritoryCodeEnum.Postcode,
+];
+export const allCompanyFieldEnum: ProjectionFieldsEnum = [
+  CompanyEnum.Siren,
+  CompanyEnum.Siret,
+  CompanyEnum.Nic,
+  CompanyEnum.LegalName,
+];
+
+export const directFields: ProjectionFieldsEnum = [
+  BasicFieldEnum.Id,
+  BasicFieldEnum.CompanyId,
+  BasicFieldEnum.Level,
+  BasicFieldEnum.Name,
+  BasicFieldEnum.Active,
+  GeoFieldEnum.Density,
+  GeoFieldEnum.Geo,
+  ActiveFieldEnum.Contacts,
+  ActiveFieldEnum.CreatedAt,
+  ActiveFieldEnum.UpdatedAt,
+];
+
+export const allProjectionFields: ProjectionFieldsEnum = [
+  ...directFields,
+  ...allAncestorRelationFieldEnum,
+  ...allCompanyFieldEnum,
+  // ...allTerritoryCodeEnum,
+];
