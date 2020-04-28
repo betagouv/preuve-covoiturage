2a7c29564f92ee73e9429804a1ad712284ec7f37
gilles boisson <gilles@dotify.fr>
2020-04-28 10:13:55 +0200

    add ancestors ids on getParentChildren action

--- shared/territory/common/interfaces/TerritoryChildrenInterface.ts
+++ shared/territory/common/interfaces/TerritoryChildrenInterface.ts
@@ -6,5 +6,6 @@ export interface TerritoryChildrenInterface {
 export interface TerritoryParentChildrenInterface {
   parent: { _id: number; name: string };
   _id: number;
+  descendant_ids: number[];
   children: { _id: number; name: string }[];
 }
