89f4747aae52162e63c3270e4f582278f946ac9d
Nicolas Mérigot <nicolas.merigot@gmail.com>
2020-07-01 16:04:04 +0200

    fix(trip): remove useles migration

--- api/db/migrations/20200406000000-update_territory_table.js
+++ api/db/migrations/20200406000000-update_territory_table.js
@@ -5,19 +5,21 @@
  * as a toString() of ObjectID objects.
  */
 var { createMigration } = require('../helpers/createMigration');
-var { setup, up, down } = createMigration([
-  'company/20200406000000_update_company_table',
-  'territory/20200406000000_update_territory_table',
-  'territory/20200406000001_migrate_territory_data',
-  'territory/20200406000002_create_territory_view',
-  'auth/20200406000000_update_territory_user_relation',
-  'policy/20200406000000_update_territory_policy_relation',
-  'carpool/20200406000000_replace_insee_by_territory_relation',
-  'trip/20200406000000_update_trip_view',
-  'trip/20200406000000_update_export_view',
-  'territory/20200406000010_cleanup_territory_table',
-  'common/20200406000000_drop_insee_table',
-], __dirname);
+var { setup, up, down } = createMigration(
+  [
+    'company/20200406000000_update_company_table',
+    'territory/20200406000000_update_territory_table',
+    'territory/20200406000001_migrate_territory_data',
+    'territory/20200406000002_create_territory_view',
+    'auth/20200406000000_update_territory_user_relation',
+    'policy/20200406000000_update_territory_policy_relation',
+    'carpool/20200406000000_replace_insee_by_territory_relation',
+    'trip/20200406000000_update_trip_view',
+    'territory/20200406000010_cleanup_territory_table',
+    'common/20200406000000_drop_insee_table',
+  ],
+  __dirname,
+);
 
 exports.setup = setup;
 exports.up = up;
