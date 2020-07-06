01b2dabc58bb1798fc2cc1131005c5632b4cb694
Jonathan Fallon <jonathan@dotify.fr>
2020-07-06 17:12:06 +0200

    add certificate meta to create action response

--- api/services/certificate/src/actions/CreateCertificateAction.ts
+++ api/services/certificate/src/actions/CreateCertificateAction.ts
@@ -88,6 +88,7 @@ export class CreateCertificateAction extends AbstractAction {
         created_at: certificate.created_at,
         pdf_url: `${this.config.get('url.certificateBaseUrl')}/pdf/${certificate.uuid}`,
         png_url: `${this.config.get('url.certificateBaseUrl')}/png/${certificate.uuid}`,
+        meta: certificate.meta,
       },
     };
   }
