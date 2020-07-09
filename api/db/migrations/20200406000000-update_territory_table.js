9bae8bef7a8634a6562302f1869c1c0aed6a2fc2
Nicolas Mérigot <nicolas.merigot@gmail.com>
2020-07-09 09:58:14 +0200

    wip(sql): update policy trip

--- api/db/migrations/20200406000000-update_territory_table.js
+++ api/db/migrations/20200406000000-update_territory_table.js
@@ -14,6 +14,7 @@ var { setup, up, down } = createMigration(
     'territory/20200406000002_create_territory_view',
     'auth/20200406000000_update_territory_user_relation',
     'policy/20200406000000_update_territory_policy_relation',
+    'policy/20200406000000_update_policy_trip_view',
     'carpool/20200406000000_replace_insee_by_territory_relation',
     'trip/20200406000000_update_trip_view',
     'territory/20200406000010_cleanup_territory_table',
