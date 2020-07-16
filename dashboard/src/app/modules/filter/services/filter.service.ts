d3676359238ecb70527c485b0fcb2820f1e4131d
Jonathan Fallon <jonathan@dotify.fr>
2020-07-16 19:32:34 +0200

    remove insee in Filter

--- dashboard/src/app/modules/filter/services/filter.service.ts
+++ dashboard/src/app/modules/filter/services/filter.service.ts
@@ -7,7 +7,6 @@ import * as moment from 'moment';
 import { FilterUxInterface } from '~/core/interfaces/filter/filterUxInterface';
 import { Filter } from '~/core/entities/filter/filter';
 import { FilterInterface } from '~/core/interfaces/filter/filterInterface';
-import { InseeAndTerritoryInterface } from '~/core/entities/campaign/ux-format/incentive-filters';
 
 @Injectable({
   providedIn: 'root',
@@ -26,13 +25,7 @@ export class FilterService {
       return;
     }
 
-    let insees = [];
-    if ('insees' in filterUx) {
-      insees = filterUx.insees.map((town: InseeAndTerritoryInterface) => town.insees).reduce((a, b) => a.concat(b), []);
-    }
-
     const filter = new Filter({
-      insee: insees,
       date: filterUx.date,
       hour: filterUx.hour as any,
       days: filterUx.days,
