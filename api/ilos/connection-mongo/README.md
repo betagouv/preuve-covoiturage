0081a9a3b9de74a9659924d9fd2274d625aa2e60
Jonathan Fallon <jonathan@dotify.fr>
2020-07-22 18:32:53 +0200

    remove connection-mongo. fix #807

--- api/ilos/connection-mongo/README.md
+++
@@ -1,3 +0,0 @@
-This package is a part of Ilos framework.
-
-See more on [http://www.ilos.tech](http://www.ilos.tech)
--- api/ilos/connection-mongo/src/index.ts
+++
@@ -1,3 +0,0 @@
-export { Collection as CollectionInterface, Db as DbInterface, ObjectId, Timestamp } from 'mongodb';
-export { MongoConnection } from './MongoConnection';
-export { MongoException } from './exceptions/MongoException';
