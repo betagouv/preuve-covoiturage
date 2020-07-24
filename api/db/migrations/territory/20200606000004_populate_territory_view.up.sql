5d093afb1ffdc31b822ab9d60236a31e566a9d63
Jonathan Fallon <jonathan@dotify.fr>
2020-07-24 20:07:38 +0200

    remove UNLOGGED in populate migration

--- api/db/migrations/territory/20200606000004_populate_territory_view.up.sql
+++ api/db/migrations/territory/20200606000004_populate_territory_view.up.sql
@@ -1,7 +1,7 @@
 CREATE SCHEMA mig;
 
 -- fast
-CREATE UNLOGGED TABLE mig.root AS
+CREATE TABLE mig.root AS
   SELECT
     t._id,
     array_remove( array_agg( distinct tr.parent_territory_id ), t._id ) AS parents,
@@ -11,8 +11,10 @@ CREATE UNLOGGED TABLE mig.root AS
   ON t._id = tr.parent_territory_id OR t._id = tr.child_territory_id
   GROUP BY t._id;
 
+SELECT 'root' as name, count(*) from mig.root;
+
 -- fast
-CREATE UNLOGGED TABLE mig.codes AS
+CREATE TABLE mig.codes AS
   SELECT
     territory_id,
     array_remove(array_agg(tc.value) FILTER(where tc.type = 'insee'), null) AS insee,
@@ -21,8 +23,10 @@ CREATE UNLOGGED TABLE mig.codes AS
   FROM territory.territory_codes tc
   GROUP BY tc.territory_id;
 
+SELECT 'codes' as name, count(*) from mig.codes;
+
 -- fast
-CREATE UNLOGGED TABLE mig.input AS
+CREATE TABLE mig.input AS
   SELECT
     r._id,
     r.parents,
@@ -35,8 +39,10 @@ CREATE UNLOGGED TABLE mig.input AS
   ON r._id = d.territory_id
   ORDER BY coalesce(array_length(r.children,1),0) ASC;
 
+SELECT 'input' as name, count(*) from mig.input;
+
 -- slow
-CREATE UNLOGGED TABLE mig.complete_parent AS
+CREATE TABLE mig.complete_parent AS
   WITH RECURSIVE complete_parent AS (
     SELECT t._id, t.parents
     FROM mig.input AS t
@@ -47,8 +53,10 @@ CREATE UNLOGGED TABLE mig.complete_parent AS
       ON t._id = any(c.parents)
   ) TABLE complete_parent;
 
+SELECT 'complete_parent' as name, count(*) from mig.complete_parent;
+
 -- slow
-CREATE UNLOGGED TABLE mig.complete_children AS
+CREATE TABLE mig.complete_children AS
   WITH RECURSIVE complete_children AS (
     SELECT t._id, t.children,t.insee,t.postcode,t.codedep
     FROM mig.input AS t
@@ -59,8 +67,10 @@ CREATE UNLOGGED TABLE mig.complete_children AS
       ON t._id = any(c.children)
   ) TABLE complete_children;
 
+SELECT 'complete_children' as name, count(*) from mig.complete_children;
+
 -- fast
-CREATE UNLOGGED TABLE mig.complete AS
+CREATE TABLE mig.complete AS
   SELECT
     cc._id,
     cc.children,
@@ -72,8 +82,10 @@ CREATE UNLOGGED TABLE mig.complete AS
     LEFT JOIN mig.complete_parent cp
     ON cp._id = cc._id;
 
+SELECT 'complete' as name, count(*) from mig.complete;
+
 --slow
-CREATE UNLOGGED TABLE mig.agg AS
+CREATE TABLE mig.agg AS
   SELECT
     c._id,
     array_remove(array_remove(array_agg(distinct p), null), c._id) AS ancestors,
@@ -92,8 +104,10 @@ CREATE UNLOGGED TABLE mig.agg AS
   ON (tr.parent_territory_id = c._id)
   GROUP BY c._id;
 
+SELECT 'agg' as name, count(*) from mig.agg;
+
 -- fast
-CREATE UNLOGGED TABLE mig.bc_array AS
+CREATE TABLE mig.bc_array AS
   SELECT
     mig.agg._id as territory_id,
     mig.agg.ancestors,
@@ -108,47 +122,51 @@ CREATE UNLOGGED TABLE mig.bc_array AS
     (SELECT array_agg(tt.name) FROM territory.territories AS tt WHERE (tt._id = ANY(agg.ancestors) OR tt._id = agg._id) and tt.level = 'towngroup') as towngroup
   FROM mig.agg;
 
-  -- fast
-  INSERT INTO territory.territories_view (
-      _id,
-      active,
-      activable,
-      level,
-      parent,
-      children,
-      ancestors,
-      descendants,
-      insee,
-      postcode,
-      codedep,
-      breadcrumb
-    )
-      SELECT
-        a._id::int,
-        t.active::boolean,
-        t.activable::boolean,
-        t.level::territory.territory_level_enum,
-        (a.ancestors[array_length(a.ancestors, 1)])::int as parent,
-        a.children::int[],
-        a.ancestors::int[],
-        a.descendants::int[],
-        a.insee::varchar[],
-        a.postcode::varchar[],
-        a.codedep::varchar[]
-        ,row(
-            bc.country[1],
-            bc.countrygroup[1],
-            bc.district[1],
-            bc.megalopolis[1],
-            bc.other[1],
-            bc.region[1],
-            bc.state[1],
-            bc.town[1],
-            bc.towngroup[1]
-        )::territory.breadcrumb as breadcrumb
+SELECT 'bc_array' as name, count(*) from mig.bc_array;
 
-    FROM mig.agg AS a
-    LEFT JOIN territory.territories AS t ON t._id = a._id
-    LEFT JOIN mig.bc_array AS bc ON bc.territory_id = a._id;
+-- fast
+INSERT INTO territory.territories_view (
+    _id,
+    active,
+    activable,
+    level,
+    parent,
+    children,
+    ancestors,
+    descendants,
+    insee,
+    postcode,
+    codedep,
+    breadcrumb
+  )
+    SELECT
+      a._id::int,
+      t.active::boolean,
+      t.activable::boolean,
+      t.level::territory.territory_level_enum,
+      (a.ancestors[array_length(a.ancestors, 1)])::int as parent,
+      a.children::int[],
+      a.ancestors::int[],
+      a.descendants::int[],
+      a.insee::varchar[],
+      a.postcode::varchar[],
+      a.codedep::varchar[]
+      ,row(
+          bc.country[1],
+          bc.countrygroup[1],
+          bc.district[1],
+          bc.megalopolis[1],
+          bc.other[1],
+          bc.region[1],
+          bc.state[1],
+          bc.town[1],
+          bc.towngroup[1]
+      )::territory.breadcrumb as breadcrumb
+
+  FROM mig.agg AS a
+  LEFT JOIN territory.territories AS t ON t._id = a._id
+  LEFT JOIN mig.bc_array AS bc ON bc.territory_id = a._id;
+
+SELECT 'view' as name, count(*) from territory.territories_view;
 
 DROP SCHEMA mig CASCADE;
