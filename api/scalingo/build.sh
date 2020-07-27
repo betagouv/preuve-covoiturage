22a7d6a72f0a0e4cb89cad1c19dd39aa19e5ba3b
Jonathan Fallon <jonathan@dotify.fr>
2020-07-28 00:25:41 +0200

    build test

--- api/scalingo/build.sh
+++ api/scalingo/build.sh
@@ -3,13 +3,17 @@
 yarn global add db-migrate db-migrate-pg
 
 npx lerna run --sort build
+
+echo -n '[app] Clean up node_modules'
 find . -type d -name node_modules -exec rm -rf {} \;  2>/dev/null
 
+echo -n '[app] Re-install dependencies'
 unset PUPPETEER_SKIP_CHROMIUM_DOWNLOAD
 yarn
 
 
 # clean up
+echo -n '[app] Clean up dev files'
 find . -type f -name "*.js.map" -delete
 find . -type f -name "*.d.ts" -delete
 
