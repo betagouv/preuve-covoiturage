b80b4f65b84e47e5bc0d770fb9cceb421b65b0c2
Jonathan Fallon <jonathan@dotify.fr>
2020-07-27 12:41:39 +0200

    fetch territory descendants in trip:list

--- api/services/trip/src/actions/ListAction.ts
+++ api/services/trip/src/actions/ListAction.ts
@@ -1,5 +1,6 @@
+import { get, set } from 'lodash';
 import { Action } from '@ilos/core';
-import { handler, ContextType } from '@ilos/common';
+import { handler, ContextType, KernelInterfaceResolver } from '@ilos/common';
 
 import { handlerConfig, ParamsInterface, ResultInterface } from '../shared/trip/list.contract';
 import { TripRepositoryProvider } from '../providers/TripRepositoryProvider';
@@ -15,16 +16,16 @@ import { alias } from '../shared/trip/list.schema';
         ['trip.list'],
         [
           (params, context): string => {
-            if (
-              'territory_id' in params &&
-              context.call.user.territory_id &&
-              context.call.user.authorizedTerritories.length &&
-              params.territory_id.length &&
-              params.territory_id.filter((id: number) => !context.call.user.authorizedTerritories.includes(id))
-                .length === 0
-            ) {
-              return 'territory.trip.list';
-            }
+            // if (
+            //   'territory_id' in params &&
+            //   context.call.user.territory_id &&
+            //   context.call.user.authorizedTerritories.length &&
+            //   params.territory_id.length &&
+            //   params.territory_id.filter((id: number) => !context.call.user.authorizedTerritories.includes(id))
+            //     .length === 0
+            // ) {
+            return 'territory.trip.list';
+            // }
           },
           (params, context): string => {
             if (
@@ -42,11 +43,23 @@ import { alias } from '../shared/trip/list.schema';
   ],
 })
 export class ListAction extends Action {
-  constructor(private pg: TripRepositoryProvider) {
+  constructor(private pg: TripRepositoryProvider, private kernel: KernelInterfaceResolver) {
     super();
   }
 
   public async handle(params: ParamsInterface, context: ContextType): Promise<ResultInterface> {
+    // handle territories
+    if ('territory_id' in params) {
+      const response = await this.kernel.call(
+        'territory:getParentChildren',
+        { _id: get(context, 'call.user.territory_id', null) },
+        { call: { user: { permissions: ['territory.read'] } }, channel: { service: 'stats' } },
+      );
+
+      // merge all territory_id together
+      set(params, 'territory_id', [...params.territory_id, ...(response.descendant_ids || [])]);
+    }
+
     // get visible operators from user context
     const result = await this.pg.search(params);
 
