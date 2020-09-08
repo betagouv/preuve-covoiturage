31d613205f26316142ed6f0fe07021eac5be80b3
Jonathan Fallon <jonathan@dotify.fr>
2020-09-08 18:46:47 +0200

    set .env

--- api/proxy/src/config/sentry.ts
+++ api/proxy/src/config/sentry.ts
@@ -2,4 +2,4 @@ import { env } from '@ilos/core';
 
 export const dsn = env('APP_SENTRY_DSN', null);
 export const environment = env('APP_SENTRY_ENV', env('NODE_ENV', 'local'));
-export const version = env('DEPLOY_GIT_REF', new Date().toISOString());
+export const version = env('APP_VERSION', new Date().toISOString());
--- api/scalingo/build.sh
+++ api/scalingo/build.sh
@@ -14,4 +14,8 @@ find . -type f -name "*.d.ts" -delete
 
 echo -n "[app] size: "; du -ch | tail -n 1
 
-# echo "APP_VERSION=$DEPLOY_GIT_REF" > .env
+# set the application version from git commit number
+# git and .git folder are not available at this building stage
+# a more readable value would be $(git describe --tags)
+echo "[app] version: $DEPLOY_GIT_REF"
+echo "APP_VERSION=$DEPLOY_GIT_REF" > .env
