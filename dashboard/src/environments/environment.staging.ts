5de8e56609fa3295938900ae617bcc51b495c32b
Gilles Boisson <gilles@dotify.fr>
2020-09-18 17:30:40 +0200

    add Beta gouv sentry DSN to non local environment

--- dashboard/src/environments/environment.staging.ts
+++ dashboard/src/environments/environment.staging.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   name: 'staging',
   apiUrl: 'https://api-staging.covoiturage.beta.gouv.fr/',
-  sentryDSN: 'https://681044a14bfe4370903b2700a8121883@sentry.data.gouv.fr/56',
+  sentryDSN: 'https://7701d6142633431a9acecd5a7047be2c@sentry.data.gouv.fr/58',
 };
