d345d20d5cde107ae4442fcbb70e6081aeac16c6
Jonathan Fallon <jonathan@dotify.fr>
2020-09-21 15:07:33 +0200

    fix

--- dashboard/src/environments/environment.ts
+++ dashboard/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   name: 'local',
-  apiUrl: 'http://dev.rpc.fr/',
-  sentryDSN: 'https://f94f483dba804c47977bb96e74644d87@sentry.dotify.eu/22',
+  apiUrl: 'http://localhost:8080/',
+  sentryDSN: 'https://7701d6142633431a9acecd5a7047be2c@sentry.data.gouv.fr/58',
 };
--- dashboard/src/main.ts
+++ dashboard/src/main.ts
@@ -2,13 +2,10 @@ import 'hammerjs';
 import * as moment from 'moment';
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { Sentry } from '@sentry/angular';
 
 import { AppModule } from '~/app.module';
 
 import { environment } from './environments/environment';
-
-Sentry.init({ dsn: 'https://57ba75d39edb43139f837ee75b877d06@sentry.data.gouv.fr/27' });
 
 if (environment.production) {
   enableProdMode();
