e18e847414dfbb49ebf36a1dc9f1e4d9e137a1e7
Jonathan Fallon <jonathan@dotify.fr>
2020-09-22 14:50:08 +0200

    add demo environment and auto-deploy

---
+++ dashboard/src/environments/environment.demo.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  name: 'demo',
+  apiUrl: 'https://api-demo.covoiturage.beta.gouv.fr/',
+  sentryDSN: 'https://7701d6142633431a9acecd5a7047be2c@sentry.data.gouv.fr/58',
+};
