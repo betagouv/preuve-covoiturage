683d774c42cc5f76ad590317a05985eefa33a841
Jonathan Fallon <jonathan@dotify.fr>
2020-10-02 17:00:57 +0200

    prefix rate-limiter stores

--- api/proxy/src/middlewares/rateLimiter.ts
+++ api/proxy/src/middlewares/rateLimiter.ts
@@ -1,42 +1,64 @@
-import rateLimit, { Store, RateLimit, Message } from 'express-rate-limit';
+import rateLimit, { Store, RateLimit } from 'express-rate-limit';
 import RedisStore from 'rate-limit-redis';
 
 type RateLimiterOptions = Partial<{
   store: Store;
   max: number;
   windowMs: number;
-  message: string | Buffer | Message;
 }>;
 
-export function rateLimiter(opts: RateLimiterOptions = {}): RateLimit {
+export function rateLimiter(opts: RateLimiterOptions = {}, prefix = 'rl'): RateLimit {
   return rateLimit({
     store: new RedisStore({
+      prefix,
       redisURL: process.env.APP_REDIS_URL,
     }),
     windowMs: 5 * 60000,
     max: 100,
-    message: {
-      status: 429,
-      message: 'Too many requests, please try again later',
+    handler(req, res) {
+      res.status(429).json({
+        error: {
+          code: 429,
+          message: 'Too many requests',
+          ...req.rateLimit,
+        },
+      });
     },
     ...opts,
   });
 }
 
 // shortcut for authentication routes
+export function loginRateLimiter(opts: RateLimiterOptions = {}): RateLimit {
+  return rateLimiter(
+    {
+      windowMs: 60000,
+      max: 5,
+      ...opts,
+    },
+    'rl-login',
+  );
+}
+
 export function authRateLimiter(opts: RateLimiterOptions = {}): RateLimit {
-  return rateLimiter({
-    windowMs: 60000,
-    max: 10,
-    ...opts,
-  });
+  return rateLimiter(
+    {
+      windowMs: 60000,
+      max: 100,
+      ...opts,
+    },
+    'rl-auth',
+  );
 }
 
 // shortcut for api routes
 export function apiRateLimiter(opts: RateLimiterOptions = {}): RateLimit {
-  return rateLimiter({
-    windowMs: 5 * 60000,
-    max: 5 * 200,
-    ...opts,
-  });
+  return rateLimiter(
+    {
+      windowMs: 5 * 60000,
+      max: 5 * 200,
+      ...opts,
+    },
+    'rl-api',
+  );
 }
