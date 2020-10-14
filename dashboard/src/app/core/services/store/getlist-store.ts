ce04ef40d586d1507e4c6b839e6c147eb8d2b941
Gilles Boisson <gilles@dotify.fr>
2020-10-14 15:00:10 +0200

    front / global debounce on store.load-list

--- dashboard/src/app/core/services/store/getlist-store.ts
+++ dashboard/src/app/core/services/store/getlist-store.ts
@@ -40,7 +40,7 @@ export abstract class GetListStore<
   protected _pagination$ = this.paginationSubject.asObservable();
   protected _entity$ = this.entitySubject.asObservable();
   protected _loadCount = 0;
-
+  protected __debounceTimeId = 0;
   // filter subject
   protected _filterSubject = new BehaviorSubject<any>(null);
 
@@ -90,24 +90,31 @@ export abstract class GetListStore<
     this.entitySubject.next(null);
   }
 
-  loadList(): void {
-    if (!this.dismissGetSubject) return;
-    this.dismissGetSubject.next();
-    this.dismissGetListSubject.next();
-    this._loadCount += 1;
-    this.rpcGetList
-      .getList(this.filterSubject.value)
-      .pipe(
-        takeUntil(this.dismissGetListSubject),
-        finalize(() => {
-          if (this._loadCount > 0) this._loadCount -= 1;
-        }),
-      )
-      .subscribe((list) => {
-        this.entitiesSubject.next(list.data);
+  loadList(debounce = 300): void {
+    clearTimeout(this.__debounceTimeId);
+    if (debounce > 0) {
+      this.__debounceTimeId = setTimeout(() => {
+        this.loadList(0);
+      }, debounce) as any;
+    } else {
+      if (!this.dismissGetSubject) return;
+      this.dismissGetSubject.next();
+      this.dismissGetListSubject.next();
+      this._loadCount += 1;
+      this.rpcGetList
+        .getList(this.filterSubject.value)
+        .pipe(
+          takeUntil(this.dismissGetListSubject),
+          finalize(() => {
+            if (this._loadCount > 0) this._loadCount -= 1;
+          }),
+        )
+        .subscribe((list) => {
+          this.entitiesSubject.next(list.data);
 
-        this.paginationSubject.next(list.meta && list.meta.pagination ? list.meta.pagination : defaultPagination());
-      });
+          this.paginationSubject.next(list.meta && list.meta.pagination ? list.meta.pagination : defaultPagination());
+        });
+    }
   }
 
   dismissAllRpcActions(): void {
