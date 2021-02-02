cb604743835bca03a8a5de9dbb897d13571cfc71
Jonathan Fallon <jonathan@dotify.fr>
2021-02-02 20:54:01 +0100

    fix ApiGraphTimeMode import

--- api/services/trip/src/providers/TripRepositoryProvider.ts
+++ api/services/trip/src/providers/TripRepositoryProvider.ts
@@ -8,14 +8,18 @@ import { PostgresConnection, Cursor } from '@ilos/connection-postgres';
 import {
   TripSearchInterfaceWithPagination,
   TripSearchInterface,
-  ApiGraphTimeMode,
 } from '../shared/trip/common/interfaces/TripSearchInterface';
+import { ApiGraphTimeMode } from '../shared/trip/common/interfaces/ApiGraphTimeMode';
+import { ResultWithPagination } from '../shared/common/interfaces/ResultWithPagination';
+import { StatInterface, FinancialStatInterface } from '../interfaces/StatInterface';
+import { TripStatInterface } from '../shared/trip/common/interfaces/TripStatInterface';
 import {
   TzResultInterface,
   ExportTripInterface,
   TripRepositoryInterface,
   TripRepositoryProviderInterfaceResolver,
 } from '../interfaces';
+
 
 import { ResultWithPagination } from '../shared/common/interfaces/ResultWithPagination';
 import { StatInterface, FinancialStatInterface } from '../interfaces/StatInterface';
