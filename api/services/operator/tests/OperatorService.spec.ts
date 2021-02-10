a066a53d0b2b641e02eb797d3f8583a3a7b422cf
Nicolas Mérigot <nicolas.merigot@beta.gouv.fr>
2021-02-10 16:42:08 +0100

    fix(operator): switch test from mocha to ava

--- api/services/operator/tests/OperatorService.spec.ts
+++ api/services/operator/tests/OperatorService.spec.ts
@@ -1,177 +1,98 @@
-import supertest from 'supertest';
-import path from 'path';
-import chai from 'chai';
-import chaiAsPromised from 'chai-as-promised';
-import { describe } from 'mocha';
+import anyTest from 'ava';
+import { httpMacro } from '@pdc/helper-test';
 
 import { bootstrap } from '../src/bootstrap';
+import { ContextType } from '@ilos/common';
 
-chai.use(chaiAsPromised);
-const { expect } = chai;
+interface TestContext {
+  _id: number;
+}
+const { test } = httpMacro<TestContext>(anyTest, () => bootstrap.boot('http', 0));
 
-describe('Operator service', () => {
-  let transport;
-  let request;
-
-  const callFactory = (method: string, data: any, permissions: string[]): object => ({
-    method,
-    id: 1,
-    jsonrpc: '2.0',
-    params: {
-      params: data,
-      _context: {
-        channel: {
-          service: 'proxy',
-          transport: 'http',
-        },
-        call: {
-          user: {
-            permissions,
-          },
-        },
+function contextFactory(permissions: string[]): ContextType {
+  return {
+    channel: {
+      service: 'proxy',
+      transport: 'http',
+    },
+    call: {
+      user: {
+        permissions,
       },
     },
-  });
+  };
+}
 
-  before(async () => {
-    const configDir = process.env.APP_CONFIG_DIR ? process.env.APP_CONFIG_DIR : './config';
-    process.env.APP_CONFIG_DIR = path.join('..', 'dist', configDir);
+test.serial('Fails on wrong permissions', async (t) => {
+  const result = await t.context.request(
+    'operator:create',
+    {
+      name: 'Toto',
+      legal_name: 'Toto inc.',
+    },
+    contextFactory(['wrong.permission']),
+  );
+  t.true('error' in result);
+  t.is(result.error.data, 'Invalid permissions');
+});
 
-    transport = await bootstrap.boot('http', 0);
-    request = supertest(transport.getInstance());
-  });
+test.serial('Create an operator', async (t) => {
+  const result = await t.context.request(
+    'operator:create',
+    {
+      name: 'Toto',
+      legal_name: 'Toto inc.',
+      siret: `${String(Math.random() * Math.pow(10, 16)).substr(0, 14)}`,
+    },
+    contextFactory(['operator.create']),
+  );
+  t.true('_id' in result);
+  t.is(result.name, 'Toto');
+  t.is(result.legal_name, 'Toto inc.');
+  t.context._id = result._id;
+});
 
-  after(async () => {
-    await transport.down();
-  });
+test.serial('Find an operator', async (t) => {
+  const result = await t.context.request(
+    'operator:find',
+    {
+      _id: t.context._id,
+    },
+    contextFactory(['operator.read']),
+  );
+  t.true('_id' in result);
+  t.is(result.name, 'Toto');
+  t.is(result.legal_name, 'Toto inc.');
+});
 
-  it('Fails on wrong permissions', () => {
-    return request
-      .post('/')
-      .send(
-        callFactory(
-          'operator:create',
-          {
-            name: 'Toto',
-            legal_name: 'Toto inc.',
-          },
-          ['wrong.permission'],
-        ),
-      )
-      .set('Accept', 'application/json')
-      .set('Content-Type', 'application/json')
-      .expect((response: supertest.Response) => {
-        expect(response.status).to.equal(403);
-        expect(response.body).to.have.property('error');
-        expect(response.body.error.data).to.eq('Invalid permissions');
-      });
-  });
+test.serial('Update the operator', async (t) => {
+  const result = await t.context.request(
+    'operator:update',
+    {
+      _id: t.context._id,
+      name: 'Yop',
+    },
+    contextFactory(['operator.update']),
+  );
+  t.true('_id' in result);
+  t.is(result.name, 'Yop');
+  t.is(result.legal_name, 'Toto inc.');
+});
 
-  // id returned by database
-  let _id: number;
+test.serial('List all operators', async (t) => {
+  const result = await t.context.request('operator:list', {}, contextFactory(['operator.list']));
+  t.true(Array.isArray(result));
+  const operator = result.filter((r) => r._id === t.context._id);
+  t.is(operator.length, 1);
+  t.is(operator[0]._id, t.context._id);
+  t.is(operator[0].name, 'Yop');
+});
 
-  it('Create an operator', () => {
-    return request
-      .post('/')
-      .send(
-        callFactory(
-          'operator:create',
-          {
-            name: 'Toto',
-            legal_name: 'Toto inc.',
-            siret: `${String(Math.random() * Math.pow(10, 16)).substr(0, 14)}`,
-          },
-          ['operator.create'],
-        ),
-      )
-      .set('Accept', 'application/json')
-      .set('Content-Type', 'application/json')
-      .expect((response: supertest.Response) => {
-        expect(response.status).to.equal(200);
-        expect(response.body).to.have.property('result');
-        expect(response.body.result).to.have.property('_id');
-        expect(response.body.result).to.have.property('name', 'Toto');
-        expect(response.body.result).to.have.property('legal_name', 'Toto inc.');
-
-        // store the _id
-        _id = response.body.result._id;
-      });
-  });
-
-  it('Find an operator', () => {
-    return request
-      .post('/')
-      .send(
-        callFactory(
-          'operator:find',
-          {
-            _id,
-          },
-          ['operator.read'],
-        ),
-      )
-      .set('Accept', 'application/json')
-      .set('Content-Type', 'application/json')
-      .expect((response: supertest.Response) => {
-        expect(response.status).to.equal(200);
-        expect(response.body).to.have.property('result');
-        expect(response.body.result).to.have.property('_id');
-        expect(response.body.result).to.have.property('name', 'Toto');
-        expect(response.body.result).to.have.property('legal_name', 'Toto inc.');
-      });
-  });
-  it('Update the operator', () => {
-    return request
-      .post('/')
-      .send(
-        callFactory(
-          'operator:update',
-          {
-            _id,
-            name: 'Yop',
-          },
-          ['operator.update'],
-        ),
-      )
-      .set('Accept', 'application/json')
-      .set('Content-Type', 'application/json')
-      .expect((response: supertest.Response) => {
-        expect(response.status).to.equal(200);
-        expect(response.body).to.have.property('result');
-        expect(response.body.result).to.have.property('_id', _id);
-        expect(response.body.result).to.have.property('name', 'Yop');
-        expect(response.body.result).to.have.property('legal_name', 'Toto inc.');
-      });
-  });
-
-  it('List all operators', () => {
-    return request
-      .post('/')
-      .send(callFactory('operator:list', {}, ['operator.list']))
-      .set('Accept', 'application/json')
-      .set('Content-Type', 'application/json')
-      .expect((response: supertest.Response) => {
-        expect(response.status).to.equal(200);
-        expect(response.body).to.have.property('result');
-
-        const results = [...response.body.result.filter((r) => r._id === _id)];
-        expect(results.length).to.eq(1);
-        expect(results[0]).to.have.property('_id', _id);
-        expect(results[0]).to.have.property('name', 'Yop');
-        expect(results[0]).to.have.property('legal_name', 'Toto inc.');
-        expect(results[0]).not.to.have.property('bank');
-        expect(results[0]).not.to.have.property('contacts');
-      });
-  });
-
-  it('Delete the operator', () => {
-    return request
-      .post('/')
-      .send(callFactory('operator:delete', { _id }, ['operator.delete']))
-      .set('Accept', 'application/json')
-      .set('Content-Type', 'application/json')
-      .expect((response: supertest.Response) => {
-        expect(response.status).to.equal(200);
-      });
-  });
+test.serial('Delete the operator', async (t) => {
+  const result = await t.context.request(
+    'operator:delete',
+    { _id: t.context._id },
+    contextFactory(['operator.delete']),
+  );
+  t.is(result, null);
 });
