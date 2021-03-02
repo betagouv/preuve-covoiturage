17a1b51b10699db16f1ba28ff0c6aee39e5de254
Nicolas Mérigot <nicolas.merigot@beta.gouv.fr>
2021-03-02 13:29:04 +0100

    test: add get credentials helper

--- tests/cypress/support/parameters/userType.ts
+++ tests/cypress/support/parameters/userType.ts
@@ -1,17 +1,8 @@
 import { defineParameterType } from 'cypress-cucumber-preprocessor/steps';
-
-const usersCredentials = new Map([
-  [
-    'administrateur du registre',
-    {
-      login: process.env.REGISTRY_ADMIN_USERNAME ?? 'admin@example.com',
-      password: process.env.REGISTRY_ADMIN_PASSWORD ?? 'admin1234',
-    },
-  ],
-]);
+import { usersCredentials, getUserCredentials } from '../helpers/getUserCredentials';
 
 defineParameterType({
   name: 'userType',
   regexp: new RegExp(Array.from(usersCredentials.keys()).join('|')),
-  transformer: (userType) => usersCredentials.get(userType),
+  transformer: (userType) => getUserCredentials(userType),
 });
