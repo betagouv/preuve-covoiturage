d944baefed78ac4a55939d7737ff41cd52878bbd
Nicolas Mérigot <nicolas.merigot@beta.gouv.fr>
2021-03-04 10:35:02 +0100

    dashboard: remove cypress

--- dashboard/cypress/support/reusables/auth/cypress_logout.ts
+++
@@ -1,9 +0,0 @@
-import { closeNotification } from '../notification.cypress';
-
-export function cypress_logout() {
-  it('clicks on logout', () => {
-    cy.get('.Header-user').click();
-    cy.get('.mat-menu-item:nth-child(2)').click();
-  });
-  closeNotification();
-}
