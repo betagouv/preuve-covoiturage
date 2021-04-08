2ef9eba31b644f215953d12eedcd3233d6fe2ad1
Jonathan Fallon <jonathan.fallon@beta.gouv.fr>
2021-04-08 15:25:27 +0200

    Update tokenMacro.ts

--- api/providers/validator/src/keywords/macros/tokenMacro.ts
+++ api/providers/validator/src/keywords/macros/tokenMacro.ts
@@ -1,9 +1,8 @@
-export function tokenMacro(): { type: string; minLength: number; maxLength: number; sanitize: boolean; trim: boolean } {
+export function tokenMacro(): { type: string; minLength: number; maxLength: number; trim: boolean } {
   return {
     type: 'string',
     minLength: 32,
     maxLength: 64,
-    sanitize: true,
     trim: true,
   };
 }
