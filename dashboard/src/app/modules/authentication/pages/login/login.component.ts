893b217c93896b46fd9f6a5e62dab8f53fda0db4
Jonathan Fallon <jonathan@dotify.fr>
2021-05-03 16:42:59 +0200

    #1292 add error message on wrong credentials

--- dashboard/src/app/modules/authentication/pages/login/login.component.ts
+++ dashboard/src/app/modules/authentication/pages/login/login.component.ts
@@ -1,4 +1,6 @@
+import { throwError } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
+import { ToastrService } from 'ngx-toastr';
 
 import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
@@ -6,6 +8,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 import { AuthenticationService } from '~/core/services/authentication/authentication.service';
 import { DestroyObservable } from '~/core/components/destroy-observable';
+import { catchHttpStatus } from '~/core/operators/catchHttpStatus';
 
 @Component({
   selector: 'app-login',
@@ -16,7 +19,12 @@ export class LoginComponent extends DestroyObservable implements OnInit {
   public loginForm: FormGroup;
   public passwordType = 'password';
 
-  constructor(private fb: FormBuilder, private authService: AuthenticationService, private router: Router) {
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthenticationService,
+    private router: Router,
+    private toaster: ToastrService,
+  ) {
     super();
   }
 
@@ -32,7 +40,13 @@ export class LoginComponent extends DestroyObservable implements OnInit {
 
     this.authService
       .login(email, password)
-      .pipe(takeUntil(this.destroy$))
+      .pipe(
+        takeUntil(this.destroy$),
+        catchHttpStatus(403, (e) => {
+          this.toaster.error('Mauvais email ou mot de passe', 'Erreur de connexion');
+          return throwError(e);
+        }),
+      )
       .subscribe(() => {
         this.router.navigate(['/']);
       });
