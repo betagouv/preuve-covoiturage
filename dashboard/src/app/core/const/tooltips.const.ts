c4d1d1e679d3fc81b3c904eee333782d222ac8f2
Jonathan Fallon <jonathan@dotify.fr>
2021-05-04 15:37:02 +0200

    add tooltip to rank in trip list #1206

---
+++ dashboard/src/app/core/const/tooltips.const.ts
@@ -0,0 +1,8 @@
+export const TOOLTIPS = {
+  classes: {
+    A: 'Classe A : l’opérateur certifie la mise en relation avec intention de covoiturer',
+    B: 'Classe B : l’opérateur certifie la mise en relation et le trajet d’un occupant du véhicule',
+    C:
+      'Classe C : l’opérateur certifie la mise en relation, les trajets des occupants du véhicule et une identité distincte des occupants',
+  },
+};
