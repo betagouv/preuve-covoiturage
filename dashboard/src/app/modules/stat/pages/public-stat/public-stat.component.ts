ad05bd0765b2f45f678c31a1fb2d8965220a2201
Julien Parmentier <julien.parmentier@beta.gouv.fr>
2021-05-20 10:09:09 +0200

    fix: public stat view style, chart and githook link, add missing titles

--- dashboard/src/app/modules/stat/pages/public-stat/public-stat.component.ts
+++ dashboard/src/app/modules/stat/pages/public-stat/public-stat.component.ts
@@ -1,56 +1,29 @@
-import { get } from 'lodash-es';
-import { Component, OnInit } from '@angular/core';
-import { takeUntil } from 'rxjs/operators';
+import { get } from "lodash-es";
+import { Component, OnInit, Input } from "@angular/core";
+import { takeUntil } from "rxjs/operators";
 
-import { StatNumber } from '~/core/entities/stat/statNumber';
-import { statNumbers } from '~/modules/stat/config/statNumbers';
-import { URLS } from '~/core/const/main.const';
-import { PUBLIC_STATS } from '~/modules/stat/config/stat';
-import { StatPublicService } from '~/modules/stat/services/stat-public.service';
-import { DestroyObservable } from '~/core/components/destroy-observable';
+import { StatNavName } from "~/core/types/stat/statDataNameType";
+import { StatNumber } from "~/core/entities/stat/statNumber";
+import { statNumbers } from "~/modules/stat/config/statNumbers";
+import { URLS } from "~/core/const/main.const";
+import { PUBLIC_STATS } from "~/modules/stat/config/stat";
+import { StatPublicService } from "~/modules/stat/services/stat-public.service";
+import { DestroyObservable } from "~/core/components/destroy-observable";
 
 @Component({
-  selector: 'app-public-stat',
-  templateUrl: './public-stat.component.html',
-  styleUrls: ['./public-stat.component.scss'],
+  selector: "app-public-stat",
+  templateUrl: "./public-stat.component.html",
+  styleUrls: ["./public-stat.component.scss"],
 })
-export class PublicStatComponent extends DestroyObservable implements OnInit {
-  statNumbers: StatNumber[] = [];
+export class PublicStatComponent extends DestroyObservable {
   gitbookLinkStats = URLS.gitbookLinkStats;
 
-  statNumberNames = PUBLIC_STATS;
+  @Input() statsList = PUBLIC_STATS;
 
   hideStats = false;
 
   constructor(private publicStatService: StatPublicService) {
     super();
-  }
-
-  ngOnInit(): void {
-    this.loadStat();
-    this.publicStatService.stat$.pipe(takeUntil(this.destroy$)).subscribe(() => {
-      const statNumbersArray = [];
-      for (const statName of this.statNumberNames) {
-        let title = get(this.publicStatService.stat, statNumbers[statName].path);
-        const statCard = statNumbers[statName];
-
-        if (statName === 'co2') {
-          title = (title / 1000).toFixed(2);
-          statCard.unit = 'tonnes';
-        }
-
-        statNumbersArray.push(
-          new StatNumber({
-            title,
-            hint: statCard.hint,
-            svgIcon: statCard.svgIcon,
-            unit: statCard.unit,
-          }),
-        );
-      }
-
-      this.statNumbers = statNumbersArray;
-    });
   }
 
   get loading(): boolean {
@@ -59,12 +32,5 @@ export class PublicStatComponent extends DestroyObservable implements OnInit {
 
   get loaded(): boolean {
     return this.publicStatService.loaded;
-  }
-
-  private loadStat(): void {
-    if (this.publicStatService.loading) {
-      return;
-    }
-    this.publicStatService.loadOne().subscribe();
   }
 }
