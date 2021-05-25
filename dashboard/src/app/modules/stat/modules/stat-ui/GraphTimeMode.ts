1675926c5fc57e2daff70d168a76415da1be4e17
Julien Parmentier <julien.parmentier@beta.gouv.fr>
2021-05-25 09:42:17 +0200

    fix: GraphTimeMode lower case for middle words

--- dashboard/src/app/modules/stat/modules/stat-ui/GraphTimeMode.ts
+++ dashboard/src/app/modules/stat/modules/stat-ui/GraphTimeMode.ts
@@ -1,17 +1,13 @@
 export enum GraphTimeMode {
-  Month = "month",
-  Day = "day",
-  Cumulative = "cumulative",
+  Month = 'month',
+  Day = 'day',
+  Cumulative = 'cumulative',
 }
 
 export const GraphTimeModeLabel: { [key: string]: string } = {
-  [GraphTimeMode.Cumulative]: "Cumulée",
-  [GraphTimeMode.Day]: "Par jour",
-  [GraphTimeMode.Month]: "Par mois",
+  [GraphTimeMode.Cumulative]: 'Cumulée',
+  [GraphTimeMode.Day]: 'par jour',
+  [GraphTimeMode.Month]: 'par mois',
 };
 
-export const GraphTimeModeNavList: GraphTimeMode[] = [
-  GraphTimeMode.Cumulative,
-  GraphTimeMode.Day,
-  GraphTimeMode.Month,
-];
+export const GraphTimeModeNavList: GraphTimeMode[] = [GraphTimeMode.Cumulative, GraphTimeMode.Day, GraphTimeMode.Month];
