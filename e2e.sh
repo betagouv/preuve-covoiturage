53314638ab32aa0302387460de394dc44e06c53e
Nicolas Mérigot <nicolas.merigot@beta.gouv.fr>
2021-05-27 14:01:40 +0200

    fix: add a wait container

--- e2e.sh
+++ e2e.sh
@@ -1,26 +1,44 @@
+DC="$(which docker-compose) -f docker-compose.e2e.yml --env-file e2e.env"
+
+rebuild() {
+  $DC build api
+  $DC build dashboard
+}
+
 start_services() {
-  docker-compose -f docker-compose.e2e.yml --env-file e2e.env up -d s3 postgres redis smtp
+  $DC up -d s3 postgres redis smtp
 }
 
 start_app() {
-  docker-compose -f docker-compose.e2e.yml --env-file e2e.env up -d api dashboard
+  $DC up -d api dashboard
+}
+
+wait_for_app() {
+  $DC run wait
 }
 
 seed_data() {
-  docker-compose -f docker-compose.e2e.yml --env-file e2e.env run api yarn workspace @pdc/proxy ilos seed
+  $DC run api yarn workspace @pdc/proxy ilos seed
 }
 
 create_bucket() {
-  docker-compose -f docker-compose.e2e.yml --env-file e2e.env run -e BUCKET=$1 s3-init
+  $DC run -e BUCKET=$1 s3-init
 }
 
 e2e() {
   mkdir -p /tmp/cypress/videos
-  docker-compose -f docker-compose.e2e.yml --env-file e2e.env run cypress
+  $DC run cypress
 }
 
 stop() {
-  docker-compose -f docker-compose.e2e.yml --env-file e2e.env down -v
+  $DC down -v
 }
 
-start_services && seed_data && create_bucket local && start_app && e2e
+if [ "$1" = "rebuild" ]; then
+  rebuild
+fi
+
+start_services && seed_data && create_bucket local && start_app && wait_for_app && e2e 2> /dev/null
+EXIT=$?
+stop
+exit $EXIT
