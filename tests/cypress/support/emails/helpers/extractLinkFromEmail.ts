72cc421850da42695a5fc544c63c38dbd40a0067
Jonathan Fallon <jonathan@dotify.fr>
2021-07-01 13:38:26 +0200

    fix e2e network and regex

--- tests/cypress/support/emails/helpers/extractLinkFromEmail.ts
+++ tests/cypress/support/emails/helpers/extractLinkFromEmail.ts
@@ -1,17 +1,17 @@
 import { escapeRegExp } from './escapeRegExp';
 
 export function extractLinkFromEmail(baseUrl: string, emailText: string): string {
-  if (!emailText) {
-    throw new Error(`Invalid email payload (missing text)`);
+  if (!emailText || !emailText.length) {
+    throw new Error(`Invalid email payload (missing text): ${emailText}`);
   }
 
   const regstr = `\\\[(${escapeRegExp(baseUrl)}([^\\]]*))`;
   const regexp = new RegExp(regstr);
   // remove ugly newlines from email spec
-  const result = regexp.exec(emailText.replace(/\=(20)?[\n\r]/g, ''));
+  const result = regexp.exec(emailText.replace(/\=(20)?[\n\r]+/g, ''));
 
   if (!result) {
-    throw new Error(`URL not found in email body`);
+    throw new Error(`URL not found in email body\n${baseUrl}\n\n\n${emailText}`);
   }
 
   return result[1].replace(/[^a-z0-9-_:;&#\?\/%.]/gi, '');
