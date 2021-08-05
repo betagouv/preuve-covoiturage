f15b81dbbe14a8da659913c76750bbeb99fbbe11
Julien Parmentier <julien.parmentier@beta.gouv.fr>
2021-08-05 20:13:13 +0200

    conf(eslint): vscode configuration for eslint vs code plugin

--- docker/lint/.eslintrc.js
+++ docker/lint/.eslintrc.js
@@ -10,20 +10,22 @@ module.exports = {
   },
   ignorePatterns: ['dist', 'api/providers/qrcode/src/lib/*'], // some JS converted to TS. needs to be cleaned up
   rules: {
-    'semi': ['error', 'always'],
-    'prettier/prettier': ['error',      // /!\ integrating prettier for eslint not working -> https://github.com/prettier/eslint-plugin-prettier
-                          {  
-                            printWidth: 120,
-                            singleQuote: true,
-                            useTabs: false,
-                            tabWidth: 2,
-                            semi: true,
-                            bracketSpacing: true,
-                            arrowParens: 'always',
-                            trailingComma: 'all',
-                            endOfLine: 'lf',
-                            jsxSingleQuote: true
-                          }],
+    semi: ['error', 'always'],
+    'prettier/prettier': [
+      'error', // /!\ integrating prettier for eslint not working -> https://github.com/prettier/eslint-plugin-prettier
+      {
+        printWidth: 120,
+        singleQuote: true,
+        useTabs: false,
+        tabWidth: 2,
+        semi: true,
+        bracketSpacing: true,
+        arrowParens: 'always',
+        trailingComma: 'all',
+        endOfLine: 'lf',
+        jsxSingleQuote: true,
+      },
+    ],
     'max-len': ['warn', { code: 120 }],
     'no-console': ['error', { allow: ['warn', 'error', 'info', 'debug', 'table'] }],
     '@typescript-eslint/no-empty-interface': ['error', { allowSingleExtends: true }],
