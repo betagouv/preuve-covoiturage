5012a1a3f84d9dcdf0bb642418a0a4dd9af75680
Julien Parmentier <julien.parmentier@beta.gouv.fr>
2021-08-05 16:51:55 +0200

    conf(eslint.js): add prettier config since plugin not using prettierrc conf

--- docker/lint/.eslintrc.js
+++ docker/lint/.eslintrc.js
@@ -10,8 +10,20 @@ module.exports = {
   },
   ignorePatterns: ['dist', 'api/providers/qrcode/src/lib/*'], // some JS converted to TS. needs to be cleaned up
   rules: {
-    semi: ['error', 'always'],
-    'prettier/prettier': 'error',
+    'semi': ['error', 'always'],
+    'prettier/prettier': ['error',      // /!\ integrating prettier for eslint not working -> https://github.com/prettier/eslint-plugin-prettier
+                          {  
+                            printWidth: 120,
+                            singleQuote: true,
+                            useTabs: false,
+                            tabWidth: 2,
+                            semi: true,
+                            bracketSpacing: true,
+                            arrowParens: 'always',
+                            trailingComma: 'all',
+                            endOfLine: 'lf',
+                            jsxSingleQuote: true
+                          }],
     'max-len': ['warn', { code: 120 }],
     'no-console': ['error', { allow: ['warn', 'error', 'info', 'debug', 'table'] }],
     '@typescript-eslint/no-empty-interface': ['error', { allowSingleExtends: true }],
