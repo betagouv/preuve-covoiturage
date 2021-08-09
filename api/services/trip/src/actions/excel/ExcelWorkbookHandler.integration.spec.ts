02c0ef351dcc6fbe0fd7a7c2491edfb10023f07c
Julien Parmentier <julien.parmentier@beta.gouv.fr>
2021-08-09 14:31:22 +0200

    fix: fix integration test for excel file and remove data

--- api/services/trip/src/actions/excel/ExcelWorkbookHandler.integration.spec.ts
+++ api/services/trip/src/actions/excel/ExcelWorkbookHandler.integration.spec.ts
@@ -8,12 +8,11 @@ test.before((t) => {
   excelWorkbookHandler = new ExcelWorkbookHandler();
 });
 
-// TODO: order of sheet should not be tested
+// TODO: order of sheets should not be tested
 test('ExcelWorkbookHandler: should load excel template file', async (t) => {
   const workbook: Workbook = await excelWorkbookHandler.loadWorkbookTemplate();
   t.true(workbook.worksheets.length !== 0);
   t.is(workbook.worksheets[0].name, 'data');
-  t.is(workbook.worksheets[1].name, 'Résumé opérateurs');
-  t.is(workbook.worksheets[2].name, 'Résumé jours');
-  t.is(workbook.worksheets[3].name, 'Résumé départements');
+  t.is(workbook.worksheets[1].name, 'Résumé jour');
+  t.is(workbook.worksheets[2].name, 'Contrôle INSEE');
 });
--- api/services/trip/src/commands/BuildExcelExportCommand.ts
+++ api/services/trip/src/commands/BuildExcelExportCommand.ts
@@ -1,6 +1,9 @@
-
 import { command, CommandInterface, CommandOptionType, KernelInterfaceResolver } from '@ilos/common';
-import { ParamsInterface as BuilExcelExportActionInterface, ResultInterface as BuilExcelExportActionInterfaceResultInterface, signature as buildExcelEportSignature } from '../shared/trip/buildExcelExport.contract';
+import {
+  ParamsInterface as BuilExcelExportActionInterface,
+  ResultInterface as BuilExcelExportActionInterfaceResultInterface,
+  signature as buildExcelEportSignature,
+} from '../shared/trip/buildExcelExport.contract';
 
 @command()
 export class BuildExcelExportCommand implements CommandInterface {
@@ -9,7 +12,7 @@ export class BuildExcelExportCommand implements CommandInterface {
   static readonly options: CommandOptionType[] = [
     {
       signature: '-c, --campaign_id <campaign_id>',
-      description: 'Campaign id to make te export for'
+      description: 'Campaign id to make te export for',
     },
     {
       signature: '-t, --territory_id <territory_id>',
@@ -21,11 +24,14 @@ export class BuildExcelExportCommand implements CommandInterface {
 
   public async call(options: { territory_id: number; campaign_id: number }): Promise<void> {
     const { territory_id, campaign_id } = options;
-    const builExcelExportActionInterface: BuilExcelExportActionInterface = { query: { campaign_id: [campaign_id] }, format: { tz : 'Europe/Paris'}};
+    const builExcelExportActionInterface: BuilExcelExportActionInterface = {
+      query: { campaign_id: [campaign_id] },
+      format: { tz: 'Europe/Paris' },
+    };
     await this.kernel.call<BuilExcelExportActionInterface, BuilExcelExportActionInterfaceResultInterface>(
-      buildExcelEportSignature, 
-      builExcelExportActionInterface, 
-        { channel: {service : buildExcelEportSignature} }
-      )
+      buildExcelEportSignature,
+      builExcelExportActionInterface,
+      { channel: { service: buildExcelEportSignature } },
+    );
   }
 }
