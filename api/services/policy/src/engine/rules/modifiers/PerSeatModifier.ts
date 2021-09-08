9a5d3d6701a90405973ecdcda237fc7fa1f32c23
Nicolas Mérigot <nicolas.merigot@beta.gouv.fr>
2021-09-08 10:03:17 +0200

    arch: remove per seat rule

--- api/services/policy/src/engine/rules/modifiers/PerSeatModifier.ts
+++ api/services/policy/src/engine/rules/modifiers/PerSeatModifier.ts
@@ -1,11 +1,12 @@
 import { RuleHandlerContextInterface } from '../../interfaces';
 import { ModifierRule } from '../ModifierRule';
 
+// This rule is deprecated, please migrate to per_passenger_modifier
 export class PerSeatModifier extends ModifierRule {
   static readonly slug: string = 'per_seat_modifier';
   static readonly description: string = 'Le montant est multiplié par le nombre de sièges';
 
   modify(ctx: RuleHandlerContextInterface, result: number): number {
-    return result * (ctx.person.seats || 1);
+    return result;
   }
 }
