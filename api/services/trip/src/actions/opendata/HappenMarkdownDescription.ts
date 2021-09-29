c7381579ef4b750c2cd099c7a9522cfc0f07157a
Julien Parmentier <julien.parmentier@beta.gouv.fr>
2021-09-29 11:32:41 +0200

    refactor: more readable code

--- api/services/trip/src/actions/opendata/HappenMarkdownDescription.ts
+++ api/services/trip/src/actions/opendata/HappenMarkdownDescription.ts
@@ -8,34 +8,27 @@ export class HappenMarkdownDescription {
 
   async call(queryParam: OpenDataTripSearchInterface, description = ''): Promise<string> {
     const total_truncated: string = await (await this.tripRepository.searchCount(queryParam)).count;
-    const intersection: number = queryParam.excluded_start_territory_id.filter((t) =>
-      queryParam.excluded_end_territory_id.includes(t),
-    ).length;
     const start_deleted = queryParam.excluded_start_territory_id.length;
-    console.debug(start_deleted);
     const end_deleted = queryParam.excluded_end_territory_id.length;
+    const total: string = await this.getTotal(queryParam);
+    const deleted = parseInt(total) - parseInt(total_truncated);
+    const intersection = start_deleted + end_deleted - deleted;
+    return description + this.build(total, total_truncated, deleted, start_deleted, end_deleted, intersection);
+  }
+
+  private async getTotal(queryParam: OpenDataTripSearchInterface) {
     const queryParamCopy: OpenDataTripSearchInterface = {
       ...queryParam,
     };
     delete queryParamCopy.excluded_end_territory_id;
     delete queryParamCopy.excluded_start_territory_id;
     const total: string = await (await this.tripRepository.searchCount(queryParamCopy)).count;
-    return (
-      description +
-      this.buildDescription(
-        total,
-        total_truncated,
-        parseInt(total) - parseInt(total_truncated),
-        start_deleted,
-        end_deleted,
-        intersection,
-      )
-    );
+    return total;
   }
 
-  private buildDescription(
+  private build(
     total: string,
-    trunc_total: string,
+    total_truncated: string,
     deleted: number,
     start_delete: number,
     end_deleted: number,
@@ -45,7 +38,7 @@ export class HappenMarkdownDescription {
 Les données concernent également les trajets dont le point de départ OU d'arrivée est situé en dehors du territoire français.
 
 * Nombre trajets collectés et validés par le registre de preuve de covoiturage **${total}**
-* Nombre de trajets exposés dans le jeu de données : **${trunc_total}**
+* Nombre de trajets exposés dans le jeu de données : **${total_truncated}**
 * Nombre de trajets supprimés du jeu de données : **${deleted} = ${start_delete} + ${end_deleted} - ${intersection}**
     * Nombre d’occurrences du code INSEE de départ est < 6 : **${start_delete}**
     * Nombre d’occurrences du code INSEE d'arrivée est < 6 : **${end_deleted}**
