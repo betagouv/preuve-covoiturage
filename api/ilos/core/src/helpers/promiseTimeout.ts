ad7126a4252dd8a47d1994d065652add048ac2cc
Jonathan Fallon <jonathan@dotify.fr>
2021-09-30 16:10:15 +0200

    add logging to kernel timeout handler

--- api/ilos/core/src/helpers/promiseTimeout.ts
+++ api/ilos/core/src/helpers/promiseTimeout.ts
@@ -1,12 +1,26 @@
 import { TimeoutException } from '@ilos/common';
 
-export function promiseTimeout<T>(ms: number, promise: Promise<T>): Promise<T> {
+export function promiseTimeout<T>(ms: number, promise: Promise<T>, signature?: string): Promise<T> {
+  const s = new Date();
+  let id = null;
   const timeout = new Promise((_resolve, reject) => {
-    const id = setTimeout(() => {
+    if (id) clearTimeout(id);
+    id = setTimeout(() => {
       clearTimeout(id);
+      console.debug(`[kernel] ${signature || ''} timeout expired (${ms}ms)`);
       reject(new TimeoutException(`Timeout Exception (${ms}ms)`));
     }, ms);
   });
 
-  return Promise.race([promise, timeout]) as Promise<T>;
+  return Promise.race([promise, timeout])
+    .then((res) => {
+      clearTimeout(id);
+      console.debug(`[kernel] ${signature || ''} succeeded in ${(new Date().getTime() - s.getTime()) / 1000}s`);
+      return res;
+    })
+    .catch((err) => {
+      clearTimeout(id);
+      console.error(`[kernel] ${signature || ''} failed`, { message: err.message });
+      throw err;
+    }) as Promise<T>;
 }
