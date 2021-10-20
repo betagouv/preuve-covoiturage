6de7bd29ab4ad07664452243aa11b6763c59feaf
Julien Parmentier <julien.parmentier@beta.gouv.fr>
2021-10-20 18:25:54 +0200

    feat: push resource description on resource instead of dataset

--- api/services/trip/src/actions/opendata/BuildResourceDescription.spec.ts
+++ api/services/trip/src/actions/opendata/BuildResourceDescription.spec.ts
@@ -1,10 +1,10 @@
-import { OpenDataContextMetadata } from './AppendMarkdownDescription';
 /* eslint-disable max-len */
 import anyTest, { TestInterface } from 'ava';
 import { TripRepositoryProvider } from '../../providers/TripRepositoryProvider';
 import { OpenDataTripSearchInterface } from '../../shared/trip/common/interfaces/TripSearchInterface';
-import { AppendMarkdownDescription } from './AppendMarkdownDescription';
 import sinon, { SinonStub } from 'sinon';
+import { OpenDataContextMetadata } from '../../interfaces/OpenDataContextMetadata';
+import { BuildResourceDescription } from './BuildResourceDescription';
 
 interface Context {
   // Injected tokens
@@ -12,7 +12,7 @@ interface Context {
   // Injected tokens method's stubs
   tripRepositoryProviderStub: SinonStub;
   // Tested token
-  happenMarkdownDescription: AppendMarkdownDescription;
+  happenMarkdownDescription: BuildResourceDescription;
   // Constants
 }
 
@@ -20,16 +20,14 @@ const test = anyTest as TestInterface<Partial<Context>>;
 
 test.beforeEach((t) => {
   t.context.tripRepositoryProvider = new TripRepositoryProvider(null);
-  t.context.happenMarkdownDescription = new AppendMarkdownDescription(t.context.tripRepositoryProvider);
+  t.context.happenMarkdownDescription = new BuildResourceDescription(t.context.tripRepositoryProvider);
   t.context.tripRepositoryProviderStub = sinon.stub(t.context.tripRepositoryProvider, 'searchCount');
 });
 
 test.afterEach((t) => {});
 
-test('AppendMarkdownDescription: should happen description to existing one', async (t) => {
+test('BuildResourceDescription: should happen description to existing one', async (t) => {
   // Arrange
-  const existingDescription =
-    "# Spécificités jeu de données janvier 2021\nLes données concernent également les trajets dont le point de départ OU d'arrivée est situé en dehors du territoire français.\n\n* Nombre trajets collectés et validés par le registre de preuve de covoiturage **96 012**\n* Nombre de trajets exposés dans le jeu de données : **90 443**\n* Nombre de trajets supprimés du jeu de données : **5 569 = 3 103 + 2 998 - 532**\n    * Nombre d’occurrences du code INSEE de départ est < 6 : **3 103**\n    * Nombre d’occurrences du code INSEE d'arrivée est < 6 : **2 998**\n    * Nombre d’occurrences du code INSEE de départ ET d'arrivée est < 6 : **532**";
   t.context.tripRepositoryProviderStub.onCall(0).resolves({ count: '21' });
   t.context.tripRepositoryProviderStub.onCall(1).resolves({ count: '30' });
 
@@ -81,12 +79,12 @@ test('AppendMarkdownDescription: should happen description to existing one', asy
   };
 
   // Act
-  const description: string = await t.context.happenMarkdownDescription.call(openDataContext, existingDescription);
+  const description: string = await t.context.happenMarkdownDescription.call(openDataContext);
 
   // Assert
   const expected_happened_description =
-    "\n\n# Spécificités jeu de données septembre 2021\nLes données concernent également les trajets dont le point de départ OU d'arrivée est situé en dehors du territoire français.\n\n* Nombre trajets collectés et validés par le registre de preuve de covoiturage **30**\n* Nombre de trajets exposés dans le jeu de données : **21**\n* Nombre de trajets supprimés du jeu de données : **9 = 5 + 6 - 2**\n    * Nombre de trajets dont l’occurrence du code INSEE de départ est < 6 : **5**\n    * Nombre de trajets dont l’occurrence du code INSEE d'arrivée est < 6 : **6**\n    * Nombre de trajets dont l’occurrence du code INSEE de départ ET d'arrivée est < 6 : **2**";
-  t.deepEqual(description, `${existingDescription}${expected_happened_description}`);
+    "# Spécificités jeu de données septembre 2021\nLes données concernent également les trajets dont le point de départ OU d'arrivée est situé en dehors du territoire français.\n\n* Nombre trajets collectés et validés par le registre de preuve de covoiturage **30**\n* Nombre de trajets exposés dans le jeu de données : **21**\n* Nombre de trajets supprimés du jeu de données : **9 = 5 + 6 - 2**\n    * Nombre de trajets dont l’occurrence du code INSEE de départ est < 6 : **5**\n    * Nombre de trajets dont l’occurrence du code INSEE d'arrivée est < 6 : **6**\n    * Nombre de trajets dont l’occurrence du code INSEE de départ ET d'arrivée est < 6 : **2**";
+  t.deepEqual(description, expected_happened_description);
   sinon.assert.calledWithMatch(t.context.tripRepositoryProviderStub.firstCall, openDataQueryParam);
   const openDataQueryParamCopy: OpenDataTripSearchInterface = {
     ...openDataQueryParam,
