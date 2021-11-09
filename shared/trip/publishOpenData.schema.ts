f4fcf6338956931b9f3f937c61ccce56e2689ba2
Julien Parmentier <julien.parmentier@beta.gouv.fr>
2021-11-09 15:39:15 +0100

    fix: add minimal schema validation for publicOpendata

--- shared/trip/publishOpenData.schema.ts
+++ shared/trip/publishOpenData.schema.ts
@@ -1,10 +1,35 @@
 export const schema = {
   type: 'object',
   additionalProperties: false,
-  required: ['filepath'],
+  required: ['filepath', 'tripSearchQueryParam'],
   properties: {
     filepath: {
       macro: 'varchar',
+    },
+    tripSearchQueryParam: {
+      type: 'object',
+      additionalProperties: false,
+      required: ['date'],
+      properties: {
+        date: {
+          type: 'object',
+        },
+        status: {
+          macro: 'varchar',
+        },
+        excluded_start_territory_id: {
+          type: 'array',
+          items: { macro: 'serial' },
+        },
+        excluded_end_territory_id: {
+          type: 'array',
+          items: { macro: 'serial' },
+        },
+      },
+    },
+    excludedTerritories: {
+      type: 'array',
+      items: { type: 'object' },
     },
   },
 };
