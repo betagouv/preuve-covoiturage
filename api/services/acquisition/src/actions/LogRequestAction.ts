4c40c45ccd9ff787c69c0036a7f182267f5bb96c
Jonathan Fallon <jonathan@dotify.fr>
2021-11-10 16:56:29 +0100

    fix some metadata acquisition when logging errors

--- api/services/acquisition/src/actions/LogRequestAction.ts
+++ api/services/acquisition/src/actions/LogRequestAction.ts
@@ -18,14 +18,21 @@ export class LogRequestAction extends AbstractAction {
 
   protected async handle(params: ParamsInterface, context: ContextType): Promise<ResultInterface> {
     // clean up sensitive data
-    delete params.headers.cookie;
+    if ('headers' in params && 'cookie' in params.headers) {
+      delete params.headers.cookie;
+    }
+
+    if ('headers' in params && 'authorization' in params.headers) {
+      delete params.headers.authorization;
+    }
 
     await this.repo.log({
       error_stage: ErrorStage.Acquisition,
       error_line: null,
       operator_id: get(context, 'call.user.operator_id', params.operator_id || 0),
-      journey_id: params.journey_id,
+      journey_id: get(params, 'body.journey_id', get(params, 'journey_id', null)),
       source: 'logrequest',
+      request_id: get(params, 'request_id', get(params, 'headers.x-request-id'), null),
       error_message: null,
       error_code: null,
       auth: get(context, 'call.user'),
