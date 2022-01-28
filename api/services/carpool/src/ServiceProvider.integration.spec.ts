a32254ec14f4164c9cb653469893eab42439cdbf
Nicolas Mérigot <nicolas.merigot@beta.gouv.fr>
2022-01-28 10:16:00 +0100

    fix: new serviceProviderMacro signature

--- api/services/carpool/src/ServiceProvider.integration.spec.ts
+++ api/services/carpool/src/ServiceProvider.integration.spec.ts
@@ -1,7 +1,17 @@
-import anyTest from 'ava';
-import { serviceProviderMacro } from '@pdc/helper-test';
+import anyTest, { TestFn } from 'ava';
+import { serviceProviderMacro, ServiceProviderMacroContext } from '@pdc/helper-test';
 import { ServiceProvider } from './ServiceProvider';
 
-const { test, boot } = serviceProviderMacro(anyTest, ServiceProvider);
+const test = anyTest as TestFn<ServiceProviderMacroContext>;
+const { before, after, boot } = serviceProviderMacro(ServiceProvider);
+
+test.before(async (t) => {
+  const { kernel } = await before();
+  t.context.kernel = kernel;
+});
+
+test.after.always(async (t) => {
+  await after({ kernel: t.context.kernel });
+});
 
 test(boot);
