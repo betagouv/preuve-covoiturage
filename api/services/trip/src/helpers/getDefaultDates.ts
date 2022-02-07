80b8068641df3ab66ba1260b27209981b96a8c0f
Julien Parmentier <julien.parmentier@beta.gouv.fr>
2022-02-07 19:24:21 +0100

    fix: compensate time zone offset for replay opendata

--- api/services/trip/src/helpers/getDefaultDates.ts
+++ api/services/trip/src/helpers/getDefaultDates.ts
@@ -1,24 +1,25 @@
-import { utcToZonedTime } from 'date-fns-tz';
+import { zonedTimeToUtc } from 'date-fns-tz';
 
 export function endOfMonth(date: Date = new Date(), tz?: string): Date {
   const endOfMonth: Date = new Date(date.getFullYear(), date.getMonth() + 1, 1, 0, 0, 0, -1);
-  return tz ? utcToZonedTime(endOfMonth, tz) : endOfMonth;
+  return tz ? zonedTimeToUtc(endOfMonth, tz) : endOfMonth;
 }
 
 export function startOfMonth(date: Date = new Date(), tz?: string): Date {
-  return new Date(date.getFullYear(), date.getMonth(), 1);
+  const startOfMonth: Date = new Date(date.getFullYear(), date.getMonth(), 1);
+  return tz ? zonedTimeToUtc(startOfMonth, tz) : startOfMonth;
 }
 
 export function endOfPreviousMonthDate(tz?: string): Date {
   const defaultEndDate = new Date();
   defaultEndDate.setDate(1);
   defaultEndDate.setHours(0, 0, 0, -1);
-  return tz ? utcToZonedTime(defaultEndDate, tz) : defaultEndDate;
+  return tz ? zonedTimeToUtc(defaultEndDate, tz) : defaultEndDate;
 }
 
 export function startOfPreviousMonthDate(endDate: Date, tz?: string): Date {
   const startDate = new Date(endDate.valueOf());
   startDate.setDate(1);
   startDate.setHours(0, 0, 0, 0);
-  return tz ? utcToZonedTime(startDate, tz) : startDate;
+  return tz ? zonedTimeToUtc(startDate, tz) : startDate;
 }
