b85611eb35ba52777f15283a49b6ce572198c163
Julien Parmentier <julien.parmentier@beta.gouv.fr>
2022-02-07 18:22:25 +0100

    fix: use tz param when present

--- api/services/trip/src/helpers/getDefaultDates.ts
+++ api/services/trip/src/helpers/getDefaultDates.ts
@@ -1,42 +1,25 @@
 import { utcToZonedTime } from 'date-fns-tz';
 
-export function endOfPreviousMonthDate(): Date {
+export function endOfPreviousMonthDate(tz?: string): Date {
   const defaultEndDate = new Date();
   defaultEndDate.setDate(1);
   defaultEndDate.setHours(0, 0, 0, -1);
-  // Use a default timezone and not the server one
-  // const timeZoneOffest: number = defaultEndDate.getTimezoneOffset() * 60000
-  // console.debug(`old offset ${defaultEndDate.getTimezoneOffset()}`);
-  // console.debug(`old ${defaultEndDate}`);
-  // defaultEndDate.setTime(defaultEndDate.getTime() + timeZoneOffest)
-  // const europeParis: Date = utcToZonedTime(defaultEndDate, 'Europe/Paris');
-  // console.debug(`new offset -> ${europeParis.getTimezoneOffset()}`);
-  // console.debug(`new -> ${europeParis}`);
-  return defaultEndDate;
+  return tz ? utcToZonedTime(defaultEndDate, tz) : defaultEndDate;
 }
 
-export function endOfMonth(date: Date = new Date()): Date {
+export function endOfMonth(date: Date = new Date(), tz?: string): Date {
   const endOfMonth: Date = new Date(date.getFullYear(), date.getMonth() + 1, 1);
   endOfMonth.setHours(0, 0, 0, -1);
-  return endOfMonth;
+  return tz ? utcToZonedTime(endOfMonth, tz) : endOfMonth;
 }
 
-export function startOfMonth(date: Date = new Date()): Date {
+export function startOfMonth(date: Date = new Date(), tz?: string): Date {
   return new Date(date.getFullYear(), date.getMonth(), 1);
 }
 
-// export function endOfPreviousMonthDate(): Date {
-//   let defaultEndDate = new Date();
-//   defaultEndDate = new Date(Date.UTC(defaultEndDate.getFullYear(), defaultEndDate.getMonth(), 0, 23,59,59,999))
-//   // defaultEndDate.setDate(1);
-//   // defaultEndDate.setHours(0, 0, 0, -1);
-//   return defaultEndDate;
-// }
-
-// export function endOfMonth(date: Date = new Date()): Date {
-//   return new Date(Date.UTC(date.getFullYear(), date.getMonth() + 1, 0, 23,59,59,999));
-// }
-
-// export function startOfMonth(date: Date = new Date()): Date {
-//   return new Date(Date.UTC(date.getFullYear(), date.getMonth(), 1));
-// }
+export function startOfPreviousMonthDate(endDate: Date, tz?: string): Date {
+  const startDate = new Date(endDate.valueOf());
+  startDate.setDate(1);
+  startDate.setHours(0, 0, 0, 0);
+  return tz ? utcToZonedTime(startDate, tz) : startDate;
+}
