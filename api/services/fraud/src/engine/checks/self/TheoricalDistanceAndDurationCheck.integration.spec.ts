2b4fca4f16c90ff84bc5e0611aadac833b22229e
Julien Parmentier <julien.parmentier@beta.gouv.fr>
2022-02-08 18:06:59 +0100

    test: reproduce issue with reunion

--- api/services/fraud/src/engine/checks/self/TheoricalDistanceAndDurationCheck.integration.spec.ts
+++ api/services/fraud/src/engine/checks/self/TheoricalDistanceAndDurationCheck.integration.spec.ts
@@ -14,3 +14,18 @@ test('between by duration', range, { driver_duration: 500, driver_calc_duration:
 
 test('max by null duration', range, { driver_duration: 10, driver_calc_duration: 0 }, 0.99, 1);
 test('between by null distance', range, { driver_distance: 0, driver_calc_distance: 10 }, 0.4, 0.6);
+
+test(
+  'theorical duration and distance check for Reunion should pass',
+  range,
+  {
+    driver_distance: 23557,
+    driver_duration: 1638,
+    driver_start_lat: -20.9251842499,
+    driver_start_lon: 55.5831298828,
+    driver_end_lat: -21.0335330963,
+    driver_end_lon: 55.7130279541,
+  },
+  0,
+  0.1,
+);
