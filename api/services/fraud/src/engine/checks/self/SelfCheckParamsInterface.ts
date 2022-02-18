1933b8533d153f0d3e6689b05e0228e6745ead64
Nicolas Mérigot <nicolas.merigot@beta.gouv.fr>
2022-02-18 15:45:22 +0100

    fix: avoid rising error on distance check in COM #1660

--- api/services/fraud/src/engine/checks/self/SelfCheckParamsInterface.ts
+++ api/services/fraud/src/engine/checks/self/SelfCheckParamsInterface.ts
@@ -1,14 +1,18 @@
 export interface SelfCheckParamsInterface {
+  driver_start_geocode: string;
   driver_start_lat: number;
   driver_start_lon: number;
+  driver_end_geocode: string;
   driver_end_lat: number;
   driver_end_lon: number;
   driver_distance: number;
   driver_calc_distance: number;
   driver_duration: number;
   driver_calc_duration: number;
+  passenger_start_geocode: string;
   passenger_start_lat: number;
   passenger_start_lon: number;
+  passenger_end_geocode: string;
   passenger_end_lat: number;
   passenger_end_lon: number;
   passenger_distance: number;
--- api/services/fraud/src/engine/checks/self/TheoricalDistanceAndDurationCheck.integration.spec.ts
+++ api/services/fraud/src/engine/checks/self/TheoricalDistanceAndDurationCheck.integration.spec.ts
@@ -14,3 +14,18 @@ test('between by duration', range, { driver_duration: 500, driver_calc_duration:
 
 test('max by null duration', range, { driver_duration: 10, driver_calc_duration: 0 }, 0.99, 1);
 test('between by null distance', range, { driver_distance: 0, driver_calc_distance: 10 }, 0.4, 0.6);
+
+test(
+  'should be deactivate in COM 97',
+  range,
+  { driver_end_geocode: '97501', driver_duration: 1, driver_calc_duration: 1000 },
+  0,
+  0,
+);
+test(
+  'should be deactivate in COM 98',
+  range,
+  { passenger_end_geocode: '98501', passenger_duration: 1, passenger_calc_duration: 1000 },
+  0,
+  0,
+);
