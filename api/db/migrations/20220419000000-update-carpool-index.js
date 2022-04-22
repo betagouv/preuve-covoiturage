aa16f2626214c12c8d57f0270c823655a4e2a521
Nicolas Mérigot <nicolas.merigot@beta.gouv.fr>
2022-04-22 14:54:28 +0200

    feat: geo helpers + policy trip

--- api/db/migrations/20220419000000-update-carpool-index.js
+++ api/db/migrations/20220419000000-update-carpool-index.js
@@ -2,6 +2,7 @@
 
 var { createMigration } = require('../helpers/createMigration');
 var { setup, up, down } = createMigration([
+  'geo/20220419000000_create_geo_helper',
   'carpool/20220419000000_update_carpool_indexes',
   'trip/20220419000000_refactor_trip_list',
   'policy/20220419000000_update_policy_trips_view',
