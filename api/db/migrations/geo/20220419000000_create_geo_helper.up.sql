d053f685d03e6e316f35da84fcc760d144d4e0cd
Nicolas Mérigot <nicolas.merigot@beta.gouv.fr>
2022-04-22 14:54:28 +0200

    feat: geo helpers + policy trip

---
+++ api/db/migrations/geo/20220419000000_create_geo_helper.up.sql
@@ -0,0 +1,211 @@
+CREATE INDEX IF NOT EXISTS geo_perimeters_year_idx ON geo.perimeters(year);
+CREATE INDEX IF NOT EXISTS geo_perimeters_surface_idx ON geo.perimeters(surface);
+CREATE INDEX IF NOT EXISTS geo_perimeters_arr_idx ON geo.perimeters(arr);
+CREATE INDEX IF NOT EXISTS geo_perimeters_aom_idx ON geo.perimeters(aom);
+
+CREATE OR REPLACE FUNCTION geo.get_latest_by_point(lon float, lat float) returns table (
+  year smallint,
+  l_arr varchar,
+  arr varchar,
+  l_com varchar,
+  com varchar,
+  l_epci varchar,
+  epci varchar,
+  l_dep varchar,
+  dep varchar,
+  l_reg varchar,
+  reg varchar,
+  l_country varchar,
+  country varchar,
+  l_aom varchar,
+  aom varchar,
+  l_reseau varchar,
+  reseau int,
+  pop int,
+  surface real 
+) as $$
+  SELECT
+    year,
+    l_arr,
+    arr,
+    l_com,
+    com,
+    l_epci,
+    epci,
+    l_dep,
+    dep,
+    l_reg,
+    reg,
+    l_country,
+    country,
+    l_aom,
+    aom,
+    l_reseau,
+    reseau,
+    pop,
+    surface
+  FROM geo.perimeters
+  WHERE
+    geom IS NOT NULL AND
+    ST_WITHIN(
+      ST_SETSRID(
+        ST_POINT($1, $2),
+        '4326'
+      ),
+      geom
+    )
+  ORDER BY surface ASC, year DESC
+  LIMIT 1
+$$ language sql;
+
+CREATE OR REPLACE FUNCTION geo.get_latest_by_code(code varchar) returns table (
+  year smallint,
+  l_arr varchar,
+  arr varchar,
+  l_com varchar,
+  com varchar,
+  l_epci varchar,
+  epci varchar,
+  l_dep varchar,
+  dep varchar,
+  l_reg varchar,
+  reg varchar,
+  l_country varchar,
+  country varchar,
+  l_aom varchar,
+  aom varchar,
+  l_reseau varchar,
+  reseau int,
+  pop int,
+  surface real 
+) as $$
+  SELECT 
+    year,
+    l_arr,
+    arr,
+    l_com,
+    com,
+    l_epci,
+    epci,
+    l_dep,
+    dep,
+    l_reg,
+    reg,
+    l_country,
+    country,
+    l_aom,
+    aom,
+    l_reseau,
+    reseau,
+    pop,
+    surface
+  FROM geo.perimeters
+  WHERE
+    arr = $1
+  ORDER BY year DESC
+  LIMIT 1
+$$ language sql;
+
+CREATE OR REPLACE FUNCTION geo.get_by_point(lon float, lat float, year smallint) returns table (
+  year smallint,
+  l_arr varchar,
+  arr varchar,
+  l_com varchar,
+  com varchar,
+  l_epci varchar,
+  epci varchar,
+  l_dep varchar,
+  dep varchar,
+  l_reg varchar,
+  reg varchar,
+  l_country varchar,
+  country varchar,
+  l_aom varchar,
+  aom varchar,
+  l_reseau varchar,
+  reseau int,
+  pop int,
+  surface real 
+) as $$
+  SELECT
+    year,
+    l_arr,
+    arr,
+    l_com,
+    com,
+    l_epci,
+    epci,
+    l_dep,
+    dep,
+    l_reg,
+    reg,
+    l_country,
+    country,
+    l_aom,
+    aom,
+    l_reseau,
+    reseau,
+    pop,
+    surface
+  FROM geo.perimeters
+  WHERE
+    geom IS NOT NULL AND
+    year = $3 AND
+    ST_WITHIN(
+      ST_SETSRID(
+        ST_POINT($1, $2),
+        '4326'
+      ),
+      geom
+    )
+  ORDER BY surface ASC, year DESC
+  LIMIT 1
+$$ language sql stable;
+
+CREATE OR REPLACE FUNCTION geo.get_by_code(code varchar, year smallint) returns table (
+  year smallint,
+  l_arr varchar,
+  arr varchar,
+  l_com varchar,
+  com varchar,
+  l_epci varchar,
+  epci varchar,
+  l_dep varchar,
+  dep varchar,
+  l_reg varchar,
+  reg varchar,
+  l_country varchar,
+  country varchar,
+  l_aom varchar,
+  aom varchar,
+  l_reseau varchar,
+  reseau int,
+  pop int,
+  surface real 
+) as $$
+  SELECT 
+    year,
+    l_arr,
+    arr,
+    l_com,
+    com,
+    l_epci,
+    epci,
+    l_dep,
+    dep,
+    l_reg,
+    reg,
+    l_country,
+    country,
+    l_aom,
+    aom,
+    l_reseau,
+    reseau,
+    pop,
+    surface
+  FROM geo.perimeters
+  WHERE
+    arr = $1 AND
+    year = $2
+  LIMIT 1
+$$ language sql stable;
--- api/db/migrations/policy/20220419000000_update_policy_trips_view.up.sql
+++ api/db/migrations/policy/20220419000000_update_policy_trips_view.up.sql
@@ -16,8 +16,79 @@ CREATE VIEW policy.trips AS (
     (CASE WHEN ci.travel_pass_user_id IS NOT NULL THEN true ELSE false END) as has_travel_pass,
     (CASE WHEN ci.over_18 IS NOT NULL THEN ci.over_18 ELSE null END) as is_over_18,
     ci.uuid as identity_uuid,
-    cp.start_geo_code AS start_insee,
-    cp.end_geo_code AS end_insee
+    cp.start_geo_code,
+    cp.end_geo_code,
+    (
+      SELECT
+        json_build_object(
+          'arr',
+          arr,
+          'com',
+          com,
+          'epci',
+          epci,
+          'dep',
+          dep,
+          'reg',
+          reg,
+          'country',
+          country,
+          'aom',
+          aom,
+          'reseau',
+          reseau
+        )
+      FROM geo.get_by_code(cp.start_geo_code::varchar, EXTRACT(year FROM cp.datetime)::smallint) AS position 
+    ) as carpool_start,
+    (
+      SELECT
+        json_build_object(
+          'arr',
+          arr,
+          'com',
+          com,
+          'epci',
+          epci,
+          'dep',
+          dep,
+          'reg',
+          reg,
+          'country',
+          country,
+          'aom',
+          aom,
+          'reseau',
+          reseau
+        )
+      FROM geo.get_by_code(cp.end_geo_code::varchar, EXTRACT(year FROM cp.datetime)::smallint) AS position
+    ) as carpool_end
   FROM carpool.carpools as cp
   LEFT JOIN carpool.identities as ci ON cp.identity_id = ci._id
 );
+
+CREATE OR REPLACE FUNCTION policy.get_com_by_territory_id(_id int, year smallint) returns table (com varchar) as $$
+  with data as (select * from territory.territory_group_selector where territory_group_id = $1)
+  select gp.com from geo.perimeters gp join data d 
+    on (d.selector_type = 'arr' OR d.selector_type = 'com') and d.selector_value = gp.arr
+  where year = $2
+  union
+  select gp.com from geo.perimeters gp join data d 
+    on d.selector_type = 'aom' and d.selector_value = gp.aom
+  where year = $2;
+$$ language sql stable;
+
+CREATE OR REPLACE FUNCTION policy.get_territory_id_by_selector(com varchar default null, aom varchar default null) returns table (_id int) as $$
+  SELECT
+    territory_group_id as _id
+  FROM territory.territory_group_selector
+  WHERE
+    (selector_type = 'com' OR selector_type = 'arr') AND
+    selector_value = $1
+  UNION
+  SELECT
+    territory_group_id as _id
+  FROM territory.territory_group_selector
+  WHERE
+    selector_type = 'aom' AND
+    selector_value = $2
+$$ language sql stable;
