4394e8edd7f6a41f5cea43eb050e2eebea3e84f7
Julien Parmentier <julien.parmentier@beta.gouv.fr>
2022-06-21 17:02:21 +0200

    fix(territory-autocomplete): remove unused

--- dashboard/src/app/modules/territory/modules/territory-ui/components/territories-autocomplete/territories-autocomplete.component.ts
+++ dashboard/src/app/modules/territory/modules/territory-ui/components/territories-autocomplete/territories-autocomplete.component.ts
@@ -20,7 +20,6 @@ import { SingleResultInterface as SingleGeoResult } from '~/shared/territory/lis
   styleUrls: ['./territories-autocomplete.component.scss'],
 })
 export class TerritoriesAutocompleteComponent extends DestroyObservable implements OnInit {
-  // with control 'territoryIds'
   @Input() parentForm: FormGroup;
   @Input() placeholder = 'Ajouter un territoire';
   @Input() title = 'Territoires';
@@ -29,7 +28,6 @@ export class TerritoriesAutocompleteComponent extends DestroyObservable implemen
   @ViewChild('territoryInput') territoryInput: ElementRef;
 
   public filteredTerritories: SingleGeoResult[];
-  public selectedTerritories: SingleGeoResult[] = [];
 
   public territoryCtrl = new FormControl();
 
@@ -54,26 +52,13 @@ export class TerritoriesAutocompleteComponent extends DestroyObservable implemen
   }
 
   public remove(territory: SingleGeoResult): void {
-    const index = this.territoryIdsControl.value.indexOf(territory);
-
-    if (index >= 0) {
-      const territories = [...this.territoryIdsControl.value];
-      territories.splice(index, 1);
-      this.territoryIdsControl.setValue(territories);
-    }
-
-    const selectedTerritoriesInd = this.selectedTerritories.findIndex((terr) => terr.insee === territory.insee);
-
-    if (selectedTerritoriesInd >= 0) {
-      this.selectedTerritories.splice(selectedTerritoriesInd, 1);
-    }
+    const territoryIdsControlFiltered = this.territoryIdsControl.value.filter((terr) => terr.insee != territory.insee);
+    this.territoryIdsControl.setValue(territoryIdsControlFiltered);
   }
 
   public onTerritorySelect(event: MatAutocompleteSelectedEvent): void {
     const territories: SingleGeoResult[] = this.territoryIdsControl.value || [];
     territories.push(event.option.value);
-
-    this.selectedTerritories.push(this.filteredTerritories.find((terr) => terr.insee === event.option.value));
 
     this.territoryIdsControl.setValue(territories);
     this.territoryInput.nativeElement.value = null;
