485d16c3b8c9d4a81db69758ad8f03100a4ecb5c
Julien Parmentier <julien.parmentier@beta.gouv.fr>
2022-07-04 14:08:50 +0200

    refactor(MutateCampaignInseeFilter): factorize code

--- api/services/policy/src/actions/campaign/MutateCampaignInseesFilter.ts
+++ api/services/policy/src/actions/campaign/MutateCampaignInseesFilter.ts
@@ -1,10 +1,7 @@
-import { ExportAction } from './../../../../trip/src/actions/ExportAction';
 import { KernelInterfaceResolver, provider } from '@ilos/common';
 
 import { RuleInterface } from '../../engine/interfaces';
 import { TerritoryParamsInterface } from '../../engine/rules/filters/TerritoryFilter';
-import { TerritoryFilterRuleDecorated } from '../../interfaces/TerritoryFilterRuleDecorated';
-import { TerritoryCodeEnum } from '../../shared/territory/common/interfaces/TerritoryCodeInterface';
 import {
   handlerConfig,
   ParamsInterface,
@@ -28,75 +25,48 @@ export class MutateCampaignInseesFilter {
     if (!inseeBlacklistFilterRules && !inseeWhitelistFilterRules) {
       return global_rules;
     }
-
     if (inseeBlacklistFilterRules && inseeBlacklistFilterRules.parameters) {
-      const territorySelectorsInterface: TerritoryParamsInterface[] = inseeBlacklistFilterRules.parameters;
-      const params: ParamsInterface = {
-        epci: territorySelectorsInterface.flatMap((s) => this.concatStartEndInsee(s.start?.epci, s.end?.epci)),
-        aom: territorySelectorsInterface.flatMap((s) => this.concatStartEndInsee(s.start?.aom, s.end?.aom)),
-        com: territorySelectorsInterface.flatMap((s) => this.concatStartEndInsee(s.start?.com, s.end?.com)),
-      };
-      const selectors: ResultInterface = await this.kernel.call<ParamsInterface, ResultInterface>(signature, params, {
-        channel: { service: handlerConfig.service },
-      });
-
-      inseeBlacklistFilterRules.parameters = inseeBlacklistFilterRules.parameters.map((p) => {
-        if (p.start.epci) {
-          p.start.epci = p.start.epci.map((e) => this.findTerritoryInsee(selectors, 'epci', e));
-        }
-        if (p.end.epci) {
-          p.end.epci = p.end.epci.map((e) => this.findTerritoryInsee(selectors, 'epci', e));
-        }
-        if (p.start.aom) {
-          p.start.aom = p.start.aom.map((e) => this.findTerritoryInsee(selectors, 'aom', e));
-        }
-        if (p.end.aom) {
-          p.end.aom = p.end.aom.map((e) => this.findTerritoryInsee(selectors, 'aom', e));
-        }
-        if (p.start.com) {
-          p.start.com = p.start.com.map((e) => this.findTerritoryInsee(selectors, 'com', e));
-        }
-        if (p.end.com) {
-          p.end.com = p.end.com.map((e) => this.findTerritoryInsee(selectors, 'com', e));
-        }
-        return p;
-      });
+      inseeBlacklistFilterRules.parameters = await this.territoryFiltersWithName(inseeBlacklistFilterRules);
     }
-
     if (inseeWhitelistFilterRules && inseeWhitelistFilterRules.parameters) {
-      const territorySelectorsInterface: TerritoryParamsInterface[] = inseeWhitelistFilterRules.parameters;
-      const params: ParamsInterface = {
-        epci: territorySelectorsInterface.flatMap((s) => this.concatStartEndInsee(s.start?.epci, s.end?.epci)),
-        aom: territorySelectorsInterface.flatMap((s) => this.concatStartEndInsee(s.start?.aom, s.end?.aom)),
-        com: territorySelectorsInterface.flatMap((s) => this.concatStartEndInsee(s.start?.com, s.end?.com)),
-      };
-      const selectors: ResultInterface = await this.kernel.call<ParamsInterface, ResultInterface>(signature, params, {
-        channel: { service: handlerConfig.service },
-      });
-
-      inseeWhitelistFilterRules.parameters = inseeWhitelistFilterRules.parameters.map((p) => {
-        if (p.start.epci) {
-          p.start.epci = p.start.epci.map((e) => this.findTerritoryInsee(selectors, 'epci', e));
-        }
-        if (p.end.epci) {
-          p.end.epci = p.end.epci.map((e) => this.findTerritoryInsee(selectors, 'epci', e));
-        }
-        if (p.start.aom) {
-          p.start.aom = p.start.aom.map((e) => this.findTerritoryInsee(selectors, 'aom', e));
-        }
-        if (p.end.aom) {
-          p.end.aom = p.end.aom.map((e) => this.findTerritoryInsee(selectors, 'aom', e));
-        }
-        if (p.start.com) {
-          p.start.com = p.start.com.map((e) => this.findTerritoryInsee(selectors, 'com', e));
-        }
-        if (p.end.com) {
-          p.end.com = p.end.com.map((e) => this.findTerritoryInsee(selectors, 'com', e));
-        }
-        return p;
-      });
+      inseeWhitelistFilterRules.parameters = await this.territoryFiltersWithName(inseeWhitelistFilterRules);
     }
     return mutatedGlobalRules;
+  }
+
+  private async territoryFiltersWithName(territoryFilterRules: RuleInterface) {
+    const territorySelectorsInterface: TerritoryParamsInterface[] = territoryFilterRules.parameters;
+    const params: ParamsInterface = {
+      epci: territorySelectorsInterface.flatMap((s) => this.concatStartEndInsee(s.start?.epci, s.end?.epci)),
+      aom: territorySelectorsInterface.flatMap((s) => this.concatStartEndInsee(s.start?.aom, s.end?.aom)),
+      com: territorySelectorsInterface.flatMap((s) => this.concatStartEndInsee(s.start?.com, s.end?.com)),
+    };
+
+    const selectors: ResultInterface = await this.kernel.call<ParamsInterface, ResultInterface>(signature, params, {
+      channel: { service: handlerConfig.service },
+    });
+
+    return territoryFilterRules.parameters.map((parameter) => {
+      if (parameter.start.epci) {
+        parameter.start.epci = parameter.start.epci.map((e) => this.findTerritoryInsee(selectors, 'epci', e));
+      }
+      if (parameter.end.epci) {
+        parameter.end.epci = parameter.end.epci.map((e) => this.findTerritoryInsee(selectors, 'epci', e));
+      }
+      if (parameter.start.aom) {
+        parameter.start.aom = parameter.start.aom.map((e) => this.findTerritoryInsee(selectors, 'aom', e));
+      }
+      if (parameter.end.aom) {
+        parameter.end.aom = parameter.end.aom.map((e) => this.findTerritoryInsee(selectors, 'aom', e));
+      }
+      if (parameter.start.com) {
+        parameter.start.com = parameter.start.com.map((e) => this.findTerritoryInsee(selectors, 'com', e));
+      }
+      if (parameter.end.com) {
+        parameter.end.com = parameter.end.com.map((e) => this.findTerritoryInsee(selectors, 'com', e));
+      }
+      return parameter;
+    });
   }
 
   private findTerritoryInsee(selectors: ResultInterface, selectorType: string, insee: string) {
