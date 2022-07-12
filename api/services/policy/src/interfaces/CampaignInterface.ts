5317d67ff356b23b4e98d80924cc19005c8369ab
Nicolas Mérigot <nicolas.merigot@beta.gouv.fr>
2022-07-12 11:52:01 +0200

    arch: clean up interfaces

--- api/services/policy/src/interfaces/CampaignInterface.ts
+++ api/services/policy/src/interfaces/CampaignInterface.ts
@@ -1,18 +1,12 @@
-import { RuleInterface } from '../engine/interfaces/RuleInterface';
-
 export interface CampaignInterface {
   _id?: number;
   parent_id?: number;
-  ui_status?: any;
   territory_id: number;
   name: string;
-  description: string;
   start_date: Date;
   end_date: Date;
-  unit: string;
+  uses: string;
   status: string;
-  global_rules: RuleInterface[];
-  rules: RuleInterface[][];
 }
 
 export interface CampaignStatsInterface {
