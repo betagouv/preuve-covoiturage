c0dd4a50f6bfa046f755001672e1a974f6c64c7f
Nicolas Mérigot <nicolas.merigot@beta.gouv.fr>
2022-07-20 14:33:12 +0200

    arch: remove old engin

--- api/services/policy/src/engine/faker/AbstractVariant.ts
+++
@@ -1,9 +0,0 @@
-import { PersonInterface } from '../../shared/policy/common/interfaces/PersonInterface';
-
-export class AbstractVariant<P> {
-  constructor(params: P) {}
-
-  public generate(people: PersonInterface[]): PersonInterface[] {
-    return people;
-  }
-}
--- api/services/policy/src/engine/templates/index.ts
+++
@@ -1,7 +0,0 @@
-export { eventTrafficLimitPolicy } from './eventTrafficLimitPolicy';
-export { freeTravelForPassengerPolicy } from './freeTravelForPassengerPolicy';
-export { nonFinancialIncentivePolicy } from './nonFinancialIncentivePolicy';
-export { weekdayTrafficLimitPolicy } from './weekdayTrafficLimitPolicy';
-export { financialIncentivePolicy } from './financialIncentivePolicy';
-export { pollutionLimitPolicy } from './pollutionLimitPolicy';
-export { weekendTrafficLimitPolicy } from './weekendTrafficLimitPolicy';
