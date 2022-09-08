73c60f237a0191870ca9147a2c55619daa5b1401
Nicolas Mérigot <nicolas.merigot@beta.gouv.fr>
2022-09-08 09:40:32 +0200

    feat: add missing column on acquisition

--- api/db/migrations/acquisition/20220905000000-add_api_version.down.sql
+++ api/db/migrations/acquisition/20220905000000-add_api_version.down.sql
@@ -1,1 +1,11 @@
 ALTER TABLE acquisition.acquisitions DROP COLUMN api_version;
+ALTER TABLE acquisition.acquisitions
+  DROP COLUMN status;
+ALTER TABLE acquisition.acquisitions
+  DROP COLUMN error_stage;
+ALTER TABLE acquisition.acquisitions
+  DROP COLUMN errors;
+ALTER TABLE acquisition.acquisitions
+  DROP COLUMN request_id;
+
+DROP TYPE acquisition.acquisition_status_enum;
--- api/db/migrations/acquisition/20220905000000-add_api_version.up.sql
+++ api/db/migrations/acquisition/20220905000000-add_api_version.up.sql
@@ -1,2 +1,20 @@
+ALTER TABLE acquisition.acquisitions ADD COLUMN IF NOT EXISTS api_version SMALLINT NOT NULL DEFAULT 2;
 
-ALTER TABLE acquisition.acquisitions ADD COLUMN IF NOT EXISTS api_version SMALLINT;
+ALTER TABLE acquisition.acquisitions
+  ADD COLUMN IF NOT EXISTS request_id varchar;
+
+CREATE TYPE acquisition.acquisition_status_enum AS enum('ok', 'error', 'todo');
+
+ALTER TABLE acquisition.acquisitions
+  ADD COLUMN status acquisition.acquisition_status_enum IF NOT EXISTS NOT NULL DEFAULT 'todo';
+
+ALTER TABLE acquisition.acquisitions
+  ADD COLUMN try_count SMALLINT NOT NULL DEFAULT 0;
+
+ALTER TABLE acquisition.acquisitions
+  ADD COLUMN error_stage varchar(255);
+
+ALTER TABLE acquisition.acquisitions
+  ADD COLUMN errors JSON;
+
+CREATE INDEX IF NOT EXISTS acquisition_request_id_idx ON acquisition.acquisitions (request_id);
