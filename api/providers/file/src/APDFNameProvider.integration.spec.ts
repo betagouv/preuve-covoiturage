d74f06e374e3087c4fcb0ad993cd75f93c3dbb5e
Jonathan Fallon <jonathan@dotify.fr>
2022-10-11 17:27:12 +0200

    fix APDF tests

--- api/providers/file/src/APDFNameProvider.integration.spec.ts
+++ api/providers/file/src/APDFNameProvider.integration.spec.ts
@@ -1,24 +1,42 @@
 import test from 'ava';
+import path from 'path';
+import os from 'os';
 import { APDFNameProvider } from './APDFNameProvider';
 
-function re(str: string): RegExp {
-  return new RegExp(`APDF-${str}.xslx`);
-}
-
-test('Stringify APDF', (t) => {
+test('Stringify APDF: filename', (t) => {
   const provider = new APDFNameProvider();
-  const str = provider.stringify({
-    name: 'YOLO',
-    datetime: new Date('2022-01-01T00:00:00Z'),
-    campaign_id: 1,
-    operator_id: 2,
-  });
-  t.regex(str, re('2022-01-1-2-yolo'));
+  t.is(
+    provider.stringify({
+      name: 'YOLO',
+      datetime: new Date('2022-01-01T00:00:00Z'),
+      campaign_id: 1,
+      operator_id: 2,
+    }),
+    'APDF-2022-01-1-2-yolo.xlsx',
+  );
+});
+
+test('Stringify APDF: filepath with string', (t) => {
+  const provider = new APDFNameProvider();
+  t.is(provider.filepath('APDF-2022-01-1-2-yolo.xlsx'), path.join(os.tmpdir(), 'APDF-2022-01-1-2-yolo.xlsx'));
+});
+
+test('Stringify APDF: filepath with object', (t) => {
+  const provider = new APDFNameProvider();
+  t.is(
+    provider.filepath({
+      name: 'YOLO',
+      datetime: new Date('2022-01-01T00:00:00Z'),
+      campaign_id: 1,
+      operator_id: 2,
+    }),
+    path.join(os.tmpdir(), 'APDF-2022-01-1-2-yolo.xlsx'),
+  );
 });
 
 test('Parse APDF: filename', (t) => {
   const provider = new APDFNameProvider();
-  t.deepEqual(provider.parse('APDF-2022-01-1-2-abc123.xslx'), {
+  t.deepEqual(provider.parse('APDF-2022-01-1-2-abc123.xlsx'), {
     name: 'abc123',
     datetime: new Date('2022-01-01T00:00:00Z'),
     campaign_id: 1,
@@ -28,7 +46,7 @@ test('Parse APDF: filename', (t) => {
 
 test('Parse APDF: filename with prefix', (t) => {
   const provider = new APDFNameProvider();
-  t.deepEqual(provider.parse('1/APDF-2022-01-1-2-abc123.xslx'), {
+  t.deepEqual(provider.parse('1/APDF-2022-01-1-2-abc123.xlsx'), {
     name: 'abc123',
     datetime: new Date('2022-01-01T00:00:00Z'),
     campaign_id: 1,
