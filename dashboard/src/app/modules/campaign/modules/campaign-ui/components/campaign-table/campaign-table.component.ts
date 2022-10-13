83e43ee9140e021eb69861039eeabaf9afb05960
Julien Parmentier <julien.parmentier@beta.gouv.fr>
2022-10-13 15:44:17 +0200

    feat(campaign-list): add finished campaign status to list

--- dashboard/src/app/modules/campaign/modules/campaign-ui/components/campaign-table/campaign-table.component.ts
+++ dashboard/src/app/modules/campaign/modules/campaign-ui/components/campaign-table/campaign-table.component.ts
@@ -27,9 +27,4 @@ export class CampaignTableComponent {
   isEuro(unit: IncentiveUnitEnum): boolean {
     return unit === IncentiveUnitEnum.EUR;
   }
-
-  isRunning(campaign: PolicyInterface): boolean {
-    const today = new Date();
-    return campaign.status === 'active' && new Date(campaign.end_date) > today && new Date(campaign.start_date) < today;
-  }
 }
