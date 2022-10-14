4835f0da214ae7d6e5e07c1e1bd1f456ee1ac228
Julien Parmentier <julien.parmentier@beta.gouv.fr>
2022-10-14 16:00:44 +0200

    fix: typo and use euro pipe in policy list view

--- dashboard/src/app/modules/campaign/modules/campaign-ui/campaign-ui.module.ts
+++ dashboard/src/app/modules/campaign/modules/campaign-ui/campaign-ui.module.ts
@@ -1,3 +1,4 @@
+import { SharedModule } from './../../../../shared/shared.module';
 /* eslint-disable max-len */
 
 import { NgModule } from '@angular/core';
@@ -19,7 +20,7 @@ import { MaterialModule } from '../../../../shared/modules/material/material.mod
     CampaignMainMetricsComponent,
     CampaignTableComponent,
   ],
-  imports: [CommonModule, MaterialModule, FormsModule, ReactiveFormsModule, ChartjsModule, RouterModule],
+  imports: [CommonModule, MaterialModule, FormsModule, ReactiveFormsModule, ChartjsModule, RouterModule, SharedModule],
   exports: [
     CampaignAutoCompleteComponent,
     CampaignSummaryTextComponent,
