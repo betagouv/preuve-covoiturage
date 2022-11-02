11a83c9f5121801190b837505ac08e5e4232573f
Nicolas Mérigot <nicolas.merigot@beta.gouv.fr>
2022-11-02 11:37:14 +0100

    feat: add pmgf

---
+++ api/services/policy/src/engine/policies/Pmgf.html.ts
@@ -0,0 +1,17 @@
+export const description = `<p _ngcontent-fyn-c231="" id="summary" class="campaignSummaryText-content-text">
+<p>Campagne d’incitation au covoiturage du <b> 2 novembre 2022 au 31 décembre 2022</b></p>
+<p>Cette campagne est limitée à
+  <b>100 000 euros </b>.
+</p>
+<p>Les <b> conducteurs </b> effectuant un trajet d'au moins 4 km sont incités selon les règles suivantes : </p>
+<ul>
+  <li><b>De 2 à 20 km : 2 euros par trajet par passager si le trajet démarre et arrive au sein du PMGF, 1 euros le cas échéant.</b></li>
+  <li><b>De 20 à 40 km : 0.1 euro par trajet par km par passager</b></li>
+</ul>
+<p>Les restrictions suivantes seront appliquées :</p>
+<ul>
+  <li><b>120 euros par conducteur par mois.</b></li>
+</ul>
+<p>La campagne est limitée aux opérateurs BlaBlaCar Daily, Karos, Klaxit, proposant des preuves de classe <b>B
+    ou C</b>.</p>
+</p>`;
--- api/services/policy/src/engine/policies/index.ts
+++ api/services/policy/src/engine/policies/index.ts
@@ -4,9 +4,10 @@ import { Laval } from './Laval';
 import { Lannion } from './Lannion';
 import { Mrn } from './Mrn';
 import { Nm } from './Nm';
+import { Pmgf } from './Pmgf';
 import { Pdll } from './Pdll';
 import { Smt } from './Smt';
 
 export const policies: Map<string, PolicyHandlerStaticInterface> = new Map(
-  [Idfm, Nm, Mrn, Pdll, Smt, Lannion, Laval].map((h) => [h.id, h]),
+  [Idfm, Nm, Mrn, Pdll, Pmgf, Smt, Lannion, Laval].map((h) => [h.id, h]),
 );
