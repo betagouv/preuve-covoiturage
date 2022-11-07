b26445c5a281fc4e3ef667863eb8b1512edd5ae1
Jonathan Fallon <jonathan.fallon@beta.gouv.fr>
2022-11-07 17:46:28 +0100

    change column names in APDF listing

--- dashboard/src/app/modules/campaign/pages/campaign-fundingrequests/campaign-fundingrequests.component.ts
+++ dashboard/src/app/modules/campaign/pages/campaign-fundingrequests/campaign-fundingrequests.component.ts
@@ -2,12 +2,13 @@ import { Component, Input, OnInit } from '@angular/core';
 import { combineLatest } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { CommonDataService } from '~/core/services/common-data.service';
+import { AuthenticationService as Auth } from '~/core/services/authentication/authentication.service';
 import {
   EnrichedFundingRequestType,
   ResultsInterface as FundingRequestsListResult,
 } from '~/shared/policy/fundingRequestsList.contract';
 import { FundingRequestsApiService } from '../../services/fundingrequests-api.service';
-import { PolicyInterface } from './../../../../../../../shared/policy/common/interfaces/PolicyInterface';
+import { PolicyInterface } from '~/shared/policy/common/interfaces/PolicyInterface';
 
 @Component({
   selector: 'app-campaign-fundingrequests',
@@ -18,11 +19,19 @@ export class CampaignFundingRequestsComponent implements OnInit {
   @Input() campaign: PolicyInterface;
 
   public fRequestsList: FundingRequestsListResult;
-  public displayedColumns: string[] = ['month', 'operator', 'trips', 'subsidized', 'amount', 'filesize', 'action'];
+  public displayedColumns: string[] = [];
 
-  constructor(private fRequestsApiService: FundingRequestsApiService, private commonData: CommonDataService) {}
+  constructor(
+    private fRequestsApiService: FundingRequestsApiService,
+    private commonData: CommonDataService,
+    private auth: Auth,
+  ) {}
 
   ngOnInit(): void {
+    this.displayedColumns = this.auth.isSuperAdmin()
+      ? ['month', 'operator', 'trips', 'subsidized', 'amount', 'filesize', 'action']
+      : ['month', 'operator', 'subsidized', 'amount', 'action'];
+
     // fetch funding requests for a given campaign
     // add some display data as operator name and sort the results
     combineLatest([this.commonData.operators$, this.fRequestsApiService.list(this.campaign._id)])
@@ -30,7 +39,7 @@ export class CampaignFundingRequestsComponent implements OnInit {
         map(([operators, freq]: [any, any]) =>
           freq
             .map((fr: EnrichedFundingRequestType) => {
-              const op = operators.find((o) => o._id === fr.operator_id);
+              const op = (operators || []).find((o: { _id: number }) => o._id === fr.operator_id);
               const operator = op?.name;
               // the datetime is a string here as it hasn't been cast to Date
               const month = (fr.datetime as unknown as string).substring(0, 7);
