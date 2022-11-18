016ef8b0505eebca10beb542583335a0707f4be6
Julien Parmentier <julien.parmentier@beta.gouv.fr>
2022-11-18 17:59:25 +0100

    feat(TripRepository) fetch trip within geo perimeters implementation

--- api/services/policy/src/providers/TripRepositoryProvider.ts
+++ api/services/policy/src/providers/TripRepositoryProvider.ts
@@ -15,14 +15,36 @@ export class TripRepositoryProvider implements TripRepositoryProviderInterfaceRe
 
   constructor(protected connection: PostgresConnection) {}
 
-  async *findTripByGeo(
-    coms: string[],
-    from: Date,
-    to: Date,
-    batchSize?: number,
-    override?: boolean,
-  ): AsyncGenerator<CarpoolInterface[], void, void> {
-    yield null;
+  async *findTripByGeo(coms: string[], from: Date, to: Date): AsyncGenerator<CarpoolInterface[], void, void> {
+    const query = {
+      text: `SELECT
+      t.carpool_id as _id,
+      t.trip_id,
+      t.operator_siret,
+      t.operator_class,
+      t.passenger_identity_uuid,
+      t.passenger_has_travel_pass,
+      t.passenger_is_over_18,
+      t.driver_identity_uuid,
+      t.driver_has_travel_pass,
+      t.datetime,
+      t.seats,
+      t.duration,
+      t.distance,
+      t.cost,
+      t.carpool_start as start,
+      t.carpool_end as end,
+      t.start_geo_code
+    FROM policy.trips t
+    WHERE
+    (t.start_geo_code = ANY($1::varchar[]) OR t.end_geo_code = ANY($1::varchar[])) AND
+    t.datetime >= $2::timestamp AND
+    t.datetime < $3::timestamp AND
+      t.carpool_status = 'ok'
+    ORDER BY t.datetime ASC`,
+      values: [coms, from, to],
+    };
+    await (yield* this.queryAndYieldRows(query, 300));
   }
 
   async *findTripByPolicy(
@@ -86,6 +108,13 @@ export class TripRepositoryProvider implements TripRepositoryProviderInterfaceRe
       values: override ? [com, from, to] : [com, from, to, policy._id],
     };
 
+    await (yield* this.queryAndYieldRows(query, batchSize));
+  }
+
+  private async *queryAndYieldRows(
+    query: { text: string; values: (number | Date | string[])[] },
+    batchSize: number,
+  ): AsyncGenerator<CarpoolInterface[], void, void> {
     const client = await this.connection.getClient().connect();
     const cursor = client.query(new Cursor(query.text, query.values));
     const promisifiedCursorRead = promisify(cursor.read.bind(cursor));
