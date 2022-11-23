5d08cb6d301da4b13a95c9529e42821ec6cd1ca3
Nicolas Mérigot <nicolas.merigot@beta.gouv.fr>
2022-11-23 09:55:51 +0100

    fix: add is_specific prop

--- api/db/migrations/policy/20221116000000-create_cee_tables.up.sql
+++ api/db/migrations/policy/20221116000000-create_cee_tables.up.sql
@@ -1,4 +1,5 @@
 CREATE TYPE policy.journey_type_enum AS enum('short', 'long');
+CREATE TYPE policy.application_type_enum AS enum('specific', '2022');
 
 CREATE TABLE IF NOT EXISTS policy.cee_applications (
   _id uuid PRIMARY KEY DEFAULT uuid_generate_v1mc(),
@@ -6,18 +7,19 @@ CREATE TABLE IF NOT EXISTS policy.cee_applications (
   updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
   operator_id INT NOT NULL REFERENCES operator.operators,
   journey_type policy.journey_type_enum NOT NULL,
+  is_specific BOOLEAN NOT NULL DEFAULT false,
   last_name_trunc VARCHAR(4) NOT NULL,
   phone_trunc VARCHAR(32) NOT NULL,
   datetime TIMESTAMP NOT NULL,
   carpool_id INT REFERENCES carpool.carpools,
   driving_license VARCHAR(64),
   CONSTRAINT cee_driving_license_constraint CHECK(
-    CASE WHEN journey_type = 'old'
+    CASE WHEN is_specific = true
       THEN driving_license IS NULL
       ELSE driving_license IS NOT NULL END
   ),
   CONSTRAINT cee_carpool_id_constraint CHECK(
-    CASE WHEN journey_type = 'short'
+    CASE WHEN journey_type = 'short' AND is_specific <> true
       THEN carpool_id IS NOT NULL
       ELSE carpool_id IS NULL END
   )
@@ -28,3 +30,4 @@ CREATE INDEX IF NOT EXISTS cee_jtype_idx ON policy.cee_applications(journey_type
 CREATE INDEX IF NOT EXISTS cee_identity_idx ON policy.cee_applications(phone_trunc, last_name_trunc);
 CREATE INDEX IF NOT EXISTS cee_datetime_idx ON policy.cee_applications(datetime);
 CREATE INDEX IF NOT EXISTS cee_license_idx ON policy.cee_applications(driving_license);
+CREATE INDEX IF NOT EXISTS cee_atype_idx ON policy.cee_applications(is_specific);
