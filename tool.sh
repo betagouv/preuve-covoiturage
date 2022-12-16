aa3bcd62bf163a9462b22fe2e712d35ef4dc9f2d
Julien Parmentier <julien.parmentier@beta.gouv.fr>
2022-12-16 13:46:03 +0100

    Revert "skip migration during seed"
    This reverts commit b9bf5fde9836dd27f75fd884af6c2e9de2bc9ee6.

--- tool.sh
+++ tool.sh
@@ -60,7 +60,7 @@ wait_for_app() {
 
 seed_data() {
   echo "Seed data"
-  $DC run --rm api yarn workspace @pdc/proxy ilos seed --skip-migration
+  $DC run --rm api yarn workspace @pdc/proxy ilos seed
 }
 
 create_bucket() {
