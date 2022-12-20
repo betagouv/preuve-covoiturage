7ffa8b0d00a5bd19a5684b2a6fa5a5ee6910da90
Nicolas Mérigot <nicolas.merigot@beta.gouv.fr>
2022-12-20 15:11:26 +0100

    feat: add raw payment property

--- api/services/policy/src/interfaces/engine/index.ts
+++ api/services/policy/src/interfaces/engine/index.ts
@@ -15,10 +15,12 @@ export interface CarpoolMetaInterface {
 export interface CarpoolInterface {
   _id: number;
   trip_id: string;
+  passenger_payment: number;
   passenger_identity_uuid: string;
   passenger_has_travel_pass: boolean;
   passenger_is_over_18: boolean | null;
   passenger_meta?: CarpoolMetaInterface;
+  driver_payment: number;
   driver_identity_uuid: string;
   driver_has_travel_pass: boolean;
   driver_meta?: CarpoolMetaInterface;
