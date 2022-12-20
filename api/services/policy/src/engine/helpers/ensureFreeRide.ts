f1265c7a29dad0eb742828ce8dbb4fe963e9ed02
Nicolas Mérigot <nicolas.merigot@beta.gouv.fr>
2022-12-20 16:46:22 +0100

    fix: lint

--- api/services/policy/src/engine/helpers/ensureFreeRide.ts
+++ api/services/policy/src/engine/helpers/ensureFreeRide.ts
@@ -1,8 +1,5 @@
 import { StatelessContextInterface } from '../../interfaces';
 
-export const ensureFreeRide = (
-  ctx: StatelessContextInterface,
-  amount: number,
-): number => {
+export const ensureFreeRide = (ctx: StatelessContextInterface, amount: number): number => {
   return Math.max(0, ctx.carpool.cost - amount);
 };
