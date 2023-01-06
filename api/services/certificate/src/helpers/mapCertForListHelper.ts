93493436ff4ef86467f1b0adae691fc31c0dea9a
Jonathan Fallon <jonathan@scopopop.com>
2023-01-06 01:00:31 +0100

    remove obsolete certificate format not building with new tsc

--- api/services/certificate/src/helpers/mapCertForListHelper.ts
+++ api/services/certificate/src/helpers/mapCertForListHelper.ts
@@ -2,23 +2,6 @@ import { CertificateInterface } from '../shared/certificate/common/interfaces/Ce
 import { ResultRowInterface, RowType } from '../shared/certificate/common/interfaces/ResultRowInterface';
 
 export function mapCertForListHelper(cert: CertificateInterface): ResultRowInterface {
-  // return empty values for old format certificates
-  if (!('driver' in cert.meta) || !('passenger' in cert.meta)) {
-    return {
-      type: RowType.EXPIRED,
-      uuid: cert.uuid,
-      tz: cert.meta.tz,
-      start_at: cert.start_at,
-      end_at: cert.end_at,
-      created_at: cert.created_at,
-      positions: [],
-      identity: { uuid: cert.identity_uuid },
-      operator: { _id: cert.operator_id, ...cert.meta.operator },
-      driver: { total: { trips: 0, week_trips: 0, weekend_trips: 0, km: 0, euros: 0 }, trips: [] },
-      passenger: { total: { trips: 0, week_trips: 0, weekend_trips: 0, km: 0, euros: 0 }, trips: [] },
-    };
-  }
-
   return {
     type: RowType.OK,
     uuid: cert.uuid,
