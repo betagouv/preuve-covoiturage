bbe4f68ccbf5922238ff36668de46f8247476890
Julien Parmentier <julien.parmentier@beta.gouv.fr>
2023-01-19 17:00:49 +0100

    feat(policy): add basic rules and description for Normandie

---
+++ api/services/policy/src/engine/policies/Normandie.html.ts
@@ -0,0 +1,20 @@
+/* eslint-disable max-len */
+export const description = `<div _ngcontent-fyn-c231="" id="summary" class="campaignSummaryText-content-text">
+<p>Campagne d’incitation au covoiturage</p>
+<p>Cette campagne est limitée à
+  <b>70 000 euros </b>.
+</p>
+<p>Les <b> conducteurs </b> effectuant un trajet jusqu'à 30 km sont incités selon les règles suivantes : </p>
+<ul>
+  <li>Départ et arrivée dans la région Occitanie</li>
+  <li><b>de 0 à 20 km :</b> 2 euros par trajet par passager moins la contribution du passager</li>
+  <li><b>de 20 à 30 km :</b> 0.1 euro par trajet par km par passager moins la contribution du passager, plafonné à 2 euros</li>
+</ul>
+<p>Les restrictions suivantes seront appliquées :</p>
+<ul>
+  <li>2 trajets par jour par passager</li>
+  <li>6 trajets par jour par conducteur</li>
+  <li>Exclusion des trajets internes aux autres AOM de la Région (ex. Narbonne -> Narbonne)</li>
+</ul>
+<p>La campagne est limitée à tous les opérateurs proposant des preuves de classe <b>C</b>.</p>
+</div>`;
---
+++ api/services/policy/src/engine/policies/Normandie.ts
@@ -0,0 +1,71 @@
+import {
+  PolicyHandlerInterface,
+  PolicyHandlerParamsInterface,
+  PolicyHandlerStaticInterface,
+  StatelessContextInterface,
+} from '../../interfaces';
+import { NotEligibleTargetException } from '../exceptions/NotEligibleTargetException';
+import {
+  ConfiguredLimitInterface,
+  isOperatorClassOrThrow,
+  LimitTargetEnum,
+  onDistanceRangeOrThrow,
+  startsAndEndsAt,
+  watchForGlobalMaxAmount,
+  watchForPersonMaxTripByDay,
+} from '../helpers';
+import { AbstractPolicyHandler } from './AbstractPolicyHandler';
+import { description } from './Normandie.html';
+
+// Politique de la région Normandie
+export const Normandie: PolicyHandlerStaticInterface = class
+  extends AbstractPolicyHandler
+  implements PolicyHandlerInterface
+{
+  static readonly id = 'normandie_2023';
+  protected operator_class = ['C'];
+  private readonly MAX_GLOBAL_AMOUNT_LIMIT = 70_000_00;
+  private readonly NO_MOBILITY_COMPETENCE_EPCI = ['200069516', '200066801', '241400878', '200071652'];
+
+  protected limits: Array<ConfiguredLimitInterface> = [
+    ['E8E1B5F5-64D5-48B9-8BBB-A64C33C500D8', 6, watchForPersonMaxTripByDay, LimitTargetEnum.Driver],
+    ['CB39AF21-5ED5-4792-AA81-1F19EACB901C', 2, watchForPersonMaxTripByDay, LimitTargetEnum.Passenger],
+    ['6D6D0BBA-09C1-40C4-B3C7-2EECF1C6A2A3', this.MAX_GLOBAL_AMOUNT_LIMIT, watchForGlobalMaxAmount],
+  ];
+
+  protected processExclusion(ctx: StatelessContextInterface) {
+    onDistanceRangeOrThrow(ctx, { min: 5_000 });
+    isOperatorClassOrThrow(ctx, this.operator_class);
+
+    // Dans la région
+    if (!startsAndEndsAt(ctx, { reg: ['28'] })) {
+      throw new NotEligibleTargetException('Journey start & end not in region');
+    }
+
+    // En excluant les trajets intra aom excepté les epci n'ayant pas la compétence mobilité
+    if (
+      ctx.carpool.start.aom === ctx.carpool.end.aom &&
+      this.NO_MOBILITY_COMPETENCE_EPCI.includes(ctx.carpool.start.epci)
+    ) {
+      throw new NotEligibleTargetException('Journey start/end inside aom');
+    }
+  }
+
+  processStateless(ctx: StatelessContextInterface): void {
+    this.processExclusion(ctx);
+    super.processStateless(ctx);
+    ctx.incentive.set(100);
+  }
+
+  params(): PolicyHandlerParamsInterface {
+    return {
+      limits: {
+        glob: this.MAX_GLOBAL_AMOUNT_LIMIT,
+      },
+    };
+  }
+
+  describe(): string {
+    return description;
+  }
+};
