fb6d36a7ebca00c17a3c295d6963a5e2786365a1
Ludovic Delhomme <ludovic.delhomme@beta.gouv.fr>
2023-02-10 18:57:29 +0100

    add params to actions

--- api/services/observatory/src/actions/distribution/InsertLastMonthDistributionAction.ts
+++ api/services/observatory/src/actions/distribution/InsertLastMonthDistributionAction.ts
@@ -1,7 +1,11 @@
 import { Action as AbstractAction } from '@ilos/core';
 import { handler, InitHookInterface, KernelInterfaceResolver } from '@ilos/common';
 import { internalOnlyMiddlewares } from '@pdc/provider-middleware';
-import { handlerConfig, signature } from '../../shared/observatory/distribution/insertLastMonthDistribution.contract';
+import {
+  handlerConfig,
+  ParamsInterface,
+  signature,
+} from '../../shared/observatory/distribution/insertMonthlyDistribution.contract';
 import { DistributionRepositoryInterfaceResolver } from '../../interfaces/DistributionRepositoryProviderInterface';
 
 @handler({
@@ -16,21 +20,34 @@ export class InsertLastMonthDistributionAction extends AbstractAction implements
     super();
   }
 
+  get currentYear() {
+    return new Date().getFullYear();
+  }
+
+  get currentMonth() {
+    return new Date().getMonth();
+  }
+
   public async init(): Promise<void> {
-    await this.kernel.notify<void>(signature, null, {
-      channel: {
-        service: handlerConfig.service,
-        metadata: {
-          jobId: 'observatory.InsertMonthlyDistribution.cron',
-          repeat: {
-            cron: '0 6 8 * *',
+    await this.kernel.notify<ParamsInterface>(
+      signature,
+      { year: this.currentYear, month: this.currentMonth },
+      {
+        channel: {
+          service: handlerConfig.service,
+          metadata: {
+            jobId: 'observatory.InsertMonthlyDistribution.cron',
+            repeat: {
+              cron: '0 6 8 * *',
+            },
           },
         },
       },
-    });
+    );
   }
 
-  public async handle(): Promise<void> {
-    return this.distributionRepository.insertLastMonthDistribution();
+  public async handle(params: ParamsInterface): Promise<void> {
+    await this.distributionRepository.deleteOneMonthDistribution(params);
+    return this.distributionRepository.insertOneMonthDistribution(params);
   }
 }
