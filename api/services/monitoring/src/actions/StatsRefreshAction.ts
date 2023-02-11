c63f7f66e0caad77b437d3969e620f6eb877c47b
Jonathan Fallon <jonathan@scopopop.com>
2023-02-11 13:22:38 +0100

    refresh materialised views based on their prefix

--- api/services/monitoring/src/actions/StatsRefreshAction.ts
+++ api/services/monitoring/src/actions/StatsRefreshAction.ts
@@ -5,6 +5,9 @@ import { internalOnlyMiddlewares } from '@pdc/provider-middleware/dist';
 import { handlerConfig, ParamsInterface, ResultInterface } from '../shared/monitoring/statsrefresh.contract';
 import { alias } from '../shared/monitoring/statsrefresh.schema';
 
+const CronFrequencies = ['daily', 'weekly', 'monthly'] as const;
+type CronFrequency = typeof CronFrequencies[number];
+
 @handler({
   ...handlerConfig,
   middlewares: [...internalOnlyMiddlewares('proxy'), ['validate', alias]],
@@ -30,7 +33,26 @@ export class StatsRefreshAction extends AbstractAction {
         return;
       }
 
-      for (const { matviewname: table } of views.rows) {
+      // get the frequencies that can run today
+      const frequencies = this.todayFrequencies();
+
+      // Filter the tables based on today's frequencies
+      // Unprefixed tables will be run every day
+      const tables = views.rows
+        .map((t: { matviewname: string }) => t.matviewname)
+        .map((table: string) => {
+          const [freq] = table.split('_', 1);
+          return {
+            table,
+            freq: CronFrequencies.indexOf(freq as CronFrequency) > -1 ? freq : 'daily',
+          };
+        })
+        .filter(({ freq }) => frequencies.indexOf(freq as CronFrequency) > -1)
+        .map(({ table }: { table: string }) => table);
+
+      console.info(`[stats:refresh] Refresh materialised views: ${tables.sort().join(', ')}`);
+
+      for (const table of tables) {
         const bench = new Date().getTime();
         await this.cn.query(`REFRESH MATERIALIZED VIEW stats.${table}`);
         const ms = (new Date().getTime() - bench) / 1000;
@@ -41,5 +63,18 @@ export class StatsRefreshAction extends AbstractAction {
     } finally {
       this.cn.release();
     }
+  }
+
+  private todayFrequencies(d?: Date | string): CronFrequency[] {
+    const freq: CronFrequency[] = ['daily'];
+    const now = new Date(d || new Date());
+
+    // on Mondays
+    if (now.getDay() === 1) freq.push('weekly');
+
+    // on first day of the month
+    if (now.getDate() === 1) freq.push('monthly');
+
+    return freq;
   }
 }
