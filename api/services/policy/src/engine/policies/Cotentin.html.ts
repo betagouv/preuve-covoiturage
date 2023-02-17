2670412fdb2f92ee7e60b694886c2dc69a958b1c
Julien Parmentier <julien.parmentier@beta.gouv.fr>
2023-02-17 11:44:51 +0100

    feat(policy): add cotentin

---
+++ api/services/policy/src/engine/policies/Cotentin.html.ts
@@ -0,0 +1,19 @@
+export const description = `<p _ngcontent-pmm-c231="" id="summary" class="campaignSummaryText-content-text">
+<p>Campagne d’incitation au covoiturage du <b> 1 janvier 2023 au 31 décembre 2023</b>, toute la semaine
+</p>
+<p>Cette campagne est limitée à
+  <b>150 000 euros </b>.
+</p>
+<p>Les <b> passagers </b> effectuant un trajet d'au moins 2 km, avec pour origine ou destination la CA du Cotentin, 
+sont incités selon les règles suivantes : </p>
+<ul>
+  <li><b>De 2 à 20km : 2 euro par trajet par passager.</b></li>
+  <li><b>De 20 à 40km : 0.1 euro par km par passager avec un maximum de 2€</b></li>
+</ul>
+<p>Les restrictions suivantes seront appliquées :</p>
+<ul>
+  <li><b>12 trajets maximum pour le conducteur par jour.</b></li>
+</ul>
+<p>La campagne est limitée à l'opérateur <b>Klaxit</b> proposant des preuves de classe <b>B ou C</b>.</p>
+<p></p>
+</p>`;
