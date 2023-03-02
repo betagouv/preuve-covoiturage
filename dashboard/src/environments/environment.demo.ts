41e89d85fecf4b6f6df18d2c698f6bf7eaa0896b
Julien Parmentier <julien.parmentier@beta.gouv.fr>
2023-03-02 14:48:00 +0100

    fix(demo): update endpoint for new infra

--- dashboard/src/environments/environment.demo.ts
+++ dashboard/src/environments/environment.demo.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   name: 'demo',
-  apiUrl: 'https://api-demo.covoiturage.beta.gouv.fr/',
+  apiUrl: 'https://api.demo.covoiturage.beta.gouv.fr/',
   sentryDSN: 'https://5218434081c44340bdde712a3e45fb8a@sentry.incubateur.net/67',
 };
