453657738dcbbca1bd3dbb8ffcbe87a47516f3a1
Julien Parmentier <julien.parmentier@beta.gouv.fr>
2023-03-09 17:50:28 +0100

    try: remove seed and update 2m postgres pool timeout

--- integration.sh
+++ integration.sh
@@ -70,7 +70,7 @@ run() {
   ensure_certs && \
   ensure_keys && \
   start_services && \
-  seed_data && \
+  # seed_data && \
   create_bucket local-pdc-export && \
   create_bucket local-pdc-appels-de-fonds && \ 
   create_bucket local-pdc-public && \
