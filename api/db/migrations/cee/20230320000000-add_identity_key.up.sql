fea619b957ee15d8f199c30c6fc925138098e3cc
Nicolas Mérigot <nicolas.merigot@beta.gouv.fr>
2023-03-21 10:34:58 +0100

    fix: increase varchar size

--- api/db/migrations/cee/20230320000000-add_identity_key.up.sql
+++ api/db/migrations/cee/20230320000000-add_identity_key.up.sql
@@ -1,4 +1,2 @@
 ALTER TABLE cee.cee_applications
-  ADD COLUMN identity_key VARCHAR(64);
-
-CREATE INDEX IF NOT EXISTS cee_identity_key_idx ON cee.cee_applications(identity_key);
+  ADD COLUMN identity_key VARCHAR(128);
