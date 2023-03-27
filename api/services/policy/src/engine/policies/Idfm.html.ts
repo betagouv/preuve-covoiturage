456fa7acc07e1f3845719a9262b4df33f4ccede2
Julien Parmentier <julien.parmentier@beta.gouv.fr>
2023-03-27 12:15:58 +0200

    fix(idfm): add strike (#2087)

--- api/services/policy/src/engine/policies/Idfm.html.ts
+++ api/services/policy/src/engine/policies/Idfm.html.ts
@@ -29,6 +29,8 @@ export const description = `<p id="summary">
     <li><b>Le 16/03/2023</b></li>
     <li><b>Le 17/03/2023</b></li>
     <li><b>Le 23/03/2023</b></li>
+    <li><b>Le 24/03/2023</b></li>
+    <li><b>Le 28/03/2023</b></li>
   </ul>
   <p>
   La campagne est limitée aux opérateurs BlaBlaCar Daily, Karos, Klaxit proposant des preuves de classe C</b>.
