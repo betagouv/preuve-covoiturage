fa4073935e115b84414dbf29a0605b1152899e8d
Nicolas Mérigot <nicolas.merigot@beta.gouv.fr>
2023-05-02 10:26:15 +0200

    fix: explicit TPaylod type in generic

--- api/providers/normalization/src/interfaces/NormalizationProviderInterface.ts
+++ api/providers/normalization/src/interfaces/NormalizationProviderInterface.ts
@@ -1,12 +1,12 @@
 import { JourneyInterface } from '../shared/common/interfaces/JourneyInterface';
 import { CreateJourneyDTO } from '../shared/acquisition/common/interfaces/CreateJourneyDTO';
 
-export interface Acquisition<P> {
+export interface Acquisition<TPayload> {
   _id: number;
   api_version: number;
   operator_id: number;
   created_at: Date;
-  payload: P;
+  payload: TPayload;
 }
 
 import { ParamsInterface as CrossCheckParamsInterface } from '../shared/carpool/crosscheck.contract';
@@ -15,6 +15,6 @@ export { JourneyInterface as PayloadV2 };
 export { CreateJourneyDTO as PayloadV3 };
 export type ResultInterface = CrossCheckParamsInterface;
 
-export interface NormalizationProviderInterface<P> {
-  handle(params: Acquisition<P>): Promise<ResultInterface>;
+export interface NormalizationProviderInterface<TPayload> {
+  handle(params: Acquisition<TPayload>): Promise<ResultInterface>;
 }
