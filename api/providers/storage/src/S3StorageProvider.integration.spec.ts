4d8f75b9717396897ed4a1f759a3358ba6660806
Jonathan Fallon <jonathan@scopopop.com>
2023-05-23 16:05:43 +0200

    remove NODE_TLS_REJECT_UNAUTHORIZED in test

--- api/providers/storage/src/S3StorageProvider.integration.spec.ts
+++ api/providers/storage/src/S3StorageProvider.integration.spec.ts
@@ -2,15 +2,17 @@ import { ConfigStore } from '@ilos/core/dist/extensions';
 import test from 'ava';
 import axios from 'axios';
 import { writeFile } from 'fs/promises';
+import https from 'https';
 import { tmpdir } from 'os';
 import { join } from 'path';
 import { BucketName } from './interfaces/BucketName';
 import { S3StorageProvider } from './S3StorageProvider';
 
 test('should be uploading file with bucket as sub-domain', async (t) => {
-  process.env.AWS_BUCKET_EXPORT_URL = 'https://s3.covoiturage.test';
-  process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
-  const s3 = new S3StorageProvider(new ConfigStore({}));
+  const httpsAgent = new https.Agent({ rejectUnauthorized: false });
+  const config = new ConfigStore({ file: { bucket: { options: { httpOptions: { agent: httpsAgent } } } } });
+  process.env.AWS_BUCKET_EXPORT_URL = 'https://local-pdc-export.s3.covoiturage.test';
+  const s3 = new S3StorageProvider(config);
   await s3.init();
   const filename = 'test2.csv';
   const filecontent = { hello: 'world' };
@@ -18,7 +20,6 @@ test('should be uploading file with bucket as sub-domain', async (t) => {
   await writeFile(filepath, JSON.stringify(filecontent));
   await s3.upload(BucketName.Export, filepath, filename);
   const url = await s3.getPublicUrl(BucketName.Export, filename);
-  const response = await axios.get(url);
+  const response = await axios.get(url, { httpsAgent });
   t.deepEqual(filecontent, response.data);
-  delete process.env.NODE_TLS_REJECT_UNAUTHORIZED;
 });
