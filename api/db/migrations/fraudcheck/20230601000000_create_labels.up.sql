f1c3b576b39f23f1e829fe25a7ee29ebf069fa45
Julien Parmentier <julien.parmentier@beta.gouv.fr>
2023-06-06 11:09:00 +0200

    feat: fix migration, and update carpool query

--- api/db/migrations/fraudcheck/20230601000000_create_labels.up.sql
+++ api/db/migrations/fraudcheck/20230601000000_create_labels.up.sql
@@ -3,9 +3,8 @@ CREATE TABLE IF NOT EXISTS fraudcheck.labels
   _id serial primary key,
   created_at timestamp with time zone NOT NULL DEFAULT NOW(),
   carpool_id integer NOT NULL REFERENCES carpool.carpools(_id),
-  geo_code varchar(9);
-  label string NOT NULL,
+  label varchar NOT NULL,
+  geo_code varchar(9)
 );
 
 CREATE UNIQUE INDEX on fraudcheck.labels (carpool_id, label);
-CREATE INDEX on fraudcheck.labels (carpool_id);
