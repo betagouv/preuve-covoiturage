03b44651d7ebab0124d6ba76ddbd722818f65cdf
nmrgt <nicolas.merigot@gmail.com>
2023-07-10 14:30:49 +0200

    fix: avoid applying ratelimit if token is provided (#2201)

---
+++ api/proxy/src/middlewares/metricsMiddleware.ts
@@ -0,0 +1,14 @@
+import { hostname as osHostname } from 'os';
+import { NextFunction, Request, Response } from 'express';
+import { rateLimiter } from './rateLimiter';
+
+export function metricsMiddleware(endpoint: string) {
+  const rl = rateLimiter({ windowMs: 60 * 1000 * 5, max: 100 }, `rate-${endpoint}-${osHostname()}`);
+  const token = process.env.APP_MONITORING_TOKEN;
+  return async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    if (token && req.headers['x-monitoring-token'] === token) {
+      return next();
+    }
+    return rl(req, res, next);
+  };
+}
