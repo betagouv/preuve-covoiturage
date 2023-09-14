20680449c9f2bfd0c377bede5cace9717eecadd1
Jonathan Fallon <jonathan@scopopop.com>
2023-09-14 15:28:58 +0200

    fix localhost url

--- api/proxy/src/config/proxy.ts
+++ api/proxy/src/config/proxy.ts
@@ -1,10 +1,10 @@
 import { hostname as osHostname } from 'os';
 import { env } from '@ilos/core';
 
-export const appUrl = env.or_fail('APP_APP_URL', 'http://localhost:4200');
-export const apiUrl = env.or_fail('APP_API_URL', 'http://localhost:8080');
-export const certUrl = env.or_fail('APP_CERT_URL', 'https://localhost:4200');
-export const showcase = env.or_fail('APP_SHOWCASE_URL', 'https://localhost:1313');
+export const appUrl = env('APP_APP_URL', 'http://localhost:4200');
+export const apiUrl = env('APP_API_URL', 'http://localhost:8080');
+export const certUrl = env('APP_CERT_URL', 'http://localhost:4200');
+export const showcase = env('APP_SHOWCASE_URL', 'https://localhost:1313');
 
 export const port = env.or_int('PORT', 8080);
 export const hostname = osHostname();
