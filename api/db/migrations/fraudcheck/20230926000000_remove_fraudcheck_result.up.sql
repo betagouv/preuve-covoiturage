b1415105170015eccec0a2ab453fa08e1b3843eb
Julien Parmentier <julien.parmentier@beta.gouv.fr>
2023-09-28 17:40:12 +0200

    fix(migration): remove index and trigger sql function

--- api/db/migrations/fraudcheck/20230926000000_remove_fraudcheck_result.up.sql
+++ api/db/migrations/fraudcheck/20230926000000_remove_fraudcheck_result.up.sql
@@ -1,2 +1,11 @@
+DROP INDEX fraudcheck.fraudchecks_acquisition_id_idx;
+DROP INDEX fraudcheck.fraudchecks_created_at_idx;
+DROP INDEX fraudcheck.fraudchecks_status_idx;
+
+DROP INDEX fraudcheck.results_acquisition_id_idx;
+DROP INDEX fraudcheck.results_uuid_idx;
+
+DROP TRIGGER IF EXISTS touch_fraud_updated_at on fraudcheck.fraudchecks;
+
 DROP TABLE fraudcheck.fraudchecks;
 DROP TABLE fraudcheck.results;
