5b4f9f6a689a232515f62a060a8343008f156ed5
Nicolas Mérigot <nicolas.merigot@beta.gouv.fr>
2023-10-02 13:34:51 +0200

    wip

---
+++ api/providers/carpool/src/repositories/CarpoolRepository.integration.spec.mts
@@ -0,0 +1,100 @@
+import anyTest, { TestFn } from 'ava';
+import { makeDbBeforeAfter, DbContext } from '@pdc/helper-test';
+import { CarpoolRepository } from './CarpoolRepository';
+import { insertableCarpool } from '../mocks/database/carpool';
+
+interface TestContext {
+  repository: CarpoolRepository;
+  operator_id: number;
+  db: DbContext;
+}
+
+const test = anyTest as TestFn<TestContext>;
+const { before, after } = makeDbBeforeAfter();
+
+test.before(async (t) => {
+  t.context.operator_id = 1;
+  const db = await before();
+  t.context.db = db;
+  t.context.repository = new CarpoolRepository(t.context.db.connection);
+});
+
+test.after.always(async (t) => {
+  await after(t.context.db);
+});
+
+
+test.serial('Should create carpool', async (t) => {
+  const { operator_id } = t.context;
+  const data = { ...insertableCarpool };
+
+  const carpool = await t.context.repository.register(data);
+  const result = await t.context.db.connection.getClient().query({
+    text: `
+      SELECT *
+      FROM ${t.context.repository.table}
+      WHERE _id = $1
+    `,
+    values: [carpool._id],
+  });
+
+  t.deepEqual(
+    result,
+    { ...result, ...data},
+  );
+});
+
+// test.serial('Should update acquisition', async (t) => {
+//   const { operator_id } = t.context;
+//   await t.context.db.connection.getClient().query({
+//     text: `
+//       UPDATE ${t.context.repository.table}
+//       SET status = 'ok', try_count = 50
+//       WHERE operator_id = $1 AND journey_id = $2
+//     `,
+//     values: [operator_id, '2'],
+//   });
+//   const initialData = [{ operator_journey_id: '3' }, { operator_journey_id: '4' }, { operator_journey_id: '5' }].map(
+//     createPayload,
+//   );
+//   const data = [
+//     { operator_journey_id: '1', request_id: 'other request id' },
+//     { operator_journey_id: '2', request_id: 'other request id' },
+//   ].map(createPayload);
+// 
+//   // 2 is not update because 'ok' status
+//   const acqs = await t.context.repository.createOrUpdateMany(data);
+//   t.deepEqual(
+//     acqs.map((v) => v.operator_journey_id),
+//     ['1'],
+//   );
+// 
+//   const result = await t.context.db.connection.getClient().query({
+//     text: `
+//       SELECT
+//         operator_id,
+//         journey_id as operator_journey_id,
+//         application_id,
+//         api_version,
+//         request_id,
+//         payload,
+//         status,
+//         try_count
+//       FROM ${t.context.repository.table}
+//       WHERE operator_id = $1
+//       AND request_id IS NOT NULL
+//       ORDER BY journey_id
+//     `,
+//     values: [operator_id],
+//   });
+// 
+//   t.is(result.rowCount, 5);
+//   t.deepEqual(
+//     result.rows,
+//     [...data, ...initialData].map((d) => {
+//       if (d.operator_journey_id !== '2') return { ...d, status: 'pending', try_count: 0 };
+//       return { ...d, request_id: 'my request id', status: 'ok', try_count: 50 };
+//     }),
+//   );
+// });
+// 
--- api/providers/carpool/src/repositories/CarpoolRepository.ts
+++ api/providers/carpool/src/repositories/CarpoolRepository.ts
@@ -1,16 +1,93 @@
 import { provider } from '@ilos/common';
 import { PoolClient, PostgresConnection } from '@ilos/connection-postgres';
-import sql, { empty, join, raw } from 'sql-template-tag';
+import sql, { join, raw } from 'sql-template-tag';
 import { Id, InsertableCarpool, UpdatableCarpool, Uuid, WritenCarpool } from '../interfaces';
 
 @provider()
 export class CarpoolRepository {
+  readonly table = 'carpool_v2.carpools';
+
   constructor(protected connection: PostgresConnection) {}
 
   public async register(data: InsertableCarpool, client?: PoolClient): Promise<WritenCarpool> {
     const cl = client ?? this.connection.getClient();
-    const sqlQuery = sql`SELECT`
-    throw new Error();
+    const sqlQuery = sql`
+      INSERT INTO ${raw(this.table)} VALUES (
+        operator_id,
+        operator_journey_id,
+        operator_trip_id,
+        operator_class,
+        start_datetime,
+        start_position,
+        end_datetime,
+        end_position,
+        distance,
+        licence_plate,
+        driver_identity_key,
+        driver_operator_user_id,
+        driver_phone,
+        driver_phone_trunc,
+        driver_firstname,
+        driver_lastname,
+        driver_email,
+        driver_company,
+        driver_travelpass_name,
+        driver_travelpass_user_id,
+        driver_revenue,
+        passenger_identity_key,
+        passenger_operator_user_id,
+        passenger_phone,
+        passenger_phone_trunc,
+        passenger_firstname,
+        passenger_lastname,
+        passenger_email,
+        passenger_company,
+        passenger_travelpass_name,
+        passenger_travelpass_user_id,
+        passenger_over_18,
+        passenger_seats,
+        passenger_contribution,
+        passenger_payments
+      ) VALUES (
+        ${data.operator_id},
+        ${data.operator_journey_id},
+        ${data.operator_trip_id},
+        ${data.operator_class},
+        ${data.start_datetime},
+        ${data.start_position},
+        ${data.end_datetime},
+        ${data.end_position},
+        ${data.distance},
+        ${data.licence_plate},
+        ${data.driver_identity_key},
+        ${data.driver_operator_user_id},
+        ${data.driver_phone},
+        ${data.driver_phone_trunc},
+        ${data.driver_firstname},
+        ${data.driver_lastname},
+        ${data.driver_email},
+        ${data.driver_company},
+        ${data.driver_travelpass_name},
+        ${data.driver_travelpass_user_id},
+        ${data.driver_revenue},
+        ${data.passenger_identity_key},
+        ${data.passenger_operator_user_id},
+        ${data.passenger_phone},
+        ${data.passenger_phone_trunc},
+        ${data.passenger_firstname},
+        ${data.passenger_lastname},
+        ${data.passenger_email},
+        ${data.passenger_company},
+        ${data.passenger_travelpass_name},
+        ${data.passenger_travelpass_user_id},
+        ${data.passenger_over_18},
+        ${data.passenger_seats},
+        ${data.passenger_contribution},
+        ${data.passenger_payments}
+      ) RETURNING _id, created_at, updated_at
+    `;
+    const result = await cl.query<WritenCarpool>(sqlQuery);
+    return result.rows.pop();
   }
 
   public async update(
@@ -19,6 +96,24 @@ export class CarpoolRepository {
     data: UpdatableCarpool,
     client?: PoolClient,
   ): Promise<WritenCarpool> {
-    throw new Error();
+    const cl = client ?? this.connection.getClient();
+    const keys = Object.keys(data)
+      .filter(key => key in data)
+      .map(key => sql`${raw(key)} = ${data[key]}`);
+
+    if (!!keys.length) {
+      throw new Error('No data provided to be updated');
+    }
+  
+    const sqlQuery = sql`
+       UPDATE ${raw(this.table)}
+       SET ${join(keys, ',')}
+       WHERE
+         operator_id = ${operator_id} AND
+         operator_journey_id = ${operator_journey_id}
+       RETURNING _id, created_at, updated_at
+    `;
+    const result = await cl.query<WritenCarpool>(sqlQuery);
+    return result.rows.pop();
   }
 }
