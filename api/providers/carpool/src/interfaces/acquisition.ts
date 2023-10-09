65cd8373f56d50ade3711fd374bb971eddcd83b4
Nicolas Mérigot <nicolas.merigot@beta.gouv.fr>
2023-10-09 13:56:39 +0200

    chore: replace I prefix

--- api/providers/carpool/src/interfaces/acquisition.ts
+++ api/providers/carpool/src/interfaces/acquisition.ts
@@ -1,18 +1,18 @@
 import { ApiVersion, CancelCode, CancelMessage, Id, Uuid } from './common';
 import { InsertableCarpool, UpdatableCarpool } from './database/carpool';
 
-interface IApiVersion {
+interface ApiVersionObject {
   api_version: ApiVersion;
 }
 
-export type RegisterRequest = InsertableCarpool & IApiVersion;
+export type RegisterRequest = InsertableCarpool & ApiVersionObject;
 
-export interface UpdateRequest extends UpdatableCarpool, IApiVersion {
+export interface UpdateRequest extends UpdatableCarpool, ApiVersionObject {
   operator_id: Id;
   operator_journey_id: Uuid;
 }
 
-export interface CancelRequest extends IApiVersion {
+export interface CancelRequest extends ApiVersionObject {
   operator_id: Id;
   operator_journey_id: Uuid;
   cancel_code: CancelCode;
