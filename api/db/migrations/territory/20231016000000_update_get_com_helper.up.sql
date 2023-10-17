484e3ecd7253aeeb5db2118a12c5db6b18658054
Jonathan Fallon <jonathan.fallon@beta.gouv.fr>
2023-10-17 09:14:09 +0200

    cast year to ::smallint

--- api/db/migrations/territory/20231016000000_update_get_com_helper.up.sql
+++ api/db/migrations/territory/20231016000000_update_get_com_helper.up.sql
@@ -2,23 +2,24 @@ CREATE OR REPLACE FUNCTION territory.get_com_by_territory_id(_id int, year small
   with data as (select * from territory.territory_group_selector where territory_group_id = $1)
   select gp.arr as com from geo.perimeters gp join data d 
     on (d.selector_type = 'arr' OR d.selector_type = 'com') and d.selector_value = gp.arr
-  where year = $2
+  where year = $2::smallint
   union
   select gp.arr as com from geo.perimeters gp join data d 
     on d.selector_type = 'aom' and d.selector_value = gp.aom
-  where year = $2
+  where year = $2::smallint
   union
   select gp.arr as com from geo.perimeters gp join data d 
     on d.selector_type = 'epci' and d.selector_value = gp.epci
-  where year = $2
+  where year = $2::smallint
   union
   select gp.arr as com from geo.perimeters gp join data d 
     on d.selector_type = 'reg' and d.selector_value = gp.reg
-  where year = $2
+  where year = $2::smallint
     union
   select gp.arr as com from geo.perimeters gp join data d 
     on d.selector_type = 'dep' and d.selector_value = gp.dep
-  where year = $2;
+  where year = $2::smallint
+  ;
 
 $$ language sql stable;
 
