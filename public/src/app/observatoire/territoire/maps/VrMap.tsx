6e0c3954f16553c403798956c74d9aa0e6b89f7e
Ludovic Delhomme <ludovic.delhomme@datayama.fr>
2023-11-15 12:35:22 +0100

    fix error Vrmap

--- public/src/app/observatoire/territoire/maps/VrMap.tsx
+++ public/src/app/observatoire/territoire/maps/VrMap.tsx
@@ -1,18 +1,18 @@
 import AppMap from '@/components/observatoire/maps/Map';
 import { Config } from '@/config';
 import { SearchParamsInterface } from '@/interfaces/observatoire/componentsInterfaces';
-import { LineLayer, Layer, Popup, Source } from 'react-map-gl/maplibre';
+import { LineLayer, Layer, Source } from 'react-map-gl/maplibre';
 import { LngLatBoundsLike } from 'maplibre-gl';
 import { cmsHost } from "@/helpers/cms";
 import { useJson } from '@/hooks/useJson';
 import { FeatureCollection } from 'geojson';
 
-export default function VrMap({ title, params }: { title: string; params: SearchParamsInterface }) {
+export default function VrMap({ title}: { title: string }) {
   const mapTitle = title;
   const mapStyle = Config.get<string>('observatoire.mapStyle');
   const bounds = [-5.225, 41.333, 9.55, 51.2] as LngLatBoundsLike;
   const url = `${cmsHost}/assets/897ba3a7-847e-4522-aead-7d8dd0db63c6?download`;
-  const { data, error, loading } = useJson<FeatureCollection>(url);
+  const { data } = useJson<FeatureCollection>(url);
   const layer: LineLayer = {
     id: 'vr',
     source:'vr',
