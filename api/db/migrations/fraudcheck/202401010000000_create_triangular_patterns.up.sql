608627def3ff81dfd09a64c0c0c78efd92b21efc
panayo <papoutsispanayotis@gmail.com>
2024-01-09 15:27:48 +0200

    Add scrips sql for triangular and user history

---
+++ api/db/migrations/fraudcheck/202401010000000_create_triangular_patterns.up.sql
@@ -0,0 +1,17 @@
+CREATE SCHEMA IF NOT EXISTS fraudcheck;
+
+CREATE TABLE IF NOT EXISTS fraudcheck.triangular_patterns (
+    id SERIAL PRIMARY KEY,
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+    phone_trunc VARCHAR(20),
+    num_participants 
+    num_trips FLOAT,
+    operator_list VARCHAR(255),
+    num_operators INTEGER,
+    average_duration FLOAT,
+    departure_date TIMESTAMP WITH TIME ZONE,
+    end_date TIMESTAMP WITH TIME ZONE,
+    average_daily_trips FLOAT,
+    "level" INTEGER
+);
+CREATE UNIQUE INDEX on fraudcheck.triangular_patterns on('phone_trunc', 'departure_date', 'end_date');
---
+++ api/db/migrations/fraudcheck/202401010000000_create_user_phone_change_history.down.sql
@@ -0,0 +1,3 @@
+DROP TABLE IF EXISTS fraudcheck.user_phone_change_history;
+
+DROP SCHEMA IF EXISTS fraudcheck;
