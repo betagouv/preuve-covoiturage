4bab01def333585bc96f71c19536b07af3b97dcc
Julien Parmentier <julien.parmentier@beta.gouv.fr>
2024-01-17 15:39:46 +0100

    fix(sql) avoid exception if index do not exist

--- api/db/migrations/fraudcheck/20230926000000_remove_fraudcheck_result.up.sql
+++ api/db/migrations/fraudcheck/20230926000000_remove_fraudcheck_result.up.sql
@@ -1,9 +1,9 @@
-DROP INDEX fraudcheck.fraudchecks_acquisition_id_idx;
-DROP INDEX fraudcheck.fraudchecks_created_at_idx;
-DROP INDEX fraudcheck.fraudchecks_status_idx;
+DROP INDEX IF EXISTS fraudcheck.fraudchecks_acquisition_id_idx;
+DROP INDEX IF EXISTS fraudcheck.fraudchecks_created_at_idx;
+DROP INDEX IF EXISTS fraudcheck.fraudchecks_status_idx;
 
-DROP INDEX fraudcheck.results_acquisition_id_idx;
-DROP INDEX fraudcheck.results_uuid_idx;
+DROP INDEX IF EXISTS fraudcheck.results_acquisition_id_idx;
+DROP INDEX IF EXISTS fraudcheck.results_uuid_idx;
 
 DROP TRIGGER IF EXISTS touch_fraud_updated_at on fraudcheck.fraudchecks;
 
