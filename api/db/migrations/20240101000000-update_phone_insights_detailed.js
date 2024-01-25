95433c7e74eb9ef5d484356f0966ce02f996e6ae
panayo <papoutsispanayotis@gmail.com>
2024-01-25 13:01:56 +0200

    Add js script to alter table

---
+++ api/db/migrations/20240101000000-update_phone_insights_detailed.js
@@ -0,0 +1,8 @@
+'use strict';
+
+var { createMigration } = require('../helpers/createMigration');
+var { setup, up, down } = createMigration(['fraudcheck/20240101000000_update_phone_insights_detailed'], __dirname);
+
+exports.setup = setup;
+exports.up = up;
+exports.down = down;
