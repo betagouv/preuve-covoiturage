4ab52aa93d68d839d409eaa0254be89bfb0701a1
Jonathan Fallon <jonathan.fallon@beta.gouv.fr>
2024-02-08 15:24:31 +0100

    fix(campaign): campagnes (#2405)
    * update Pays Basque Adour (946) description
    * update Cannes (1018)
    * add PETR Lunevillois S1 2023 campaign

---
+++ api/services/policy/src/engine/policies/PetrLunevilloisS12023.html.ts
@@ -0,0 +1,22 @@
+/* eslint-disable */
+
+export const description = `<div _ngcontent-pmm-c231="" id="summary" class="campaignSummaryText-content-text">
+
+  <p>Campagne d'incitation au covoiturage du <b> 8 janvier 2024 au 7 juin 2024</b>, toute la semaine</p>
+  <p>Cette campagne est limitée à <b>10 000 euros</b>.</p>
+
+  <p>Les <b> conducteurs </b> effectuant un trajet en covoiturage d'au moins 2 km sont incités selon les règles suivantes :</p>
+
+  <ul>
+    <li><b>De 2 à 60 km : 7 centimes d'euro par km et par trajet par passager transporté</b></li>
+  </ul>
+  
+  <p>Les restrictions suivantes seront appliquées :</p>
+  
+  <ul>
+    <li><b>2 trajets maximum pour le conducteur par jour.</b></li>
+  </ul>
+  
+  <p>La campagne est limitée à l'opérateur Mobicoop proposant des preuves de classe <b>C</b>.</p>
+
+</div>`;
