2245549429570a56df7c9849c47901046fc6ca6e
Jonathan Fallon <jonathan@scopopop.com>
2024-02-08 15:06:01 +0100

    add PETR Lunevillois S1 2023 campaign

--- api/services/policy/src/engine/helpers/isInside.ts
+++ api/services/policy/src/engine/helpers/isInside.ts
@@ -4,27 +4,27 @@ import { Feature, MultiPolygon, Polygon, Properties, multiPolygon, point, polygo
 import booleanPointInPolygon from '@turf/boolean-point-in-polygon';
 
 interface IsCloseToParams {
-  shape: GeoJSON,
+  shape: GeoJSON;
 }
 
 export const isInside: StatelessRuleHelper<IsCloseToParams> = (
   ctx: StatelessContextInterface,
   params: IsCloseToParams,
 ): boolean => {
-    const start = point([ctx.carpool.start_lon, ctx.carpool.start_lat]);
-    const end = point([ctx.carpool.end_lon, ctx.carpool.end_lat]);
-    const shape = getShapeFromGeoJSON(params.shape);
-    
-    return booleanPointInPolygon(start, shape) || booleanPointInPolygon(end, shape);
+  const start = point([ctx.carpool.start_lon, ctx.carpool.start_lat]);
+  const end = point([ctx.carpool.end_lon, ctx.carpool.end_lat]);
+  const shape = getShapeFromGeoJSON(params.shape);
+
+  return booleanPointInPolygon(start, shape) || booleanPointInPolygon(end, shape);
 };
 
-function getShapeFromGeoJSON(data: GeoJSON): Feature<Polygon|MultiPolygon, Properties> {
-    if (data.type === 'Polygon') {
-      return polygon(data.coordinates);
-    }
+function getShapeFromGeoJSON(data: GeoJSON): Feature<Polygon | MultiPolygon, Properties> {
+  if (data.type === 'Polygon') {
+    return polygon(data.coordinates);
+  }
 
-    if (data.type === 'MultiPolygon') {
-      return multiPolygon(data.coordinates);
-    }
-    throw new Error('Invalid GeoJSON');
+  if (data.type === 'MultiPolygon') {
+    return multiPolygon(data.coordinates);
+  }
+  throw new Error('Invalid GeoJSON');
 }
--- api/services/policy/src/engine/policies/index.ts
+++ api/services/policy/src/engine/policies/index.ts
@@ -1,64 +1,66 @@
-import { Cotentin } from './Cotentin';
-import { Pdll2023 } from './Pdll2023';
-import { Vitre } from './Vitre';
-import { Montpellier } from './Montpellier';
 import { PolicyHandlerStaticInterface } from '../../interfaces';
+import { Atmb } from './Atmb';
+import { Cannes } from './Cannes';
+import { Cotentin } from './Cotentin';
+import { GrandPoitiers } from './GrandPoitiers';
 import { Idfm } from './Idfm';
+import { LaRochelle } from './LaRochelle';
 import { Lannion } from './Lannion';
 import { Laval } from './Laval';
+import { MetropoleSavoie } from './MetropoleSavoie';
+import { Montpellier } from './Montpellier';
 import { Mrn } from './Mrn';
 import { Nm } from './Nm';
 import { Occitanie } from './Occitanie';
+import { PaysBasque } from './PaysBasque';
 import { Pdll } from './Pdll';
+import { Pdll2023 } from './Pdll2023';
+import { Pdll2024 } from './Pdll2024';
+import { PetrLunevilloisS12023 } from './PetrLunevilloisS12023';
 import { Pmgf } from './Pmgf';
+import { Pmgf2023 } from './Pmgf2023';
+import { PmgfLate2023 } from './PmgfLate2023';
+import { SMTC } from './SMTC';
 import { Smt } from './Smt';
+import { Smt2023 } from './Smt2023';
+import { Vitre } from './Vitre';
 import { PolicyTemplateOne } from './unbound/PolicyTemplateOne';
 import { PolicyTemplateThree } from './unbound/PolicyTemplateThree';
 import { PolicyTemplateTwo } from './unbound/PolicyTemplateTwo';
-import { MetropoleSavoie } from './MetropoleSavoie';
-import { Smt2023 } from './Smt2023';
-import { LaRochelle } from './LaRochelle';
-import { PaysBasque } from './PaysBasque';
-import { Atmb } from './Atmb';
-import { Pmgf2023 } from './Pmgf2023';
-import { GrandPoitiers } from './GrandPoitiers';
-import { PmgfLate2023 } from './PmgfLate2023';
-import { Pdll2024 } from './Pdll2024';
-import { SMTC } from './SMTC';
-import { Cannes } from './Cannes';
 
 export const policies: Map<string, PolicyHandlerStaticInterface> = new Map(
   // disable prettier to avoid having it reformat to a single line
   // this helps with git conflicts when modifying the list.
   /* eslint-disable prettier/prettier */
   [
+    Atmb,
+    Cannes,
+    Cotentin,
+    GrandPoitiers,
     Idfm,
     Lannion,
+    LaRochelle,
     Laval,
+    MetropoleSavoie,
+    Montpellier,
     Mrn,
     Nm,
     Occitanie,
+    PaysBasque,
     Pdll,
+    Pdll2023,
+    Pdll2024,
+    PetrLunevilloisS12023,
     Pmgf,
+    Pmgf2023,
+    PmgfLate2023,
     PolicyTemplateOne,
     PolicyTemplateThree,
     PolicyTemplateTwo,
     Smt,
-    Montpellier,
-    MetropoleSavoie,
     Smt2023,
-    Vitre,
-    Pdll2023,
-    Cotentin,
-    LaRochelle,
-    PaysBasque,
-    Atmb,
-    Pmgf2023,
-    GrandPoitiers,
-    PmgfLate2023,
-    Pdll2024,
     SMTC,
-    Cannes
+    Vitre,
   ].map((h) => [h.id, h]),
   /* eslint-enable prettier/prettier */
 );
