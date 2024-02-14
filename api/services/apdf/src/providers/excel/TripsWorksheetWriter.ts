3081e2a124d94fc3ba4af2907df2ba12ccd03395
Jonathan Fallon <jonathan@scopopop.com>
2024-02-14 13:09:41 +0100

    style trips tab columns

--- api/services/apdf/src/providers/excel/TripsWorksheetWriter.ts
+++ api/services/apdf/src/providers/excel/TripsWorksheetWriter.ts
@@ -39,6 +39,39 @@ export class TripsWorksheetWriter extends AbstractWorksheetWriter {
   ): Promise<void> {
     const worksheet: Worksheet = this.initWorkSheet(workbookWriter, this.WORKSHEET_NAME, this.WORKSHEET_COLUMN_HEADERS);
 
+    // style columns and apply optimised width
+    const font = { name: 'Mono', family: 3, size: 10 };
+    const columns = {
+      A: { width: 33, font },
+      B: { width: 33, font },
+      C: { width: 33, font },
+      D: { width: 24, font },
+      E: { width: 24, font },
+      F: { width: 24, font },
+      G: { width: 36, font },
+      H: { width: 11, font },
+      I: { width: 24, font },
+      J: { width: 36, font },
+      K: { width: 11, font },
+      L: { width: 11, font },
+      M: { width: 11, font },
+      N: { width: 20, font },
+      O: { width: 20, font },
+      P: { width: 33, font },
+      Q: { width: 33, font },
+      R: { width: 14, font },
+      S: { width: 14, font },
+    };
+
+    Object.entries(columns).forEach(([key, value]) => {
+      Object.entries(value).forEach(([k, v]) => {
+        worksheet.getColumn(key)[k] = v;
+      });
+    });
+
+    // style the header
+    worksheet.getRow(1).font = { name: 'Mono', family: 3, bold: true, size: 10 };
+
     const b1 = new Date();
     let results: APDFTripInterface[] = await cursor.read(this.CURSOR_BATCH_SIZE);
     while (results.length !== 0) {
