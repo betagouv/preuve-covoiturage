fd349c726d4a758768b7377e39cc6dce31b4315e
Nicolas Mérigot <nicolas.merigot@beta.gouv.fr>
2024-02-27 10:50:48 +0100

    chore: remove old test

--- api/src/pdc/service-acquisition/tests/mocks/test4FailsOnMethodNotFound.ts
+++
@@ -1,6 +0,0 @@
-export const test4FailsOnMethodNotFound = {
-  id: 1,
-  jsonrpc: '2.0',
-  method: 'acquisition:createUnknown',
-  params: {},
-};
--- api/src/pdc/service-acquisition/tests/mocks/test6Nobody.ts
+++
@@ -1,4 +0,0 @@
-export const test6Nobody = {
-  journey_id: 'test6Nobody',
-  operator_class: 'A',
-};
