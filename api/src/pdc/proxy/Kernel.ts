78565dbc0fb6247f4339f91fd6d0b9a4dff096f2
Nicolas Mérigot <nicolas.merigot@beta.gouv.fr>
2024-02-27 17:09:10 +0100

    chore: drop unneeded bootstrap files

--- api/src/pdc/proxy/Kernel.ts
+++ api/src/pdc/proxy/Kernel.ts
@@ -4,23 +4,23 @@ import { Kernel as BaseKernel } from '@ilos/framework';
 import { RedisConnection } from '@ilos/connection-redis';
 import { SentryProvider } from '@pdc/providers/sentry';
 import { TokenProvider } from '@pdc/providers/token';
-import { bootstrap as acquisitionBootstrap } from '@pdc/services/acquisition/bootstrap';
-import { bootstrap as apdfBootstrap } from '@pdc/services/apdf/bootstrap';
-import { bootstrap as applicationBootstrap } from '@pdc/services/application/bootstrap';
-import { bootstrap as carpoolBootstrap } from '@pdc/services/carpool/bootstrap';
-import { bootstrap as ceeBootstrap } from '@pdc/services/cee/bootstrap';
-import { bootstrap as certificateBootstrap } from '@pdc/services/certificate/bootstrap';
-import { bootstrap as companyBootstrap } from '@pdc/services/company/bootstrap';
-import { bootstrap as exportBootstrap } from '@pdc/services/export/bootstrap';
-import { bootstrap as honorBootstrap } from '@pdc/services/honor/bootstrap';
-import { bootstrap as monitoringBootstrap } from '@pdc/services/monitoring/bootstrap';
-import { bootstrap as operatorBootstrap } from '@pdc/services/operator/bootstrap';
-import { bootstrap as policyBootstrap } from '@pdc/services/policy/bootstrap';
-import { bootstrap as territoryBootstrap } from '@pdc/services/territory/bootstrap';
-import { bootstrap as tripcheckBootstrap } from '@pdc/services/trip/bootstrap';
-import { bootstrap as userBootstrap } from '@pdc/services/user/bootstrap';
-import { bootstrap as observatoryBootstrap } from '@pdc/services/observatory/bootstrap';
-import { bootstrap as geoBootstrap } from '@pdc/services/geo/bootstrap';
+import { ServiceProvider as AcquisitionServiceProvider } from '@pdc/services/acquisition/ServiceProvider';
+import { ServiceProvider as ApdfServiceProvider } from '@pdc/services/apdf/ServiceProvider';
+import { ServiceProvider as ApplicationServiceProvider } from '@pdc/services/application/ServiceProvider';
+import { ServiceProvider as CarpoolServiceProvider } from '@pdc/services/carpool/ServiceProvider';
+import { ServiceProvider as CeeServiceProvider } from '@pdc/services/cee/ServiceProvider';
+import { ServiceProvider as CertificateServiceProvider } from '@pdc/services/certificate/ServiceProvider';
+import { ServiceProvider as CompanyServiceProvider } from '@pdc/services/company/ServiceProvider';
+import { ServiceProvider as ExportServiceProvider } from '@pdc/services/export/ServiceProvider';
+import { ServiceProvider as HonorServiceProvider } from '@pdc/services/honor/ServiceProvider';
+import { ServiceProvider as MonitoringServiceProvider } from '@pdc/services/monitoring/ServiceProvider';
+import { ServiceProvider as OperatorServiceProvider } from '@pdc/services/operator/ServiceProvider';
+import { ServiceProvider as PolicyServiceProvider } from '@pdc/services/policy/ServiceProvider';
+import { ServiceProvider as TerritoryServiceProvider } from '@pdc/services/territory/ServiceProvider';
+import { ServiceProvider as TripcheckServiceProvider } from '@pdc/services/trip/ServiceProvider';
+import { ServiceProvider as UserServiceProvider } from '@pdc/services/user/ServiceProvider';
+import { ServiceProvider as ObservatoryServiceProvider } from '@pdc/services/observatory/ServiceProvider';
+import { ServiceProvider as GeoServiceProvider } from '@pdc/services/geo/ServiceProvider';
 import { SeedCommand } from './commands/SeedCommand';
 import { config } from './config';
 import { PostgresConnection } from '../../ilos/connection-postgres';
@@ -28,23 +28,23 @@ import { PostgresConnection } from '../../ilos/connection-postgres';
 @kernel({
   config,
   children: [
-    ...acquisitionBootstrap.serviceProviders,
-    ...apdfBootstrap.serviceProviders,
-    ...applicationBootstrap.serviceProviders,
-    ...carpoolBootstrap.serviceProviders,
-    ...ceeBootstrap.serviceProviders,
-    ...certificateBootstrap.serviceProviders,
-    ...companyBootstrap.serviceProviders,
-    ...exportBootstrap.serviceProviders,
-    ...honorBootstrap.serviceProviders,
-    ...monitoringBootstrap.serviceProviders,
-    ...operatorBootstrap.serviceProviders,
-    ...policyBootstrap.serviceProviders,
-    ...territoryBootstrap.serviceProviders,
-    ...tripcheckBootstrap.serviceProviders,
-    ...userBootstrap.serviceProviders,
-    ...observatoryBootstrap.serviceProviders,
-    ...geoBootstrap.serviceProviders,
+    AcquisitionServiceProvider,
+    ApdfServiceProvider,
+    ApplicationServiceProvider,
+    CarpoolServiceProvider,
+    CeeServiceProvider,
+    CertificateServiceProvider,
+    CompanyServiceProvider,
+    ExportServiceProvider,
+    HonorServiceProvider,
+    MonitoringServiceProvider,
+    OperatorServiceProvider,
+    PolicyServiceProvider,
+    TerritoryServiceProvider,
+    TripcheckServiceProvider,
+    UserServiceProvider,
+    ObservatoryServiceProvider,
+    GeoServiceProvider,
   ],
   providers: [SentryProvider, TokenProvider],
   commands: [SeedCommand, Commands.CallCommand],
