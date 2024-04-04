b0a4a98e8479ebf1b02506fbf1235b4df1c7ae62
Ludovic Delhomme <ludovic.delhomme@datayama.fr>
2024-04-04 16:24:04 +0200

    wip etl

---
+++ etl/src/datasets/AiresCovoiturage.ts
@@ -0,0 +1,86 @@
+import { AbstractDataset, ArchiveFileTypeEnum, FileTypeEnum, StaticAbstractDataset } from '@betagouvpdc/evolution-geo';
+
+export function AiresCovoiturage(url: string): StaticAbstractDataset {
+  return class extends AbstractDataset {
+    static producer = 'transport_data_gouv';
+    static dataset = 'aires_covoiturage';
+    static year = 2023;
+    static url = url;
+    static skipStatePersistence = true;
+    static table = `aires_covoiturage_temp`;
+    readonly targetTable = 'aires_covoiturage';
+    readonly fileArchiveType: ArchiveFileTypeEnum = ArchiveFileTypeEnum.None;
+    readonly rows: Map<string, [string, string]> = new Map([
+      ['id_lieu', ['id_lieu', 'varchar']],
+      ['nom_lieu', ['nom_lieu', 'varchar']],
+      ['ad_lieu', ['ad_lieu', 'varchar']],
+      ['com_lieu', ['com_lieu', 'varchar']],
+      ['insee', ['insee', 'varchar']],
+      ['type', ['type', 'varchar']],
+      ['date_maj', ['date_maj', 'date']],
+      ['ouvert', ['ouvert', 'boolean']],
+      ['source', ['source', 'varchar']],
+      ['xlong', ['Xlong', 'varchar']],
+      ['ylat', ['Ylat', 'varchar']],
+      ['nbre_pl', ['nbre_pl', 'varchar']],
+      ['nbre_pmr', ['nbre_pmr', 'varchar']],
+      ['duree', ['duree', 'varchar']],
+      ['horaires', ['horaires', 'varchar']],
+      ['proprio', ['proprio', 'varchar']],
+      ['lumiere', ['lumiere', 'varchar']],
+      ['comm', ['comm', 'varchar']],
+    ]);
+    fileType: FileTypeEnum = FileTypeEnum.Csv;
+    sheetOptions = {
+      delimiter: ',',
+      columns: true,
+    };
+
+    readonly importSql = `
+      INSERT INTO ${this.targetTableWithSchema} (
+        id_lieu,
+        nom_lieu,
+        ad_lieu,
+        com_lieu,
+        insee,
+        type,
+        date_maj,
+        ouvert,
+        source,
+        xlong,
+        ylat,
+        nbre_pl,
+        nbre_pmr,
+        duree,
+        horaires,
+        proprio,
+        lumiere,
+        comm,
+        geom
+      ) SELECT
+        id_lieu,
+        nom_lieu,
+        ad_lieu,
+        com_lieu,
+        insee,
+        type,
+        date_maj,
+        ouvert,
+        source,
+        xlong,
+        ylat,
+        nbre_pl,
+        nbre_pmr,
+        duree,
+        horaires,
+        proprio,
+        lumiere,
+        comm,
+        ST_SetSRID(ST_Point(trim(xlong,chr(160))::float,trim(ylat,chr(160))::float),4326)
+      FROM ${this.tableWithSchema} 
+      ON CONFLICT 
+      ON CONSTRAINT ${this.targetTable}_id_lieu_key
+      DO NOTHING;
+    `;
+  };
+}
---
+++ etl/src/helpers.ts
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+export async function getAiresLastUrl(url: string) {
+  try {
+    const response = await axios.get(url);
+    const fileUrl = response.data.history[0].payload.permanent_url;
+    console.debug(`fileUrl: ${fileUrl}`);
+    return fileUrl;
+  } catch (e) {
+    console.error(e.response.data);
+  }
+}
