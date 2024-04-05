36fac5ef4c1a6f31657be7fb16a9de5e37493790
Ludovic Delhomme <ludovic.delhomme@datayama.fr>
2024-04-05 10:05:04 +0200

    fix inject config in constructor

--- api/src/pdc/services/territory/actions/geo/IndexAllGeoAction.ts
+++ api/src/pdc/services/territory/actions/geo/IndexAllGeoAction.ts
@@ -1,22 +1,25 @@
-import { handler } from '@ilos/common';
+import { ConfigInterfaceResolver, handler } from '@ilos/common';
 import { Action as AbstractAction } from '@ilos/core';
 import { internalOnlyMiddlewares } from '@pdc/providers/middleware';
 import { GeoRepositoryProviderInterfaceResolver } from '../../interfaces/GeoRepositoryProviderInterface';
 import { handlerConfig } from '@shared/territory/indexAllGeo.contract';
 import { indexData } from '../../helpers/meilisearch';
 import { SingleResultInterface as AllGeoResultInterface } from '@shared/territory/allGeo.contract';
-import { host, apiKey, index, batchSize } from '../../config/meilisearch';
 
 @handler({
   ...handlerConfig,
   middlewares: [...internalOnlyMiddlewares(handlerConfig.service)],
 })
 export class IndexAllGeoAction extends AbstractAction {
-  constructor(private geoRepository: GeoRepositoryProviderInterfaceResolver) {
+  constructor(
+    private geoRepository: GeoRepositoryProviderInterfaceResolver,
+    private config: ConfigInterfaceResolver,
+  ) {
     super();
   }
 
   public async handle(): Promise<void> {
+    const { host, apiKey, index, batchSize } = this.config.get('meilisearch');
     const allGeo = await this.geoRepository.getAllGeo();
     const response = await indexData<AllGeoResultInterface>({host: host, apiKey: apiKey}, index, batchSize, allGeo);
     return response;
