016a9366f838ab039b59dea66e4736ab28d4ee61
Jonathan Fallon <jonathan@scopopop.com>
2024-04-05 11:08:16 +0200

    add campaign:sync command

--- api/src/pdc/services/policy/ServiceProvider.ts
+++ api/src/pdc/services/policy/ServiceProvider.ts
@@ -3,6 +3,15 @@ import { ServiceProvider as AbstractServiceProvider } from '@ilos/core';
 import { defaultMiddlewareBindings } from '@pdc/providers/middleware';
 import { APDFNameProvider } from '@pdc/providers/storage';
 import { ValidatorExtension, ValidatorMiddleware } from '@pdc/providers/validator';
+import { binding as applySchemaBinding } from '@shared/policy/apply.schema';
+import { binding as finalizeSchemaBinding } from '@shared/policy/finalize.schema';
+import { binding as findSchemaBinding } from '@shared/policy/find.schema';
+import { binding as listSchemaBinding } from '@shared/policy/list.schema';
+import { binding as simulateOnFutureSchemaBinding } from '@shared/policy/simulateOnFuture.schema';
+import { binding as simulateOnSchemaBinding } from '@shared/policy/simulateOnPast.schema';
+import { binding as simulateOnPastGeoSchemaBinding } from '@shared/policy/simulateOnPastGeo.schema';
+import { binding as statsSchemaBinding } from '@shared/policy/stats.schema';
+import { binding as syncIncentiveSumSchemaBinding } from '@shared/policy/syncIncentiveSum.schema';
 import { ApplyAction } from './actions/ApplyAction';
 import { FinalizeAction } from './actions/FinalizeAction';
 import { FindAction } from './actions/FindAction';
@@ -16,21 +25,13 @@ import { syncIncentiveSumAction } from './actions/SyncIncentiveSumAction';
 import { ApplyCommand } from './commands/ApplyCommand';
 import { FinalizeCommand } from './commands/FinalizeCommand';
 import { StatsCommand } from './commands/StatsCommand';
+import { SyncCommand } from './commands/SyncCommand';
 import { config } from './config';
 import { IncentiveRepositoryProvider } from './providers/IncentiveRepositoryProvider';
 import { MetadataRepositoryProvider } from './providers/MetadataRepositoryProvider';
 import { PolicyRepositoryProvider } from './providers/PolicyRepositoryProvider';
 import { TerritoryRepositoryProvider } from './providers/TerritoryRepositoryProvider';
 import { TripRepositoryProvider } from './providers/TripRepositoryProvider';
-import { binding as applySchemaBinding } from '@shared/policy/apply.schema';
-import { binding as finalizeSchemaBinding } from '@shared/policy/finalize.schema';
-import { binding as findSchemaBinding } from '@shared/policy/find.schema';
-import { binding as listSchemaBinding } from '@shared/policy/list.schema';
-import { binding as simulateOnFutureSchemaBinding } from '@shared/policy/simulateOnFuture.schema';
-import { binding as simulateOnSchemaBinding } from '@shared/policy/simulateOnPast.schema';
-import { binding as simulateOnPastGeoSchemaBinding } from '@shared/policy/simulateOnPastGeo.schema';
-import { binding as statsSchemaBinding } from '@shared/policy/stats.schema';
-import { binding as syncIncentiveSumSchemaBinding } from '@shared/policy/syncIncentiveSum.schema';
 
 @serviceProvider({
   config,
@@ -65,7 +66,7 @@ import { binding as syncIncentiveSumSchemaBinding } from '@shared/policy/syncInc
     StatsAction,
     syncIncentiveSumAction,
   ],
-  commands: [ApplyCommand, FinalizeCommand, StatsCommand],
+  commands: [ApplyCommand, FinalizeCommand, StatsCommand, SyncCommand],
   queues: ['campaign', 'policy'],
   middlewares: [...defaultMiddlewareBindings, ['validate', ValidatorMiddleware]],
 })
---
+++ api/src/pdc/services/policy/commands/SyncCommand.ts
@@ -0,0 +1,23 @@
+import { command, CommandInterface, ResultType } from '@ilos/common';
+import { PolicyRepositoryProviderInterfaceResolver } from '../interfaces';
+
+@command()
+export class SyncCommand implements CommandInterface {
+  static readonly signature: string = 'campaign:sync';
+  static readonly description: string = 'Sync incentive sum and status for all campaigns';
+  static readonly options = [];
+
+  constructor(protected policyRepository: PolicyRepositoryProviderInterfaceResolver) {}
+
+  public async call(): Promise<ResultType> {
+    // sync status
+    console.info('Syncing campaign statuses');
+    await this.policyRepository.updateAllCampaignStatuses();
+
+    // sync incentive sum
+    console.info('Syncing campaign incentive_sum');
+    for (const campaign_id of await this.policyRepository.listApplicablePoliciesId()) {
+      await this.policyRepository.syncIncentiveSum(campaign_id);
+    }
+  }
+}
