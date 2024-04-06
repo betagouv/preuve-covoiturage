40f7cba1ff288728edef1371893eb5ddca3f7b05
Jonathan Fallon <jonathan@scopopop.com>
2024-04-06 08:44:50 +0200

    fix: update SMTC2024 description

--- api/src/pdc/services/policy/engine/policies/SMTC2024.html.ts
+++ api/src/pdc/services/policy/engine/policies/SMTC2024.html.ts
@@ -1,28 +1,41 @@
 export const description = `<p _ngcontent-pmm-c231="" id="summary" class="campaignSummaryText-content-text">
-<p>Campagne d’incitation au covoiturage du <b> 01 janvier 2024 au 31 décembre 2025</b>
-</p>
-<p>Cette campagne est limitée à
-  <b>30 000 euros </b>.
-</p>
+
 <p>
-Le périmètre géographique de la campagne comprend les zones suivantes
+  Campagne d’incitation au covoiturage du <b> 01 janvier 2024 au 31 décembre 2025</b>
 </p>
+
+<p>
+  Cette campagne est limitée à <b>30 000 €</b>.
+</p>
+
+<p>
+  Le périmètre géographique de la campagne comprend les zones suivantes
+</p>
+
 <ul>
   <li>Syndicat Mixte des Transports en Commun de l’Agglomération Clermontoise (SMTC)</li>
 </ul>
-<p>Les <b> conducteurs </b> effectuant un trajet entre 5 km et 80 km,
-avec pour origine OU destination le périmètre ci-dessus,
-sont incités selon les règles suivantes : </p>
+
+<p>
+  Les <b> conducteurs </b> effectuant un trajet entre 5 km et 80 km,
+  avec pour origine OU destination le périmètre ci-dessus,
+  sont incités selon les règles suivantes :
+</p>
+
 <ul>
-  <li><b>De 5 à 80 km : 1.5 euros par trajet par passager transporté.</b></li>
+  <li><b>De 5 à 80 km : 1,50 € par trajet par passager transporté</b></li>
 </ul>
+
 <p>Les restrictions suivantes seront appliquées :</p>
+
 <ul>
-  <li><b>90 € maximum pour le conducteur par mois.</b></li>
+  <li><b>90,00 € maximum pour le conducteur par mois.</b></li>
   <li><b>4 trajets par jour maximum par conducteur.</b></li>
-  <li><b>540 € maximum pour le conducteur sur la durée de la campagne.</b></li>
+  <li><b>540,00 € maximum pour le conducteur sur la durée de la campagne.</b></li>
 </ul>
-<p>La campagne est limitée aux opérateurs <b>Mobicoop</b>
-proposant des preuves de classe <b>B ou C</b>.</p>
-<p></p>
-</p>`;
+
+<p>
+  La campagne est limitée aux opérateurs <b>Mov'ici</b>
+  proposant des preuves de classe <b>B ou C</b>.
+</p>
+`;
