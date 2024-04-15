9708bd0b3e45ee9b2d21533332a5bbe3b4f879ca
Jonathan Fallon <jonathan@scopopop.com>
2024-04-15 09:52:19 +0200

    use env vars for export config

--- api/src/pdc/services/export/config/export.ts
+++ api/src/pdc/services/export/config/export.ts
@@ -1,5 +1,7 @@
-// default min start 2 years ago
-export const minStartDefault = 1000 * 60 * 60 * 24 * 365 * 3;
+import { env } from '@ilos/core';
+
+// default min start 3 years ago
+export const minStartDefault = env.or_int('APP_EXPORT_MIN_START', 1000 * 60 * 60 * 24 * 365 * 3);
 
 // max end is 5 days ago due to computation and validation time
-export const maxEndDefault = 1000 * 60 * 60 * 24 * 5;
+export const maxEndDefault = env.or_int('APP_EXPORT_MAX_END', 1000 * 60 * 60 * 24 * 5);
