d00ba927d0a5393dda4a80aa01cd88f27dfeec19
Ludovic Delhomme <ludovic.delhomme@datayama.fr>
2024-04-23 10:38:57 +0200

    add cli

--- etl/src/helpers.ts
+++ etl/src/helpers.ts
@@ -1,10 +1,9 @@
 import axios from 'axios';
 
-export async function getAiresLastUrl(url: string) {
+export async function getAiresLastUrl(url: string): Promise<string> {
   try {
     const response = await axios.get(url);
     const fileUrl = response.data.history[0].payload.permanent_url;
-    console.debug(`fileUrl: ${fileUrl}`);
     return fileUrl;
   } catch (e) {
     console.error(e.response.data);
