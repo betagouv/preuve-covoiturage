f1fc43c3ad04e348a74589643eedbd967601e065
Jonathan Fallon <jonathan.fallon@beta.gouv.fr>
2024-05-27 13:57:47 +0200

    Fixes sur les campagnes (#2499)
    * PDLL2020: add tests on AOM exclusions
    
    * Grand Poitiers: fix slice boundary
    
    * SMTC comment AOM code
    
    * comments

--- api/src/pdc/services/policy/engine/policies/GrandPoitiers.ts
+++ api/src/pdc/services/policy/engine/policies/GrandPoitiers.ts
@@ -22,6 +22,8 @@ import { AbstractPolicyHandler } from './AbstractPolicyHandler';
 import { description } from './GrandPoitiers.html';
 
 // Politique Grands poitiers
+// territory_id: 323
+// aom: 200069854
 export const GrandPoitiers: PolicyHandlerStaticInterface = class
   extends AbstractPolicyHandler
   implements PolicyHandlerInterface
@@ -61,7 +63,7 @@ export const GrandPoitiers: PolicyHandlerStaticInterface = class
   protected slices: RunnableSlices = [
     {
       start: 5_000,
-      end: 79_999,
+      end: 80_000,
       fn: (ctx: StatelessContextInterface) => perSeat(ctx, 150),
     },
   ];
