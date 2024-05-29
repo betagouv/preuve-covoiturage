b7350dd7ee606e26ae01dcd9c40f08d66b7c852c
Luis Ventura <30115537+Lawiss@users.noreply.github.com>
2024-05-29 17:57:44 +0200

    Updates config of dbt project

---
+++ dbt/macros/generate_schema_name.sql
@@ -0,0 +1,17 @@
+{% macro generate_schema_name(custom_schema_name, node) -%}
+
+    {%- set default_schema = target.schema -%}
+    {%- if custom_schema_name is none -%}
+
+        {{ default_schema }}
+
+    {% elif custom_schema_name == '' %}
+
+        {{ custom_schema_name | trim }}
+
+    {%- else -%}
+
+        {{ custom_schema_name | trim }}_{{ default_schema }}
+        
+    {%- endif -%}
+{%- endmacro %}
