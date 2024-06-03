138b998e7d8c251c80d6a9d683c55993ce5767c4
Jonathan Fallon <jonathan@scopopop.com>
2024-06-03 15:23:31 +0200

    add SMTC 2024 passenger campaign

---
+++ api/src/pdc/services/policy/engine/policies/SMTC2024Passenger.html.ts
@@ -0,0 +1,39 @@
+export const description = `<p _ngcontent-pmm-c231="" id="summary" class="campaignSummaryText-content-text">
+
+<p>
+  Campagne d’incitation au covoiturage du <b>01 janvier 2024 au 31 décembre 2025</b>
+</p>
+
+<p>
+  Cette campagne est limitée à <b>15 000,00 €</b>.
+</p>
+
+<p>
+  Le périmètre géographique de la campagne comprend les zones suivantes
+</p>
+
+<ul>
+  <li>Syndicat Mixte des Transports en Commun de l’Agglomération Clermontoise (SMTC)</li>
+</ul>
+
+<p>
+  Les <b> passagers </b> effectuant un trajet entre 5 km et 30 km,
+  avec pour origine OU destination le périmètre ci-dessus,
+  sont incités selon les règles suivantes :
+</p>
+
+<ul>
+  <li><b>De 5 à 30 km : 0,10 € par kilomètre</b></li>
+</ul>
+
+<p>Les restrictions suivantes seront appliquées :</p>
+
+<ul>
+  <li><b>2 trajets par jour maximum.</b></li>
+</ul>
+
+<p>
+  La campagne est limitée aux opérateurs <b>Mov'ici</b>
+  proposant des preuves de classe <b>B ou C</b>.
+</p>
+`;
---
+++ api/src/pdc/services/policy/engine/policies/SMTC2024Passenger.ts
@@ -0,0 +1,84 @@
+import {
+  OperatorsEnum,
+  PolicyHandlerInterface,
+  PolicyHandlerParamsInterface,
+  PolicyHandlerStaticInterface,
+  StatelessContextInterface,
+} from '../../interfaces';
+import { RunnableSlices } from '../../interfaces/engine/PolicyInterface';
+import {
+  LimitTargetEnum,
+  isOperatorClassOrThrow,
+  isOperatorOrThrow,
+  onDistanceRange,
+  onDistanceRangeOrThrow,
+  perKm,
+  perSeat,
+  watchForGlobalMaxAmount,
+} from '../helpers';
+import { watchForPersonMaxTripByDay } from '../helpers/limits';
+import { AbstractPolicyHandler } from './AbstractPolicyHandler';
+import { description } from './SMTC2024Passenger.html';
+
+// Politique Syndicat Mixte des Transports en Commun de l’Agglomération Clermontoise (SMTC)
+// aom = 256300120
+export const SMTC2024Passenger: PolicyHandlerStaticInterface = class
+  extends AbstractPolicyHandler
+  implements PolicyHandlerInterface
+{
+  static readonly id = 'smtc_2024_passenger';
+  protected operators = [OperatorsEnum.MOV_ICI];
+  protected operator_class = ['B', 'C'];
+
+  constructor(public max_amount: number) {
+    super();
+    this.limits = [
+      ['be9c45cf-5e07-4c37-b8bf-59601e3607f7', 2, watchForPersonMaxTripByDay, LimitTargetEnum.Passenger],
+      ['4a17073a-e8d7-40ba-bcc2-26d126b41ca4', this.max_amount, watchForGlobalMaxAmount],
+    ];
+  }
+
+  protected slices: RunnableSlices = [
+    {
+      start: 5_000,
+      end: 30_001,
+      fn: (ctx: StatelessContextInterface) => perSeat(ctx, perKm(ctx, { amount: 10 })),
+    },
+  ];
+
+  protected processExclusion(ctx: StatelessContextInterface) {
+    isOperatorOrThrow(ctx, this.operators);
+    onDistanceRangeOrThrow(ctx, { min: 5_000, max: 30_001 });
+    isOperatorClassOrThrow(ctx, this.operator_class);
+  }
+
+  processStateless(ctx: StatelessContextInterface): void {
+    this.processExclusion(ctx);
+    super.processStateless(ctx);
+
+    // Par kilomètre
+    let amount = 0;
+    for (const { start, fn } of this.slices) {
+      if (onDistanceRange(ctx, { min: start })) {
+        amount += fn(ctx);
+      }
+    }
+
+    ctx.incentive.set(amount);
+  }
+
+  params(): PolicyHandlerParamsInterface {
+    return {
+      tz: 'Europe/Paris',
+      slices: this.slices,
+      operators: this.operators,
+      limits: {
+        glob: this.max_amount,
+      },
+    };
+  }
+
+  describe(): string {
+    return description;
+  }
+};
