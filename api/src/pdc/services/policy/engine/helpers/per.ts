c88930e91e77087623474e0f9210df94d8fc86ba
Jonathan Fallon <jonathan@scopopop.com>
2024-06-03 15:22:46 +0200

    document helpers

--- api/src/pdc/services/policy/engine/helpers/per.ts
+++ api/src/pdc/services/policy/engine/helpers/per.ts
@@ -15,6 +15,25 @@ export interface PerKmParams {
   limit?: number;
 }
 
+/**
+ * Multiply the carpool distance by the amount
+ *
+ * @example
+ * // 0,10 € per km
+ * perKm(ctx, { amount: 10 })
+ *
+ * @example
+ * // 0,10 € per km, but limit to 30 km
+ * perKm(ctx, { amount: 10, limit: 30_000 })
+ *
+ * @example
+ * // 0,10 € per km, but start at 5 km
+ * perKm(ctx, { amount: 10, offset: 5_000 })
+ *
+ * @param {StatelessContextInterface} ctx
+ * @param {PerKmParams} params
+ * @returns {number}
+ */
 export const perKm = (ctx: StatelessContextInterface, params: PerKmParams): number => {
   let { distance } = ctx.carpool;
 
@@ -33,6 +52,17 @@ export const perKm = (ctx: StatelessContextInterface, params: PerKmParams): numb
   return (distance / 1000) * params.amount;
 };
 
+/**
+ * Multiply the amount by the number of seats booked by the passenger
+ *
+ * @example
+ * // 0,10 € per seat
+ * perSeat(ctx, 10)
+ *
+ * @param {StatelessContextInterface} ctx
+ * @param {number} amount
+ * @returns {number}
+ */
 export const perSeat = (ctx: StatelessContextInterface, amount: number): number => {
   return (ctx.carpool.seats || 1) * amount;
 };
