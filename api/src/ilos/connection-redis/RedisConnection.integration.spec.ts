85422ad09c0ce978c98bd0d680bf71bb22dc6f42
Jonathan Fallon <jonathan@scopopop.com>
2024-06-07 11:50:20 +0200

    migrate ilos/connection-redis

--- api/src/ilos/connection-redis/RedisConnection.integration.spec.ts
+++ api/src/ilos/connection-redis/RedisConnection.integration.spec.ts
@@ -1,28 +1,28 @@
-import { assertEquals, assert, assertFalse, assertThrows, assertObjectMatch, afterEach, beforeEach, afterAll, beforeAll, describe, it } from '@/dev_deps.ts';
-import { RedisConnection } from './RedisConnection.ts';
+import { process } from "@/deps.ts";
+import { afterAll, assertEquals, beforeAll, describe, it } from "@/dev_deps.ts";
+import { RedisConnection } from "./RedisConnection.ts";
 
-interface Context {
-  connection: RedisConnection;
-}
+describe("RedisConnection", () => {
+  const connection = new RedisConnection(
+    process.env.APP_REDIS_URL ?? "redis://127.0.0.1:6379",
+  );
 
-const test = anyTest as TestFn<Context>;
-
-beforeAll((t) => {
-  t.context = {
-    connection: new RedisConnection(process.env.APP_REDIS_URL ?? 'redis://127.0.0.1:6379'),
-  };
-});
-
-afterAll(async (t) => {
-  await t.context.connection.down();
-});
-
-it('Redis connection: works', async (t) => {
-  t.plan(1);
-  const client = t.context.connection.getClient();
-  client.on('ready', () => {
-    t.pass();
+  beforeAll(async () => {
+    await connection.up();
   });
-  await t.context.connection.up();
-  t.pass();
+
+  afterAll(async () => {
+    await connection.down();
+  });
+
+  it("works", async () => {
+    let assertions = 0;
+    const client = connection.getClient();
+    client.on("ready", () => {
+      ++assertions;
+    });
+    await connection.up();
+    ++assertions;
+    assertEquals(assertions, 2);
+  });
 });
