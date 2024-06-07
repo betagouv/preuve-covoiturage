f33b10a4a3c2384e7e81375b61d0ef48d0f65598
Jonathan Fallon <jonathan@scopopop.com>
2024-06-07 12:05:46 +0200

    migrate ilos/core/container

--- api/src/ilos/core/container/Container.unit.spec.ts
+++ api/src/ilos/core/container/Container.unit.spec.ts
@@ -1,120 +1,126 @@
-import { assertEquals, assert, assertFalse, assertThrows, assertObjectMatch, afterEach, beforeEach, afterAll, beforeAll, describe, it } from '@/dev_deps.ts';
-import { HandlerInterface, handler, lib, provider, inject } from '@/ilos/common/index.ts';
+import { assertEquals, describe, it } from "@/dev_deps.ts";
+import {
+  handler,
+  HandlerInterface,
+  lib,
+  provider,
+} from "@/ilos/common/index.ts";
+import { Container } from "../index.ts";
 
-import { Container } from '../index.ts';
-
-it('Container: works', async (t) => {
-  @lib()
-  class Hello {
-    public world = '!!';
-  }
-
-  @handler({
-    service: 'test',
-    method: 'hello',
-  })
-  class Test implements HandlerInterface {
-    public readonly middlewares = [];
-    constructor(public hello: Hello) {}
-
-    async call() {
-      return this.hello.world;
+describe("Container", () => {
+  it("should define a handler and call it using a generic lib()", async (t) => {
+    @lib()
+    class _Hello {
+      public world = "!!";
     }
-  }
 
-  const container = new Container();
-  const h = container.resolve(Test);
-  assertEquals(h.hello.world, '!!');
+    @handler({
+      service: "test",
+      method: "hello",
+    })
+    class Test implements HandlerInterface {
+      public readonly middlewares = [];
+      constructor(public hello: _Hello) {}
 
-  container.setHandler(Test);
+      async call() {
+        return this.hello.world;
+      }
+    }
 
-  const hBis = await container.getHandler<null, null, string>({
-    service: 'test',
-    method: 'hello',
-  })({
-    method: '',
-    params: null,
-    context: null,
+    const container = new Container();
+    const h = container.resolve(Test);
+    assertEquals(h.hello.world, "!!");
+
+    container.setHandler(Test);
+
+    const hBis = await container.getHandler<null, null, string>({
+      service: "test",
+      method: "hello",
+    })({
+      method: "",
+      params: null,
+      context: null,
+    });
+    assertEquals(hBis, "!!");
   });
-  assertEquals(hBis, '!!');
-});
 
-it('Container: works with provider', async (t) => {
-  @lib()
-  class HelloLib {
-    public world = 'yeah';
-  }
-
-  @provider()
-  class Hello {
-    public world = '!!';
-    @inject(HelloLib) helloLib: HelloLib;
-
-    boot() {
-      this.world = this.helloLib.world;
+  it("should inject a provider", async (t) => {
+    @lib()
+    class _HelloLib {
+      public world = "yeah";
     }
-  }
 
-  @handler({
-    service: 'test',
-    method: 'hello',
-  })
-  class Test implements HandlerInterface {
-    public readonly middlewares = [];
-    constructor(public hello: Hello) {}
-
-    async call() {
-      return this.hello.world;
+    @provider()
+    class _Hello {
+      public world = "!!";
+      constructor(public helloLib: _HelloLib) {}
+      boot() {
+        this.world = this.helloLib.world;
+      }
     }
-  }
 
-  const container = new Container();
-  const h = container.resolve(Test);
-  assertEquals(h.hello.world, 'yeah');
+    @handler({
+      service: "test",
+      method: "hello",
+    })
+    class Test implements HandlerInterface {
+      public readonly middlewares = [];
+      constructor(public hello: _Hello) {}
 
-  container.setHandler(Test);
-  const hbis = await container.getHandler<null, null, string>({
-    service: 'test',
-    method: 'hello',
-  })({
-    method: '',
-    params: null,
-    context: null,
+      async call() {
+        return this.hello.world;
+      }
+    }
+
+    const container = new Container();
+    const h = container.resolve(Test);
+    assertEquals(h.hello.world, "yeah");
+
+    container.setHandler(Test);
+    const hbis = await container.getHandler<null, null, string>({
+      service: "test",
+      method: "hello",
+    })({
+      method: "",
+      params: null,
+      context: null,
+    });
+    assertEquals(hbis, "yeah");
   });
-  assertEquals(hbis, 'yeah');
-});
-it('Container: works with no boot provider', async (t) => {
-  @provider()
-  class Hello {
-    public world = 'yeah';
-  }
 
-  @handler({
-    service: 'test',
-    method: 'hello',
-  })
-  class Test implements HandlerInterface {
-    public readonly middlewares = [];
-    constructor(public hello: Hello) {}
-
-    async call() {
-      return this.hello.world;
+  it("should work without a boot provider", async (t) => {
+    @provider()
+    class _Hello {
+      public world = "yeah";
     }
-  }
 
-  const container = new Container();
-  const h = container.resolve(Test);
-  assertEquals(h.hello.world, 'yeah');
+    @handler({
+      service: "test",
+      method: "hello",
+    })
+    class Test implements HandlerInterface {
+      public readonly middlewares = [];
+      constructor(public hello: _Hello) {}
 
-  container.setHandler(Test);
+      async call() {
+        return this.hello.world;
+      }
+    }
 
-  const hbis = await container.getHandler<null, null, string>({
-    service: 'test',
-    method: 'hello',
-  })({
-    method: '',
-    params: null,
-    context: null,
+    const container = new Container();
+    const h = container.resolve(Test);
+    assertEquals(h.hello.world, "yeah");
+
+    container.setHandler(Test);
+
+    const hbis = await container.getHandler<null, null, string>({
+      service: "test",
+      method: "hello",
+    })({
+      method: "",
+      params: null,
+      context: null,
+    });
+    assertEquals(hbis, "yeah");
   });
-  assertEquals(hbis, 'yeah');
 });
--- api/src/ilos/core/container/HandlerRegistry.unit.spec.ts
+++ api/src/ilos/core/container/HandlerRegistry.unit.spec.ts
@@ -1,257 +1,258 @@
-import { assertEquals, assert, assertFalse, assertThrows, assertObjectMatch, afterEach, beforeEach, afterAll, beforeAll, describe, it } from '@/dev_deps.ts';
-import { handler } from '@/ilos/common/index.ts';
+import { assert, describe, it } from "@/dev_deps.ts";
+import { handler } from "@/ilos/common/index.ts";
+import { Action } from "../foundation/Action.ts";
+import { Container } from "../index.ts";
+import { HandlerRegistry } from "./HandlerRegistry.ts";
 
-import { Container } from '../index.ts';
-import { Action } from '../foundation/Action.ts';
-import { HandlerRegistry } from './HandlerRegistry.ts';
+describe("HandlerRegistry", () => {
+  const defaultCallOptions = {
+    method: "",
+    params: null,
+    context: null,
+  };
 
-const defaultCallOptions = {
-  method: '',
-  params: null,
-  context: null,
-};
-
-it('Handler registry: works', async (t) => {
-  @handler({
-    service: 'hello',
-    method: 'world',
-  })
-  class HelloLocal extends Action {
-    async call() {
-      return 'HelloLocal';
+  it("works", async () => {
+    @handler({
+      service: "hello",
+      method: "world",
+    })
+    class HelloLocal extends Action {
+      async call() {
+        return "HelloLocal";
+      }
     }
-  }
 
-  @handler({
-    service: 'hello',
-    method: 'world',
-    queue: true,
-  })
-  class HelloLocalQueue extends Action {
-    async call() {
-      return 'HelloLocalQueue';
-    }
-  }
-
-  @handler({
-    service: 'hello',
-    method: '*',
-  })
-  class HelloLocalStar extends Action {
-    async call() {
-      return 'HelloLocalStar';
-    }
-  }
-
-  @handler({
-    service: 'hello',
-    method: 'world',
-    local: false,
-  })
-  class HelloRemote extends Action {
-    async call() {
-      return 'HelloRemote';
-    }
-  }
-
-  @handler({
-    service: 'hello',
-    method: 'world',
-    queue: true,
-    local: false,
-  })
-  class HelloRemoteQueue extends Action {
-    async call() {
-      return 'HelloRemoteQueue';
-    }
-  }
-
-  @handler({
-    service: 'hello',
-    method: '*',
-    local: false,
-  })
-  class HelloRemoteStar extends Action {
-    async call() {
-      return 'HelloRemoteStar';
-    }
-  }
-
-  const container = new Container();
-  const handlerRegistry = new HandlerRegistry(container);
-
-  handlerRegistry.set(HelloLocal);
-  handlerRegistry.set(HelloLocalQueue);
-  handlerRegistry.set(HelloLocalStar);
-  handlerRegistry.set(HelloRemote);
-  handlerRegistry.set(HelloRemoteQueue);
-  handlerRegistry.set(HelloRemoteStar);
-
-  assert(
-    (await handlerRegistry.get({
-      service: 'hello',
-      method: 'world',
-      local: true,
-    })(defaultCallOptions)) === 'HelloLocal',
-  );
-
-  assert(
-    (await handlerRegistry.get({
-      service: 'hello',
-      method: 'world',
-      local: true,
+    @handler({
+      service: "hello",
+      method: "world",
       queue: true,
-    })(defaultCallOptions)) === 'HelloLocalQueue',
-  );
+    })
+    class HelloLocalQueue extends Action {
+      async call() {
+        return "HelloLocalQueue";
+      }
+    }
 
-  assert(
-    (await handlerRegistry.get({
-      service: 'hello',
-      method: '*',
-      local: true,
-    })(defaultCallOptions)) === 'HelloLocalStar',
-  );
+    @handler({
+      service: "hello",
+      method: "*",
+    })
+    class HelloLocalStar extends Action {
+      async call() {
+        return "HelloLocalStar";
+      }
+    }
 
-  assert(
-    (await handlerRegistry.get({
-      service: 'hello',
-      method: 'notExisting',
-      local: true,
-    })(defaultCallOptions)) === 'HelloLocalStar',
-  );
-
-  assert(
-    (await handlerRegistry.get({
-      service: 'hello',
-      method: 'world',
+    @handler({
+      service: "hello",
+      method: "world",
       local: false,
-    })(defaultCallOptions)) === 'HelloRemote',
-  );
-
-  assert(
-    (await handlerRegistry.get({
-      service: 'hello',
-      method: 'world',
-      local: false,
-      queue: true,
-    })(defaultCallOptions)) === 'HelloRemote',
-  );
-
-  assert(
-    (await handlerRegistry.get({
-      service: 'hello',
-      method: '*',
-      local: false,
-    })(defaultCallOptions)) === 'HelloRemoteStar',
-  );
-
-  assert(
-    (await handlerRegistry.get({
-      service: 'hello',
-      method: 'notExisting',
-      local: false,
-    })(defaultCallOptions)) === 'HelloRemoteStar',
-  );
-});
-
-it('Handler registry: fallback to remote', async (t) => {
-  @handler({
-    service: 'hello',
-    method: 'world',
-    local: false,
-  })
-  class HelloRemote extends Action {
-    async call() {
-      return 'HelloRemote';
+    })
+    class HelloRemote extends Action {
+      async call() {
+        return "HelloRemote";
+      }
     }
-  }
 
-  @handler({
-    service: 'hello',
-    method: '*',
-    local: false,
-  })
-  class HelloRemoteStar extends Action {
-    async call() {
-      return 'HelloRemoteStar';
-    }
-  }
-
-  const container = new Container();
-  const handlerRegistry = new HandlerRegistry(container);
-
-  handlerRegistry.set(HelloRemote);
-  handlerRegistry.set(HelloRemoteStar);
-
-  assert(
-    (await handlerRegistry.get({
-      service: 'hello',
-      method: 'world',
-      local: true,
-    })(defaultCallOptions)) === 'HelloRemote',
-  );
-
-  assert(
-    (await handlerRegistry.get({
-      service: 'hello',
-      method: 'truc',
-      local: true,
+    @handler({
+      service: "hello",
+      method: "world",
       queue: true,
-    })(defaultCallOptions)) === 'HelloRemoteStar',
-  );
-});
-it('Handler registry: fallback to sync', async (t) => {
-  @handler({
-    service: 'hello',
-    method: 'world',
-  })
-  class HelloLocal extends Action {
-    async call() {
-      return 'HelloLocal';
-    }
-  }
-
-  @handler({
-    service: 'hello',
-    method: '*',
-    local: false,
-  })
-  class HelloRemoteStar extends Action {
-    async call() {
-      return 'HelloRemoteStar';
-    }
-  }
-
-  const container = new Container();
-  const handlerRegistry = new HandlerRegistry(container);
-
-  handlerRegistry.set(HelloLocal);
-  handlerRegistry.set(HelloRemoteStar);
-
-  assert(
-    (await handlerRegistry.get({
-      service: 'hello',
-      method: 'world',
-      queue: true,
-      local: true,
-    })(defaultCallOptions)) === 'HelloLocal',
-  );
-
-  assert(
-    (await handlerRegistry.get({
-      service: 'hello',
-      method: 'truc',
-      queue: true,
-      local: true,
-    })(defaultCallOptions)) === 'HelloRemoteStar',
-  );
-
-  assert(
-    (await handlerRegistry.get({
-      service: 'hello',
-      method: 'world',
       local: false,
-      queue: true,
-    })(defaultCallOptions)) === 'HelloRemoteStar',
-  );
+    })
+    class HelloRemoteQueue extends Action {
+      async call() {
+        return "HelloRemoteQueue";
+      }
+    }
+
+    @handler({
+      service: "hello",
+      method: "*",
+      local: false,
+    })
+    class HelloRemoteStar extends Action {
+      async call() {
+        return "HelloRemoteStar";
+      }
+    }
+
+    const container = new Container();
+    const handlerRegistry = new HandlerRegistry(container);
+
+    handlerRegistry.set(HelloLocal);
+    handlerRegistry.set(HelloLocalQueue);
+    handlerRegistry.set(HelloLocalStar);
+    handlerRegistry.set(HelloRemote);
+    handlerRegistry.set(HelloRemoteQueue);
+    handlerRegistry.set(HelloRemoteStar);
+
+    assert(
+      (await handlerRegistry.get<null, null>({
+        service: "hello",
+        method: "world",
+        local: true,
+      })(defaultCallOptions)) === "HelloLocal",
+    );
+
+    assert(
+      (await handlerRegistry.get<null, null>({
+        service: "hello",
+        method: "world",
+        local: true,
+        queue: true,
+      })(defaultCallOptions)) === "HelloLocalQueue",
+    );
+
+    assert(
+      (await handlerRegistry.get<null, null>({
+        service: "hello",
+        method: "*",
+        local: true,
+      })(defaultCallOptions)) === "HelloLocalStar",
+    );
+
+    assert(
+      (await handlerRegistry.get<null, null>({
+        service: "hello",
+        method: "notExisting",
+        local: true,
+      })(defaultCallOptions)) === "HelloLocalStar",
+    );
+
+    assert(
+      (await handlerRegistry.get<null, null>({
+        service: "hello",
+        method: "world",
+        local: false,
+      })(defaultCallOptions)) === "HelloRemote",
+    );
+
+    assert(
+      (await handlerRegistry.get<null, null>({
+        service: "hello",
+        method: "world",
+        local: false,
+        queue: true,
+      })(defaultCallOptions)) === "HelloRemote",
+    );
+
+    assert(
+      (await handlerRegistry.get<null, null>({
+        service: "hello",
+        method: "*",
+        local: false,
+      })(defaultCallOptions)) === "HelloRemoteStar",
+    );
+
+    assert(
+      (await handlerRegistry.get<null, null>({
+        service: "hello",
+        method: "notExisting",
+        local: false,
+      })(defaultCallOptions)) === "HelloRemoteStar",
+    );
+  });
+
+  it("fallback to remote", async () => {
+    @handler({
+      service: "hello",
+      method: "world",
+      local: false,
+    })
+    class HelloRemote extends Action {
+      async call() {
+        return "HelloRemote";
+      }
+    }
+
+    @handler({
+      service: "hello",
+      method: "*",
+      local: false,
+    })
+    class HelloRemoteStar extends Action {
+      async call() {
+        return "HelloRemoteStar";
+      }
+    }
+
+    const container = new Container();
+    const handlerRegistry = new HandlerRegistry(container);
+
+    handlerRegistry.set(HelloRemote);
+    handlerRegistry.set(HelloRemoteStar);
+
+    assert(
+      (await handlerRegistry.get<null, null>({
+        service: "hello",
+        method: "world",
+        local: true,
+      })(defaultCallOptions)) === "HelloRemote",
+    );
+
+    assert(
+      (await handlerRegistry.get<null, null>({
+        service: "hello",
+        method: "truc",
+        local: true,
+        queue: true,
+      })(defaultCallOptions)) === "HelloRemoteStar",
+    );
+  });
+  it("fallback to sync", async () => {
+    @handler({
+      service: "hello",
+      method: "world",
+    })
+    class HelloLocal extends Action {
+      async call() {
+        return "HelloLocal";
+      }
+    }
+
+    @handler({
+      service: "hello",
+      method: "*",
+      local: false,
+    })
+    class HelloRemoteStar extends Action {
+      async call() {
+        return "HelloRemoteStar";
+      }
+    }
+
+    const container = new Container();
+    const handlerRegistry = new HandlerRegistry(container);
+
+    handlerRegistry.set(HelloLocal);
+    handlerRegistry.set(HelloRemoteStar);
+
+    assert(
+      (await handlerRegistry.get<null, null>({
+        service: "hello",
+        method: "world",
+        queue: true,
+        local: true,
+      })(defaultCallOptions)) === "HelloLocal",
+    );
+
+    assert(
+      (await handlerRegistry.get<null, null>({
+        service: "hello",
+        method: "truc",
+        queue: true,
+        local: true,
+      })(defaultCallOptions)) === "HelloRemoteStar",
+    );
+
+    assert(
+      (await handlerRegistry.get<null, null>({
+        service: "hello",
+        method: "world",
+        local: false,
+        queue: true,
+      })(defaultCallOptions)) === "HelloRemoteStar",
+    );
+  });
 });
