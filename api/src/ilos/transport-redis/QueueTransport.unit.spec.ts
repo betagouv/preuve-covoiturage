2215535d9be9bd8ca7593a0d78b91805c5e075c8
Jonathan Fallon <jonathan@scopopop.com>
2024-06-10 23:50:56 +0200

    fix TransportQueue tests

--- api/src/ilos/transport-redis/QueueTransport.unit.spec.ts
+++ api/src/ilos/transport-redis/QueueTransport.unit.spec.ts
@@ -1,38 +1,13 @@
+import { process } from "@/deps.ts";
 import {
   afterAll,
   afterEach,
-  assert,
   assertEquals,
-  assertFalse,
-  assertObjectMatch,
-  assertThrows,
-  beforeAll,
   beforeEach,
   describe,
   it,
+  sinon,
 } from "@/dev_deps.ts";
-import {
-  afterAll,
-  afterEach,
-  assert,
-  assertEquals,
-  assertFalse,
-  assertObjectMatch,
-  assertThrows,
-  beforeAll,
-  beforeEach,
-  describe,
-  it,
-} from "@/dev_deps.ts";
-import { RedisConnection } from "@/ilos/connection-redis/index.ts";
-import {
-  Action,
-  Extensions,
-  Kernel,
-  ServiceProvider,
-} from "@/ilos/core/index.ts";
-import { ConnectionManagerExtension } from "@/ilos/connection-manager/index.ts";
-import { QueueExtension as ParentQueueExtension } from "@/ilos/queue/index.ts";
 import {
   ContextType,
   handler,
@@ -41,146 +16,171 @@ import {
   ResultType,
   serviceProvider,
 } from "@/ilos/common/index.ts";
+import { ConnectionManagerExtension } from "@/ilos/connection-manager/index.ts";
+import { RedisConnection } from "@/ilos/connection-redis/index.ts";
+import {
+  Action,
+  Extensions,
+  Kernel,
+  ServiceProvider,
+} from "@/ilos/core/index.ts";
+import { QueueExtension as ParentQueueExtension } from "@/ilos/queue/index.ts";
 import { QueueTransport } from "./QueueTransport.ts";
 
-interface Context {
-  sandbox: sinon.SinonSandbox;
-  transport: QueueTransport;
-}
+describe("QueueTransport", () => {
+  let sandbox: sinon.SinonSandbox | null = null;
+  let transport: QueueTransport | null = null;
 
-const test = anyTest as TestFn<Context>;
-
-beforeEach(async (t) => {
-  t.context.sandbox = sinon.createSandbox();
-  process.env.APP_WORKER = "true";
-
-  class QueueExtension extends ParentQueueExtension {
-    registerQueueHandlers() {
-      return;
-    }
-  }
-
-  @handler({
-    service: "math",
-    method: "minus",
-  })
-  class BasicAction extends Action {
-    protected async handle(
-      params: ParamsType,
-      context: ContextType,
-    ): Promise<ResultType> {
-      let count = 0;
-      if ("minus" in params) {
-        const { add } = params;
-        add.forEach((param) => {
-          count -= param;
-        });
-      } else {
-        throw new Error("Please provide add param");
-      }
-      return count;
-    }
-  }
-
-  @handler({
-    service: "math",
-    method: "add",
-  })
-  class BasicTwoAction extends Action {
-    protected async handle(
-      params: ParamsType,
-      context: ContextType,
-    ): Promise<ResultType> {
-      let count = 0;
-      if ("add" in params) {
-        const { add } = params;
-        add.forEach((param) => {
-          count += param;
-        });
-      } else {
-        throw new Error("Please provide add param");
-      }
-      return count;
-    }
-  }
-
-  @serviceProvider({
-    handlers: [BasicAction, BasicTwoAction],
-    queues: ["math"],
-  })
-  class BasicServiceProvider extends ServiceProvider {
-    extensions = [
-      Extensions.Config,
-      Extensions.Providers,
-      Extensions.Handlers,
-      QueueExtension,
-    ];
-  }
-
-  @kernelDecorator({
-    children: [BasicServiceProvider],
-    connections: [[
-      RedisConnection,
-      new RedisConnection("redis://localhost:6379"),
-    ]],
-  })
-  class BasicKernel extends Kernel {
-    readonly extensions = [Extensions.Config, ConnectionManagerExtension];
-  }
-
-  const kernel = new BasicKernel();
-  await kernel.bootstrap();
-  t.context.transport = new QueueTransport(kernel);
-
-  const fake = (connection: any, name: string, processor: Function) => ({
-    // @ts-ignore
-    name,
-    async close() {
-      return;
-    },
-    async waitUntilReady() {
-      return this;
-    },
-    on(event: string, callback: (...args: any[]) => void) {
-      return this;
-    },
-    async add(call) {
-      return processor(call);
-    },
+  // Connection is configured but not connected
+  const redis = new RedisConnection("redis://localhost:6379", {
+    lazyConnect: true,
   });
-  // @ts-ignore
-  t.context.sandbox.stub(t.context.transport, "getWorker").callsFake(fake);
-  // @ts-ignore
-  t.context.sandbox.stub(t.context.transport, "getScheduler").callsFake(fake);
-});
 
-afterEach((t) => {
-  t.context.sandbox.restore();
-});
+  beforeEach(async () => {
+    sandbox = sinon.createSandbox();
+    process.env.APP_WORKER = "true";
 
-it("Queue transport: works", async (t) => {
-  const queueTransport = t.context.transport;
-  await queueTransport.up();
-  assertEquals(queueTransport.queues.length, 1);
-  assertEquals(queueTransport.queues[0].worker.name, "math");
-  // @ts-ignore
-  const response = await queueTransport.queues[0].worker.add({
-    data: {
-      id: null,
-      jsonrpc: "2.0",
-      method: "math@latest:add",
-      params: {
+    class QueueExtension extends ParentQueueExtension {
+      registerQueueHandlers() {
+        return;
+      }
+    }
+
+    @handler({
+      service: "math",
+      method: "minus",
+    })
+    class BasicAction extends Action {
+      protected async handle(
+        params: ParamsType,
+        _context: ContextType,
+      ): Promise<ResultType> {
+        let count = 0;
+        if ("minus" in params) {
+          const { add } = params;
+          add.forEach((param: number) => {
+            count -= param;
+          });
+        } else {
+          throw new Error("Please provide add param");
+        }
+        return count;
+      }
+    }
+
+    @handler({
+      service: "math",
+      method: "add",
+    })
+    class BasicTwoAction extends Action {
+      protected async handle(
+        params: ParamsType,
+        _context: ContextType,
+      ): Promise<ResultType> {
+        let count = 0;
+        if ("add" in params) {
+          const { add } = params;
+          add.forEach((param: number) => {
+            count += param;
+          });
+        } else {
+          throw new Error("Please provide add param");
+        }
+        return count;
+      }
+    }
+
+    @serviceProvider({
+      handlers: [BasicAction, BasicTwoAction],
+      queues: ["math"],
+    })
+    class BasicServiceProvider extends ServiceProvider {
+      extensions = [
+        Extensions.Config,
+        Extensions.Providers,
+        Extensions.Handlers,
+        QueueExtension,
+      ];
+    }
+
+    @kernelDecorator({
+      children: [BasicServiceProvider],
+
+      /**
+       * RedisConnection is connects here
+       * there no need to do a redis.up() in the beforeAll() hook
+       */
+      connections: [[RedisConnection, redis]],
+    })
+    class BasicKernel extends Kernel {
+      readonly extensions = [Extensions.Config, ConnectionManagerExtension];
+    }
+
+    const kernel = new BasicKernel();
+    await kernel.bootstrap();
+    transport = new QueueTransport(kernel);
+
+    const fake = (
+      _connection: RedisConnection,
+      name: string,
+      processor: Function,
+    ) => ({
+      name,
+      async close() {
+        return;
+      },
+      async waitUntilReady() {
+        return this;
+      },
+      on(event: string, callback: (...args: any[]) => void) {
+        return this;
+      },
+      async add(call: any) {
+        return processor(call);
+      },
+    });
+
+    sandbox.stub(transport, "getWorker").callsFake(fake);
+    sandbox.stub(transport, "getScheduler").callsFake(fake);
+  });
+
+  afterEach(() => {
+    sandbox.restore();
+  });
+
+  afterAll(async () => {
+    await redis.down();
+    await transport?.down();
+  });
+
+  it("should call a handle as a job", async () => {
+    if (!transport) {
+      throw new Error("Transport is not initialized");
+    }
+
+    await transport.up();
+    assertEquals(transport.queues.length, 1);
+    assertEquals(transport.queues[0].worker.name, "math");
+
+    // @ts-ignore
+    const response = await transport.queues[0].worker.add({
+      data: {
+        id: null,
+        jsonrpc: "2.0",
+        method: "math@latest:add",
         params: {
-          add: [1, 2],
-        },
-        _context: {
-          transport: "node:http",
-          user: "me",
-          internal: false,
+          params: {
+            add: [1, 2],
+          },
+          _context: {
+            transport: "node:http",
+            user: "me",
+            internal: false,
+          },
         },
       },
-    },
+    });
+
+    assertEquals(response, 3);
   });
-  assertEquals(response as any, 3);
-  await queueTransport.down();
 });
