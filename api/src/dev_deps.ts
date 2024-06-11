633231707b40aaaedaef0276d0cf2b6c6f3d2cd2
Nicolas Mérigot <nicolas.merigot@beta.gouv.fr>
2024-06-11 12:04:51 +0200

    chore: update test

--- api/src/dev_deps.ts
+++ api/src/dev_deps.ts
@@ -1,16 +1,11 @@
 import sinon, { SinonSandbox, SinonStub, stub } from "npm:sinon@^18";
 // @deno-types="npm:@types/supertest@^6"
 import supertest from "npm:supertest@^7";
-// @deno-types="npm:@types/supertest@^6"
-export type { Agent as SuperTestAgent } from "npm:supertest@^7";
-
-import nock from "npm:nock@^13.5";
-export { nock, sinon, stub, supertest };
-export type { SinonSandbox, SinonStub };
 export {
   assert,
   assertEquals,
   assertFalse,
+  assertNotEquals,
   assertObjectMatch,
   assertRejects,
   assertStrictEquals,
@@ -24,8 +19,14 @@ export {
   describe,
   it,
 } from "https://deno.land/std@0.224.0/testing/bdd.ts";
+// @deno-types="npm:@types/supertest@^6"
+export type { Agent as SuperTestAgent } from "npm:supertest@^7";
+export { nock, sinon, stub, supertest };
+export type { SinonSandbox, SinonStub };
+
+import nock from "npm:nock@^13.5";
 // @deno-types="npm:@types/node@^20"
-export type { Context } from "node:vm";
 export { delay } from "https://deno.land/std@0.224.0/async/delay.ts";
+export type { Context } from "node:vm";
 
 export { getAvailablePort } from "https://deno.land/x/port@1.0.0/mod.ts";
--- api/src/pdc/services/policy/providers/TripRepositoryProvider.integration.spec.ts
+++ api/src/pdc/services/policy/providers/TripRepositoryProvider.integration.spec.ts
@@ -1,102 +1,113 @@
-import { DbContext, makeDbBeforeAfter } from "@/pdc/providers/test/index.ts";
-import { PolicyStatusEnum } from "@/shared/policy/common/interfaces/PolicyInterface.ts";
 import {
   afterAll,
-  afterEach,
   assert,
   assertEquals,
-  assertFalse,
-  assertObjectMatch,
-  assertThrows,
   beforeAll,
-  beforeEach,
   describe,
   it,
 } from "@/dev_deps.ts";
+import { DbContext, makeDbBeforeAfter } from "@/pdc/providers/test/index.ts";
+import { PolicyStatusEnum } from "@/shared/policy/common/interfaces/PolicyInterface.ts";
 import { Policy } from "../engine/entities/Policy.ts";
 import { Idfm } from "../engine/policies/Idfm.ts";
 import { TripRepositoryProvider } from "./TripRepositoryProvider.ts";
 
-interface TestContext {
-  db: DbContext;
-  repository: TripRepositoryProvider;
-}
+describe("TripRepositoryProvider", () => {
+  let db: DbContext;
+  let repository: TripRepositoryProvider;
 
-const test = anyTest as TestFn<TestContext>;
-const { before, after } = makeDbBeforeAfter();
+  const { before, after } = makeDbBeforeAfter();
 
-beforeAll(async (t) => {
-  const db = await before();
-  t.context.db = db;
-  t.context.repository = new TripRepositoryProvider(t.context.db.connection);
-});
-
-test.after.always(async (t) => {
-  await after(t.context.db);
-});
-
-it("Should find carpools even with fraudcheck_error", async (t) => {
-  const start_date = new Date("2024-03-01");
-  const end_date = new Date("2024-03-30");
-
-  const policy = await Policy.import({
-    _id: 1,
-    territory_id: 1,
-    territory_selector: { aom: ["217500016"] },
-    start_date,
-    end_date,
-    tz: "Europe/Paris",
-    name: "Policy",
-    handler: Idfm.id,
-    status: PolicyStatusEnum.ACTIVE,
-    incentive_sum: 5000,
-    max_amount: 10_000_000_00,
+  beforeAll(async () => {
+    db = await before();
+    repository = new TripRepositoryProvider(db.connection);
   });
 
-  const cursor = t.context.repository.findTripByPolicy(
-    policy,
-    start_date,
-    end_date,
-  );
-  const { value } = await cursor.next(); // ok carpool
-  await cursor.next(); // ok carpool
-  await cursor.next(); // fraudcheck_error carpool
-  assertEquals((value || []).length, 3);
-  assert(Array.isArray(value));
-  assertObjectMatch(
-    (value || [])
-      .map((c) => ({
-        start: c.start,
-        end: c.end,
-        operator_uuid: c.operator_uuid,
-        datetime: c.datetime,
-        distance: c.distance,
-      }))
-      .shift(),
-    {
-      start: {
-        aom: "217500016",
-        arr: "91471",
-        com: "91471",
-        country: "XXXXX",
-        dep: "91",
-        epci: "200056232",
-        reg: "11",
-        reseau: 232,
+  afterAll(async () => {
+    await after(db);
+  });
+
+  it("Should find carpools even with fraudcheck_error", async () => {
+    const start_date = new Date("2024-03-01");
+    const end_date = new Date("2024-03-30");
+
+    const policy = await Policy.import({
+      _id: 1,
+      territory_id: 1,
+      territory_selector: { aom: ["217500016"] },
+      start_date,
+      end_date,
+      tz: "Europe/Paris",
+      name: "Policy",
+      handler: Idfm.id,
+      status: PolicyStatusEnum.ACTIVE,
+      incentive_sum: 5000,
+      max_amount: 10_000_000_00,
+    });
+
+    const cursor = repository.findTripByPolicy(
+      policy,
+      start_date,
+      end_date,
+    );
+    const { value } = await cursor.next(); // ok carpool
+    await cursor.next(); // ok carpool
+    await cursor.next(); // fraudcheck_error carpool
+    assertEquals((value || []).length, 3);
+    assert(Array.isArray(value));
+    assertEquals(
+      (value || [])
+        .map((c) => ({
+          start: {
+            aom: c.start.aom,
+            arr: c.start.arr,
+            com: c.start.com,
+            country: c.start.country,
+            dep: c.start.dep,
+            epci: c.start.epci,
+            reg: c.start.reg,
+            reseau: c.start.reseau,
+          },
+          end: {
+            aom: c.end.aom,
+            arr: c.end.arr,
+            com: c.end.com,
+            country: c.end.country,
+            dep: c.end.dep,
+            epci: c.end.epci,
+            reg: c.end.reg,
+            reseau: c.end.reseau,
+          },
+          operator_uuid: c.operator_uuid,
+          datetime: c.datetime,
+          distance: c.distance,
+        }))
+        .shift(),
+      {
+        start: {
+          aom: "217500016",
+          arr: "91471",
+          com: "91471",
+          country: "XXXXX",
+          dep: "91",
+          epci: "200056232",
+          reg: "11",
+          reseau: 232,
+        },
+        end: {
+          aom: "217500016",
+          arr: "91377",
+          com: "91377",
+          country: "XXXXX",
+          dep: "91",
+          epci: "200056232",
+          reg: "11",
+          reseau: 232,
+        },
+        operator_uuid: "25a8774f-8708-4cf7-8527-446106b64a35",
+        datetime: new Date("2024-03-15"),
+        distance: 10000,
       },
-      end: {
-        aom: "217500016",
-        arr: "91377",
-        com: "91377",
-        country: "XXXXX",
-        dep: "91",
-        epci: "200056232",
-        reg: "11",
-        reseau: 232,
-      },
-      operator_uuid: "25a8774f-8708-4cf7-8527-446106b64a35",
-      datetime: new Date("2024-03-15"),
-      distance: 10000,
-    },
-  );
+    );
+  });
 });
