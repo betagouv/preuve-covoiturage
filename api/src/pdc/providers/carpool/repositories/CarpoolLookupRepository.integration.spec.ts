bce68bfea0489f5fe8d7822d5483890ef3ae5985
Nicolas Mérigot <nicolas.merigot@beta.gouv.fr>
2024-06-14 15:49:44 +0200

    fix: carpool test

--- api/src/pdc/providers/carpool/repositories/CarpoolLookupRepository.integration.spec.ts
+++ api/src/pdc/providers/carpool/repositories/CarpoolLookupRepository.integration.spec.ts
@@ -1,17 +1,11 @@
-import { DbContext, makeDbBeforeAfter } from "@/pdc/providers/test/index.ts";
 import {
   afterAll,
-  afterEach,
-  assert,
-  assertEquals,
-  assertFalse,
   assertObjectMatch,
-  assertThrows,
   beforeAll,
-  beforeEach,
   describe,
   it,
 } from "@/dev_deps.ts";
+import { DbContext, makeDbBeforeAfter } from "@/pdc/providers/test/index.ts";
 import { Id } from "../interfaces/index.ts";
 import { insertableCarpool } from "../mocks/database/carpool.ts";
 import { insertableAcquisitionStatus } from "../mocks/database/status.ts";
@@ -19,65 +13,59 @@ import { CarpoolLookupRepository } from "./CarpoolLookupRepository.ts";
 import { CarpoolRepository } from "./CarpoolRepository.ts";
 import { CarpoolStatusRepository } from "./CarpoolStatusRepository.ts";
 
-interface TestContext {
-  repository: CarpoolLookupRepository;
-  statusRepository: CarpoolStatusRepository;
-  carpoolRepository: CarpoolRepository;
-  db: DbContext;
-  carpool_id: Id;
-}
+describe("CarpoolGeoRepository", () => {
+  let repository: CarpoolLookupRepository;
+  let statusRepository: CarpoolStatusRepository;
+  let carpoolRepository: CarpoolRepository;
+  let db: DbContext;
+  let carpool_id: Id;
 
-const test = anyTest as TestFn<TestContext>;
-const { before, after } = makeDbBeforeAfter();
+  const { before, after } = makeDbBeforeAfter();
+  beforeAll(async () => {
+    db = await before();
+    repository = new CarpoolLookupRepository(db.connection);
+    statusRepository = new CarpoolStatusRepository(db.connection);
+    carpoolRepository = new CarpoolRepository(db.connection);
+    const carpool = await carpoolRepository.register(insertableCarpool);
+    carpool_id = carpool._id;
+    const statusData = {
+      ...insertableAcquisitionStatus,
+      carpool_id: carpool._id,
+    };
+    await statusRepository.saveAcquisitionStatus(statusData);
+  });
+  afterAll(async () => {
+    await after(db);
+  });
 
-beforeAll(async (t) => {
-  const db = await before();
-  t.context.db = db;
-  t.context.repository = new CarpoolLookupRepository(t.context.db.connection);
-  t.context.statusRepository = new CarpoolStatusRepository(
-    t.context.db.connection,
-  );
-  t.context.carpoolRepository = new CarpoolRepository(t.context.db.connection);
-  const carpool = await t.context.carpoolRepository.register(insertableCarpool);
-  const statusData = {
-    ...insertableAcquisitionStatus,
-    carpool_id: carpool._id,
-  };
-  await t.context.statusRepository.saveAcquisitionStatus(statusData);
-  t.context.carpool_id = carpool._id;
-});
-
-test.after.always(async (t) => {
-  await after(t.context.db);
-});
-
-it("Should get one carpool status", async (t) => {
-  const data = { ...insertableCarpool };
-  const { _id, acquisition_status, operator_trip_id } = await t.context
-    .repository.findOneStatus(
+  it("Should get one carpool status", async (t) => {
+    const data = { ...insertableCarpool };
+    const r = await repository.findOneStatus(
       data.operator_id,
       data.operator_journey_id,
     );
-  assertObjectMatch(
-    { _id, acquisition_status, operator_trip_id },
-    {
-      _id: t.context.carpool_id,
-      acquisition_status: insertableAcquisitionStatus.status,
-      operator_trip_id: data.operator_trip_id,
-    },
-  );
-});
+    const { _id, acquisition_status, operator_trip_id } = r || {};
+    assertObjectMatch(
+      { _id, acquisition_status, operator_trip_id },
+      {
+        _id: carpool_id,
+        acquisition_status: insertableAcquisitionStatus.status,
+        operator_trip_id: data.operator_trip_id,
+      },
+    );
+  });
 
-it("Should get one carpool", async (t) => {
-  const data = { ...insertableCarpool };
-  const { _id, uuid, created_at, updated_at, ...carpool } = await t.context
-    .repository.findOne(
+  it("Should get one carpool", async (t) => {
+    const data = { ...insertableCarpool };
+    const r = await repository.findOne(
       data.operator_id,
       data.operator_journey_id,
     );
-  assertObjectMatch(carpool, {
-    ...data,
-    fraud_status: "pending",
-    acquisition_status: "canceled",
+    const { _id, uuid, created_at, updated_at, ...carpool } = r || {};
+    assertObjectMatch(carpool, {
+      ...data,
+      fraud_status: "pending",
+      acquisition_status: "canceled",
+    });
   });
 });
