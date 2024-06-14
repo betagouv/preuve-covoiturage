1b12d27432e78a59633a172121edd1948ffa7b3c
Jonathan Fallon <jonathan@scopopop.com>
2024-06-14 14:39:23 +0200

    fix handler-redis test

--- api/src/ilos/handler-redis/QueueHandler.integration.spec.ts
+++ api/src/ilos/handler-redis/QueueHandler.integration.spec.ts
@@ -1,56 +1,58 @@
 import {
   afterAll,
-  afterEach,
   assert,
-  assertEquals,
-  assertFalse,
   assertObjectMatch,
-  assertThrows,
   beforeAll,
   beforeEach,
   describe,
   it,
 } from "@/dev_deps.ts";
+import { ContextType } from "@/ilos/common/index.ts";
 import { RedisConnection } from "@/ilos/connection-redis/index.ts";
+import process from "node:process";
 import { QueueHandler } from "./QueueHandler.ts";
 import { queueHandlerFactory } from "./helpers/queueHandlerFactory.ts";
-import { ContextType } from "@/ilos/common/index.ts";
 
-interface Context {
-  handler: QueueHandler;
-  context: ContextType;
-}
-
-const test = anyTest as TestFn<Context>;
-
-beforeEach(async (t) => {
-  t.context.context = {
+describe("QueueHandler", () => {
+  let handler: QueueHandler | null = null;
+  const context: ContextType = {
     channel: {
       service: "",
     },
   };
-
   const connection = new RedisConnection(
     process.env.APP_REDIS_URL ?? "redis://127.0.0.1:6379",
+    {
+      lazyConnect: true,
+    },
   );
 
-  t.context.handler = new (queueHandlerFactory("basic", "0.0.1"))(connection);
-  await t.context.handler.init();
-});
+  beforeAll(async () => {
+    await connection.up();
+  });
 
-it("Queue handler: works", async (t) => {
-  const queueProvider = t.context.handler;
-  const defaultContext = t.context.context;
-  const result = (await queueProvider.call({
-    method: "basic@latest:method",
-    params: { add: [1, 2] },
-    context: defaultContext,
-  })) as any;
+  afterAll(async () => {
+    await connection.down();
+  });
 
-  assertObjectMatch(result.data, {
-    jsonrpc: "2.0",
-    id: null,
-    method: "basic@latest:method",
-    params: { params: { add: [1, 2] }, _context: defaultContext },
+  beforeEach(async () => {
+    handler = new (queueHandlerFactory("basic", "0.0.1"))(connection);
+    await handler.init();
+  });
+
+  it("should call the queue provider", async () => {
+    const result = handler && await handler.call({
+      method: "basic@latest:method",
+      params: { add: [1, 2] },
+      context,
+    });
+
+    assert(result);
+    assertObjectMatch(result.data, {
+      jsonrpc: "2.0",
+      id: null,
+      method: "basic@latest:method",
+      params: { params: { add: [1, 2] }, _context: context },
+    });
   });
 });
