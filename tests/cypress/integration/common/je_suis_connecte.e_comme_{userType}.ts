7cfcd962d21b8b98c38f8bab0f783cabb22eb74d
nmrgt <nicolas.merigot@beta.gouv.fr>
2024-06-20 14:16:55 +0200

    chore: remove useless test (#2513)
    * chore: remove useless test
    
    * chore: remove lint

--- tests/cypress/integration/common/je_suis_connecte.e_comme_{userType}.ts
+++
@@ -1,7 +0,0 @@
-import { Given } from 'cypress-cucumber-preprocessor/steps';
-import '../../support/parameters/userType';
-
-Given('je suis connecté.e comme {userType}', function (userCredentials) {
-  const { login, password, homepage } = userCredentials;
-  cy.login(login, password, homepage);
-});
--- tests/cypress/integration/common/je_vois_une_fenetre_avec_le_message_{string}.ts
+++
@@ -1,6 +0,0 @@
-import { Then } from 'cypress-cucumber-preprocessor/steps';
-import { getElementSelectorFromName } from '../../support/helpers/getElementSelectorFromName';
-
-Then(`je vois une fenêtre avec le message {string}`, function (msg) {
-  cy.contains(getElementSelectorFromName('une fenêtre'), msg).should('be.visible');
-});
