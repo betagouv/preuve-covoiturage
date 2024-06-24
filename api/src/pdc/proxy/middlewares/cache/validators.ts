07dee6ed0f6183249e14decc9855e2ee9f973fff
Nicolas Mérigot <nicolas.merigot@beta.gouv.fr>
2024-06-24 09:50:14 +0200

    fix: typo errors

--- api/src/pdc/proxy/middlewares/cache/validators.ts
+++ api/src/pdc/proxy/middlewares/cache/validators.ts
@@ -57,7 +57,7 @@ export async function validate(
 
   for (const validator of validators) {
     const result = await validator(params);
-    isValid = isValid && (resulassertEqualsValid ?? true);
+    isValid = isValid && (result.isValid ?? true);
     if (result.errors) errors = new Set([...errors, ...result.errors]);
     if (result.warnings) warnings = new Set([...warnings, ...result.warnings]);
     if (result.headers) headers = new Map([...headers, ...result.headers]);
