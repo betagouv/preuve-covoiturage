4e23f76e04527264f11d76d5a712ee6f6bf97db3
Ludovic Delhomme <ludovic.delhomme@datayama.fr>
2024-07-09 15:30:06 +0200

    configure cms for vitrine

---
+++ cms/src/api/page-vitrine/services/page-vitrine.ts
@@ -0,0 +1,7 @@
+/**
+ * page-vitrine service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::page-vitrine.page-vitrine');
---
+++ cms/src/api/service-tag/services/service-tag.ts
@@ -0,0 +1,7 @@
+/**
+ * service-tag service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::service-tag.service-tag');
