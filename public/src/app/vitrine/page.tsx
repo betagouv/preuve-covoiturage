3a45034c374f46b1ddebc91e1ab3581c6479261c
Ludovic Delhomme <ludovic.delhomme@datayama.fr>
2024-07-10 15:40:52 +0200

    add vitrine card and rows

--- public/src/app/vitrine/page.tsx
+++ public/src/app/vitrine/page.tsx
@@ -1,3 +1,6 @@
+import Hero from '@/components/common/Hero';
+import VitrineRows from '@/components/vitrine/VitrineRows';
+import { fetchAPI } from '@/helpers/cms';
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
@@ -5,8 +8,43 @@ export const metadata: Metadata = {
   description: 'Accélerateur de covoiturage de courte distance',
 }
 
-export default function Vitrine() {
+export default async function Vitrine() {
+  const query = {
+    filters: {
+      id: {
+        $eq: 1,
+      },
+    },
+    populate: {
+      Hero: {
+        populate: 'buttons,img'
+      },
+      Rows:{
+        populate: '*'
+      }
+    },  
+  };
+  const response  = await fetchAPI('/page-vitrines',query);
+  const data = response.data[0];
+  const hero = data.attributes.Hero;
+  const rows = data.attributes.Rows;
+
+  
   return(
-    'toto'
+    <div id='content'>
+      {hero && 
+        <Hero 
+          title={hero.title} 
+          subtitle={hero.subtitle}
+          content={hero.content} 
+          img={hero.img.data ? hero.img.data.attributes.url : undefined} 
+          alt={hero.alt} 
+          buttons={hero.buttons} 
+        />
+      }
+      {rows && 
+        <VitrineRows data={rows} />
+      }
+    </div>
   )
 }
