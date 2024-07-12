5b68a59cc72c3ae9bbd7aec2df49e21d9792cfda
Ludovic Delhomme <ludovic.delhomme@datayama.fr>
2024-07-12 11:15:27 +0200

    add directions and flux tables

---
+++ dbt/models/observatoire/directions_incentive_by_day.sql
@@ -0,0 +1,409 @@
+{{ config(materialized='incremental') }}
+with directions as (
+  select
+    "from"          as code,
+    'com'           as type,
+    'from'          as direction,
+    start_date,
+    sum(collectivite) as collectivite,
+    coalesce(
+     sum(collectivite) filter (where "from" = "to"), 0
+    )               as intra_collectivite,
+    sum(operateur)    as operateur,
+    coalesce(
+      sum(operateur) filter (where "from" = "to"), 0
+    )               as intra_operateur,
+    sum(autres) as autres,
+    coalesce(
+      sum(autres) filter (where "from" = "to"), 0
+    )               as intra_autres
+  from {{ ref('carpool_incentive_by_day') }}
+  {% if is_incremental() %}
+    where start_date >= (select max(start_date) from {{ this }})
+  {% endif %}
+  group by 1, 4
+  union
+  select
+    "to"                 as code,
+    'com'                as type,
+    'to'                 as direction,
+    start_date,
+   sum(collectivite)        as collectivite,
+    0                    as intra_collectivite,
+    sum(operateur)         as operateur,
+    0                    as intra_operateur,
+    sum(autres)      as autres,
+    0                    as intra_autres
+  from {{ ref('carpool_incentive_by_day') }}
+  {% if is_incremental() %}
+    where start_date >= (select max(start_date) from {{ this }})
+  {% endif %}
+  group by 1, 4
+  union
+  select
+    b.epci        as code,
+    'epci'        as type,
+    'from'        as direction,
+    start_date,
+   sum(collectivite) as collectivite,
+    coalesce(
+     sum(collectivite) filter (where b.epci = c.epci), 0
+    )             as intra_collectivite,
+    sum(operateur)  as operateur,
+    coalesce(
+      sum(operateur) filter (where b.epci = c.epci), 0
+    )             as intra_operateur,
+    sum(
+      autres
+    )             as autres,
+    coalesce(
+      sum(autres) filter (where b.epci = c.epci), 0
+    )             as intra_autres
+  from {{ ref('carpool_incentive_by_day') }} as a
+  left join
+    (
+      select
+        arr,
+        epci
+      from {{ source('geo','perimeters') }}
+      where year = geo.get_latest_millesime()
+    ) as b
+    on a.from = b.arr
+  left join
+    (
+      select
+        arr,
+        epci
+      from {{ source('geo','perimeters') }}
+      where year = geo.get_latest_millesime()
+    ) as c
+    on a.to = c.arr
+  {% if is_incremental() %}
+    where start_date >= (select max(start_date) from {{ this }})
+  {% endif %}
+  group by 1, 4
+  union
+  select
+    b.epci               as code,
+    'epci'               as type,
+    'to'                 as direction,
+    start_date,
+   sum(collectivite)     as collectivite,
+    0                    as intra_collectivite,
+    sum(operateur)         as operateur,
+    0                    as intra_operateur,
+    sum(autres)      as autres,
+    0                    as intra_autres
+  from {{ ref('carpool_incentive_by_day') }} as a
+  left join
+    (
+      select
+        arr,
+        epci
+      from {{ source('geo','perimeters') }}
+      where year = geo.get_latest_millesime()
+    ) as b
+    on a.to = b.arr
+  {% if is_incremental() %}
+    where start_date >= (select max(start_date) from {{ this }})
+  {% endif %}
+  group by 1, 4
+  union
+  select
+    b.aom           as code,
+    'aom'           as type,
+    'from'          as direction,
+    start_date,
+    sum(collectivite) as collectivite,
+    coalesce(
+     sum(collectivite) filter (where b.aom = c.aom), 0
+    )               as intra_collectivite,
+    sum(operateur)    as operateur,
+    coalesce(
+      sum(operateur) filter (where b.aom = c.aom), 0
+    )               as intra_operateur,
+    sum(autres) as autres,
+    coalesce(
+      sum(autres) filter (where b.aom = c.aom), 0
+    )               as intra_autres
+  from {{ ref('carpool_incentive_by_day') }} as a
+  left join
+    (
+      select
+        arr,
+        aom
+      from {{ source('geo','perimeters') }}
+      where year = geo.get_latest_millesime()
+    ) as b
+    on a.from = b.arr
+  left join
+    (
+      select
+        arr,
+        aom
+      from {{ source('geo','perimeters') }}
+      where year = geo.get_latest_millesime()
+    ) as c
+    on a.to = c.arr
+  {% if is_incremental() %}
+    where start_date >= (select max(start_date) from {{ this }})
+  {% endif %}
+  group by 1, 4
+  union
+  select
+    b.aom                as code,
+    'aom'                as type,
+    'to'                 as direction,
+    start_date,
+   sum(collectivite)        as collectivite,
+    0                    as intra_collectivite,
+    sum(operateur)         as operateur,
+    0                    as intra_operateur,
+    sum(autres)      as autres,
+    0                    as intra_autres
+  from {{ ref('carpool_incentive_by_day') }} as a
+  left join
+    (
+      select
+        arr,
+        aom
+      from {{ source('geo','perimeters') }}
+      where year = geo.get_latest_millesime()
+    ) as b
+    on a.to = b.arr
+  {% if is_incremental() %}
+    where start_date >= (select max(start_date) from {{ this }})
+  {% endif %}
+  group by 1, 4
+  union
+  select
+    b.dep           as code,
+    'dep'           as type,
+    'from'          as direction,
+    start_date,
+    sum(collectivite) as collectivite,
+    coalesce(
+     sum(collectivite) filter (where b.dep = c.dep), 0
+    )               as intra_collectivite,
+    sum(operateur)    as operateur,
+    coalesce(
+      sum(operateur) filter (where b.dep = c.dep), 0
+    )               as intra_operateur,
+    sum(autres) as autres,
+    coalesce(
+      sum(autres) filter (where b.dep = c.dep), 0
+    )               as intra_autres
+  from {{ ref('carpool_incentive_by_day') }} as a
+  left join
+    (
+      select
+        arr,
+        dep
+      from {{ source('geo','perimeters') }}
+      where year = geo.get_latest_millesime()
+    ) as b
+    on a.from = b.arr
+  left join
+    (
+      select
+        arr,
+        dep
+      from {{ source('geo','perimeters') }}
+      where year = geo.get_latest_millesime()
+    ) as c
+    on a.to = c.arr
+  {% if is_incremental() %}
+    where start_date >= (select max(start_date) from {{ this }})
+  {% endif %}
+  group by 1, 4
+  union
+  select
+    b.dep                as code,
+    'dep'                as type,
+    'to'                 as direction,
+    start_date,
+   sum(collectivite)        as collectivite,
+    0                    as intra_collectivite,
+    sum(operateur)         as operateur,
+    0                    as intra_operateur,
+    sum(autres)      as autres,
+    0                    as intra_autres
+  from {{ ref('carpool_incentive_by_day') }} as a
+  left join
+    (
+      select
+        arr,
+        dep
+      from {{ source('geo','perimeters') }}
+      where year = geo.get_latest_millesime()
+    ) as b
+    on a.to = b.arr
+  {% if is_incremental() %}
+    where start_date >= (select max(start_date) from {{ this }})
+  {% endif %}
+  group by 1, 4
+  union
+  select
+    b.reg           as code,
+    'reg'           as type,
+    'from'          as direction,
+    start_date,
+    sum(collectivite) as collectivite,
+    coalesce(
+     sum(collectivite) filter (where b.reg = c.reg), 0
+    )               as intra_collectivite,
+    sum(operateur)    as operateur,
+    coalesce(
+      sum(operateur) filter (where b.reg = c.reg), 0
+    )               as intra_operateur,
+    sum(autres) as autres,
+    coalesce(
+      sum(autres) filter (where b.reg = c.reg), 0
+    )               as intra_autres
+  from {{ ref('carpool_incentive_by_day') }} as a
+  left join
+    (
+      select
+        arr,
+        reg
+      from {{ source('geo','perimeters') }}
+      where year = geo.get_latest_millesime()
+    ) as b
+    on a.from = b.arr
+  left join
+    (
+      select
+        arr,
+        reg
+      from {{ source('geo','perimeters') }}
+      where year = geo.get_latest_millesime()
+    ) as c
+    on a.to = c.arr
+  {% if is_incremental() %}
+    where start_date >= (select max(start_date) from {{ this }})
+  {% endif %}
+  group by 1, 4
+  union
+  select
+    b.reg                as code,
+    'reg'                as type,
+    'to'                 as direction,
+    start_date,
+   sum(collectivite)        as collectivite,
+    0                    as intra_collectivite,
+    sum(operateur)         as operateur,
+    0                    as intra_operateur,
+    sum(autres)      as autres,
+    0                    as intra_autres
+  from {{ ref('carpool_incentive_by_day') }} as a
+  left join
+    (
+      select
+        arr,
+        reg
+      from {{ source('geo','perimeters') }}
+      where year = geo.get_latest_millesime()
+    ) as b
+    on a.to = b.arr
+  {% if is_incremental() %}
+    where start_date >= (select max(start_date) from {{ this }})
+  {% endif %}
+  group by 1, 4
+  union
+  select
+    b.country as code,
+    'country' as type,
+    'from'    as direction,
+    start_date,
+    sum(
+      collectivite
+    )         as collectivite,
+    coalesce(
+     sum(collectivite) filter (where b.country = c.country), 0
+    )         as intra_collectivite,
+    sum(
+      operateur
+    )         as operateur,
+    coalesce(
+      sum(operateur) filter (where b.country = c.country), 0
+    )         as intra_operateur,
+    sum(
+      autres
+    )         as autres,
+    coalesce(
+      sum(autres) filter (where b.country = c.country), 0
+    )         as intra_autres
+  from {{ ref('carpool_incentive_by_day') }} as a
+  left join
+    (
+      select
+        arr,
+        country
+      from {{ source('geo','perimeters') }}
+      where year = geo.get_latest_millesime()
+    ) as b
+    on a.from = b.arr
+  left join
+    (
+      select
+        arr,
+        country
+      from {{ source('geo','perimeters') }}
+      where year = geo.get_latest_millesime()
+    ) as c
+    on a.to = c.arr
+  {% if is_incremental() %}
+    where start_date >= (select max(start_date) from {{ this }})
+  {% endif %}
+  group by 1, 4
+  union
+  select
+    b.country            as code,
+    'country'            as type,
+    'to'                 as direction,
+    start_date,
+   sum(collectivite)        as collectivite,
+    0                    as intra_collectivite,
+    sum(operateur)         as operateur,
+    0                    as intra_operateur,
+    sum(autres)      as autres,
+    0                    as intra_autres
+  from {{ ref('carpool_incentive_by_day') }} as a
+  left join
+    (
+      select
+        arr,
+        country
+      from {{ source('geo','perimeters') }}
+      where year = geo.get_latest_millesime()
+    ) as b
+    on a.to = b.arr
+  {% if is_incremental() %}
+    where start_date >= (select max(start_date) from {{ this }})
+  {% endif %}
+  group by 1, 4
+)
+
+select
+  code,
+  type,
+  start_date,
+  direction,
+ sum(collectivite)        as collectivite,
+  sum(intra_collectivite)  as intra_collectivite,
+  sum(operateur)         as operateur,
+  sum(autres)      as autres
+from directions
+group by 1, 2, 3, 4
+union
+select
+  code,
+  type,
+  start_date,
+  'both'                                            as direction,
+ sum(collectivite) - sum(intra_collectivite)               as collectivite,
+  sum(intra_collectivite)                               as intra_collectivite,
+  sum(operateur) - sum(intra_operateur)                 as operateur,
+  sum(autres) - sum(intra_autres)           as autres
+from directions
+group by 1, 2, 3
---
+++ dbt/models/observatoire/flux_by_semester.sql
@@ -0,0 +1,232 @@
+{{ config(materialized='incremental') }}
+
+WITH flux AS (
+  SELECT
+    from,
+    to,
+    extract(
+      'year' FROM start_date
+    )::int AS year,
+    CASE
+      WHEN extract('quarter' FROM start_date)::int > 3 THEN 2 ELSE 1
+    END    AS semester,
+    sum(
+      journeys
+    )      AS journeys,
+    sum(
+      drivers
+    )      AS drivers,
+    sum(
+      passengers
+    )      AS passengers,
+    sum(
+      passenger_seats
+    )      AS passenger_seats,
+    sum(
+      distance
+    )      AS distance,
+    sum(
+      duration
+    )      AS duration
+  FROM {{ ref('carpool_by_day') }}
+  {% if is_incremental() %}
+    WHERE
+      (extract('year' FROM start_date) * 10 + CASE WHEN extract('quarter' FROM start_date)::int > 3 THEN 2 ELSE 1 END)
+      >= (SELECT max(year * 10 + semester) FROM {{ this }})
+  {% endif %}
+  GROUP BY
+    1, 2, 3, 4
+),
+
+flux_agg AS (
+  SELECT
+    year,
+    semester,
+    'com'                                    AS type,
+    least("from", "to")                      AS territory_1,
+    greatest("from", "to")                   AS territory_2,
+    sum(journeys)                            AS journeys,
+    sum(passenger_seats)                     AS passengers,
+    round(sum(distance)::numeric / 1000, 2)  AS distance,
+    extract('epoch' FROM sum(duration)) / 60 AS duration
+  FROM flux 
+  GROUP BY 1, 2, 4, 5
+  HAVING least("from", "to") IS NOT NULL OR greatest("from", "to") IS NOT NULL
+  UNION
+  SELECT
+    a.year,
+    a.semester,
+    'epci'                                   AS type,
+    least(b.epci, c.epci)                    AS territory_1,
+    greatest(b.epci, c.epci)                 AS territory_2,
+    sum(journeys)                            AS journeys,
+    sum(passenger_seats)                     AS passengers,
+    round(sum(distance)::numeric / 1000, 2)  AS distance,
+    extract('epoch' FROM sum(duration)) / 60 AS duration
+  FROM flux AS a
+  LEFT JOIN
+    (
+      SELECT arr, epci
+      FROM {{ source('geo','perimeters') }}
+      WHERE year = geo.get_latest_millesime()
+    ) AS b
+    ON a.from = b.arr
+  LEFT JOIN
+    (
+      SELECT arr, epci
+      FROM {{ source('geo','perimeters') }}
+      WHERE year = geo.get_latest_millesime()
+    ) AS c
+    ON a.to = c.arr
+  GROUP BY 1, 2, 4, 5
+  HAVING
+    least(b.epci, c.epci) IS NOT NULL
+    OR greatest(b.epci, c.epci) IS NOT NULL
+  UNION
+  SELECT
+    a.year,
+    a.semester,
+    'aom'                                    AS type,
+    least(b.aom, c.aom)                      AS territory_1,
+    greatest(b.aom, c.aom)                   AS territory_2,
+    sum(journeys)                            AS journeys,
+    sum(passenger_seats)                     AS passengers,
+    round(sum(distance)::numeric / 1000, 2)  AS distance,
+    extract('epoch' FROM sum(duration)) / 60 AS duration
+  FROM flux AS a
+  LEFT JOIN
+    (
+      SELECT arr, aom
+      FROM {{ source('geo','perimeters') }}
+      WHERE year = geo.get_latest_millesime()
+    ) AS b
+    ON a.from = b.arr
+  LEFT JOIN
+    (
+      SELECT arr, aom
+      FROM {{ source('geo','perimeters') }}
+      WHERE year = geo.get_latest_millesime()
+    ) AS c
+    ON a.to = c.arr
+  GROUP BY 1, 2, 4, 5
+  HAVING least(b.aom, c.aom) IS NOT NULL OR least(b.aom, c.aom) IS NOT NULL
+  UNION
+  SELECT
+    a.year,
+    a.semester,
+    'dep'                                    AS type,
+    least(b.dep, c.dep)                      AS territory_1,
+    greatest(b.dep, c.dep)                   AS territory_2,
+    sum(journeys)                            AS journeys,
+    sum(passenger_seats)                     AS passengers,
+    round(sum(distance)::numeric / 1000, 2)  AS distance,
+    extract('epoch' FROM sum(duration)) / 60 AS duration
+  FROM flux AS a
+  LEFT JOIN
+    (
+      SELECT arr, dep
+      FROM {{ source('geo','perimeters') }}
+      WHERE year = geo.get_latest_millesime()
+    ) AS b
+    ON a.from = b.arr
+  LEFT JOIN
+    (
+      SELECT arr, dep
+      FROM {{ source('geo','perimeters') }}
+      WHERE year = geo.get_latest_millesime()
+    ) AS c
+    ON a.to = c.arr
+  GROUP BY 1, 2, 4, 5
+  HAVING least(b.dep, c.dep) IS NOT NULL OR greatest(b.dep, c.dep) IS NOT NULL
+  UNION
+  SELECT
+    a.year,
+    a.semester,
+    'reg'                                    AS type,
+    least(b.reg, c.reg)                      AS territory_1,
+    greatest(b.reg, c.reg)                   AS territory_2,
+    sum(journeys)                            AS journeys,
+    sum(passenger_seats)                     AS passengers,
+    round(sum(distance)::numeric / 1000, 2)  AS distance,
+    extract('epoch' FROM sum(duration)) / 60 AS duration
+  FROM flux AS a
+  LEFT JOIN
+    (
+      SELECT arr, reg
+      FROM {{ source('geo','perimeters') }}
+      WHERE year = geo.get_latest_millesime()
+    ) AS b
+    ON a.from = b.arr
+  LEFT JOIN
+    (
+      SELECT arr, reg
+      FROM {{ source('geo','perimeters') }}
+      WHERE year = geo.get_latest_millesime()
+    ) AS c
+    ON a.to = c.arr
+  GROUP BY 1, 2, 4, 5
+  HAVING least(b.reg, c.reg) IS NOT NULL OR greatest(b.reg, c.reg) IS NOT NULL
+  UNION
+  SELECT
+    a.year,
+    a.semester,
+    'country'                                AS type,
+    least(b.country, c.country)              AS territory_1,
+    greatest(b.country, c.country)           AS territory_2,
+    sum(journeys)                            AS journeys,
+    sum(passenger_seats)                     AS passengers,
+    round(sum(distance)::numeric / 1000, 2)  AS distance,
+    extract('epoch' FROM sum(duration)) / 60 AS duration
+  FROM flux AS a
+  LEFT JOIN
+    (
+      SELECT arr, country
+      FROM {{ source('geo','perimeters') }}
+      WHERE year = geo.get_latest_millesime()
+    ) AS b
+    ON a.from = b.arr
+  LEFT JOIN
+    (
+      SELECT arr, country
+      FROM {{ source('geo','perimeters') }}
+      WHERE year = geo.get_latest_millesime()
+    ) AS c
+    ON a.to = c.arr
+  GROUP BY 1, 2, 4, 5
+  HAVING
+    least(b.country, c.country) IS NOT NULL
+    OR greatest(b.country, c.country) IS NOT NULL
+)
+
+SELECT
+  a.year,
+  a.semester,
+  a.type,
+  a.territory_1,
+  b.l_territory AS l_territory_1,
+  a.territory_2,
+  c.l_territory AS l_territory_2,
+  a.journeys,
+  a.passengers,
+  a.distance,
+  a.duration,
+  st_x(b.geom)  AS lng_1,
+  st_y(b.geom)  AS lat_1,
+  st_x(c.geom)  AS lng_2,
+  st_y(c.geom)  AS lat_2
+FROM flux_agg AS a
+LEFT JOIN
+  (
+    SELECT territory, type, l_territory, geom
+    FROM {{ source('geo','perimeters_centroid') }}
+    WHERE year = geo.get_latest_millesime()
+  ) AS b
+  ON concat(a.territory_1, a.type) = concat(b.territory, b.type)
+LEFT JOIN
+  (
+    SELECT territory, type, l_territory, geom
+    FROM {{ source('geo','perimeters_centroid') }}
+    WHERE year = geo.get_latest_millesime()
+  ) AS c
+  ON concat(a.territory_2, a.type) = concat(c.territory, c.type)
+ORDER BY a.territory_1, a.territory_2
