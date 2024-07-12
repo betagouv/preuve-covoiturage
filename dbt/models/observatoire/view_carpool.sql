4305d09d41a45db46d1cbab5f5c5c0e46c2b60ef
Ludovic Delhomme <ludovic.delhomme@datayama.fr>
2024-07-12 09:16:11 +0200

    add views observatoire

---
+++ dbt/models/observatoire/view_carpool.sql
@@ -0,0 +1,65 @@
+{{ config(materialized='view') }}
+
+SELECT
+  a._id              AS carpool_id,
+  c.start_geo_code,
+  c.end_geo_code,
+  a.operator_id,
+  a.distance,
+  a.driver_revenue,
+  a.passenger_seats,
+  a.passenger_contribution,
+  a.passenger_payments,
+  b.acquisition_status,
+  b.fraud_status,
+  CASE
+    WHEN
+      c.start_geo_code::text ~ '^97[1-2]'::text
+      THEN (a.start_datetime AT TIME ZONE 'America/Guadeloupe'::text)
+    WHEN
+      c.start_geo_code::text ~ '^973'::text
+      THEN (a.start_datetime AT TIME ZONE 'America/Guyana'::text)
+    WHEN
+      c.start_geo_code::text ~ '^974'::text
+      THEN (a.start_datetime AT TIME ZONE 'Indian/Reunion'::text)
+    WHEN
+      c.start_geo_code::text ~ '^976'::text
+      THEN (a.start_datetime AT TIME ZONE 'Indian/Mayotte'::text)
+    ELSE (a.start_datetime AT TIME ZONE 'Europe/Paris'::text)
+  END                AS start_datetime,
+  CASE
+    WHEN
+      c.end_geo_code::text ~ '^97[1-2]'::text
+      THEN (a.end_datetime AT TIME ZONE 'America/Guadeloupe'::text)
+    WHEN
+      c.end_geo_code::text ~ '^973'::text
+      THEN (a.end_datetime AT TIME ZONE 'America/Guyana'::text)
+    WHEN
+      c.end_geo_code::text ~ '^974'::text
+      THEN (a.end_datetime AT TIME ZONE 'Indian/Reunion'::text)
+    WHEN
+      c.end_geo_code::text ~ '^976'::text
+      THEN (a.end_datetime AT TIME ZONE 'Indian/Mayotte'::text)
+    ELSE (a.end_datetime AT TIME ZONE 'Europe/Paris'::text)
+  END                AS end_datetime,
+  a.end_datetime
+  - a.start_datetime AS duration,
+  coalesce(
+    a.driver_identity_key,
+    a.driver_operator_user_id,
+    a.driver_phone,
+    a.driver_phone_trunc
+  )                  AS driver_id,
+  coalesce(
+    a.passenger_identity_key,
+    a.passenger_operator_user_id,
+    a.passenger_phone,
+    a.passenger_phone_trunc
+  )                  AS passenger_id
+FROM {{ source('carpool', 'carpools') }} AS a
+LEFT JOIN {{ source('carpool', 'status') }} AS b ON a._id = b.carpool_id
+LEFT JOIN {{ source('carpool','geo') }} AS c ON a._id = c.carpool_id
+WHERE
+  c.start_geo_code IS NOT NULL
+  AND c.end_geo_code IS NOT NULL
+  AND date_part('year', a.start_datetime) >= 2020
---
+++ dbt/models/observatoire/view_distribution.sql
@@ -0,0 +1,29 @@
+{{ config(materialized='view') }}
+
+select 
+  coalesce(
+    b.new_com,
+    a.start_geo_code
+  )                              AS start,
+  coalesce(
+    c.new_com,
+    a.end_geo_code
+  )                              AS end,
+  a.start_datetime::date         AS start_date,
+  extract('hour' from  a.start_datetime) as hour,
+  CASE WHEN a.distance < 10000 then '0-10'
+    WHEN (a.distance >= 10000 AND a.distance < 20000) THEN '10-20'
+    WHEN (a.distance >= 20000 AND a.distance < 30000) THEN '20-30'
+    WHEN (a.distance >= 30000 AND a.distance < 40000) THEN '30-40'
+    WHEN (a.distance >= 40000 AND a.distance < 50000) THEN '40-50'
+  ELSE '>50' END as dist_classes
+  FROM {{ ref('view_carpool') }} AS a
+  LEFT JOIN
+    (SELECT * FROM {{ source('geo','com_evolution') }} WHERE year >= 2020) AS b
+    ON a.start_geo_code = b.old_com
+  LEFT JOIN
+    (SELECT * FROM {{ source('geo','com_evolution') }} WHERE year >= 2020) AS c
+    ON a.end_geo_code = c.old_com
+  WHERE
+    a.acquisition_status = 'processed'
+    AND a.fraud_status = 'passed'
