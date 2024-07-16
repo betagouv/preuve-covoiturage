200cc7283aa67d73e9727c19017f74e218e405c0
Jonathan Fallon <jonathan@scopopop.com>
2024-07-16 16:32:21 +0200

    update ATMB 2023 description

--- api/src/pdc/services/policy/engine/policies/20230502_ATMB.html.ts
+++ api/src/pdc/services/policy/engine/policies/20230502_ATMB.html.ts
@@ -1,9 +1,9 @@
 export const description =
   `<p _ngcontent-pmm-c231="" id="summary" class="campaignSummaryText-content-text">
-<p>Campagne d’incitation au covoiturage du <b> 2 mai 2023 au 31 décembre 2023</b>, toute la semaine
+<p>Campagne d’incitation au covoiturage du <b> 2 mai 2023 au 20 décembre 2024</b>, toute la semaine
 </p>
 <p>Cette campagne est limitée à
-  <b>24 400 euros </b>.
+  <b>48 400 euros </b>.
 </p>
 <p>
 Le périmètre géographique de la campagne comprend les zones suivantes
