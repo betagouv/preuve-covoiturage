fa9a7878260a9d3bebbca8a2cf5edc8e6f9462e4
Ludovic Delhomme <ludovic.delhomme@datayama.fr>
2024-08-22 11:32:49 +0200

    fix: view_location

--- dbt/models/observatoire/view_location.sql
+++ dbt/models/observatoire/view_location.sql
@@ -2,6 +2,14 @@
 
 SELECT
   a._id              AS carpool_id,
+  coalesce(
+    d.new_com,
+    c.start_geo_code
+  ) AS start_geo_code,
+  coalesce(
+    e.new_com,
+    c.end_geo_code
+  ) AS end_geo_code,
   CASE
     WHEN
       c.start_geo_code::text ~ '^97[1-2]'::text
@@ -24,6 +32,12 @@ SELECT
 FROM {{ source('carpool', 'carpools') }} AS a
 LEFT JOIN {{ source('carpool', 'status') }} AS b ON a._id = b.carpool_id
 LEFT JOIN {{ source('carpool','geo') }} AS c ON a._id = c.carpool_id
+LEFT JOIN
+    (SELECT * FROM {{ source('geo','com_evolution') }} WHERE year >= 2020) AS d
+    ON c.start_geo_code = d.old_com
+  LEFT JOIN
+    (SELECT * FROM {{ source('geo','com_evolution') }} WHERE year >= 2020) AS e
+    ON c.end_geo_code = e.old_com
 WHERE
   date_part('year', a.start_datetime) >= 2020
   AND a.start_datetime < now() - interval '8' day
