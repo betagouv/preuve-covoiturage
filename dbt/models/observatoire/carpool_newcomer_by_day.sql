3285646ae7c3a9863813d60e1c60eb6834abdda4
Ludovic Delhomme <74348195+Datayama38@users.noreply.github.com>
2024-09-04 11:10:55 +0200

    Dbt models for observatory (#2564)
    * feat: add justfile
    
    * configure dbt
    
    * fix dbt profile
    
    * change profile to default
    
    * add sources and geo_view
    
    * add views observatoire
    
    * add carpool tables
    
    * add directions and flux tables
    
    * fix select in flux queries
    
    * add occupation tables
    
    * add incentive tables
    
    * add distribution tables
    
    * wip fix flux queries
    
    * fix cast on occupation
    
    * fix: occupation queries
    
    * feat: add view_location
    
    * fix: view_location
    
    * fix: flux_by_trimester
    
    * fix: flux_by_trimester
    
    * fix: profile dev schema
    
    * fix: incremental queries for distribution
    
    ---------
    
    Co-authored-by: Nicolas Mérigot <nicolas.merigot@beta.gouv.fr>

---
+++ dbt/models/observatoire/carpool_newcomer_by_day.sql
@@ -0,0 +1,35 @@
+{{ config(materialized='incremental') }}
+
+SELECT
+  coalesce(
+    b.new_com,
+    a.start_geo_code
+  )                              AS from,
+  coalesce(
+    c.new_com,
+    a.end_geo_code
+  )                              AS to,
+  a.start_datetime::date         AS start_date,
+  count(distinct a.driver_id) filter (where d.first_date = a.start_datetime::date)  AS new_drivers,
+  count(distinct a.passenger_id) filter (where e.first_date = a.start_datetime::date)  AS new_passengers
+FROM {{ ref('view_carpool') }} AS a
+LEFT JOIN
+  (SELECT * FROM {{ source('geo','com_evolution') }} WHERE year >= 2020) AS b
+  ON a.start_geo_code = b.old_com
+LEFT JOIN
+  (SELECT * FROM {{ source('geo','com_evolution') }} WHERE year >= 2020) AS c
+  ON a.end_geo_code = c.old_com
+LEFT JOIN 
+(select * from {{ref('view_first_date_driver')}}) AS d
+  ON a.driver_id = d.driver_id
+LEFT JOIN 
+(select * from {{ref('view_first_date_passenger')}}) AS e
+  ON a.passenger_id = e.passenger_id
+WHERE
+  a.acquisition_status = 'processed'
+  AND a.fraud_status = 'passed'
+{% if is_incremental() %}
+  AND a.start_datetime::date >= (SELECT MAX(start_date) FROM {{ this }})::date
+{% endif %}
+GROUP BY
+  1, 2, 3
---
+++ dbt/models/observatoire/incentive_by_semester.sql
@@ -0,0 +1,34 @@
+{{ config(materialized='incremental') }}
+
+SELECT
+  extract('year' FROM start_date)::int  AS year,
+  case
+    when extract('quarter' from start_date)::int > 3 then 2 else 1
+  end                             as semester,
+  a.code,
+  b.l_territory AS libelle,
+  a.type,
+  a.direction,
+  sum(a.collectivite)                         AS collectivite,
+  sum(a.operateur)                         AS operateur,
+  sum(a.autres)                         AS autres
+FROM {{ ref('directions_incentive_by_day') }} AS a
+LEFT JOIN
+  (
+    SELECT
+      territory,
+      l_territory,
+      type,
+      geom
+    FROM {{ source('geo', 'perimeters_centroid') }}
+    WHERE year = geo.get_latest_millesime()
+  ) AS b
+  ON a.code = b.territory AND a.type = b.type
+WHERE
+  a.code IS NOT null
+  {% if is_incremental() %}
+    AND
+      (extract('year' FROM start_date) * 10 + CASE WHEN extract('quarter' FROM start_date)::int > 3 THEN 2 ELSE 1 END)
+      >= (SELECT max(year * 10 + semester) FROM {{ this }})
+  {% endif %}
+GROUP BY 1, 2, 3, 4, 5, 6
