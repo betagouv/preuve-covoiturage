d6e00f7c93a2bc9559fdc6031fa97f48041d2f74
Ludovic Delhomme <74348195+Datayama38@users.noreply.github.com>
2024-09-04 11:10:55 +0200

    Dbt models for observatory (#2564)
    * feat: add justfile
    
    * configure dbt
    
    * fix dbt profile
    
    * change profile to default
    
    * add sources and geo_view
    
    * add views observatoire
    
    * add carpool tables
    
    * add directions and flux tables
    
    * fix select in flux queries
    
    * add occupation tables
    
    * add incentive tables
    
    * add distribution tables
    
    * wip fix flux queries
    
    * fix cast on occupation
    
    * fix: occupation queries
    
    * feat: add view_location
    
    * fix: view_location
    
    * fix: flux_by_trimester
    
    * fix: flux_by_trimester
    
    * fix: profile dev schema
    
    * fix: incremental queries for distribution
    
    ---------
    
    Co-authored-by: Nicolas Mérigot <nicolas.merigot@beta.gouv.fr>

---
+++ dbt/models/observatoire/distribution_by_trimester.sql
@@ -0,0 +1,91 @@
+{{ config(materialized='incremental') }}
+
+with distances as (
+  select
+    year,
+    trimester,
+    code,
+    type,
+    direction,
+    json_agg(
+      json_build_object('dist_classes', dist_classes, 'journeys', journeys)
+    ) as distances
+  from (
+    select
+      extract('year' from start_date)         as year,
+      extract('quarter' from start_date)::int as trimester,
+      code,
+      type,
+      direction,
+      dist_classes,
+      sum(journeys)                           as journeys
+    from {{ ref('distribution_distances_by_day') }}
+    group by 1, 2, 3, 4, 5, 6
+    order by 1, 2, 3, 4, 5, 6
+  ) as t
+  {% if is_incremental() %}
+    where
+      (year * 10 + trimester)
+      >= (SELECT max(year * 10 + trimester) FROM {{ this }})
+  {% endif %}
+  group by 1, 2, 3, 4, 5
+),
+
+hours as (
+  select
+    year,
+    trimester,
+    code,
+    type,
+    direction,
+    json_agg(json_build_object('hour', hour, 'journeys', journeys)) as hours
+  from (
+    select
+      extract('year' from start_date)         as year,
+      extract('quarter' from start_date)::int as trimester,
+      code,
+      type,
+      direction,
+      hour,
+      sum(journeys)                           as journeys
+    from {{ ref('distribution_hours_by_day') }}
+    group by 1, 2, 3, 4, 5, 6
+    order by 1, 2, 3, 4, 5, 6
+  ) as t
+  {% if is_incremental() %}
+    where
+      (year * 10 + trimester)
+      >= (SELECT max(year * 10 + trimester) FROM {{ this }})
+  {% endif %}
+  group by 1, 2, 3, 4, 5
+)
+
+select
+  a.year,
+  a.trimester,
+  a.code,
+  c.l_territory as libelle,
+  a.type,
+  a.direction,
+  a.hours,
+  b.distances
+from hours as a
+left join
+  distances as b
+  on
+    a.year = b.year
+    and a.trimester = b.trimester
+    and a.code = b.code
+    and a.direction = b.direction
+    and a.type = b.type
+left join
+  (
+    select
+      territory,
+      l_territory,
+      type
+    from {{ source('geo','perimeters_centroid') }}
+    where year = geo.get_latest_millesime()
+  ) as c
+  on a.code = c.territory and a.type = c.type
+order by 1, 2, 3, 4, 5
---
+++ dbt/models/observatoire/occupation_by_month.sql
@@ -0,0 +1,52 @@
+{{ config(materialized='incremental') }}
+WITH sum_distance AS (
+  SELECT
+    code,
+    type,
+    direction,
+    extract('year' FROM start_date)::int  AS year,
+    extract('month' FROM start_date)::int AS month,
+    sum(journeys)                         AS journeys,
+    round(sum(distance) / sum(journeys))
+    * sum(passenger_seats)                AS passengers_distance,
+    round(sum(distance) / sum(journeys))
+    * sum(drivers)                        AS drivers_distance
+  FROM {{ ref('directions_by_day') }}
+  {% if is_incremental() %}
+    WHERE
+      (extract('year' FROM start_date) * 100 + extract('month' FROM start_date))
+      >= (SELECT max(year * 100 + month) FROM {{ this }})
+  {% endif %}
+  GROUP BY 1, 2, 3, 4, 5
+  HAVING sum(journeys) > 0
+)
+
+SELECT
+  a.year,
+  a.month,
+  a.code,
+  b.l_territory AS libelle,
+  a.type,
+  a.direction,
+  a.journeys::int,
+  round(
+    (drivers_distance + passengers_distance) / drivers_distance, 2
+  )::float             AS occupation_rate,
+  st_asgeojson(b.geom,6)::json as geom
+FROM sum_distance AS a
+LEFT JOIN
+  (
+    SELECT
+      territory,
+      l_territory,
+      type,
+      geom
+    FROM {{ source('geo', 'perimeters_centroid') }}
+    WHERE year = geo.get_latest_millesime()
+  ) AS b
+  ON a.code = b.territory AND a.type = b.type
+WHERE
+  a.code IS NOT null
+  AND a.drivers_distance > 0
+  AND a.passengers_distance > 0
+ORDER BY 1, 2, 3, 5, 6
