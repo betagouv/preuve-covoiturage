c994a18917d3459c4c6f86b053f2fbe46bcffd85
Ludovic Delhomme <ludovic.delhomme@datayama.fr>
2024-09-13 11:15:55 +0200

    fix: move perimeters_aggregate to dbt

--- api/src/pdc/services/observatory/providers/IncentiveCampaignsRepositoryProvider.ts
+++ api/src/pdc/services/observatory/providers/IncentiveCampaignsRepositoryProvider.ts
@@ -14,42 +14,43 @@ import {
 export class IncentiveCampaignsRepositoryProvider
   implements IncentiveCampaignsRepositoryInterface {
   private readonly table = "observatoire_stats.incentive_campaigns";
-  private readonly perim_table = "observatoire_stats.perimeters_aggregate";
+  private readonly perim_table = "geo_stats.perimeters_aggregate";
 
   constructor(private pg: PostgresConnection) {}
 
   async getCampaigns(
     params: CampaignsParamsInterface,
   ): Promise<CampaignsResultInterface> {
-    const type = params.type !== undefined
+    const typeParam = params.type !== undefined
       ? checkTerritoryParam(params.type)
       : null;
-    const year = params.year !== undefined
-      ? Number(params.year)
-      : new Date().getFullYear();
-    const sql = {
-      values: params.code !== undefined ? [year, params.code] : [year],
-      text: `SELECT a.*, ST_AsGeoJSON(b.geom,6)::json as geom
+    const queryValues: (string | number)[] = [
+      params.year !== undefined ? params.year : new Date().getFullYear(),
+    ];
+    const conditions = [
+      `right(a.date_debut,4) = $1::varchar`,
+      `right(a.date_fin,4) = $1::varchar`,
+      `b.geom IS NOT NULL`,
+    ];
+    if (params.code) {
+      queryValues.push(params.code);
+      conditions.push(`left(a.code,9) = $2`);
+      conditions.push(`to_date(a.date_fin,'DD/MM/YYYY') > now()`);
+    }
+    if (typeParam) {
+      conditions.push(`a.type = '${typeParam}'`);
+    }
+    const queryText = `
+      SELECT a.*, ST_AsGeoJSON(b.geom,6)::json as geom
       FROM ${this.table} a 
-      LEFT JOIN ${this.perim_table} b on a.type = b.type AND left(a.code,9) = b.territory 
+      LEFT JOIN ${this.perim_table} b on a.type = b.type AND left(a.code,9) = b.code 
       AND b.year = geo.get_latest_millesime_or($1)
-      WHERE 
-      ${params.code !== undefined ? `left(a.code,9) = $2 AND` : ""}
-      ${type ? `a.type = '${type}' AND` : ""}
-      b.geom IS NOT NULL
-      ${
-        params.year !== undefined
-          ? `AND (right(a.date_debut,4) = $1::varchar AND right(a.date_fin,4) = $1::varchar)`
-          : `${
-            params.code !== undefined
-              ? ``
-              : `AND to_date(a.date_fin,'DD/MM/YYYY') > now()`
-          }`
-      }
-      ;`,
-    };
-    const response: { rows: CampaignsResultInterface } = await this.pg
-      .getClient().query<any>(sql);
+      WHERE ${conditions.join(" AND ")}
+    `;
+    const response = await this.pg.getClient().query({
+      text: queryText,
+      values: queryValues,
+    });
     return response.rows;
   }
 }
