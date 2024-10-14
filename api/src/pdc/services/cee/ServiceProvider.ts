ec6dd9450750914e1e6b7bb920d89aff7e07ca0d
Nicolas Mérigot <nicolas.merigot@beta.gouv.fr>
2024-10-14 17:35:01 +0200

    feat: add actions + provider interface

--- api/src/pdc/services/cee/ServiceProvider.ts
+++ api/src/pdc/services/cee/ServiceProvider.ts
@@ -10,17 +10,22 @@ import {
   ValidatorMiddleware,
 } from "@/pdc/providers/validator/index.ts";
 
-import { config } from "./config/index.ts";
-import { binding as importIdentityCeeBinding } from "@/shared/cee/importApplicationIdentity.schema.ts";
+import { binding as deleteCeeBinding } from "@/shared/cee/deleteApplication.schema.ts";
+import { binding as findCeeBinding } from "@/shared/cee/findApplication.schema.ts";
 import { binding as importCeeBinding } from "@/shared/cee/importApplication.schema.ts";
+import { binding as importIdentityCeeBinding } from "@/shared/cee/importApplicationIdentity.schema.ts";
 import { binding as registerCeeBinding } from "@/shared/cee/registerApplication.schema.ts";
 import { binding as simulateCeeBinding } from "@/shared/cee/simulateApplication.schema.ts";
+import { config } from "./config/index.ts";
 
-import { CeeRepositoryProvider } from "./providers/CeeRepositoryProvider.ts";
+import { DeleteCeeAction } from "./actions/DeleteCeeAction.ts";
+import { FindCeeAction } from "./actions/FindCeeAction.ts";
 import { ImportCeeAction } from "./actions/ImportCeeAction.ts";
+import { ImportCeeIdentityAction } from "./actions/ImportCeeIdentityAction.ts";
 import { RegisterCeeAction } from "./actions/RegisterCeeAction.ts";
 import { SimulateCeeAction } from "./actions/SimulateCeeAction.ts";
-import { ImportCeeIdentityAction } from "./actions/ImportCeeIdentityAction.ts";
+
+import { CeeRepositoryProvider } from "./providers/CeeRepositoryProvider.ts";
 
 @serviceProvider({
   config,
@@ -30,12 +35,16 @@ import { ImportCeeIdentityAction } from "./actions/ImportCeeIdentityAction.ts";
     importIdentityCeeBinding,
     registerCeeBinding,
     simulateCeeBinding,
+    findCeeBinding,
+    deleteCeeBinding,
   ],
   handlers: [
     ImportCeeAction,
     RegisterCeeAction,
     SimulateCeeAction,
     ImportCeeIdentityAction,
+    FindCeeAction,
+    DeleteCeeAction,
   ],
   middlewares: [...defaultMiddlewareBindings, [
     "validate",
