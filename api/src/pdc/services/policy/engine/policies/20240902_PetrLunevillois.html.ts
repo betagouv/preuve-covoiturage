1f0f566f16b870773b4d67d181d48a2fd16d73ad
Jonathan Fallon <jonathan.fallon@beta.gouv.fr>
2024-10-16 16:38:17 +0200

    fix: add PETR Lunevillois 2024/2025 (#2659)

---
+++ api/src/pdc/services/policy/engine/policies/20240902_PetrLunevillois.html.ts
@@ -0,0 +1,30 @@
+export const description = `
+<div _ngcontent-pmm-c231="" id="summary" class="campaignSummaryText-content-text">
+
+  <p>
+    Campagne d'incitation au covoiturage du <b> 2 septembre 2024 au 28 février 2025</b>,
+    toute la semaine
+  </p>
+
+  <p>Cette campagne est limitée à <b>10 000,00 €</b>.</p>
+
+  <p>
+    Les <b> conducteurs </b> effectuant un trajet en covoiturage d'au moins 2 km
+    sont incités selon les règles suivantes :
+  </p>
+
+  <ul>
+    <li><b>De 2 à 60 km : 0,07 € par km et par trajet par passager transporté</b></li>
+  </ul>
+
+  <p>Les restrictions suivantes seront appliquées :</p>
+
+  <ul>
+    <li><b>2 trajets maximum pour le conducteur par jour.</b></li>
+  </ul>
+
+  <p>
+    La campagne est limitée à l'opérateur Mobicoop proposant des preuves de classe <b>C</b>.
+  </p>
+
+</div>`;
