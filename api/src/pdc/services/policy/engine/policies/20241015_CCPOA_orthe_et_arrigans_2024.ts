f3e8675a85becc5b325525a32178a12f609784cf
Jonathan Fallon <jonathan.fallon@beta.gouv.fr>
2024-11-13 01:34:28 +0100

    fix: add CCPOA 2024_10 campaign (#2686)

---
+++ api/src/pdc/services/policy/engine/policies/20241015_CCPOA_orthe_et_arrigans_2024.ts
@@ -0,0 +1,113 @@
+import { getOperatorsAt, TimestampedOperators } from "@/pdc/services/policy/engine/helpers/getOperatorsAt.ts";
+import { isOperatorClassOrThrow } from "@/pdc/services/policy/engine/helpers/isOperatorClassOrThrow.ts";
+import { isOperatorOrThrow } from "@/pdc/services/policy/engine/helpers/isOperatorOrThrow.ts";
+import {
+  LimitTargetEnum,
+  watchForGlobalMaxAmount,
+  watchForPersonMaxAmountByMonth,
+  watchForPersonMaxTripByDay,
+} from "@/pdc/services/policy/engine/helpers/limits.ts";
+import { onDistanceRange, onDistanceRangeOrThrow } from "@/pdc/services/policy/engine/helpers/onDistanceRange.ts";
+import { perKm, perSeat } from "@/pdc/services/policy/engine/helpers/per.ts";
+import { startsOrEndsAtOrThrow } from "@/pdc/services/policy/engine/helpers/startsOrEndsAtOrThrow.ts";
+import { AbstractPolicyHandler } from "@/pdc/services/policy/engine/policies/AbstractPolicyHandler.ts";
+import { RunnableSlices } from "@/pdc/services/policy/interfaces/engine/PolicyInterface.ts";
+import {
+  OperatorsEnum,
+  PolicyHandlerInterface,
+  PolicyHandlerParamsInterface,
+  PolicyHandlerStaticInterface,
+  StatelessContextInterface,
+} from "@/pdc/services/policy/interfaces/index.ts";
+import { description } from "./20241015_CCPOA_orthe_et_arrigans_2024.html.ts";
+
+/* eslint-disable-next-line */
+export const CCPOA202410: PolicyHandlerStaticInterface = class extends AbstractPolicyHandler
+  implements PolicyHandlerInterface {
+  static readonly id = "ccpoa_202410";
+
+  protected operators: TimestampedOperators = [
+    {
+      date: new Date("2024-10-15T00:00:00+0200"),
+      operators: [OperatorsEnum.BLABLACAR_DAILY],
+    },
+  ];
+
+  // 7 cts per km per passenger
+  protected slices: RunnableSlices = [
+    {
+      start: 5_000,
+      end: 20_000,
+      fn: (ctx: StatelessContextInterface) => perSeat(ctx, 1_50),
+    },
+    {
+      start: 20_000,
+      end: 30_000,
+      fn: (ctx: StatelessContextInterface) => perSeat(ctx, perKm(ctx, { amount: 10, offset: 20_000, limit: 30_000 })),
+    },
+    {
+      start: 30_000,
+      end: 80_000,
+      fn: (_ctx: StatelessContextInterface) => 0,
+    },
+  ];
+
+  constructor(public max_amount: number) {
+    super();
+    this.limits = [
+      [
+        "9ea04dd8-2044-41ba-98fe-668bce5c3b81",
+        max_amount,
+        watchForGlobalMaxAmount,
+      ],
+      [
+        "bf7e8a51-e663-4e15-896f-717fb34fd9e2",
+        6,
+        watchForPersonMaxTripByDay,
+        LimitTargetEnum.Driver,
+      ],
+      [
+        "3b5ed8ab-fe9f-4032-9953-48bc58e3a00f",
+        100_00,
+        watchForPersonMaxAmountByMonth,
+        LimitTargetEnum.Driver,
+      ],
+    ];
+  }
+
+  protected processExclusion(ctx: StatelessContextInterface) {
+    isOperatorOrThrow(ctx, getOperatorsAt(this.operators, ctx.carpool.datetime));
+    onDistanceRangeOrThrow(ctx, { min: 5_000, max: 80_000 });
+    isOperatorClassOrThrow(ctx, ["C"]);
+    startsOrEndsAtOrThrow(ctx, { epci: ["200069417"] });
+  }
+
+  processStateless(ctx: StatelessContextInterface): void {
+    this.processExclusion(ctx);
+    super.processStateless(ctx);
+
+    let amount = 0;
+    for (const { start, fn } of this.slices) {
+      if (onDistanceRange(ctx, { min: start })) {
+        amount += fn(ctx);
+      }
+    }
+
+    ctx.incentive.set(amount);
+  }
+
+  params(): PolicyHandlerParamsInterface {
+    return {
+      tz: "Europe/Paris",
+      slices: this.slices,
+      operators: getOperatorsAt(this.operators),
+      limits: {
+        glob: this.max_amount,
+      },
+    };
+  }
+
+  describe(): string {
+    return description;
+  }
+};
