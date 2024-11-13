b1934082c10f1666fbb4c7be9629e97c4cce756b
Jonathan Fallon <jonathan.fallon@beta.gouv.fr>
2024-11-13 15:48:46 +0100

    fix: remove stream-json (#2688)

--- api/src/etl/helpers/data/streamJson.ts
+++ api/src/etl/helpers/data/streamJson.ts
@@ -1,26 +1,29 @@
-import { createReadStream, jsonPickUrl, jsonStreamArrayUrl } from "@/deps.ts";
-import type { JsonOptions } from "../../interfaces/index.ts";
-const { default: Pick } = await import(jsonPickUrl);
-const { default: StreamArray } = await import(jsonStreamArrayUrl);
+// import { createReadStream, jsonPickUrl, jsonStreamArrayUrl } from "@/deps.ts";
+// import type { JsonOptions } from "../../interfaces/index.ts";
+// const { default: Pick } = await import(jsonPickUrl);
+// const { default: StreamArray } = await import(jsonStreamArrayUrl);
 
-export async function* streamJson<T>(
-  filepath: string,
-  sheetOptions: JsonOptions,
-  chunkSize = 100,
-): AsyncIterable<T[]> {
-  const pipe = createReadStream(filepath, { encoding: "utf-8" }).pipe(
-    Pick.withParser(sheetOptions),
-  ).pipe(new StreamArray());
+// export async function* streamJson<T>(
+//   filepath: string,
+//   sheetOptions: JsonOptions,
+//   chunkSize = 100,
+// ): AsyncIterable<T[]> {
+//   const pipe = createReadStream(filepath, { encoding: "utf-8" }).pipe(
+//     Pick.withParser(sheetOptions),
+//   ).pipe(new StreamArray());
 
-  let chunk: T[] = [];
-  for await (const line of pipe) {
-    if (chunk.length === chunkSize) {
-      yield chunk;
-      chunk = [];
-    }
-    chunk.push(line);
-  }
-  yield chunk;
+//   let chunk: T[] = [];
+//   for await (const line of pipe) {
+//     if (chunk.length === chunkSize) {
+//       yield chunk;
+//       chunk = [];
+//     }
+//     chunk.push(line);
+//   }
+//   yield chunk;
 
-  return;
-}
+//   return;
+// }
+
+// TODO FIXME
+export async function* streamJson<T>(f: string, s: any, c = 100) {}
