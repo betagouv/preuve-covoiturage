d207846ad4ea4ff63cce1719595733f9f6ecf994
Julien Parmentier <julien.parmentier@beta.gouv.fr>
2024-11-22 09:36:35 +0100

    feat(policy): add la rochelle 2024

---
+++ api/src/pdc/services/policy/engine/policies/20240101_LaRochelle.html.ts
@@ -0,0 +1,21 @@
+export const description = `<p _ngcontent-moy-c231="" id="summary" class="campaignSummaryText-content-text">
+  <p>Campagne d’incitation au covoiturage du <b> 01 janvier 2024 au 31 décembre 2024</b>
+  </p>
+  <p>Cette campagne est limitée à <b>433 740,19</b> euros
+  </p>
+  <p>Les <b> conducteurs </b> effectuant un trajet d'au moins 5 km sont incités selon les règles suivantes :
+  </p>
+  <ul>
+    <li><b>De 5 à 10 km : 1 euro par trajet par passager transporté </b></li>
+    <li><b>De 10 km à 20km : 0.1 euro par km par passager </b></li>
+  </ul>
+  <p>Les restrictions suivantes seront appliquées :</p>
+  <ul>
+    <li><b>80€ maximum pour le conducteur par mois.</b></li>
+    <li><b>6 trajets maximum pour le conducteur par jour.</b></li>
+  </ul>
+
+
+  <p>La campagne est limitée à l'opérateur <b>Blablacar Daily</b> proposant des preuves de classe <b>B ou C</b>.</p>
+  <p></p>
+  </p>`;
