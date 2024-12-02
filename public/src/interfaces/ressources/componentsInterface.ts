2955a726c8b84c4fa05065b17e7afd851438efdf
Ludovic Delhomme <74348195+Datayama38@users.noreply.github.com>
2024-12-02 16:03:00 +0100

    Obs/cms feat update (#2682)
    * wip cms changes
    
    * feat: add rich-text for video
    
    * fix: review

--- public/src/interfaces/ressources/componentsInterface.ts
+++ public/src/interfaces/ressources/componentsInterface.ts
@@ -1,10 +1,13 @@
- export interface RessourceCardProps {
-  title: string,
-  content: string,
-  date: string,
-  img: string,
-  href:string,
-  link?: string,
-  file?: string,
-  horizontal?: false | undefined
+import { Button } from "../cms/collectionsInterface";
+
+export interface RessourceCardProps {
+  title: string;
+  content: string;
+  date: string;
+  img: string;
+  href: string;
+  link?: string;
+  file?: string;
+  horizontal?: false;
+  buttons?: Button[];
 }
