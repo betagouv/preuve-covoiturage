14a5a57161447ceb730446aaacf4450ba608484f
Ludovic Delhomme <ludovic.delhomme@datayama.fr>
2024-12-04 16:23:45 +0100

    start service for dashboard

--- api/src/pdc/proxy/Kernel.ts
+++ api/src/pdc/proxy/Kernel.ts
@@ -12,6 +12,7 @@ import { ApplicationServiceProvider } from "../services/application/ApplicationS
 import { CeeServiceProvider } from "../services/cee/CeeServiceProvider.ts";
 import { CertificateServiceProvider } from "../services/certificate/CertificateServiceProvider.ts";
 import { CompanyServiceProvider } from "../services/company/CompanyServiceProvider.ts";
+import { DashboardServiceProvider } from "../services/dashboard/DashboardServiceProvider.ts";
 import { ExportServiceProvider } from "../services/export/ExportServiceProvider.ts";
 import { GeoServiceProvider } from "../services/geo/GeoServiceProvider.ts";
 import { HonorServiceProvider } from "../services/honor/HonorServiceProvider.ts";
@@ -40,6 +41,7 @@ import { config } from "./config/index.ts";
     TerritoryServiceProvider,
     UserServiceProvider,
     ObservatoryServiceProvider,
+    DashboardServiceProvider,
     GeoServiceProvider,
   ],
   commands: [SeedCommand, ListCommand],
