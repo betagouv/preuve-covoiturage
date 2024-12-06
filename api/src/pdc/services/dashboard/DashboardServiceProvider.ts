049588b246f5c475bb9d783a53df825f9bb9a2ba
Ludovic Delhomme <ludovic.delhomme@datayama.fr>
2024-12-06 11:58:11 +0100

    wip add route operators-by-month

--- api/src/pdc/services/dashboard/DashboardServiceProvider.ts
+++ api/src/pdc/services/dashboard/DashboardServiceProvider.ts
@@ -3,15 +3,24 @@ import { ExtensionInterface, NewableType, serviceProvider } from "@/ilos/common/
 import { ServiceProvider as AbstractServiceProvider } from "@/ilos/core/index.ts";
 import { defaultMiddlewareBindings } from "@/pdc/providers/middleware/index.ts";
 import { ValidatorExtension, ValidatorMiddleware } from "@/pdc/providers/validator/index.ts";
+import { OperatorsAction } from "@/pdc/services/dashboard/actions/OperatorsAction.ts";
 import { config } from "./config/index.ts";
+import { binding as OperatorsByMonthBinding } from "./contracts/operators/operatorsByMonth.schema.ts";
+import { OperatorsRepositoryProvider } from "./providers/OperatorsRepositoryProvider.ts";
 
 /* eslint-enable */
 @serviceProvider({
   config,
   commands: [],
-  providers: [],
-  validator: [],
-  handlers: [],
+  providers: [
+    OperatorsRepositoryProvider,
+  ],
+  validator: [
+    OperatorsByMonthBinding,
+  ],
+  handlers: [
+    OperatorsAction,
+  ],
   middlewares: [...defaultMiddlewareBindings, [
     "validate",
     ValidatorMiddleware,
