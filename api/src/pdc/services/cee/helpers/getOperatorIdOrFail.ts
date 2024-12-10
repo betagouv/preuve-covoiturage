24cbaa47b8c01197005f6539143ad6c6f84092c6
Jonathan Fallon <jonathan.fallon@beta.gouv.fr>
2024-12-10 11:42:09 +0100

    MAJ documentation technique (#2724)
    * update PR template with details tag
    
    * fix response to match API documentation
    
    * factor responses and cleanup
    
    * add Open Data topic

--- api/src/pdc/services/cee/helpers/getOperatorIdOrFail.ts
+++ api/src/pdc/services/cee/helpers/getOperatorIdOrFail.ts
@@ -1,9 +1,9 @@
-import { ContextType, UnauthorizedException } from "@/ilos/common/index.ts";
+import { ContextType, ForbiddenException } from "@/ilos/common/index.ts";
 
 export function getOperatorIdOrFail(context: ContextType): number {
   const { operator_id }: { operator_id: number } = context.call?.user || {};
   if (!operator_id || Number.isNaN(operator_id)) {
-    throw new UnauthorizedException();
+    throw new ForbiddenException();
   }
   return operator_id;
 }
