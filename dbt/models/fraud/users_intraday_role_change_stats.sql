4ec5c3da8f43d1a20855cf8bb9fa61b44f4ec8ba
Luis Ventura <30115537+Lawiss@users.noreply.github.com>
2024-12-11 17:02:55 +0100

    Adds dbt fraud models

---
+++ dbt/models/fraud/users_intraday_role_change_stats.sql
@@ -0,0 +1,50 @@
+{{
+  config(
+    materialized = 'table',
+    indexes = [
+      {
+        "columns":["user_identity_key"],
+        "unique": true
+      }
+    ]
+    )
+}}
+
+with roles as (
+  select
+    user_identity_key,
+    start_datetime,
+    role
+  from
+    {{ ref('users_trips') }}
+),
+
+lagged as (
+  select
+    *,
+    lag(role, -1)
+      over (
+        partition by user_identity_key, date_trunc('day', start_datetime)
+        order by start_datetime
+      )
+    as next_role
+  from roles
+),
+
+intraday_stats as (
+  select
+    user_identity_key,
+    start_datetime                as start_day,
+    sum((role != next_role)::int) as count_consecutive_changes
+  from lagged
+  group by 1, 2
+)
+
+select
+  user_identity_key,
+  count(start_day) as total_traveled_days,
+  sum(count_consecutive_changes) as total_consecutives_role_changes,
+  avg(count_consecutive_changes) as avg_daily_consecutive_role_changes,
+  max(count_consecutive_changes) as max_daily_consecutive_role_change
+from intraday_stats
+group by 1
---
+++ dbt/models/fraud/users_trips.sql
@@ -0,0 +1,21 @@
+{{
+  config(
+    materialized = 'ephemeral',
+    )
+}}
+
+
+
+select
+  *,
+  driver_identity_key as user_identity_key,
+  'driver'            as role
+from
+  {{ source('carpool', 'carpools') }}
+union all
+select
+  *,  
+  passenger_identity_key as user_identity_key,
+  'passenger'            as role
+from
+  {{ source('carpool', 'carpools') }}
