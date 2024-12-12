fdafc28b897073086ab6ac92125f1252cd0fc4c4
Ludovic Delhomme <74348195+Datayama38@users.noreply.github.com>
2024-12-12 11:10:53 +0100

    Feat/dbt add dashboard tables (#2718)
    * add sources policy, territory and run dbt-osmosis
    
    * add territory_group_selector in sources
    
    * add view_join_territories
    
    * add tables carpools segmented by operators
    
    * add incented_journeys as indicator

---
+++ dbt/models/geo/view_perimeters_territories.sql
@@ -0,0 +1,8 @@
+{{ config(materialized='view') }}
+
+SELECT distinct d.arr, d.l_arr, a._id as territory_id, a.name as l_territory
+from territory.territories a 
+JOIN territory.territory_group b on b.company_id = a.company_id
+JOIN territory.territory_group_selector c on c.territory_group_id = b._id
+JOIN geo.perimeters d on (c.selector_value = d.arr or c.selector_value = d.epci or c.selector_value = d.aom or c.selector_value = d.dep or c.selector_value=d.reg) and d.year = 2023
+order by  d.arr
