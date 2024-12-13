99e4fca32b2525cd1c9786a01b63fa68fa517691
Ludovic Delhomme <74348195+Datayama38@users.noreply.github.com>
2024-12-13 15:11:06 +0100

    fix: stream_json and migrations (#2697)
    * fix: stream_json
    
    fix: deactivate datasets in etl
    
    fix: deactivate datasets in etl
    
    fix type
    
    * fix fraudcheck.labels migration
    
    ---------
    
    Co-authored-by: Jonathan Fallon <jonathan@scopopop.com>

--- api/src/etl/helpers/data/streamJson.ts
+++ api/src/etl/helpers/data/streamJson.ts
@@ -1,29 +1,39 @@
-// import { createReadStream, jsonPickUrl, jsonStreamArrayUrl } from "@/deps.ts";
-// import type { JsonOptions } from "../../interfaces/index.ts";
-// const { default: Pick } = await import(jsonPickUrl);
-// const { default: StreamArray } = await import(jsonStreamArrayUrl);
+import { JsonOptions } from "../../interfaces/index.ts";
 
-// export async function* streamJson<T>(
-//   filepath: string,
-//   sheetOptions: JsonOptions,
-//   chunkSize = 100,
-// ): AsyncIterable<T[]> {
-//   const pipe = createReadStream(filepath, { encoding: "utf-8" }).pipe(
-//     Pick.withParser(sheetOptions),
-//   ).pipe(new StreamArray());
+export async function* streamJson<T>(
+  filepath: string,
+  sheetOptions: JsonOptions,
+  chunkSize = 100,
+): AsyncIterable<T[]> {
+  // Lire tout le fichier comme une seule chaîne
+  const file = await Deno.readTextFile(filepath);
+  try {
+    // Parse tout le fichier
+    const parsed = JSON.parse(file);
+    // Si un filtre est défini, extraire le sous-ensemble correspondant
+    const data = sheetOptions.filter ? parsed[sheetOptions.filter] : parsed;
+    // Vérifiez si c'est un tableau JSON
+    if (Array.isArray(data)) {
+      let chunk: T[] = [];
+      for (const line of data) {
+        chunk.push(line);
 
-//   let chunk: T[] = [];
-//   for await (const line of pipe) {
-//     if (chunk.length === chunkSize) {
-//       yield chunk;
-//       chunk = [];
-//     }
-//     chunk.push(line);
-//   }
-//   yield chunk;
-
-//   return;
-// }
-
-// TODO FIXME
-export async function* streamJson<T>(f: string, s: any, c = 100) {}
+        if (chunk.length === chunkSize) {
+          yield chunk;
+          chunk = [];
+        }
+      }
+      // Renvoyer le dernier chunk s'il reste des éléments
+      if (chunk.length > 0) {
+        yield chunk;
+      }
+    } else {
+      console.error("Le fichier JSON n'est pas un tableau !");
+      throw new Error("Le fichier JSON doit être un tableau.");
+    }
+    return;
+  } catch (error) {
+    console.error("Erreur lors du parsing du fichier JSON :", error);
+    throw error;
+  }
+}
