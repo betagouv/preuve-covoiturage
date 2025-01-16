3118f5f40df7a547e6e27447701549088dabde9d
Jonathan Fallon <jonathan@scopopop.com>
2025-01-16 18:32:50 +0100

    add GrandPoitiers2025 campaign

---
+++ api/src/pdc/services/policy/engine/policies/20250101_GrandPoitiers2025.html.ts
@@ -0,0 +1,21 @@
+export const description = `
+<div _ngcontent-pmm-c231="" id="summary" class="campaignSummaryText-content-text">
+  <p>Campagne d’incitation au covoiturage du <b>01 janvier 2025 au 31 décembre 2025</b>, toute la semaine</p>
+  <p>Cette campagne est limitée à <b>75 000 euros</b>.</p>
+  <p>Le périmètre géographique de la campagne comprend les zones suivantes :</p>
+  <ul>
+    <li>CU du Grand Poitiers</li>
+  </ul>
+  <p>Les <b>conducteurs</b> effectuant un trajet entre 5km et 80km, avec pour origine OU destination le périmètre ci-dessus, sont incités selon les règles suivantes :</p>
+  <ul>
+    <li><b>De 5 à 80 km : 1,5 euros par trajet par passager transporté.</b></li>
+  </ul>
+  <p>Les restrictions suivantes seront appliquées :</p>
+  <ul>
+    <li><b>120 € maximum pour le conducteur par mois.</b></li>
+    <li><b>6 trajets maximum pour le conducteur par jour.</b></li>
+    <li><b>2 trajets maximum pour le passager par jour.</b></li>
+  </ul>
+  <p>La campagne est limitée aux trajets de classe <b>C</b> des opérateurs <b>Karos</b>, <b>Mobicoop</b>, <b>BlaBlaCar Daily</b> et <b>Coccinelle</b>.</p>
+</div>
+`;
