f011f5f6cc99fc52bbbca746203f0ee20a0020cb
Jonathan Fallon <jonathan.fallon@beta.gouv.fr>
2025-01-17 11:03:11 +0100

    fix(policy): Grand Poitiers 2025 (#2756)
    * update OperatorsEnum list
    
    * add GrandPoitiers2025 campaign

--- api/src/pdc/services/policy/engine/policies/index.ts
+++ api/src/pdc/services/policy/engine/policies/index.ts
@@ -20,6 +20,7 @@ import { PMGF2023 } from "@/pdc/services/policy/engine/policies/20230502_PMGF.ts
 import { GrandPoitiers } from "@/pdc/services/policy/engine/policies/20230927_GrandPoitiers.ts";
 import { PMGFOctobre2023 } from "@/pdc/services/policy/engine/policies/20231001_PMGF.ts";
 import { Cannes2024 } from "@/pdc/services/policy/engine/policies/20240101_Cannes.ts";
+import { LaRochelle2024 } from "@/pdc/services/policy/engine/policies/20240101_LaRochelle.ts";
 import { NantesMetropole2024 } from "@/pdc/services/policy/engine/policies/20240101_NantesMetropole.ts";
 import { PaysDeLaLoire2024 } from "@/pdc/services/policy/engine/policies/20240101_PaysDeLaLoire.ts";
 import { SMTC2024Passenger } from "@/pdc/services/policy/engine/policies/20240101_SMTC2024Passenger.ts";
@@ -29,15 +30,15 @@ import { GrandChatellerault2024 } from "@/pdc/services/policy/engine/policies/20
 import { TerresTouloises2024 } from "@/pdc/services/policy/engine/policies/20240201_TerresTouloises.ts";
 import { LannionTregor2024 } from "@/pdc/services/policy/engine/policies/20240401_LannionTregor.ts";
 import { PMGFxATMB2024 } from "@/pdc/services/policy/engine/policies/20240401_PMGFxATMB.ts";
+import { CCVMM202405 } from "@/pdc/services/policy/engine/policies/20240805_CCVMM_2024_05.ts";
 import { SiouleLimagne } from "@/pdc/services/policy/engine/policies/20240901_SiouleLimagne_2024_09.ts";
 import { PetrLunevillois092024032025 } from "@/pdc/services/policy/engine/policies/20240902_PetrLunevillois.ts";
 import { CCPOA202410 } from "@/pdc/services/policy/engine/policies/20241015_CCPOA_orthe_et_arrigans_2024.ts";
+import { GrandPoitiers2025 } from "@/pdc/services/policy/engine/policies/20250101_GrandPoitiers2025.ts";
 import { PolicyTemplateOne } from "@/pdc/services/policy/engine/policies/unbound/PolicyTemplateOne.ts";
 import { PolicyTemplateThree } from "@/pdc/services/policy/engine/policies/unbound/PolicyTemplateThree.ts";
 import { PolicyTemplateTwo } from "@/pdc/services/policy/engine/policies/unbound/PolicyTemplateTwo.ts";
 import { PolicyHandlerStaticInterface } from "@/pdc/services/policy/interfaces/index.ts";
-import { LaRochelle2024 } from "./20240101_LaRochelle.ts";
-import { CCVMM202405 } from "./20240805_CCVMM_2024_05.ts";
 
 export const policies: Map<string, PolicyHandlerStaticInterface> = new Map(
   [
@@ -48,6 +49,7 @@ export const policies: Map<string, PolicyHandlerStaticInterface> = new Map(
     Cotentin2023,
     GrandChatellerault2024,
     GrandPoitiers,
+    GrandPoitiers2025,
     IDFMPeriodeNormale2021,
     Lannion202305,
     LannionTregor2024,
