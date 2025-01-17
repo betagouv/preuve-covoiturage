f5b122d37ab18c557b66de1616df9a025984b38c
Jonathan Fallon <jonathan.fallon@beta.gouv.fr>
2025-01-17 11:04:53 +0100

    fix(policy): Haute Correze 2025 (#2757)

---
+++ api/src/pdc/services/policy/engine/policies/20240902_haute_correze_2025.html.ts
@@ -0,0 +1,34 @@
+export const description = `
+<div _ngcontent-pmm-c231="" id="summary" class="campaignSummaryText-content-text">
+
+  <p>
+    Campagne d'incitation au covoiturage du <b> 02 septembre 2024 au 01 septembre 2025</b>,
+    toute la semaine
+  </p>
+
+  <p>Cette campagne est limitée à <b>10 000,00 €</b>.</p>
+
+  <p>
+    Les <b> conducteurs </b> effectuant un trajet en covoiturage d'au moins 2 km
+    au départ ou à l'arrivée de la CC Haute-Corrèze Communauté
+    sont incités selon les règles suivantes :
+  </p>
+
+  <ul>
+    <li><b>De 2 à 15 km : 1,50 € par trajet et par passager transporté</b></li>
+    <li><b>De 15 à 30 km : 1,50 € + 0,10 € par km supplémentaire par trajet et par passager transporté</b></li>
+    <li><b>De 30 à 80 km : 3,00 € par trajet et par passager transporté</b></li>
+  </ul>
+
+  <p>Les restrictions suivantes seront appliquées :</p>
+
+  <ul>
+    <li><b>6 trajets maximum pour le conducteur par jour.</b></li>
+    <li><b>150,00 € maximum pour le conducteur par mois.</b></li>
+  </ul>
+
+  <p>
+    La campagne est limitée à l'opérateur Blablacar Daily proposant des preuves de classe <b>B</b> ou <b>C</b>.
+  </p>
+
+</div>`;
