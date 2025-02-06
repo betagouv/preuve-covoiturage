2f92e8b2c982e3f18c76110aeba1e461bc14b31b
Jonathan Fallon <jonathan.fallon@beta.gouv.fr>
2025-02-06 17:09:07 +0100

    External data (#2795)
    * export sha256sum and sha1sum functions
    
    * remove console.log (#2794)
    
    * feat: add helper for campaigns url
    
    * export sha256sum and sha1sum functions
    
    * feat: add helper for campaigns url
    
    * fix geo migrations
    
    * fix type for datasets
    
    ---------
    
    Co-authored-by: Ludovic Delhomme <ludovic.delhomme@datayama.fr>

--- api/src/db/external_data/helpers.ts
+++ api/src/db/external_data/helpers.ts
@@ -1,12 +1,21 @@
 export async function getAiresLastUrl(url: string): Promise<string> {
   const response = await fetch(url, { method: "get" });
   const res = await response.json();
-  const list = res
-    ? res.history.filter((h) => h.payload.schema_name !== null)
-    : [];
-  const fileUrl = list.length > 0 ? list[0].payload.permanent_url : "";
   if (!response.ok) {
     console.error(res.error.data);
   }
+  const list = res ? res.history.filter((h: any) => h.payload.schema_name !== null) : [];
+  const fileUrl = list.length > 0 ? list[0].payload.permanent_url : "";
+  return fileUrl;
+}
+
+export async function getCampaignsLastUrl(url: string): Promise<string> {
+  const response = await fetch(url, { method: "get" });
+  const res = await response.json();
+  if (!response.ok) {
+    console.error(res.error.data);
+  }
+  const list = res ? res.resources : [];
+  const fileUrl = list.length > 0 ? list[0].latest : "";
   return fileUrl;
 }
