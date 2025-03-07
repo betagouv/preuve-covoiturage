1615f70847af5e565db9598f6ba7bb7df7c9b57c
Nicolas Mérigot <nicolas.merigot@beta.gouv.fr>
2025-03-07 12:18:47 +0100

    fix: missing old jwt config

--- api/src/pdc/services/auth/config/index.ts
+++ api/src/pdc/services/auth/config/index.ts
@@ -1,7 +1,9 @@
 import * as oidc from "./oidc.ts";
+import * as oldjwt from "./old_jwt.ts";
 import * as permissions from "./permissions.ts";
 
 export const config = {
   permissions,
   oidc,
+  jwt: oldjwt,
 };
