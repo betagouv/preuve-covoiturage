2c9b007aec283947e34a812deddc041fb50d3464
Jonathan Fallon <jonathan.fallon@beta.gouv.fr>
2025-03-31 09:36:20 +0200

    add IF EXISTS to migration statements

--- api/src/db/migrations/20250326000000-alter-territories.sql
+++ api/src/db/migrations/20250326000000-alter-territories.sql
@@ -1,12 +1,12 @@
-ALTER TABLE territory.territory_group ALTER COLUMN shortname DROP NOT NULL;
+ALTER TABLE IF EXISTS territory.territory_group ALTER COLUMN shortname DROP NOT NULL;
 
-ALTER TABLE territory.territory_group ALTER COLUMN address DROP NOT NULL;
+ALTER TABLE IF EXISTS territory.territory_group ALTER COLUMN address DROP NOT NULL;
 
-ALTER TABLE territory.territory_group ALTER COLUMN contacts DROP NOT NULL;
+ALTER TABLE IF EXISTS territory.territory_group ALTER COLUMN contacts DROP NOT NULL;
 
-DROP TABLE territory.territory_codes CASCADE;
-DROP TABLE territory.territories CASCADE;
-DROP TABLE territory.territory_relation CASCADE;
-DROP TABLE territory.territory_operators_legacy CASCADE;
-DROP TABLE territory.insee CASCADE;
+DROP TABLE IF EXISTS territory.territory_codes CASCADE;
+DROP TABLE IF EXISTS territory.territories CASCADE;
+DROP TABLE IF EXISTS territory.territory_relation CASCADE;
+DROP TABLE IF EXISTS territory.territory_operators_legacy CASCADE;
+DROP TABLE IF EXISTS territory.insee CASCADE;
 
