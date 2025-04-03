5ff95e5359bbad06ffa9fba425e1ed93ddfc954e
Nicolas Mérigot <nicolas.merigot@beta.gouv.fr>
2025-04-03 15:03:19 +0200

    feat: add pc oidc provider

--- api/src/pdc/services/auth/actions/AccessTokenAction.ts
+++ api/src/pdc/services/auth/actions/AccessTokenAction.ts
@@ -1,7 +1,7 @@
 import { handler } from "@/ilos/common/Decorators.ts";
 import { Action as AbstractAction } from "@/ilos/core/index.ts";
 import { AccessTokenParams, AccessTokenResult } from "../dto/AccessToken.ts";
-import { OIDCProvider } from "../providers/OIDCProvider.ts";
+import { DexOIDCProvider } from "../providers/DexOIDCProvider.ts";
 
 @handler({
   service: "auth",
@@ -22,14 +22,14 @@ import { OIDCProvider } from "../providers/OIDCProvider.ts";
 })
 export class AccessTokenAction extends AbstractAction {
   constructor(
-    private OIDCProvider: OIDCProvider,
+    private dexOIDCProvider: DexOIDCProvider,
   ) {
     super();
   }
 
   protected override async handle(params: AccessTokenParams): Promise<AccessTokenResult> {
     const { username, password } = params;
-    const access_token = await this.OIDCProvider.getToken(username, password);
+    const access_token = await this.dexOIDCProvider.getToken(username, password);
     return { access_token };
   }
 }
