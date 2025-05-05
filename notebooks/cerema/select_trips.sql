4d6f4b4654011fdf3abd7932fc585554ccf4b9b2
Jonathan Fallon <jonathan@scopopop.com>
2025-05-05 11:20:42 +0200

    CEREMA PDLL Notebooks

---
+++ notebooks/cerema/select_trips.sql
@@ -0,0 +1,80 @@
+\COPY (SELECT
+  journey_id,
+  operator_trip_id,
+  operator_journey_id,
+  operator_class,
+  acquisition_status,
+  fraud_status,
+  anomaly_status,
+  start_datetime,
+  start_date,
+  start_time,
+  end_datetime,
+  end_date,
+  end_time,
+  duration,
+  distance,
+  start_lat,
+  start_lon,
+  end_lat,
+  end_lon,
+  start_insee,
+  start_commune,
+  start_departement,
+  start_epci,
+  start_aom,
+  start_region,
+  start_pays,
+  end_insee,
+  end_commune,
+  end_departement,
+  end_epci,
+  end_aom,
+  end_region,
+  end_pays,
+  operator,
+  operator_passenger_id,
+  passenger_identity_key,
+  operator_driver_id,
+  driver_identity_key,
+  driver_revenue,
+  passenger_contribution,
+  passenger_seats,
+  cee_application,
+  incentive_0_siret,
+  incentive_0_name,
+  incentive_0_amount,
+  incentive_1_siret,
+  incentive_1_name,
+  incentive_1_amount,
+  incentive_2_siret,
+  incentive_2_name,
+  incentive_2_amount,
+  incentive_rpc_0_campaign_id,
+  incentive_rpc_0_campaign_name,
+  incentive_rpc_0_siret,
+  incentive_rpc_0_name,
+  incentive_rpc_0_amount,
+  incentive_rpc_1_campaign_id,
+  incentive_rpc_1_campaign_name,
+  incentive_rpc_1_siret,
+  incentive_rpc_1_name,
+  incentive_rpc_1_amount,
+  incentive_rpc_2_campaign_id,
+  incentive_rpc_2_campaign_name,
+  incentive_rpc_2_siret,
+  incentive_rpc_2_name,
+  incentive_rpc_2_amount
+FROM export.dbt_trips
+WHERE _start_at >= '2025-01-01T00:00:00+0100'
+  AND _start_at  < '2025-05-01T00:00:00+0200'
+  AND (_start_region = '52' OR _end_region = '52')
+  -- AND NOT (_start_aom = '244400404' AND _end_aom = '244400404')
+  -- AND NOT (_start_aom = '244900015' AND _end_aom = '244900015')
+  -- AND NOT (_start_aom = '247200132' AND _end_aom = '247200132')
+  -- AND NOT (_start_aom = '200071678' AND _end_aom = '200071678')
+  AND acquisition_status = 'processed'
+  -- AND _operator_id IN (3,4,9,14)
+  -- AND operator_class = 'C'
+ORDER BY _start_at ASC
+) TO 'pdll2025.csv' WITH CSV HEADER ENCODING 'UTF8';
