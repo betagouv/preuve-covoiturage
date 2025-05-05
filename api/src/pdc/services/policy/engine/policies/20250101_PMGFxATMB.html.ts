70f2fcb2c65ad01e6780acebe3e66eb11b67ae29
Jonathan Fallon <jonathan@scopopop.com>
2025-05-05 12:15:37 +0200

    update PMGFxATMB rules

--- api/src/pdc/services/policy/engine/policies/20250101_PMGFxATMB.html.ts
+++ api/src/pdc/services/policy/engine/policies/20250101_PMGFxATMB.html.ts
@@ -4,6 +4,8 @@ export const description = `<div _ngcontent-fyn-c231="" id="summary" class="camp
   
   <p>Cette campagne est limitée à <b>116 804,00 €</b>.</p>
 
+  <p><strong>Règles applicables avant le 21/04/2025</strong></p>
+
   <p>
     Les <b> conducteurs </b> effectuant un trajet entre 5 et 40 km
     au départ ou à l'arrivée du Pôle Métropolitain Genevois
@@ -18,7 +20,9 @@ export const description = `<div _ngcontent-fyn-c231="" id="summary" class="camp
     <li><b>Au delà de 40 km : 4,00 € pour le conducteur par trajet et par passager.</b></li>
   </ul>
 
+  <!-- ------------------------------------------------------------------------------------ -->
   <p><strong>Départ <u>ou</u> Arrivée au sein du PMGF :</strong></p>
+  <!-- ------------------------------------------------------------------------------------ -->
 
   <ul>
     <li><b>De 5 à 20 km : 0,50 € pour le conducteur par trajet&nbsp;;</b></li>
@@ -27,11 +31,49 @@ export const description = `<div _ngcontent-fyn-c231="" id="summary" class="camp
   </ul>
 
   <p>Les restrictions suivantes seront appliquées :</p>
-  
+
   <ul>
     <li><b>50€ maximum pour le conducteur par mois.</b></li>
   </ul>
-  
+
+  <p>
+    La campagne est éligible à l'opérateur de covoiturage
+    Blablacar Daily
+    proposant des preuves de classe <b>B</b> ou <b>C</b>
+  </p>
+
+  <!-- ------------------------------------------------------------------------------------ -->
+  <p><strong>Règles applicables après le 21/04/2025</strong></p>
+  <!-- ------------------------------------------------------------------------------------ -->
+
+  <p>
+    Les <b> conducteurs </b> effectuant un trajet entre 5 et 40 km
+    au départ ou à l'arrivée du Pôle Métropolitain Genevois
+    sont incités selon les règles suivantes.
+  </p>
+
+  <p><strong>Départ <u>et</u> Arrivée au sein du PMGF :</strong></p>
+
+  <ul>
+    <li><b>De 5 à 20 km : 1,50 € par passager transporté&nbsp;;</b></li>
+    <li><b>De 20 à 30 km : 1,50 € par passager transporté + 0,15 € par passager par kilomètre supplémentaire soit 3,00 € pour 30 km&nbsp;;</b></li>
+    <li><b>De 30 à 40 km : 1,50 € par passager transporté + 0,10 € par passager par kilomètre supplémentaire soit 4,00 € pour 40 km&nbsp;;</b></li>
+    <li><b>Au delà de 40 km : 4,00 € par passager transporté.</b></li>
+  </ul>
+
+  <p><strong>Départ <u>ou</u> Arrivée au sein du PMGF :</strong></p>
+
+  <ul>
+    <li><b>De 5 à 21 km : 0,50 € par passager transporté&nbsp;;</b></li>
+    <li><b>Au delà de 21 km : 1,00 € par passager transporté.</b></li>
+  </ul>
+
+  <p>Les restrictions suivantes seront appliquées :</p>
+
+  <ul>
+    <li><b>50€ maximum par conducteur par mois.</b></li>
+  </ul>
+
   <p>
     La campagne est éligible à l'opérateur de covoiturage
     Blablacar Daily
