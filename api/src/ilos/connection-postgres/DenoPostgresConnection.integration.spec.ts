0eba88a09047d214c2981d3f81b5faa0d24587c2
Jonathan Fallon <jonathan@scopopop.com>
2025-05-05 17:28:05 +0200

    [wip] add tests

---
+++ api/src/ilos/connection-postgres/DenoPostgresConnection.integration.spec.ts
@@ -0,0 +1,220 @@
+import { afterAll, assert, beforeAll, describe, it } from "@/dev_deps.ts";
+import { env } from "@/lib/env/index.ts";
+import sql from "@/lib/pg/sql.ts";
+import { ClientOptions, TransactionError } from "dep:postgres";
+import { DenoPostgresConnection } from "./DenoPostgresConnection.ts";
+
+describe("DenoPostgresConnection - connection", () => {
+  async function testConnection(params?: string | ClientOptions | undefined) {
+    const connection = new DenoPostgresConnection(params);
+    await connection.up();
+    assert(await connection.isReady());
+    await connection.down();
+    assert(await connection.isReady() === false);
+  }
+
+  it("should connect without parameters", async () => {
+    // Force insecure connection to bypass the missing certificate
+    Deno.env.set("APP_POSTGRES_INSECURE", "true");
+    await testConnection();
+  });
+
+  it("should connect with a connection string", async () => {
+    // Force insecure connection to bypass the missing certificate
+    Deno.env.set("APP_POSTGRES_INSECURE", "true");
+    await testConnection(env("APP_POSTGRES_URL"));
+  });
+
+  it("should crash on missing certificate without forcing insecure", async () => {
+    // Remove the insecure connection
+    Deno.env.delete("APP_POSTGRES_CA");
+    Deno.env.delete("APP_POSTGRES_INSECURE");
+
+    let connection: DenoPostgresConnection | null = null;
+    try {
+      connection = new DenoPostgresConnection();
+      await connection.up();
+      assert(false, "Should have thrown an error");
+    } catch (e) {
+      assert(e instanceof Error);
+      assert(e.message.includes("APP_POSTGRES_CA or APP_POSTGRES_INSECURE must be set"));
+    } finally {
+      connection && await connection.down();
+    }
+  });
+
+  it("should crash on rogue certificate", async () => {
+    try {
+      // Remove the insecure connection
+      Deno.env.set("APP_POSTGRES_CA", "rogue-certificate");
+      Deno.env.delete("APP_POSTGRES_INSECURE");
+
+      let connection: DenoPostgresConnection | null = null;
+      try {
+        connection = new DenoPostgresConnection();
+        await connection.up();
+        assert(false, "Should have thrown an error");
+      } catch (_e) {
+        connection && await connection.down();
+      }
+    } catch (e) {
+      assert(e instanceof Error);
+      assert(e.message.includes("The server isn't accepting TLS connections"));
+    }
+  });
+});
+
+describe("DenoPostgresConnection - poolSize", () => {
+  it("should set the pool size", async () => {
+    // Force insecure connection to bypass the missing certificate
+    Deno.env.set("APP_POSTGRES_INSECURE", "true");
+
+    // Set the pool size to 5
+    Deno.env.set("APP_POSTGRES_POOL_SIZE", "5");
+    assert(new DenoPostgresConnection().poolSize === 5);
+
+    // Use the default pool size
+    Deno.env.delete("APP_POSTGRES_POOL_SIZE");
+    assert(new DenoPostgresConnection().poolSize === 3);
+  });
+});
+
+describe("DenoPostgresConnection - debug levels", () => {
+  it("should set the debug level", async () => {
+    // Force insecure connection to bypass the missing certificate
+    Deno.env.set("APP_POSTGRES_INSECURE", "true");
+
+    // Set the debug level to queries
+    Deno.env.set("APP_POSTGRES_DEBUG", "queries");
+    let connection = new DenoPostgresConnection();
+    assert(typeof connection.poolConfig.controls!.debug === "object");
+    assert(connection.poolConfig.controls!.debug!.queries === true);
+    assert(connection.poolConfig.controls!.debug!.notices === false);
+    assert(connection.poolConfig.controls!.debug!.results === false);
+    assert(connection.poolConfig.controls!.debug!.queryInError === false);
+
+    // Set the debug level to notices
+    Deno.env.set("APP_POSTGRES_DEBUG", "notices");
+    connection = new DenoPostgresConnection();
+    assert(typeof connection.poolConfig.controls!.debug === "object");
+    assert(connection.poolConfig.controls!.debug!.queries === false);
+    assert(connection.poolConfig.controls!.debug!.notices === true);
+    assert(connection.poolConfig.controls!.debug!.results === false);
+    assert(connection.poolConfig.controls!.debug!.queryInError === false);
+
+    // Set the debug level to notices and results
+    Deno.env.set("APP_POSTGRES_DEBUG", "notices,results");
+    connection = new DenoPostgresConnection();
+    assert(typeof connection.poolConfig.controls!.debug === "object");
+    assert(connection.poolConfig.controls!.debug!.queries === false);
+    assert(connection.poolConfig.controls!.debug!.notices === true);
+    assert(connection.poolConfig.controls!.debug!.results === true);
+    assert(connection.poolConfig.controls!.debug!.queryInError === false);
+
+    // Set the debug level to all
+    Deno.env.set("APP_POSTGRES_DEBUG", "all");
+    connection = new DenoPostgresConnection();
+    assert(typeof connection.poolConfig.controls!.debug === "boolean");
+    assert(connection.poolConfig.controls!.debug);
+
+    // Set the debug level to queries,notices,results,queryInError in uppercase with pipe separator
+    Deno.env.set("APP_POSTGRES_DEBUG", "QUERIES|NOTICES|RESULTS|QUERYINERROR");
+    connection = new DenoPostgresConnection();
+    assert(typeof connection.poolConfig.controls!.debug === "object");
+    assert(connection.poolConfig.controls!.debug!.queries === true);
+    assert(connection.poolConfig.controls!.debug!.notices === true);
+    assert(connection.poolConfig.controls!.debug!.results === true);
+    assert(connection.poolConfig.controls!.debug!.queryInError === true);
+  });
+});
+
+describe("DenoPostgresConnection - query helpers", () => {
+  const connection = new DenoPostgresConnection();
+  const query = sql`SELECT * FROM generate_series(1, 10)`;
+
+  beforeAll(async () => {
+    Deno.env.set("APP_POSTGRES_INSECURE", "true");
+    Deno.env.delete("APP_POSTGRES_DEBUG");
+    await connection.up();
+  });
+
+  afterAll(async () => {
+    await connection.down();
+  });
+
+  it("should use query as queryObject", async () => {
+    const results = await connection.query<{ generate_series: number }>(query);
+    assert(results.length === 10);
+    assert(results[0].generate_series === 1);
+    assert(results[9].generate_series === 10);
+  });
+
+  it("should use queryArray", async () => {
+    const results = await connection.queryArray<{ generate_series: number }>(query);
+    assert(results.length === 10);
+    assert(results[0][0] === 1);
+    assert(results[9][0] === 10);
+  });
+
+  it("should use queryObject", async () => {
+    const results = await connection.queryObject<{ generate_series: number }>(query);
+    assert(results.length === 10);
+    assert(results[0].generate_series === 1);
+    assert(results[9].generate_series === 10);
+  });
+});
+
+describe("DenoPostgresConnection - transaction", () => {
+  const connection = new DenoPostgresConnection();
+  const query = sql`SELECT 'yes' AS answer`;
+
+  beforeAll(async () => {
+    Deno.env.set("APP_POSTGRES_INSECURE", "true");
+    Deno.env.delete("APP_POSTGRES_DEBUG");
+    await connection.up();
+    await connection.query(sql`CREATE TABLE IF NOT EXISTS test_transaction (id SERIAL PRIMARY KEY)`);
+  });
+
+  afterAll(async () => {
+    await connection.query(sql`DROP TABLE IF EXISTS test_transaction`);
+    await connection.down();
+  });
+
+  it("should use transaction and rollback", async () => {
+    using client = await connection.pool.connect();
+    const transaction = client.createTransaction(DenoPostgresConnection.id());
+    await transaction.begin();
+    await transaction.queryObject(sql`INSERT INTO test_transaction (id) VALUES (1)`);
+    await transaction.queryObject(sql`INSERT INTO test_transaction (id) VALUES (2)`);
+    await transaction.queryObject(sql`INSERT INTO test_transaction (id) VALUES (3)`);
+
+    // Insert entries in a transsaction
+    let { rows } = await transaction.queryObject<{ id: number }>(sql`SELECT * FROM test_transaction ORDER BY id`);
+    assert(rows.length === 3);
+    assert(rows[0].id === 1);
+    assert(rows[1].id === 2);
+    assert(rows[2].id === 3);
+
+    // Rollback to discard the new entries
+    await transaction.rollback();
+
+    // Check that the entries are not present
+    ({ rows } = await client.queryObject<{ id: number }>(sql`SELECT * FROM test_transaction ORDER BY id`));
+    assert(rows.length === 0);
+  });
+
+  it("should use a read_only transaction by default", async () => {
+    using client = await connection.pool.connect();
+    const id = DenoPostgresConnection.id();
+
+    try {
+      const transaction = client.createTransaction(id, { read_only: true });
+      await transaction.begin();
+      await transaction.queryObject(sql`INSERT INTO test_transaction (id) VALUES (1)`);
+      await transaction.commit();
+    } catch (e) {
+      assert(e instanceof TransactionError);
+      assert(e.message.includes(`The transaction "${id}" has been aborted`));
+    }
+  });
+});
--- api/src/ilos/connection-postgres/DenoPostgresConnection.ts
+++ api/src/ilos/connection-postgres/DenoPostgresConnection.ts
@@ -1,18 +1,27 @@
 import { ConnectionInterface, DestroyHookInterface, InitHookInterface } from "@/ilos/common/index.ts";
-import { env_or_default, env_or_fail, env_or_false } from "@/lib/env/index.ts";
+import { env_or_default, env_or_false } from "@/lib/env/index.ts";
 import { logger } from "@/lib/logger/index.ts";
-import { ClientOptions, Pool } from "dep:postgres";
+import sql, { Sql } from "@/lib/pg/sql.ts";
+import { ClientOptions, Pool, TransactionOptions } from "dep:postgres";
+import { array, assert, boolean, number, object, pattern, refine, string, union } from "dep:superstruct";
 
-export class DenoPostgreConnection implements ConnectionInterface<Pool>, InitHookInterface, DestroyHookInterface {
-  #pool: Pool;
+export class DenoPostgresConnection implements ConnectionInterface<Pool>, InitHookInterface, DestroyHookInterface {
+  #pool: Pool | null = null;
   #poolSize = 3;
   #poolLazy = false;
   #poolConfig: ClientOptions = {};
 
-  constructor(config: ClientOptions) {
-    // Get and parse the connection string
-    const connectionString: string = env_or_fail("APP_POSTGRES_URL");
-    const { hostname, username, password, pathname, port } = new URL(connectionString!);
+  /**
+   * Generate a unique transaction name
+   */
+  static id(prefix: "transaction" | "cursor" = "transaction"): string {
+    return `${prefix}-${new Date().getTime()}-${Math.random().toString(36).slice(2)}`.toLowerCase();
+  }
+
+  constructor(params?: string | ClientOptions | undefined) {
+    // Get and parse the config and the connection string
+    const { connectionString, config } = this.#configure(params);
+    const { hostname, username, password, pathname, port } = new URL(connectionString);
 
     /**
      * Check or override the TLS certificate
@@ -46,7 +55,7 @@ export class DenoPostgreConnection implements ConnectionInterface<Pool>, InitHoo
      */
     const debugString: string = env_or_default("APP_POSTGRES_DEBUG", "").toLocaleLowerCase();
 
-    this.#poolConfig = {
+    this.#poolConfig = this.#validateConfig({
       applicationName: "deno-postgres",
       connection: {
         attempts: 1,
@@ -62,25 +71,31 @@ export class DenoPostgreConnection implements ConnectionInterface<Pool>, InitHoo
         caCertificates: forceInsecure ? [] : [certificate!],
       },
       controls: {
-        debug: {
+        debug: debugString === "all" ? true : {
           queries: debugString.includes("queries"),
           notices: debugString.includes("notices"),
           results: debugString.includes("results"),
-          queryInError: debugString.includes("queryInError"),
+          queryInError: debugString.includes("queryinerror"),
         },
       },
       ...config,
-    };
+    });
   }
 
-  async up(): Promise<void> {
-    this.#pool = new Pool(this.#poolConfig, this.#poolSize, this.#poolLazy);
-    logger.debug(`[pg] connected to ${this.#poolConfig.database}`);
+  get pool(): Pool {
+    if (!this.#poolIsValid()) {
+      throw new Error("Pool is not initialized");
+    }
+
+    return this.#pool!;
   }
 
-  async down(): Promise<void> {
-    await this.#pool.end();
-    logger.debug(`[pg] disconnected from ${this.#poolConfig.database}`);
+  get poolSize(): number {
+    return this.#poolSize;
+  }
+
+  get poolConfig(): ClientOptions {
+    return this.#poolConfig;
   }
 
   /**
@@ -103,14 +118,173 @@ export class DenoPostgreConnection implements ConnectionInterface<Pool>, InitHoo
    * ```
    */
   getClient(): Pool {
-    return this.#pool;
+    if (!this.#poolIsValid()) {
+      throw new Error("Pool is not initialized");
+    }
+
+    return this.#pool!;
   }
 
+  // InitHookInterface mapping
   async init(): Promise<void> {
     await this.up();
   }
 
+  // DestroyHookInterface mapping
   async destroy(): Promise<void> {
     await this.down();
+  }
+
+  async up(): Promise<void> {
+    if (await this.isReady()) {
+      logger.debug(`[pg] already connected to ${this.#poolConfig.database}`);
+      return;
+    }
+
+    this.#pool = new Pool(this.#poolConfig, this.#poolSize, this.#poolLazy);
+    await this.query(sql`SELECT 1`);
+    logger.debug(`[pg] connected to ${this.#poolConfig.database}`);
+  }
+
+  async down(): Promise<void> {
+    this.#pool && await this.#pool.end();
+    this.#pool = null;
+    logger.debug(`[pg] disconnected from ${this.#poolConfig.database}`);
+  }
+
+  async isReady(): Promise<boolean> {
+    if (!this.#poolIsValid()) {
+      return false;
+    }
+
+    return await this.#pool!.initialized() > 0;
+  }
+
+  async query<TResult>(sql: Sql): Promise<TResult[]> {
+    return this.#wrap<TResult>(sql, "object");
+  }
+
+  async queryArray<TResult>(sql: Sql): Promise<TResult[]> {
+    return this.#wrap<TResult>(sql, "array");
+  }
+
+  async queryObject<TResult>(sql: Sql): Promise<TResult[]> {
+    return this.#wrap<TResult>(sql, "object");
+  }
+
+  async cursor<TResult>(sql: Sql, transactionConfig: TransactionOptions = {}): Promise<{
+    read: (rowCount?: number) => Promise<TResult[]>;
+    [Symbol.asyncDispose]: () => Promise<void>;
+  }> {
+    using client = await this.#pool!.connect();
+
+    // Create a transaction
+    const transaction = client.createTransaction(DenoPostgresConnection.id(), {
+      isolation_level: "read_committed",
+      read_only: true,
+      ...transactionConfig,
+    });
+    await transaction.begin();
+
+    // Create a cursor
+    const cursorName = DenoPostgresConnection.id("cursor");
+    await transaction.queryArray(`DECLARE ${cursorName} CURSOR FOR ${sql.sql}`, sql.values);
+
+    return {
+      read: async (rowCount: number = 100): Promise<TResult[]> => {
+        const { rows } = await transaction.queryObject<TResult>(`FETCH FORWARD ${rowCount} FROM ${cursorName}`);
+        return rows;
+      },
+      [Symbol.asyncDispose]: async () => {
+        await transaction.queryArray(`CLOSE ${cursorName}`);
+
+        // The transaction has been rolled back on SQL errors, we can safely commit
+        await transaction.commit();
+      },
+    };
+  }
+
+  async #wrap<TResult>(sql: Sql, format: "array" | "object"): Promise<TResult[]> {
+    // Explicit Resource Management handles the disposal of the client
+    using client = await this.#pool!.connect();
+
+    // Create a transaction
+    // The transaction is rolled back on SQL errors but stays open on JS errors
+    // hence the need to use a try/catch block
+    const transaction = client.createTransaction(DenoPostgresConnection.id());
+
+    try {
+      await transaction.begin();
+      const result = format === "object"
+        ? await transaction.queryObject<TResult>({ text: sql.sql, args: sql.values })
+        : await transaction.queryArray({ text: sql.sql, args: sql.values }); // FIXME type queryArray !
+      await transaction.commit();
+      return result.rows as TResult[];
+    } catch (e) {
+      transaction && await transaction.rollback();
+      e instanceof Error && logger.error("[pg] transaction error", e.message);
+      throw e;
+    }
+  }
+
+  #poolIsValid(): boolean {
+    return this.#pool !== null && this.#pool instanceof Pool;
+  }
+
+  #configure(config: string | ClientOptions | undefined): { connectionString: string; config: ClientOptions } {
+    switch (typeof config) {
+      case "string":
+        return { connectionString: config, config: {} };
+      case "object":
+      case "undefined": {
+        const connectionString = env_or_default("APP_POSTGRES_URL", "");
+        if (!connectionString.length) {
+          throw new Error("APP_POSTGRES_URL is not set");
+        }
+        return { connectionString, config: config || {} };
+      }
+      default:
+        throw new Error("Invalid configuration");
+    }
+  }
+
+  #validateConfig(config: ClientOptions): ClientOptions {
+    // Define custom structs for string formats and numerical limits
+    const AppName = refine(string(), "AppName", (value: string) => value.length <= 50);
+    const Hostname = pattern(string(), /^[a-zA-Z0-9.-]+$/);
+    const Database = pattern(string(), /^[a-zA-Z0-9_-]+$/);
+    const Username = pattern(string(), /^[a-zA-Z0-9_-]+$/);
+    const Port = refine(number(), "Port", (val: number) => val > 0 && val <= 65535);
+    const Attempts = refine(number(), "Attempts", (i: number) => i === undefined || (i >= 1 && i <= 10));
+
+    const ClientOptionsStruct = object({
+      applicationName: AppName,
+      connection: object({ attempts: Attempts }),
+      hostname: Hostname,
+      port: Port,
+      database: Database,
+      user: Username,
+      password: string(),
+      tls: object({
+        enabled: boolean(),
+        enforce: boolean(),
+        caCertificates: array(string()),
+      }),
+      controls: object({
+        debug: union([
+          boolean(),
+          object({
+            queries: boolean(),
+            notices: boolean(),
+            results: boolean(),
+            queryInError: boolean(),
+          }),
+        ]),
+      }),
+    });
+
+    assert(config, ClientOptionsStruct);
+
+    return config;
   }
 }
