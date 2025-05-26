c58323160788fc608f1836e273f8d8a3b8ae6b17
Luis Ventura <30115537+Lawiss@users.noreply.github.com>
2025-05-26 11:57:34 +0200

    Updates elementary to latest version
    Also adds two models to monitor campaigns statistics.

---
+++ dbt/models/metrics/monthly_stats_campaigns_revenues_by_operator.sql
@@ -0,0 +1,130 @@
+{{
+  config(
+    materialized = 'incremental',
+    unique_key = ['month',"operator_id","siret"],
+    incremental_strategy = "delete+insert",
+    indexes = [{"columns":['month',"operator_id","siret"],"unique":true}]
+    )
+}}
+with trips as (
+  select
+    c._id,
+    oi.siret,
+    max(c.start_datetime)         as start_datetime,
+    max(c.distance)               as distance,
+    max(operator_id)              as operator_id,
+    max(c.driver_revenue)         as driver_revenue,
+    max(c.passenger_contribution) as passenger_contribution,
+    sum(oi.amount)                as amount
+  from carpool_v2.carpools as c
+  left join carpool_v2.operator_incentives as oi
+    on
+      c._id = oi.carpool_id
+      {% if is_incremental() %}
+        and c.start_datetime
+        >= coalesce((select max(month) from {{ this }}), '1900-01-01')
+      {% else %}
+  where c.start_datetime >= '2022-01-01'
+{% endif %}
+  -- account for multiple incentives amounts for the same siret on the same trip
+  group by 1, 2
+),
+
+agg_data as (
+  select
+    date_trunc(
+      'month', start_datetime
+    ) as month,
+    operator_id,
+    siret,
+    count(
+      distinct _id
+    ) as journeys_count,
+    avg(
+      driver_revenue / 100
+    ) as avg_driver_revenue,
+    avg(
+      distance / 1000
+    ) as avg_distance,
+    min(
+      driver_revenue / 100
+    ) as min_revenue,
+    10
+    * avg(
+      driver_revenue::numeric / distance
+    ) as avg_driver_revenue_per_km,
+    10
+    * avg(driver_revenue::numeric / distance) filter (
+      where distance between 18000 and 30000
+    ) as avg_driver_revenue_per_km_18_30,
+    10
+    * avg(driver_revenue::numeric / distance) filter (
+      where amount > 0
+    ) as avg_driver_revenue_per_km_filtered,
+    10
+    * avg(driver_revenue::numeric / distance) filter (
+      where (distance between 18000 and 30000) and amount > 0
+    ) as avg_driver_revenue_per_km_filtered_18_30,
+    10
+    * avg(
+      passenger_contribution::numeric / distance
+    ) as avg_passenger_contribution_per_km,
+    10
+    * avg(passenger_contribution::numeric / distance) filter (
+      where distance between 18000 and 30000
+    ) as avg_passenger_contribution_per_km_18_30,
+    10
+    * avg(passenger_contribution::numeric / distance) filter (
+      where amount > 0
+    ) as avg_passenger_contribution_per_km_filtered,
+    10
+    * avg(passenger_contribution::numeric / distance) filter (
+      where (distance between 18000 and 30000) and amount > 0
+    ) as avg_passenger_contribution_per_km_filtered_18_30,
+    10
+    * avg(
+      amount::numeric / distance
+    ) as avg_incentive_per_km,
+    10
+    * avg(amount::numeric / distance) filter (
+      where distance between 18000 and 30000
+    ) as avg_incentive_per_km_18_30,
+    10
+    * avg(amount::numeric / distance) filter (
+      where amount > 0
+    ) as avg_incentive_per_km_filtered,
+    10
+    * avg(amount::numeric / distance) filter (
+      where (distance between 18000 and 30000) and amount > 0
+    ) as avg_incentive_per_km_filtered_18_30
+  from trips
+  where distance > 0
+  group by 1, 2, 3
+  order by 1
+)
+
+select
+  a.month,
+  a.operator_id,
+  o.name as operator_name,
+  a.siret,
+  c.legal_name,
+  a.journeys_count,
+  a.avg_driver_revenue,
+  a.avg_distance,
+  a.min_revenue,
+  a.avg_driver_revenue_per_km,
+  a.avg_driver_revenue_per_km_18_30,
+  a.avg_driver_revenue_per_km_filtered,
+  a.avg_driver_revenue_per_km_filtered_18_30,
+  a.avg_passenger_contribution_per_km,
+  a.avg_passenger_contribution_per_km_18_30,
+  a.avg_passenger_contribution_per_km_filtered,
+  a.avg_passenger_contribution_per_km_filtered_18_30,
+  a.avg_incentive_per_km,
+  a.avg_incentive_per_km_18_30,
+  a.avg_incentive_per_km_filtered,
+  a.avg_incentive_per_km_filtered_18_30
+from agg_data as a
+left join company.companies as c on a.siret = c.siret
+left join operator.operators as o on a.operator_id = o._id
