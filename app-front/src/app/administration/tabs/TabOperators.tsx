4c8bb9d384741921e824105516431755c91fc5ad
Ludovic Delhomme <ludovic.delhomme@datayama.fr>
2025-06-12 00:16:39 +0200

    fix: lint

--- app-front/src/app/administration/tabs/TabOperators.tsx
+++ app-front/src/app/administration/tabs/TabOperators.tsx
@@ -5,10 +5,7 @@ export default function TabOperators() {
   const { user } = useAuth();
   return (
     <>
-      <OperatorsTable
-        title={`Gestion des opérateurs`}
-        id={user?.operator_id}
-      />
+      <OperatorsTable title={`Gestion des opérateurs`} id={user?.operator_id} />
     </>
   );
 }
