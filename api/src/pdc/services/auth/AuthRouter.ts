a3a274e393a12a023f8d62c4765c263a0e817769
Jonathan Fallon <jonathan@scopopop.com>
2025-06-17 16:51:25 +0200

    add CORS to auth/me route

--- api/src/pdc/services/auth/AuthRouter.ts
+++ api/src/pdc/services/auth/AuthRouter.ts
@@ -2,17 +2,27 @@ import { ConfigInterfaceResolver, inject, injectable, KernelInterfaceResolver, p
 import { session } from "@/pdc/proxy/config/proxy.ts";
 import { asyncHandler } from "@/pdc/proxy/helpers/asyncHandler.ts";
 import { ProConnectOIDCProvider } from "@/pdc/services/auth/providers/ProConnectOIDCProvider.ts";
+import cors from "dep:cors";
 import express, { NextFunction, Request, Response } from "dep:express";
 import { authGuard } from "../../proxy/middlewares/authGuard.ts";
 
 @injectable()
 export class AuthRouter {
+  #corsOptions: unknown;
+
   constructor(
     @inject(proxy) private app: express.Express,
     private kernel: KernelInterfaceResolver,
     private proConnectOIDCProvider: ProConnectOIDCProvider,
     private config: ConfigInterfaceResolver,
-  ) {}
+  ) {
+    this.#corsOptions = {
+      origin: [this.config.get("proxy.cors"), this.config.get("proxy.dashboardV2Cors")],
+      optionsSuccessStatus: 200,
+      // Allow-Access-Credentials lets XHR requests send Cookies to a different URL
+      credentials: true,
+    };
+  }
 
   register() {
     this.app.get(
@@ -79,8 +89,13 @@ export class AuthRouter {
       }),
     );
 
-    this.app.get("/auth/me", authGuard(this.kernel), (req: express.Request, res: express.Response) => {
-      return res.json(req.session.user);
-    });
+    this.app.get(
+      "/auth/me",
+      cors(this.#corsOptions),
+      authGuard(this.kernel),
+      (req: express.Request, res: express.Response) => {
+        return res.json(req.session.user);
+      },
+    );
   }
 }
