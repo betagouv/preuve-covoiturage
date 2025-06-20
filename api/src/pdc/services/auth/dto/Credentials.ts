a8bfbfe534a5970b70f8deb49a1bc5daf330bc78
Jonathan Fallon <jonathan@scopopop.com>
2025-06-20 10:18:19 +0200

    [wip] refactoring to credentials

---
+++ api/src/pdc/services/auth/dto/Credentials.ts
@@ -0,0 +1,72 @@
+import { array, enums, Infer, number, object, optional, string } from "@/lib/superstruct/index.ts";
+
+/**
+ * Definitions:
+ *
+ * -  `access_key`: The access key for the application.
+ * -  `secret_key`: The secret key for the application.
+ * -  `access_token`: The token returned after successful authentication with the access key and secret key.
+ * -  `operator_id`: The ID of the operator associated with the access token.
+ * -  `token_id`: The unique identifier for the access token used for retrieval.
+ * -  `role`: The role associated with the access token, typically "application".
+ */
+
+export const CredentialsPair = object({
+  access_key: string(),
+  secret_key: string(),
+});
+export type CredentialsPair = Infer<typeof CredentialsPair>;
+
+export const CredentialsRole = enums(["application"]);
+export type CredentialsRole = Infer<typeof CredentialsRole>;
+
+export const Credentials = object({
+  token_id: optional(string()),
+  operator_id: number(),
+  role: CredentialsRole,
+});
+export type Credentials = Infer<typeof Credentials>;
+
+// CRUD: DexClient
+export type DexClientReadResult = Array<Credentials>;
+export type DexClientCreateResult = CredentialsPair;
+export type DexClientDeleteResult = void;
+
+/**
+ * CRUD: Credentials
+ */
+
+// Create
+export const CreateCredentialsParams = object({
+  operator_id: number(),
+});
+export type CreateCredentialsParams = Infer<typeof CreateCredentialsParams>;
+
+export const CreateCredentialsResult = CredentialsPair;
+export type CreateCredentialsResult = Infer<typeof CreateCredentialsResult>;
+
+// Read
+export const ReadCredentialsParams = object({
+  operator_id: number(),
+});
+export type ReadCredentialsParams = Infer<typeof ReadCredentialsParams>;
+
+export const ReadCredentialsResult = array(Credentials);
+export type ReadCredentialsResult = Infer<typeof ReadCredentialsResult>;
+
+//  Delete
+export const DeleteCredentialsParams = object({
+  operator_id: number(),
+  token_id: string(),
+});
+export type DeleteCredentialsParams = Infer<typeof DeleteCredentialsParams>;
+export type DeleteCredentialsResult = void;
+
+// Create Access Token
+export const CreateAccessTokenParams = CredentialsPair;
+export type CreateAccessTokenParams = Infer<typeof CreateAccessTokenParams>;
+
+export const CreateAccessTokenResult = object({
+  access_token: string(),
+});
+export type CreateAccessTokenResult = Infer<typeof CreateAccessTokenResult>;
