a294c53fc8a295e16b17b33479c71d90c10c96f4
Ludovic Delhomme <ludovic.delhomme@datayama.fr>
2025-08-12 15:37:27 +0200

    fix: run dbt-osmosis and linter / remove geo.perimeters join

--- dbt/models/0_raw_zone/0_raw_trips.sql
+++ dbt/models/0_raw_zone/0_raw_trips.sql
@@ -1,81 +1,107 @@
 {{ 
     config(
     materialized = 'table' if target.name == 'dev' else 'incremental',
-    unique_key=[ 'cc_operator_journey_id', 'cc__id'],
+    unique_key=[ 'operator_journey_id', '_id'],
     indexes = [
       {
         'columns':[
-          'cc_start_datetime'
+          'start_datetime'
         ],
       },
       {
         'columns':[
-          'cc_operator_journey_id'
+          'operator_journey_id'
         ],
       }
     ]
   )
 }}
 
-    SELECT
-        -- Timezone-aware start_datetime
-        CASE
-            WHEN cg.start_geo_code::text ~ '^97[1-2]' THEN cc.start_datetime AT TIME ZONE 'America/Guadeloupe'
-            WHEN cg.start_geo_code::text ~ '^973'    THEN cc.start_datetime AT TIME ZONE 'America/Cayenne'
-            WHEN cg.start_geo_code::text ~ '^974'    THEN cc.start_datetime AT TIME ZONE 'Indian/Reunion'
-            WHEN cg.start_geo_code::text ~ '^976'    THEN cc.start_datetime AT TIME ZONE 'Indian/Mayotte'
-            ELSE cc.start_datetime AT TIME ZONE 'Europe/Paris'
-        END AS tz_start_datetime,
+SELECT
+  cc.*,
+  -- Timezone-aware start_datetime
+  oo.name
+    AS operator_name,
 
-        -- Timezone-aware end_datetime
-        CASE
-            WHEN cg.end_geo_code::text ~ '^97[1-2]' THEN cc.end_datetime AT TIME ZONE 'America/Guadeloupe'
-            WHEN cg.end_geo_code::text ~ '^973'    THEN cc.end_datetime AT TIME ZONE 'America/Cayenne'
-            WHEN cg.end_geo_code::text ~ '^974'    THEN cc.end_datetime AT TIME ZONE 'Indian/Reunion'
-            WHEN cg.end_geo_code::text ~ '^976'    THEN cc.end_datetime AT TIME ZONE 'Indian/Mayotte'
-            ELSE cc.end_datetime AT TIME ZONE 'Europe/Paris'
-        END AS tz_end_datetime,
+  -- Timezone-aware end_datetime
+  oo.siret
+    AS operator_siret,
+  cs._id
+    AS status_id,
+  cs.updated_at
+    AS status_updated_at,
+  cs.acquisition_status,
+  cs.fraud_status,
+  cs.anomaly_status,
+  cg._id
+    AS geo_id,
+  cg.start_geo_code,
+  -- Carpool final status
+  cg.end_geo_code,
+  -- Carpool valid status
+  cg.updated_at
+    AS geo_updated_at,
+  cg.errors
+    AS geo_errors,
+  CASE
+    WHEN
+      cg.start_geo_code::text ~ '^97[1-2]'
+      THEN cc.start_datetime AT TIME ZONE 'America/Guadeloupe'
+    WHEN
+      cg.start_geo_code::text ~ '^973'
+      THEN cc.start_datetime AT TIME ZONE 'America/Cayenne'
+    WHEN
+      cg.start_geo_code::text ~ '^974'
+      THEN cc.start_datetime AT TIME ZONE 'Indian/Reunion'
+    WHEN
+      cg.start_geo_code::text ~ '^976'
+      THEN cc.start_datetime AT TIME ZONE 'Indian/Mayotte'
+    ELSE cc.start_datetime AT TIME ZONE 'Europe/Paris'
+  END
+    AS tz_start_datetime,
+  CASE
+    WHEN
+      cg.end_geo_code::text ~ '^97[1-2]'
+      THEN cc.end_datetime AT TIME ZONE 'America/Guadeloupe'
+    WHEN
+      cg.end_geo_code::text ~ '^973'
+      THEN cc.end_datetime AT TIME ZONE 'America/Cayenne'
+    WHEN
+      cg.end_geo_code::text ~ '^974'
+      THEN cc.end_datetime AT TIME ZONE 'Indian/Reunion'
+    WHEN
+      cg.end_geo_code::text ~ '^976'
+      THEN cc.end_datetime AT TIME ZONE 'Indian/Mayotte'
+    ELSE cc.end_datetime AT TIME ZONE 'Europe/Paris'
+  END
+    AS tz_end_datetime,
+  coalesce(
+    cs.acquisition_status IN (
+      'processed', 'failed', 'canceled', 'expired', 'terms_violation_error'
+    ),
+    FALSE
+  )             AS final_status,
+  coalesce(
+    cs.acquisition_status = 'processed'
+    AND cs.anomaly_status = 'passed'
+    AND cs.fraud_status = 'passed',
+    FALSE)
+    AS valid_status
 
-        -- Carpool final state
-        CASE 
-            WHEN cs.acquisition_status IN ('processed', 'failed', 'canceled', 'expired', 'terms_violation_error') THEN TRUE
-            ELSE FALSE
-        END AS is_final_state,
-
-        -- Carpool valid state
-        CASE 
-            WHEN cs.acquisition_status = 'processed' 
-             AND cs.anomaly_status = 'passed'
-             AND cs.fraud_status = 'passed' THEN TRUE
-            ELSE FALSE
-        END AS is_valid,
-
-        {{ dbt_utils.star(from=source('carpool', 'carpools'), prefix='cc_', relation_alias='cc') }},
-        {{ dbt_utils.star(from=source('carpool', 'status'), prefix='cs_', relation_alias='cs') }},
-        {{ dbt_utils.star(from=source('carpool', 'geo'), prefix='cg_', relation_alias='cg') }},
-        oo._id as oo__id,
-        oo.name as oo_name,
-        oo.siret as oo_siret,
-  
-        {{ dbt_utils.star(from=source('geo', 'perimeters'), prefix='ps_', relation_alias='ps') }},
-        {{ dbt_utils.star(from=source('geo', 'perimeters'), prefix='pe_', relation_alias='pe') }}
-
-        
-    FROM {{ source('carpool', 'carpools') }} cc
-    JOIN {{ source('carpool', 'status') }} cs        ON cs.carpool_id = cc._id
-    JOIN {{ source('carpool', 'geo') }} cg           ON cg.carpool_id = cc._id
-    JOIN {{source('operator', 'operators') }} oo       ON oo._id = cc.operator_id
-    LEFT JOIN {{source('geo', 'perimeters') }} ps       ON cg.start_geo_code = ps.arr
-                                    AND ps.year = EXTRACT(YEAR FROM cc.start_datetime)::int
-    LEFT JOIN {{ source('geo', 'perimeters') }} pe      ON cg.end_geo_code = pe.arr
-                                    AND pe.year = EXTRACT(YEAR FROM cc.start_datetime)::int
+FROM {{ source('carpool', 'carpools') }} AS cc
+LEFT JOIN {{ source('carpool', 'status') }} AS cs ON cc._id = cs.carpool_id
+LEFT JOIN {{ source('carpool', 'geo') }} AS cg ON cc._id = cg.carpool_id
+LEFT JOIN
+  {{ source('operator', 'operators') }} AS oo
+  ON cc.operator_id = oo._id
+WHERE date_part('year', cc.start_datetime) >= 2020
 {% if is_incremental() %}
-  WHERE cc.start_datetime::date >= (
-        SELECT COALESCE(MAX(cc_start_datetime), DATE '1970-01-01')
+  AND cc.start_datetime::date >= (
+        SELECT COALESCE(MAX(start_datetime), DATE '1970-01-01')
         FROM {{ this }}
   )::date
 {% endif %}
 
 {% if target.name == 'dev' %}
-LIMIT 5000
+  LIMIT 5000
 {% endif %}
