09f49fbe825e489dfb715af38a2e758691df0a9b
Thomas Grammatico <thomas@grammatico.me>
2026-08-25 11:24:55 +0200

    feat: monthly valid carpools exposed to metabase. migration from stats.monthly_trips_ok

---
+++ datalake/models/app_metabase/mb_monthly_carpools_ok.sql
@@ -0,0 +1,11 @@
+{{ config(
+  materialized='view',
+  tags=['app_metabase', 'monthly_carpools_ok']
+) }}
+
+SELECT
+  incremental_date,
+  SUM(carpools_valid) AS count
+FROM {{ ref('fraud_month_country_from') }}
+GROUP BY incremental_date
+ORDER BY incremental_date
